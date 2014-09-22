27f012dfd69e5a71c55d9ddaf3a45de219aa4931
Steve Matsumoto <steve@stevematsumoto.net>
2014-09-22 12:05:56 +0200

    Fixed a typo in the description of the InfoOpaqueField class, and added equality tests for each of the opaque field classes.

--- lib/packet/opaque_field.py
+++ lib/packet/opaque_field.py
@@ -52,6 +52,15 @@ class OpaqueField(object):
         """
         pass
 
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.type == other.type
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self == other
+
     def __str__(self):
         pass
 
@@ -97,6 +106,15 @@ class HopOpaqueField(OpaqueField):
                               self.type, self.ingress_if, self.egress_if,
                               self.mac).bytes
 
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return (self.type == other.type and
+                    self.ingress_if == other.ingress_if and
+                    self.egress_if == other.egress_if and
+                    self.mac == other.mac)
+        else:
+            return False
+
     def __str__(self):
         s = "[Hop OF type: %u, ingress if: %u, egress if: %u, mac: %x]" % (
             self.type, self.ingress_if, self.egress_if, self.mac)
@@ -108,7 +126,7 @@ class InfoOpaqueField(OpaqueField):
     Class for the info opaque field.
 
     The info opaque field contains type info of the path (1 byte), an expiration
-    timestamp (2 bytes), the ISD ID (2 byte), # hops for this path (2 byte) and
+    timestamp (2 bytes), the ISD ID (2 byte), # hops for this path (1 byte) and
     a reserved section (2 bytes).
     """
 
@@ -144,6 +162,17 @@ class InfoOpaqueField(OpaqueField):
         return bitstring.pack("uintle:8, uintle:16, uintle:16, uintle:8,"
                               "uintle:16", self.info, self.timestamp,
                               self.isd_id, self.hops, self.reserved).bytes
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return (self.type == other.type and
+                    self.info == other.info and
+                    self.timestamp == other.timestamp and
+                    self.isd_id == other.isd_id and
+                    self.hops == other.hops and
+                    self.reserved == other.reserved)
+        else:
+            return False
 
     def __str__(self):
         s = "[Info OF info: %x, TS: %u, ISD ID: %u, hops: %u]" % (
