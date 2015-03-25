75c57bdc1f823f61f3bb78b592f089dd8382366d
Lorenzo Baesso <lorenzobaesso@nearfar.ch>
2015-03-25 13:20:05 +0100

    Added TRC/cert req/rep logic in certificates_test.py

--- test/certificates_test.py
+++ test/certificates_test.py
@@ -16,13 +16,20 @@
 ===========================================
 """
 
-from lib.crypto.certificate import verify_sig_chain_trc, CertificateChain, TRC
+from infrastructure.cert_server import CertServer
+from infrastructure.scion_elem import SCION_UDP_PORT
 from lib.crypto.asymcrypto import sign
+from lib.crypto.certificate import verify_sig_chain_trc, CertificateChain, TRC
+from lib.packet.host_addr import IPv4HostAddr
+from lib.packet.scion import (SCIONPacket, get_type, PacketType as PT,
+    CertChainRequest, CertChainReply, TRCRequest, TRCReply)
+from lib.topology import Topology
 from lib.util import (get_cert_chain_file_path, get_trc_file_path, read_file,
     get_sig_key_file_path)
-import unittest
-import logging
 import base64
+import socket
+import logging
+import unittest
 
 
 class TestCertificates(unittest.TestCase):
@@ -54,6 +61,34 @@ class TestCertificates(unittest.TestCase):
         chain = CertificateChain.from_values([])
         print('Sig test 2:', verify_sig_chain_trc(msg, sig, 'ISD:1-AD:13',
             cert10, trc, 0))
+
+        topology = Topology("../topology/ISD1/topologies/ISD:1-AD:10.json")
+        src_addr = IPv4HostAddr("127.0.0.1")
+        dst_addr = topology.certificate_servers[0].addr
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((str(src_addr), SCION_UDP_PORT))
+        
+        print("Sending TRC request (ISD:1-V:0) to local CS.")
+        msg = TRCRequest.from_values(PT.TRC_REQ_LOCAL, src_addr,
+            topology.parent_edge_routers[0].interface.if_id, topology.isd_id,
+            topology.ad_id, 1, 0).pack()
+        sock.sendto(msg, (str(dst_addr), SCION_UDP_PORT))
+
+        data, addr = sock.recvfrom(1024)
+        print("Received TRC reply from local CS.")
+        trc_reply = TRCReply(data)
+
+        print("Sending cert chain request (ISD:1-AD:16-V:0) to local CS.")
+        msg = CertChainRequest.from_values(PT.CERT_CHAIN_REQ_LOCAL, src_addr,
+            topology.parent_edge_routers[0].interface.if_id, topology.isd_id,
+            topology.ad_id, 1, 16, 0).pack()
+        sock.sendto(msg, (str(dst_addr), SCION_UDP_PORT))
+
+        data, addr = sock.recvfrom(1024)
+        print("Received cert chain reply from local CS.")
+        cert_chain_reply = CertChainReply(data)
+
+        sock.close()
 
 
 if __name__ == "__main__":
