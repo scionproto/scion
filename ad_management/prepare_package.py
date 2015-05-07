1457bd155b87f4f81e2fb911d3005cdf0c8cfa80
Anton Ovchinnikov <antono@student.ethz.ch>
2015-05-07 10:34:39 +0200

    Add optional configs to a generated package

--- ad_management/prepare_package.py
+++ ad_management/prepare_package.py
@@ -22,12 +22,15 @@ def get_package_metadata(repo, commit):
     now = str(datetime.datetime.now())
     metadata = {'commit': commit.hexsha,
                 'date': now,
-                'with_config': False,
                 }
     return metadata
 
 
-def main():
+def prepare_package(out_dir=PACKAGE_DIR_PATH, config_paths=None):
+    """
+    config_paths -- list of paths to topology dirs
+
+    """
     repo = Repo(SCION_ROOT)
     assert not repo.bare
 
@@ -39,25 +42,43 @@ def main():
     commit = repo.commit(commit_hash)
     package_name = get_package_name(repo, commit)
 
-    if not os.path.exists(PACKAGE_DIR_PATH):
-        os.mkdir(PACKAGE_DIR_PATH)
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
 
-    package_path = os.path.join(PACKAGE_DIR_PATH, package_name)
+    package_path = os.path.join(out_dir, package_name)
 
+    package_prefix = 'scion-package/'
     with open(package_path, 'wb') as out_fh:
-        repo.archive(out_fh, prefix='scion-package/')
+        repo.archive(out_fh, prefix=package_prefix)
 
-    metadata = get_package_metadata(repo, commit)
-    metadata_bytes = bytes(json.dumps(metadata, indent=2), 'utf-8')
-    metadata_stream = io.BytesIO(metadata_bytes)
+    # Append configs
+    if config_paths is not None:
+        if isinstance(config_paths, str):
+            config_paths = [config_paths]
+        with tarfile.open(package_path, 'a') as tar_fh:
+            for path in config_paths:
+                assert os.path.isdir(path)
+                dirname = os.path.basename(path)
+                arcname = os.path.join(package_prefix, 'topology', dirname)
+                tar_fh.add(path, arcname)
 
     # Append metadata
+    metadata = get_package_metadata(repo, commit)
+    metadata['with_config'] = (config_paths is not None)
+    metadata_bytes = bytes(json.dumps(metadata, indent=2), 'utf-8')
+    metadata_stream = io.BytesIO(metadata_bytes)
     with tarfile.open(package_path, 'a') as tar_fh:
         meta_tarinfo = tarfile.TarInfo('META')
         meta_tarinfo.size = len(metadata_bytes)
         tar_fh.addfile(meta_tarinfo, metadata_stream)
 
     print('Package created:   {}'.format(package_name))
+    return package_path
+
+
+def main():
+    prepare_package(config_paths='/home/tonyo/scion_ethz/scion/topology/ISD1')
+
 
 if __name__ == '__main__':
     main()
