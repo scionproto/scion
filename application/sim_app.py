3824b89d06f5e67ba5fbdc5c88a13911ab8a34f0
mskd96 <deepakrishna95@gmail.com>
2015-05-22 17:10:09 +0200

    Modified copyright year

--- application/sim_app.py
+++ application/sim_app.py
@@ -1,7 +1,7 @@
 """
 sim_app.py
 
-Copyright 2014 ETH Zurich
+Copyright 2015 ETH Zurich
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
