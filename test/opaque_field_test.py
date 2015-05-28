3ce3e8a95153205c6ce166b675dce41ca3650032
prateshg <g.pratish@gmail.com>
2015-05-28 11:19:05 +0200

    Update opaque_field_test.py
    Unittests for OpaqueField and HopOpaqueField

--- test/opaque_field_test.py
+++ test/opaque_field_test.py
@@ -1,4 +1,4 @@
-# Copyright 2014 ETH Zurich
+# Copyright 2015 ETH Zurich
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,64 +16,110 @@
 =====================================================
 """
 # Stdlib
-import unittest
+from unittest.mock import patch
+
+# External packages
+import nose
+import nose.tools as ntools
 
 # SCION
 from lib.packet.opaque_field import (
+    OpaqueFieldType,
+    OpaqueField,
     HopOpaqueField,
     InfoOpaqueField,
-    OpaqueField,
-    OpaqueFieldType,
+    TRCField,
+    SupportSignatureField,
+    SupportPeerField,
+    SupportPCBField,
 )
-from test.testcommon import SCIONCommonTest
 
 
-class TestOpaqueFields(SCIONCommonTest):
+class TestOpaqueFieldInit(object):
     """
-    Unit tests for opaque_field.py.
+    Unit tests for lib.packet.opaque_field.OpaqueField.__init__
     """
-    def test_opaque_field(self):
-        of = OpaqueField()
-        self.assertEqual(of.type, OpaqueFieldType.NORMAL_OF)
-        self.assertFalse(of.parsed)
-        self.assertFalse(of.raw)
+    def test_basic(self):
+        op_fld = OpaqueField()
+        ntools.eq_(op_fld.info, 0)
+        ntools.eq_(op_fld.type, 0)
+        ntools.assert_false(op_fld.parsed)
+        ntools.assert_true(op_fld.raw is None)
 
-    def test_equality(self):
-        """
-        Make sure that equality tests between opaque fields only succeeds for
-        the same type of opaque fields.
-        """
-        of1 = OpaqueField()
-        of2 = OpaqueField()
-        hof1 = HopOpaqueField()
-        hof2 = HopOpaqueField()
-        iof1 = InfoOpaqueField()
-        iof2 = InfoOpaqueField()
-        self.assertTrue(of1.info == of2.info)
+class TestHopOpaqueFieldInit(object):
+    """
+    Unit tests for lib.packet.opaque_field.HopOpaqueField.__init__
+    """
+    def test_basic(self):
+        hop_op_fld = HopOpaqueField()
+        ntools.eq_(hop_op_fld.info, 0)
+        ntools.eq_(hop_op_fld.type, 0)
+        ntools.eq_(hop_op_fld.exp_time, 0)
+        ntools.eq_(hop_op_fld.ingress_if, 0)
+        ntools.eq_(hop_op_fld.egress_if, 0)
+        ntools.eq_(hop_op_fld.mac, 0)
+        ntools.assert_false(hop_op_fld.parsed)
 
-        self.assertTrue(hof1.info == hof2.info and
-                        hof1.ingress_if == hof2.ingress_if and
-                        hof1.egress_if == hof2.egress_if and
-                        hof1.mac == hof2.mac)
+    @patch("lib.packet.opaque_field.HopOpaqueField.parse")
+    def test_raw(self, parse):
+        hop_op_fld = HopOpaqueField("data")
+        parse.assert_called_once_with("data")
 
-        self.assertTrue(iof1.info == iof2.info and
-                        iof1.timestamp == iof2.timestamp and
-                        iof1.isd_id == iof2.isd_id and
-                        iof1.hops == iof2.hops)
+class TestHopOpaqueFieldParse(object):
+    """
+    Unit tests for lib.packet.opaque_field.HopOpaqueField.parse
+    """
+    def test_basic(self):
+        hop_op_fld = HopOpaqueField()
+        hop_op_fld.parse(bytes([14,42,10,11,12,13,14,15]))
+        ntools.eq_(hop_op_fld.info, 14)
+        ntools.eq_(hop_op_fld.exp_time, 42)
+        ntools.eq_(hop_op_fld.ingress_if, 160)
+        ntools.eq_(hop_op_fld.egress_if, 2828)
+        ntools.eq_(hop_op_fld.mac, 855567)
+        ntools.assert_true(hop_op_fld.parsed)
 
-    def test_hop_opaque_field(self):
-        """
-        Ensure that parsing a packed opaque field results in the same opaque
-        field.
-        """
-        of = HopOpaqueField()
-        ofCopy = HopOpaqueField()
-        ofCopy.parse(of.pack())
-        self.assertTrue(of.info == ofCopy.info and
-                        of.ingress_if == ofCopy.ingress_if and
-                        of.egress_if == ofCopy.egress_if and
-                        of.mac == ofCopy.mac)
+    def test_len(self):
+        hop_op_fld = HopOpaqueField()
+        hop_op_fld.parse(bytes([14,42,10,0,0,0,0]))
+        ntools.assert_false(hop_op_fld.parsed)
+        ntools.eq_(hop_op_fld.info, 0)
+        ntools.eq_(hop_op_fld.type, 0)
+        ntools.eq_(hop_op_fld.exp_time, 0)
+        ntools.eq_(hop_op_fld.ingress_if, 0)
+        ntools.eq_(hop_op_fld.egress_if, 0)
+        ntools.eq_(hop_op_fld.mac, 0)
 
+class TestHopOpaqueFieldFromValues(object):
+    """
+    Unit tests for lib.packet.opaque_field.HopOpaqueField.from_values
+    """
+    def test_basic(self):
+        hop_op_fld = HopOpaqueField.from_values(42, 160, 2828, 855567)
+        ntools.eq_(hop_op_fld.exp_time, 42)
+        ntools.eq_(hop_op_fld.ingress_if, 160)
+        ntools.eq_(hop_op_fld.egress_if, 2828)
+        ntools.eq_(hop_op_fld.mac, 855567)
+
+    def test_less_arg(self):
+        hop_op_fld = HopOpaqueField.from_values(42)
+        ntools.eq_(hop_op_fld.exp_time, 42)
+        ntools.eq_(hop_op_fld.ingress_if, 0)
+        ntools.eq_(hop_op_fld.egress_if, 0)
+        ntools.eq_(hop_op_fld.mac, 0)               
+
+class TestHopOpaqueFieldPack(object):
+    """
+    Unit tests for lib.packet.opaque_field.HopOpaqueField.pack
+    """
+    def test_basic(self):
+        hop_op_fld = HopOpaqueField()
+        hop_op_fld.info = 14
+        hop_op_fld.exp_time = 42
+        hop_op_fld.ingress_if = 160
+        hop_op_fld.egress_if = 2828
+        hop_op_fld.mac = 855567
+        ntools.eq_(hop_op_fld.pack(),bytes([14,42,10,11,12,13,14,15]))
 
 if __name__ == "__main__":
-    unittest.main()
+    nose.run(defaultTest=__name__)
