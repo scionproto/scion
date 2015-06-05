5ed4b01c6aa6ec242fa006f714f718825484e1f1
mskd96 <deepakrishna95@gmail.com>
2015-06-05 12:53:41 +0200

    Core AD's identification done

--- BRITE/Java/topology_generator.py
+++ BRITE/Java/topology_generator.py
@@ -21,44 +21,80 @@ import sys
 
 import networkx as nx
 
-outputGraph = False
+output_graph = True
 
 def parse(topo_file):
     fd = open(topo_file, 'r')
-    parsingNodes = False
-    parsingEdges = False
+    parsing_nodes = False
+    parsing_edges = False
 
-    G = nx.Graph()
+    original_graph = nx.Graph()
+    num_outedges = list()
     for line in fd:
         values = line.split(" ")
         
-        if parsingNodes:
+        if parsing_nodes:
             if line == "\n":
-                parsingNodes = False
+                parsing_nodes = False
             else:
                 values = line.split("\t")
-                G.add_node(int(values[0]))
+                original_graph.add_node(int(values[0]), is_core=False)
+                num_outedges.append((int(values[0]), int(values[3])))
 
-        if parsingEdges:
+        if parsing_edges:
             if line == "\n":
-                parsingEdges = False
+                parsing_edges = False
             else:
                 values = line.split("\t")
-                G.add_edge(int(values[1]), int(values[2]))
+                original_graph.add_edge(int(values[1]), int(values[2]))
 
         if values[0] == "Nodes:":
-            parsingNodes = True
+            parsing_nodes = True
         elif values[0] == "Edges:":
-            parsingEdges = True
+            parsing_edges = True
+    
+    NUM_CORE_ADS = int(len(original_graph.nodes())/10)
+    num_outedges = sorted(num_outedges, key=lambda tup:tup[1], 
+                          reverse=True)
+    core_ads = [(i[0]) for i in num_outedges[:NUM_CORE_ADS]]
+    core_ad_graph = original_graph.subgraph(core_ads)
 
+    # Ensuring that core ad graph is connected
+    if not nx.is_connected(core_ad_graph):
+        # If not connected, the new core ad graph is formed from 
+        # the largest connected component. Nodes are added to it from its 
+        # neighbors to make size of core_ad_graph = NUM_CORE_ADS
+        graphs = list(nx.connected_component_subgraphs(core_ad_graph))
+        graphs = sorted(graphs, key=lambda graph:len(graph.nodes()), 
+                        reverse=True)
+        core_ad_graph = graphs[0]
+        core_ads = core_ad_graph.nodes()
+        num_extra_nodes = NUM_CORE_ADS - len(core_ads)
+        neighbor_nodes = set()
+        for neighbor in [original_graph.neighbors(node) for node in core_ad_graph]:
+            for node in neighbor:
+                if node not in core_ad_graph.nodes():
+                    neighbor_nodes.add(node)
 
-    # print(G.nodes(), G.edges())
-    if outputGraph:
+        neighbor_nodes = sorted(neighbor_nodes, 
+                                key=lambda tup:len(original_graph[tup]), 
+                                reverse=True)
+        core_ads = core_ads + neighbor_nodes[:num_extra_nodes]
+        core_ad_graph = original_graph.subgraph(core_ads)
+        print neighbor_nodes[:num_extra_nodes]
+
+    print(core_ad_graph.nodes(), core_ad_graph.edges())
+
+    for core_ad in core_ads:
+        original_graph.node[core_ad]['color'] = 'red'
+        original_graph.node[core_ad]['is_core'] = True
+
+    print(original_graph.nodes(data=True))
+    if output_graph:
         # convert to a graphviz agraph
-        A=nx.to_agraph(G)
-
-        # write to dot file
-        A.write('topo1.dot')
+        A=nx.to_agraph(original_graph)
+        A.layout(prog='dot')
+        A.draw('topo2.png')
 
 
 def main():
