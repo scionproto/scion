9d72ab68aa2d57b16c2e513b80cb1751125a183c
Pratyaksh Sharma <pratyaksh@me.com>
2015-06-17 18:43:12 +0200

    added tests for pcb.PCBMarking

--- test/lib_packet_pcb_test.py
+++ test/lib_packet_pcb_test.py
@@ -56,5 +56,69 @@ class TestMarkingEq(object):
         ntools.assert_not_equals(marking1, marking2)
 
 
+class TestPCBMarkingInit(object):
+    """
+    Unit test for lib.packet.pcb.PCBMarking.__init__
+    """
+    @patch("lib.packet.pcb.Marking.__init__")
+    def test_basic(self, __init__):
+        pcbm = PCBMarking()
+        __init__.assert_called_once_with(pcbm)
+        ntools.eq_(pcbm.isd_id, 0)
+        ntools.eq_(pcbm.ad_id, 0)
+        ntools.assert_is_none(pcbm.hof)
+        ntools.eq_(pcbm.ig_rev_token, 32 * b"\x00")
+        ntools.eq_(pcbm.eg_rev_token, 32 * b"\x00")
+
+    @patch("lib.packet.pcb.PCBMarking.parse")
+    def test_raw(self, parse):
+        pcbm = PCBMarking('rawstring')
+        parse.assert_called_once_with('rawstring')
+
+
+class TestPCBMarkingParse(object):
+    """
+    Unit test for lib.packet.pcb.PCBMarking.parse
+    """
+    def test(self):
+        pass
+
+
+class TestPCBMarkingFromValues(object):
+    """
+    Unit test for lib.packet.pcb.PCBMarking.from_values
+    """
+    def test(self):
+        pcbm = PCBMarking.from_values(1, 2, 3, 4, 5)
+        ntools.eq_(pcbm.isd_id, 1)
+        ntools.eq_(pcbm.ad_id, 2)
+        ntools.eq_(pcbm.hof, 3)
+        ntools.eq_(pcbm.ig_rev_token, 4)
+        ntools.eq_(pcbm.eg_rev_token, 5)
+
+
+class TestPCBMarkingPack(object):
+    """
+    Unit test for lib.packet.pcb.PCBMarking.pack
+    """
+    def test(self):
+        pass
+
+
+class TestPCBMarkingEq(object):
+    """
+    Unit test for lib.packet.pcb.PCBMarking.__eq__
+    """
+    def test_same_type(self):
+        pcbm1 = PCBMarking.from_values(1, 2, 3, 4, 5)
+        pcbm2 = PCBMarking.from_values(1, 2, 3, 4, 5)
+        ntools.eq_(pcbm1, pcbm2)
+
+    def test_diff_type(self):
+        pcbm1 = PCBMarking()
+        pcbm2 = 123
+        ntools.assert_not_equals(pcbm1, pcbm2)
+
+
 if __name__ == "__main__":
     nose.run(defaultTest=__name__)
