600c3432dc38dc66d577a7946e90560542043371
prateshg <g.pratish@gmail.com>
2015-06-18 12:04:11 +0200

    Some Formatting changes

--- test/path_mgmt_test.py
+++ test/path_mgmt_test.py
@@ -18,7 +18,6 @@
 # Stdlib
 import struct
 from unittest.mock import patch
-from bitstring import BitArray
 
 # External packages
 import nose
@@ -89,7 +88,9 @@ class TestPathSegmentInfoPack(object):
         pth_seg_info.dst_isd = 0x0b0c
         pth_seg_info.src_ad = 0x0102030405060708
         pth_seg_info.dst_ad = 0x9192939495969798
-        ntools.eq_(pth_seg_info.pack(), bytes.fromhex('0e 2a0a 0b0c 0102030405060708 9192939495969798'))
+        ntools.eq_(pth_seg_info.pack(), bytes.fromhex('0e 2a0a 0b0c'  \
+                                                      '0102030405060708' \
+                                                      '9192939495969798'))
 
 
 class TestPathSegmentInfoFromValues(object):
@@ -97,7 +98,9 @@ class TestPathSegmentInfoFromValues(object):
     Unit tests for lib.packet.path_mgmt.PathSegmentInfo.from_values
     """
     def test_basic(self):
-        pth_seg_info = PathSegmentInfo.from_values(0xe, 0x2a0a, 0x0b0c, 0x0102030405060708, 0x9192939495969798)
+        pth_seg_info = PathSegmentInfo.from_values(0xe, 0x2a0a, 0x0b0c, \
+                                                   0x0102030405060708, \
+                                                   0x9192939495969798)
         ntools.eq_(pth_seg_info.type, 0xe)
         ntools.eq_(pth_seg_info.src_isd, 0x2a0a)
         ntools.eq_(pth_seg_info.dst_isd, 0x0b0c)
@@ -130,12 +133,16 @@ class TestPathSegmentRecordsParse(object):
     @patch("lib.packet.pcb.PathSegment.deserialize")
     @patch("lib.packet.path_mgmt.PathSegmentInfo.parse")
     def test_basic(self, parse, deserialize, parse_payload):
+        deserialize.return_value = "data1"
+        parse_payload.return_value = "data2"
         pth_seg_rec = PathSegmentRecords()
         data = "randomstring"
         pth_seg_rec.parse(data)
         parse_payload.assert_called_once_with(pth_seg_rec, data)
-        deserialize.assert_called_with(data[PathSegmentInfo.LEN:])
+        deserialize.assert_called_once_with(data[PathSegmentInfo.LEN:])
         parse.assert_called_once_with(data[:PathSegmentInfo.LEN])
+        pth_seg_rec.info = "data1"
+        pth_seg_rec.pcbs = "data2"
 
 
 class TestPathSegmentRecordsPack(object):
@@ -145,10 +152,12 @@ class TestPathSegmentRecordsPack(object):
     @patch("lib.packet.pcb.PathSegment.serialize")
     @patch("lib.packet.path_mgmt.PathSegmentInfo.pack")
     def test_basic(self, pack, serialize):
+        pack.return_value = "data1"
+        serialize.return_value = "data2"
         pth_seg_rec = PathSegmentRecords()
         pth_seg_rec.info = PathSegmentInfo()
         pth_seg_rec.pcbs = "data"
-        pth_seg_rec.pack()
+        ntools.eq_(pth_seg_rec.pack(), "data1"+"data2")
         serialize.assert_called_once_with("data")
         pack.assert_called_with()
 
@@ -161,7 +170,7 @@ class TestPathSegmentRecordsFromValues(object):
         pth_seg_rec = PathSegmentRecords.from_values("data1", "data2")
         ntools.eq_(pth_seg_rec.info, "data1")
         ntools.eq_(pth_seg_rec.pcbs, "data2")
-        
+
 
 class TestLeaseInfoInit(object):
     """
@@ -189,17 +198,19 @@ class TestLeaseInfoParse(object):
     """
     def test_basic(self):
         les_inf = LeaseInfo()
-        data = bytes.fromhex('0e 2a0a 0b0c 01020304') + b"superlengthybigstringoflength32."
+        data = bytes.fromhex('0e 2a0a 0b0c 01020304') + \
+               b"superlengthybigstringoflength32."
         les_inf.parse(data)
         ntools.eq_(les_inf.seg_type, 0x0e)
         ntools.eq_(les_inf.isd_id, 0x2a0a)
         ntools.eq_(les_inf.ad_id, 0x0b0c)
         ntools.eq_(les_inf.exp_time, 0x01020304)
         ntools.eq_(les_inf.seg_id, b"superlengthybigstringoflength32.")
-    
+
     def test_len(self):
         les_inf = LeaseInfo()
-        data = bytes.fromhex('0e 2a0a 0b0c 01020304') + b"superlengthybigstringoflength3"
+        data = bytes.fromhex('0e 2a0a 0b0c 01020304') + \
+               b"superlengthybigstringoflength3"
         ntools.eq_(les_inf.seg_type, PathSegmentType.DOWN)
         ntools.eq_(les_inf.isd_id, 0)
         ntools.eq_(les_inf.ad_id, 0)
@@ -212,7 +223,8 @@ class TestLeaseInfoPack(object):
     Unit tests for lib.packet.path_mgmt.LeaseInfo.from_values
     """
     def test_basic(self):
-        les_inf = LeaseInfo.from_values(0x0e, 0x2a0a, 0x0b0c, 0x01020304, b"superlengthybigstringoflength32.")
+        les_inf = LeaseInfo.from_values(0x0e, 0x2a0a, 0x0b0c, 0x01020304, \
+                                        b"superlengthybigstringoflength32.")
         ntools.eq_(les_inf.seg_type, 0x0e)
         ntools.eq_(les_inf.isd_id, 0x2a0a)
         ntools.eq_(les_inf.ad_id, 0x0b0c)
@@ -225,7 +237,8 @@ class TestLeaseInfoFromValues(object):
     Unit tests for lib.packet.path_mgmt.LeaseInfo.from_values
     """
     def test_basic(self):
-        les_inf = LeaseInfo.from_values(0x0e, 0x2a0a, 0x0b0c, 0x01020304, b"superlengthybigstringoflength32.")
+        les_inf = LeaseInfo.from_values(0x0e, 0x2a0a, 0x0b0c, 0x01020304, \
+                                        b"superlengthybigstringoflength32.")
         ntools.eq_(les_inf.seg_type, 0x0e)
         ntools.eq_(les_inf.isd_id, 0x2a0a)
         ntools.eq_(les_inf.ad_id, 0x0b0c)
@@ -259,12 +272,15 @@ class TestPathSegmentLeasesParse(object):
         pth_seg_les = PathSegmentLeases()
         data = bytes.fromhex('04')
         for i in range(0x04):
-        	data = data + bytes.fromhex('0e 2a0a 0b0c 01020304') + b"superlengthybigstringoflength32" + struct.pack("!B", i)
+            data = data + bytes.fromhex('0e 2a0a 0b0c 01020304') + \
+                   b"superlengthybigstringoflength32" + struct.pack("!B", i)
         pth_seg_les.parse(data)
         parse.assert_called()
         ntools.eq_(pth_seg_les.nleases, 0x04)
         for i in range(0x04):
-        	ntools.eq_(pth_seg_les.leases[i].pack(), bytes.fromhex('0e 2a0a 0b0c 01020304') + b"superlengthybigstringoflength32" + struct.pack("!B", i))
+            ntools.eq_(pth_seg_les.leases[i].pack(), \
+                       bytes.fromhex('0e 2a0a 0b0c 01020304') + \
+                       b"superlengthybigstringoflength32" + struct.pack("!B", i))
 
 
 class TestPathSegmentLeasesPack(object):
@@ -276,10 +292,11 @@ class TestPathSegmentLeasesPack(object):
         pth_seg_les.nleases = 0x04
         data = bytes.fromhex('04')
         for i in range(0x04):
-        	temp = bytes.fromhex('0e 2a0a 0b0c 01020304') + b"superlengthybigstringoflength32" + struct.pack("!B", i)
-        	linfo = LeaseInfo(temp)
-        	data = data + temp
-        	pth_seg_les.leases.append(linfo)
+            temp = bytes.fromhex('0e 2a0a 0b0c 01020304') + \
+                   b"superlengthybigstringoflength32" + struct.pack("!B", i)
+            linfo = LeaseInfo(temp)
+            data = data + temp
+            pth_seg_les.leases.append(linfo)
         ntools.eq_(pth_seg_les.pack(), data)
 
 
