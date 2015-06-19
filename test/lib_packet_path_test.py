4186f96a79cb0110e10e7870f0e7e316c6d1b255
Pratyaksh Sharma <pratyaksh@me.com>
2015-06-20 00:36:41 +0200

    added test for CrossOverPath.parse

--- test/lib_packet_path_test.py
+++ test/lib_packet_path_test.py
@@ -17,7 +17,7 @@
 """
 #Stdlib
 import copy
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, call
 
 # External packages
 import nose
@@ -396,12 +396,79 @@ class TestCrossOverPathInit(object):
         parse.assert_called_once_with("data")
 
 
-class TestCrossOverPathParse(BasePath):
+class TestCrossOverPathParse(object):
     """
     Unit tests for lib.packet.path.CrossOverPath.parse
     """
-    def test(self):
-        pass
+    @patch("lib.packet.path.CrossOverPath._parse_down_segment")
+    @patch("lib.packet.path.CrossOverPath._parse_up_segment")
+    def test_basic(self, parse_up, parse_down):
+        data = bytes.fromhex('0a 0b 0c')
+        parse_up.return_value = 1
+        co_path = CrossOverPath()
+        co_path.parse(data)
+        parse_up.assert_called_once_with(data)
+        parse_down.assert_called_once_with(data, 1)
+        ntools.assert_true(co_path.parsed)
+
+    def test_wrong_type(self):
+        co_path = CrossOverPath()
+        ntools.assert_raises(AssertionError, co_path.parse, 123)
+
+
+class TestCrossOverPathParseUpSegment(object):
+    """
+    Unit tests for lib.packet.path.CrossOverPath._parse_up_segment
+    """
+    @patch("lib.packet.path.HopOpaqueField")
+    @patch("lib.packet.path.InfoOpaqueField")
+    def test(self, info_of, hop_of):
+        mock_iof = MagicMock(spec=['hops'])
+        info_of.return_value = mock_iof
+        info_of.return_value.hops = 1
+        info_of.LEN = InfoOpaqueField.LEN
+        hop_of.return_value = 'data1'
+        hop_of.LEN = HopOpaqueField.LEN
+        data = 'some_very_long_data_string'
+        co_path = CrossOverPath()
+        offset = co_path._parse_up_segment(data)
+        info_of.assert_called_once_with(data[:InfoOpaqueField.LEN])
+        calls = [call(data[InfoOpaqueField.LEN:InfoOpaqueField.LEN +
+                           HopOpaqueField.LEN]),
+                 call(data[InfoOpaqueField.LEN + HopOpaqueField.LEN:
+                           InfoOpaqueField.LEN + 2 * HopOpaqueField.LEN])]
+        hop_of.assert_has_calls(calls)
+        ntools.eq_(co_path.up_segment_info, mock_iof)
+        ntools.eq_(co_path.up_segment_hops, ['data1'])
+        ntools.eq_(co_path.up_segment_upstream_ad, 'data1')
+        ntools.eq_(offset, InfoOpaqueField.LEN + 2 * HopOpaqueField.LEN)
+
+
+class TestCrossOverPathParseDownSegment(object):
+    """
+    Unit tests for lib.packet.path.CrossOverPath._parse_down_segment
+    """
+    @patch("lib.packet.path.HopOpaqueField")
+    @patch("lib.packet.path.InfoOpaqueField")
+    def test(self, info_of, hop_of):
+        mock_iof = MagicMock(spec=['hops'])
+        info_of.return_value = mock_iof
+        info_of.return_value.hops = 1
+        info_of.LEN = InfoOpaqueField.LEN
+        hop_of.return_value = 'data1'
+        hop_of.LEN = HopOpaqueField.LEN
+        data = 'some_very_long_data_string'
+        co_path = CrossOverPath()
+        co_path._parse_down_segment(data, 0)
+        info_of.assert_called_once_with(data[:InfoOpaqueField.LEN])
+        calls = [call(data[InfoOpaqueField.LEN:InfoOpaqueField.LEN +
+                           HopOpaqueField.LEN]),
+                 call(data[InfoOpaqueField.LEN + HopOpaqueField.LEN:
+                           InfoOpaqueField.LEN + 2 * HopOpaqueField.LEN])]
+        hop_of.assert_has_calls(calls)
+        ntools.eq_(co_path.down_segment_info, mock_iof)
+        ntools.eq_(co_path.down_segment_upstream_ad, 'data1')
+        ntools.eq_(co_path.down_segment_hops, ['data1'])
 
 
 class TestCrossOverPathPack(BasePath):
