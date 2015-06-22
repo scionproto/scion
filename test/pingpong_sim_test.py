9cf45b2bbe8ab3b4604f505d66a4b699501a89b5
mskd96 <deepakrishna95@gmail.com>
2015-06-22 17:19:57 +0200

    Formatting ping pong test

--- test/pingpong_sim_test.py
+++ test/pingpong_sim_test.py
@@ -16,13 +16,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import unittest
 import logging
 import profile
+import unittest
 
 from application.sim_ping_pong import SimPingApp, SimPongApp
 from endhost.sim_host import SCIONSimHost
 from ipaddress import IPv4Address
+from lib.packet.scion_addr import ISD_AD
 from simulator.simulator import generate_topology, run
 
 
@@ -40,19 +41,21 @@ class PingPongSimTest(unittest.TestCase):
         and Receiver is 127.2.26.254 in ISD:2 AD:26
         """
         generate_topology("../SIM/sim.conf")
-
-        host1 = SCIONSimHost(IPv4Address("127.1.100.254"), 
-                             "../topology/ISD1/topologies/ISD:1-AD:100.json")
-        host2 = SCIONSimHost(IPv4Address("127.1.16.254"), 
-                             "../topology/ISD1/topologies/ISD:1-AD:16.json")
-
-        ping_application = SimPingApp(host1, IPv4Address("127.1.16.254"),
-                                      16, 1)
+        src_isd_ad = ISD_AD(1,100)
+        dst_isd_ad = ISD_AD(1,16)
+        src_host_addr = IPv4Address("127.1.100.254")
+        dst_host_addr = IPv4Address("127.1.16.254")
+        src_topo_path = "../topology/ISD{}/topologies/ISD:{}-AD:{}.json"
+                        .format(src_isd_ad.isd, src_isd_ad.isd, src_isd_ad.ad)
+        dst_topo_path = "../topology/ISD{}/topologies/ISD:{}-AD:{}.json"
+                        .format(dst_isd_ad.isd, dst_isd_ad.isd, dst_isd_ad.ad)
+        host1 = SCIONSimHost(src_host_addr, src_topo_path)
+        host2 = SCIONSimHost(dst_host_addr, dst_topo_path) 
+        ping_application = SimPingApp(host1, dst_host_addr,
+                                      dst_isd_ad.ad, dst_isd_ad.isd)
         pong_application = SimPongApp(host2)
-
         app_start_time = 20.
         ping_application.start(app_start_time)
-
         run()
         logging.info("Simulation terminated")
         assert ping_application.pong_received and pong_application.ping_received
@@ -60,15 +63,12 @@ class PingPongSimTest(unittest.TestCase):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     to_profile = False
-
     def run_test():
         """
         Calls main function in unit test
         """
         unittest.main()
-
     if to_profile:
         profile.run('run_test()', sort='cumtime')
     else:
         run_test()
-
