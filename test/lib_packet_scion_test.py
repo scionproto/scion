956f893a8d6296e670ea29051f96c78129cf1224
Pratyaksh Sharma <pratyaksh@me.com>
2015-06-25 16:29:14 +0200

    Added tests for SCIONPacket

--- test/lib_packet_scion_test.py
+++ test/lib_packet_scion_test.py
@@ -28,7 +28,7 @@ from lib.packet.opaque_field import OpaqueField
 from lib.packet.path import PathBase
 from lib.packet.scion import (
     get_type,
-    SCIONCommonHdr, SCIONHeader)
+    SCIONCommonHdr, SCIONHeader, SCIONPacket)
 from lib.packet.scion_addr import SCIONAddr
 
 
@@ -568,6 +568,118 @@ class TestSCIONHeaderLen(object):
         hdr.common_hdr.hdr_len = 123
         hdr._extension_hdrs = ['ext_hdr0', 'ext_hdr01']
         ntools.eq_(len(hdr), 123 + len('ext_hdr0') + len('ext_hdr01'))
+
+
+class TestSCIONPacketInit(object):
+    """
+    Unit tests for lib.packet.scion.SCIONPacket.__init__
+    """
+    @patch("lib.packet.scion.PacketBase.__init__", autospec=True)
+    def test_basic(self, init):
+        packet = SCIONPacket()
+        init.assert_called_once_with(packet)
+        ntools.eq_(packet.payload_len, 0)
+
+    @patch("lib.packet.scion.SCIONPacket.parse", autospec=True)
+    def test_with_args(self, parse):
+        packet = SCIONPacket('data')
+        parse.assert_called_once_with(packet, 'data')
+
+
+class TestSCIONPacketFromValues(object):
+    """
+    Unit tests for lib.packet.scion.SCIONPacket.from_values
+    """
+    @patch("lib.packet.scion.SCIONPacket.set_hdr", autospec=True)
+    @patch("lib.packet.scion.SCIONPacket.set_payload", autospec=True)
+    @patch("lib.packet.scion.SCIONHeader.from_values",
+           spec_set=SCIONHeader.from_values)
+    def test_basic(self, scion_hdr, set_payload, set_hdr):
+        scion_hdr.return_value = 'hdr'
+        packet = SCIONPacket.from_values('src', 'dst', 'payload', 'path',
+                                         'ext_hdrs', 'next_hdr')
+        ntools.assert_is_instance(packet, SCIONPacket)
+        scion_hdr.assert_called_once_with('src', 'dst', 'path', 'ext_hdrs',
+                                          'next_hdr')
+        set_hdr.assert_called_once_with(packet, 'hdr')
+        set_payload.assert_called_once_with(packet, 'payload')
+
+    @patch("lib.packet.scion.SCIONPacket.set_hdr", autospec=True)
+    @patch("lib.packet.scion.SCIONPacket.set_payload", autospec=True)
+    @patch("lib.packet.scion.SCIONHeader.from_values",
+           spec_set=SCIONHeader.from_values)
+    def test_less_args(self, scion_hdr, set_payload, set_hdr):
+        packet = SCIONPacket.from_values('src', 'dst', 'payload')
+        scion_hdr.assert_called_once_with('src', 'dst', None, None, 0)
+
+
+class TestSCIONPacketSetPayload(object):
+    """
+    Unit tests for lib.packet.scion.SCIONPacket.set_payload
+    """
+    @patch("lib.packet.scion.PacketBase.set_payload", autospec=True)
+    def test(self, set_payload):
+        packet = SCIONPacket()
+        packet.payload_len = 123
+        packet._hdr = MagicMock(spec_set=['common_hdr'])
+        packet._hdr.common_hdr = MagicMock(spec_set=['total_len'])
+        packet._hdr.common_hdr.total_len = 456
+        packet.set_payload('payload')
+        set_payload.assert_called_once_with(packet, 'payload')
+        ntools.eq_(packet.payload_len, len('payload'))
+        ntools.eq_(packet.hdr.common_hdr.total_len, 456 - 123 + len('payload'))
+
+
+class TestSCIONPacketParse(object):
+    """
+    Unit tests for lib.packet.scion.SCIONPacket.parse
+    """
+    def test_bad_type(self):
+        packet = SCIONPacket()
+        ntools.assert_raises(AssertionError, packet.parse, 123)
+
+    def test_bad_length(self):
+        packet = SCIONPacket()
+        data = b'\x00' * (SCIONPacket.MIN_LEN - 1)
+        packet.parse(data)
+        ntools.assert_false(packet.parsed)
+
+    @patch("lib.packet.scion.SCIONPacket.set_payload", autospec=True)
+    @patch("lib.packet.scion.SCIONPacket.set_hdr", autospec=True)
+    @patch("lib.packet.scion.SCIONHeader", autospec=True)
+    def test_full(self, scion_hdr, set_hdr, set_payload):
+        packet = SCIONPacket()
+        packet._hdr = 'header'
+        data = bytes(range(SCIONPacket.MIN_LEN))
+        scion_hdr.return_value = 'scion_header'
+        packet.parse(data)
+        ntools.eq_(packet.raw, data)
+        scion_hdr.assert_called_once_with(data)
+        set_hdr.assert_called_once_with(packet, 'scion_header')
+        hdr_len = len(packet.hdr)
+        ntools.eq_(packet.payload_len, len(data) - hdr_len)
+        set_payload.assert_called_once_with(packet, data[hdr_len:])
+        ntools.assert_true(packet.parsed)
+
+
+class TestSCIONPacketPack(object):
+    """
+    Unit tests for lib.packet.scion.SCIONPacket.pack
+    """
+    def test_payload_packetbase(self):
+        packet = SCIONPacket()
+        packet._hdr = MagicMock(spec_set=['pack'])
+        packet._hdr.pack.return_value = b'packed_hdr'
+        packet._payload = MagicMock(spec_set=SCIONPacket)
+        packet._payload.pack.return_value = b'packed_payload'
+        ntools.eq_(packet.pack(), b'packed_hdrpacked_payload')
+
+    def test_payload_packetbase(self):
+        packet = SCIONPacket()
+        packet._hdr = MagicMock(spec_set=['pack'])
+        packet._hdr.pack.return_value = b'packed_hdr'
+        packet._payload = b'packed_payload'
+        ntools.eq_(packet.pack(), b'packed_hdrpacked_payload')
 
 
 if __name__ == "__main__":
