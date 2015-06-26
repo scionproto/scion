0082aa5f5e34054d05f7fffef0a0047d337df9c5
prateshg <g.pratish@gmail.com>
2015-06-26 15:39:29 +0200

    Addressed Comments

--- test/lib_path_store_test.py
+++ test/lib_path_store_test.py
@@ -52,25 +52,16 @@ class TestPathPolicyGetPathPolicyDict(object):
     Unit tests for lib.path_store.PathPolicy.get_path_policy_dict
     """
     def test_basic(self):
+        keys = ["best_set_size", "candidates_set_size", "history_limit",
+                "update_after_number", "update_after_time", "unwanted_ads",
+                "property_ranges", "property_weights"]
         pth_pol = PathPolicy()
-        pth_pol.best_set_size = "best_set_size"
-        pth_pol.candidates_set_size = "candidates_set_size"
-        pth_pol.history_limit = "history_limit"
-        pth_pol.update_after_number = "update_after_number"
-        pth_pol.update_after_time = "update_after_time"
-        pth_pol.unwanted_ads = "unwanted_ads"
-        pth_pol.property_ranges = "property_ranges"
-        pth_pol.property_weights = "property_weights"
+        target = {}
+        for key in keys:
+            setattr(pth_pol, key, key)
+            target[key] = key
         dict_ = pth_pol.get_path_policy_dict()
-        ntools.eq_(dict_['best_set_size'], "best_set_size")
-        ntools.eq_(dict_['candidates_set_size'], "candidates_set_size")
-        ntools.eq_(dict_['history_limit'], "history_limit")
-        ntools.eq_(dict_['update_after_number'], "update_after_number")
-        ntools.eq_(dict_['update_after_time'], "update_after_time")
-        ntools.eq_(dict_['unwanted_ads'], "unwanted_ads")
-        ntools.eq_(dict_['property_ranges'], "property_ranges")
-        ntools.eq_(dict_['property_weights'], "property_weights")
-        ntools.eq_(len(dict_), 8)
+        ntools.eq_(dict_, target)
 
 
 @patch("lib.path_store.logging.warning", autospec=True)
@@ -79,8 +70,7 @@ class TestPathPolicyCheckFilters(object):
     Unit tests for lib.path_store.PathPolicy.check_filters
     """
     def test_basic(self, wrng):
-        pcb = MagicMock(spec_set=['__class__'])
-        pcb.__class__ = PathSegment
+        pcb = MagicMock(spec_set=PathSegment)
         pth_pol = PathPolicy()
         pth_pol._check_unwanted_ads = MagicMock(spec_set=[])
         pth_pol._check_unwanted_ads.return_value = True
@@ -90,8 +80,7 @@ class TestPathPolicyCheckFilters(object):
         ntools.eq_(wrng.call_count, 0)
 
     def test_unwanted_ads(self, wrng):
-        pcb = MagicMock(spec_set=['__class__'])
-        pcb.__class__ = PathSegment
+        pcb = MagicMock(spec_set=PathSegment)
         pth_pol = PathPolicy()
         pth_pol._check_unwanted_ads = MagicMock(spec_set=[])
         pth_pol._check_unwanted_ads.return_value = False
@@ -99,8 +88,7 @@ class TestPathPolicyCheckFilters(object):
         wrng.assert_called_once_with("PathStore: pcb discarded (unwanted AD).")
 
     def test_property_ranges(self, wrng):
-        pcb = MagicMock(spec_set=['__class__'])
-        pcb.__class__ = PathSegment
+        pcb = MagicMock(spec_set=PathSegment)
         pth_pol = PathPolicy()
         pth_pol._check_unwanted_ads = MagicMock(spec_set=[])
         pth_pol._check_unwanted_ads.return_value = True
@@ -115,129 +103,134 @@ class TestPathPolicyCheckUnwantedAds(object):
     """
     Unit tests for lib.path_store.PathPolicy._check_unwanted_ads
     """
-    def test_basic(self):
-        pcb = MagicMock(spec_set=['ads'])
-        pcb.ads = [MagicMock(spec_set=['pcbm']) for i in range(5)]
+    def setUp(self):
+        self.pcb = MagicMock(spec_set=['ads'])
+        self.pcb.ads = [MagicMock(spec_set=['pcbm']) for i in range(5)]
         for i in range(5):
-            pcb.ads[i].pcbm = MagicMock(spec_set=['isd_id', 'ad_id'])
-            pcb.ads[i].pcbm.isd_id = "isd_id" + str(i)
-            pcb.ads[i].pcbm.ad_id = "ad_id" + str(i)
-        pth_pol = PathPolicy()
-        pth_pol.unwanted_ads = [("isd_id1", "ad_id1")]
-        ntools.assert_false(pth_pol._check_unwanted_ads(pcb))
+            self.pcb.ads[i].pcbm = MagicMock(spec_set=['isd_id', 'ad_id'])
+            self.pcb.ads[i].pcbm.isd_id = "isd_id" + str(i)
+            self.pcb.ads[i].pcbm.ad_id = "ad_id" + str(i)
+        self.pth_pol = PathPolicy()
+
+    def tearDown(self):
+        del self.pcb
+        del self.pth_pol
+
+    def test_basic(self):
+        self.pth_pol.unwanted_ads = [("isd_id1", "ad_id1")]
+        ntools.assert_false(self.pth_pol._check_unwanted_ads(self.pcb))
 
     def test_not_present(self):
-        pcb = MagicMock(spec_set=['ads'])
-        pcb.ads = [MagicMock(spec_set=['pcbm']) for i in range(5)]
-        for i in range(5):
-            pcb.ads[i].pcbm = MagicMock(spec_set=['isd_id', 'ad_id'])
-            pcb.ads[i].pcbm.isd_id = "isd_id" + str(i)
-            pcb.ads[i].pcbm.ad_id = "ad_id" + str(i)
-        pth_pol = PathPolicy()
-        pth_pol.unwanted_ads = []
-        ntools.assert_true(pth_pol._check_unwanted_ads(pcb))
+        self.pth_pol.unwanted_ads = []
+        ntools.assert_true(self.pth_pol._check_unwanted_ads(self.pcb))
 
 
 class TestPathPolicyCheckPropertyRanges(object):
     """
     Unit tests for lib.path_store.PathPolicy._check_property_ranges
     """
-    def __init__(self):
+    def setUp(self):
         self.d = {'PeerLinks': [], 'HopsLength': [], 'DelayTime': [],
                   'GuaranteedBandwidth': [], 'AvailableBandwidth': [],
                   'TotalBandwidth': []}
 
+    def tearDown(self):
+        del self.d
+
     def test_basic(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         ntools.assert_true(pth_pol._check_property_ranges("pcb"))
 
     def test_peer_link_true(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['PeerLinks'].extend([0, 2])
         pcb = MagicMock(spec_set=['get_n_peer_links'])
         pcb.get_n_peer_links.return_value = 1
         ntools.assert_true(pth_pol._check_property_ranges(pcb))
+        pcb.get_n_peer_links.assert_called_once_with()
 
     def test_peer_link_false(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['PeerLinks'].extend([2, 0])
         pcb = MagicMock(spec_set=['get_n_peer_links'])
-        pcb.get_n_peer_links.return_value = 1
+        pcb.get_n_peer_links.return_value = 3
         ntools.assert_false(pth_pol._check_property_ranges(pcb))
 
     def test_hop_length_true(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['HopsLength'].extend([0, 2])
         pcb = MagicMock(spec_set=['get_n_hops'])
         pcb.get_n_hops.return_value = 1
         ntools.assert_true(pth_pol._check_property_ranges(pcb))
+        pcb.get_n_hops.assert_called_once_with()
 
     def test_hop_length_false(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
-        pth_pol.property_ranges['HopsLength'].extend([2, 0])
+        pth_pol.property_ranges = self.d
+        pth_pol.property_ranges['HopsLength'].extend([0, 2])
         pcb = MagicMock(spec_set=['get_n_hops'])
-        pcb.get_n_hops.return_value = 1
+        pcb.get_n_hops.return_value = 3
         ntools.assert_false(pth_pol._check_property_ranges(pcb))
 
     @patch("lib.path_store.time.time", autospec=True)
     def test_delay_time_true(self, time_):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['DelayTime'].extend([0, 2])
+        time_.return_value = 2
         pcb = MagicMock(spec_set=['get_timestamp'])
         pcb.get_timestamp.return_value = 1
-        time_.return_value = 2
         ntools.assert_true(pth_pol._check_property_ranges(pcb))
         time_.assert_called_once_with()
+        pcb.get_timestamp.assert_called_once_with()
 
     @patch("lib.path_store.time.time", autospec=True)
     def test_delay_time_false(self, time_):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['DelayTime'].extend([2, 0])
+        time_.return_value = 2
         pcb = MagicMock(spec_set=['get_timestamp'])
         pcb.get_timestamp.return_value = 1
-        time_.return_value = 2
         ntools.assert_false(pth_pol._check_property_ranges(pcb))
 
     def test_guaranteed_bandwidth_true(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['GuaranteedBandwidth'].extend([0, 20])
         ntools.assert_true(pth_pol._check_property_ranges("pcb"))
 
     def test_guaranteed_bandwidth_false(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['GuaranteedBandwidth'].extend([20, 0])
         ntools.assert_false(pth_pol._check_property_ranges("pcb"))
 
     def test_available_bandwidth_true(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['AvailableBandwidth'].extend([0, 20])
         ntools.assert_true(pth_pol._check_property_ranges("pcb"))
 
     def test_available_bandwidth_false(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['AvailableBandwidth'].extend([20, 0])
         ntools.assert_false(pth_pol._check_property_ranges("pcb"))
 
     def test_total_bandwidth_true(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['TotalBandwidth'].extend([0, 20])
         ntools.assert_true(pth_pol._check_property_ranges("pcb"))
 
     def test_total_bandwidth_false(self):
         pth_pol = PathPolicy()
-        pth_pol.property_ranges = self.d.copy()
+        pth_pol.property_ranges = self.d
         pth_pol.property_ranges['TotalBandwidth'].extend([20, 0])
         ntools.assert_false(pth_pol._check_property_ranges("pcb"))
 
@@ -368,10 +361,13 @@ class TestPathStoreRecordEQ(object):
     """
     Unit tests for lib.path_store.PathStoreRecord.__eq__
     """
-    def __init__(self):
+    def setUp(self):
         self.pcb = MagicMock(spec_set=['__class__', 'segment_id',
                                        'get_expiration_time'])
         self.pcb.__class__ = PathSegment
+
+    def tearDown(self):
+        del self.pcb
 
     def test_eq(self):
         pth_str_rec1 = PathStoreRecord(self.pcb)
