e143783878901d2d2d24e5a4cf324dbacf0d14be
Anton Ovchinnikov <antono@student.ethz.ch>
2015-07-03 16:24:28 +0200

    Update reload_data to handle identical AD ids

--- web_scion/scripts/reload_data.py
+++ web_scion/scripts/reload_data.py
@@ -8,8 +8,10 @@ Import ISD/AD data from topology files
 import glob
 import os
 import sys
-from os.path import dirname as dir
-sys.path.insert(0, dir(dir(dir(os.path.abspath(__file__)))))
+from os.path import dirname as d
+from django.db import transaction
+
+sys.path.insert(0, d(d(d(os.path.abspath(__file__)))))
 
 # External packages
 import django
@@ -54,39 +56,77 @@ def add_users():
 
 
 def reload_data():
+    transaction.set_autocommit(False)
     clear_everything()
     add_users()
 
     # Add model instances
     topology_files = glob.glob(os.path.join(TOPOLOGY_PATH,
-                                            'ISD*', 'topologies', 'ISD*.json'))
+                                            'ISD*', 'topologies', 'ISD:*.json'))
+    ad_num = len(topology_files)
+    print("> {} topology files found".format(ad_num))
     isds = {}
-    ads = []
+    ad_topos = []
+    ad_ids = set()
 
+    same_ad_ids = False
     for topo_file in topology_files:
         topology = Topology.from_file(topo_file)
-        isds[topology.isd_id] = topology.isd_id
-        ads.append(topology)
-    ads = sorted(ads, key=lambda topo: topo.ad_id)
+        isds[topology.isd_id] = None
+        if not same_ad_ids and topology.ad_id in ad_ids:
+            same_ad_ids = True
+        ad_ids.add(topology.ad_id)
+        ad_topos.append(topology)
+
+    ad_topos = sorted(ad_topos, key=lambda t: t.ad_id)
+    assert len(ad_topos) == ad_num
+
+    id_map = {}
+    if same_ad_ids:
+        print("> Several ADs with identical IDs are found. Currently, this "
+              "case is not supported. Renumerating ADs...")
+        ad_id = 1
+        for topo in ad_topos:
+            id_map[(topo.ad_id, topo.isd_id)] = ad_id
+            topo.ad_id = ad_id
+            ad_id += 1
+
+        # Fixing routers
+        for topo in ad_topos:
+            routers = topo.get_all_edge_routers()
+            for router in routers:
+                neighbor_id = router.interface.neighbor_ad
+                new_neighbor_id = id_map[(neighbor_id,
+                                          router.interface.neighbor_isd)]
+                router.interface.neighbor_ad = new_neighbor_id
 
     # Add ISDs
-    for isd_id in isds:
+    for isd_id in sorted(isds.keys()):
         isd = ISD(id=isd_id)
         isd.save()
         isds[isd_id] = isd
-    print("> {} ISD(s) were loaded".format(len(isds)))
+    print(isds)
 
     # First, save all add ADs to avoid IntegrityError
-    for ad_topo in ads:
+    report_ranges = {int(ad_num / 10.0 * x): x * 10 for x in range(1, 11)}
+    print(report_ranges)
+    for i, ad_topo in enumerate(ad_topos, start=1):
+        if i in report_ranges:
+            print("{}%".format(report_ranges[i]))
+        # print("Saving AD {}-{}".format(ad_topo.isd_id, ad_topo.ad_id))
         ad = AD(id=ad_topo.ad_id, isd=isds[ad_topo.isd_id],
                 is_core_ad=ad_topo.is_core_ad, dns_domain=ad_topo.dns_domain)
         ad.save()
+    transaction.commit()
 
+    print("> ADs instances were added")
     # Add routers, servers, etc.
-    for ad_topo in ads:
+    for ad_topo in ad_topos:
         ad = AD.objects.get(id=ad_topo.ad_id, isd=isds[ad_topo.isd_id])
         ad.fill_from_topology(ad_topo)
         print('> AD {} is loaded'.format(ad))
+    transaction.commit()
+    transaction.set_autocommit(True)
 
 if __name__ == "__main__":
     reload_data()
