7d9c02e27b6943f224287bcffe14b679edeba524
pszalach <psz@inf.ethz.ch>
2015-08-07 10:25:49 +0200

    docker test fixed

--- docker/integration_tests.sh
+++ docker/integration_tests.sh
@@ -3,18 +3,19 @@
 set -e
 # Run SCION and wait for a start.
 ./scion.sh run
-sleep 5
+echo "Waiting for path propagation, etc..."
+sleep 15
 # Check SCION status before tests.
 ./scion.sh status
 
 # Run integration tests.
-# End2End test.
+echo "End2End test"
 cd test/integration/
 PYTHONPATH=../../ python3 end2end_test.py
 # Check SCION status after the test.
 cd ../../
 ./scion.sh status
-# Traceroute Extension test.
+echo "Traceroute Extension test"
 cd test/integration/
 PYTHONPATH=../../ python3 cli_srv_ext_test.py
 # Check SCION status after the test.
