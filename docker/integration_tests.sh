566cdf2b85873b63ea3151984ca1bec4543af59a
pszalach <psz@inf.ethz.ch>
2015-08-07 10:21:52 +0200

    ext test added to docker

--- docker/integration_tests.sh
+++ docker/integration_tests.sh
@@ -6,9 +6,17 @@ set -e
 sleep 5
 # Check SCION status before tests.
 ./scion.sh status
+
 # Run integration tests.
+# End2End test.
 cd test/integration/
 PYTHONPATH=../../ python3 end2end_test.py
-# Check SCION status after tests.
+# Check SCION status after the test.
+cd ../../
+./scion.sh status
+# Traceroute Extension test.
+cd test/integration/
+PYTHONPATH=../../ python3 cli_srv_ext_test.py
+# Check SCION status after the test.
 cd ../../
 ./scion.sh status
