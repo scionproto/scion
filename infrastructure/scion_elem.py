918d9b8c7955f7514551e21838dd94f5c8346df6
Stephen Shirley <kormat@gmail.com>
2015-08-31 15:47:48 +0200

    Make SCIONElem.addr a simple attribute.

--- infrastructure/scion_elem.py
+++ infrastructure/scion_elem.py
@@ -101,37 +101,6 @@ class SCIONElement(object):
             logging.info("%s: bound %s:%u", self.id, self.addr.host_addr,
                          SCION_UDP_PORT)
 
-    @property
-    def addr(self):
-        """
-        The address of the server as a :class:`lib.packet.scion_addr.SCIONAddr`
-        object.
-
-        :returns:
-        :type:
-        """
-        return self._addr
-
-    @addr.setter
-    def addr(self, addr):
-        """
-        Set the address of the server. Must be a
-        :class:`lib.packet.scion_addr.SCIONAddr` object.
-
-        :param addr: the new server address.
-        :type addr: :class:`lib.packet.scion_addr.SCIONAddr`
-        """
-        self.set_addr(addr)
-
-    def set_addr(self, addr):
-        """
-        Set the address of the server. Must be a lib.SCIONAddr object
-        """
-        if not (isinstance(addr, SCIONAddr) or addr is None):
-            raise TypeError("Addr must be of type 'SCIONAddr'")
-        else:
-            self._addr = addr
-
     def parse_topology(self, topo_file):
         """
         Instantiate a Topology object given 'topo_file'.
