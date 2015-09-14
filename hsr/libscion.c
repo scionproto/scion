8f65d0f8e646418d09c89b2dd5dfea6613e91bfd
Takayuki Sasaki <t-sasaki@fb.jp.nec.com>
2015-09-14 23:27:20 +0200

    update get_type() so that it can use SVC address

--- hsr/libscion.c
+++ hsr/libscion.c
@@ -6,43 +6,41 @@
 #include "scion.h"
 
 uint8_t get_type(SCIONHeader *hdr) {
+  // TODO check address type
+
   SCIONAddr *src = (SCIONAddr *)(&hdr->srcAddr);
-  //if (src->host_addr[0] != 10)
-  //  return DATA_PACKET;
-  //if (src->host_addr[1] != 224)
-  //  return DATA_PACKET;
-  //if (src->host_addr[2] != 0)
-  //  return DATA_PACKET;
-
   SCIONAddr *dst = (SCIONAddr *)(&hdr->dstAddr);
-  if (dst->host_addr[0] != 10)
-    return DATA_PACKET;
-  if (dst->host_addr[1] != 224)
-    return DATA_PACKET;
-  if (dst->host_addr[2] != 0)
-    return DATA_PACKET;
+  /*
+    int i;
+    for(i=0; i<8; i++)
+          printf("%02x",*((unsigned char *)hdr +i));
+    printf("\n");
+    for(i=0; i<8; i++)
+          printf("%02x",*((unsigned char *)src +i));
+    printf("host addr
+    %x,%x,%x,%x\n",src->host_addr[0],src->host_addr[1],src->host_addr[2],src->host_addr[3]);
+    printf("host addr
+    %x,%x,%x,%x\n",dst->host_addr[0],dst->host_addr[1],dst->host_addr[2],dst->host_addr[3]);
+    printf("%d\n",ntohs(*(uint16_t*)dst->host_addr) );
+  */
 
-  int b1 = src->host_addr[3] == BEACON_PACKET ||
-           src->host_addr[3] == PATH_MGMT_PACKET ||
-           src->host_addr[3] == CERT_CHAIN_REP_PACKET ||
-           src->host_addr[3] == TRC_REP_PACKET;
-  int b2 = dst->host_addr[3] == PATH_MGMT_PACKET ||
-           dst->host_addr[3] == TRC_REQ_PACKET ||
-           dst->host_addr[3] == TRC_REQ_LOCAL_PACKET ||
-           dst->host_addr[3] == CERT_CHAIN_REQ_PACKET ||
-           dst->host_addr[3] == CERT_CHAIN_REQ_LOCAL_PACKET ||
-           dst->host_addr[3] == IFID_PKT_PACKET;
+  uint16_t src_svc = ntohs(*(uint16_t *)src->host_addr);
+  uint16_t dst_svc = ntohs(*(uint16_t *)dst->host_addr);
 
+  int b1 = src_svc == BEACON_PACKET || src_svc == PATH_MGMT_PACKET ||
+           src_svc == CERT_CHAIN_REP_PACKET || src_svc == TRC_REP_PACKET;
+  int b2 = dst_svc == PATH_MGMT_PACKET || dst_svc == TRC_REQ_PACKET ||
+           dst_svc == TRC_REQ_LOCAL_PACKET ||
+           dst_svc == CERT_CHAIN_REQ_PACKET ||
+           dst_svc == CERT_CHAIN_REQ_LOCAL_PACKET || dst_svc == IFID_PKT_PACKET;
 
   if (b1)
-    return src->host_addr[3];
+    return src_svc;
   else if (b2)
-    return dst->host_addr[3];
+    return dst_svc;
   else
     return DATA_PACKET;
-  return &hdr->srcAddr;
 }
-
 
 uint8_t is_on_up_path(InfoOpaqueField *currOF) {
   if ((currOF->info & 0x1) ==
@@ -76,4 +74,3 @@ uint8_t is_xovr(HopOpaqueField *currOF) {
   }
   return 1;
 }
-
