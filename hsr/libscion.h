179fa6c486576047294714afeb68f516da4ca313
Takayuki Sasaki <t-sasaki@fb.jp.nec.com>
2015-09-14 16:23:18 +0200

    clang-format

--- hsr/libscion.h
+++ hsr/libscion.h
@@ -1,9 +1,8 @@
 #include "scion.h"
 
 uint8_t get_type(SCIONHeader *hdr);
-uint8_t is_on_up_path(InfoOpaqueField *currOF) ;
-uint8_t is_last_path_of(SCIONCommonHeader *sch) ;
-uint8_t is_regular(HopOpaqueField *currOF) ;
-uint8_t is_continue(HopOpaqueField *currOF) ;
-uint8_t is_xovr(HopOpaqueField *currOF) ;
-
+uint8_t is_on_up_path(InfoOpaqueField *currOF);
+uint8_t is_last_path_of(SCIONCommonHeader *sch);
+uint8_t is_regular(HopOpaqueField *currOF);
+uint8_t is_continue(HopOpaqueField *currOF);
+uint8_t is_xovr(HopOpaqueField *currOF);
