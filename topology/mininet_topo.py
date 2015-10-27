c12cb954ca6e77b9706e0ac8d23453a76d1afd45
David Barrera <david.barrera@inf.ethz.ch>
2015-10-27 11:47:09 +0100

    fixing style and adding extra checks for loopbacks

--- topology/mininet_topo.py
+++ topology/mininet_topo.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python2
+# Copyright 2015 ETH Zurich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# Stdlib
+import configparser
+import ipaddress
+import sys
+import os.path
+
+# External packages
 from mininet.net import Mininet
-from mininet.node import OVSKernelSwitch
+from mininet.node import OVSKernelSwitch, OVSController
 from mininet.topo import Topo
 from mininet.log import lg
 from mininet.cli import CLI
-import configparser
-import ipaddress
-
-import sys
-flush = sys.stdout.flush
 
 
 class ScionTopo(Topo):
@@ -55,15 +70,26 @@ class ScionTopo(Topo):
                     self.addLink(name, switchname, intfName1=ifName1,
                                  intfName2=ifName2, params1={'ip': str(hostip)})
                     hostcounter = hostcounter + 1
-
-if __name__ == '__main__':
+def runMininet():
     lg.setLogLevel('info')
     topology = configparser.ConfigParser(interpolation=None)
-    topology.read('../gen/networks.conf')
+    if os.path.isfile('../gen/networks.conf'):
+        topology.read('../gen/networks.conf')
+        # should also check if this is a 127.0.x.x file, or a 100.64.x.x
+        if ( ipaddress.IPv4Interface(topology.sections()[0]).is_loopback ):
+            print "gen/networks appears to be using loopback addresses. Try running scion.sh topology -m"
+            sys.exit(-1)
+    else:
+        print "gen/networks.conf does not exist. Have you run scion.sh topology -m?"
+        sys.exit(-1)
+
     topo = ScionTopo(topology)
-    net = Mininet(topo=topo, switch=OVSKernelSwitch)
+    net = Mininet(topo=topo, switch=OVSKernelSwitch, controller=OVSController)
     net.start()
-    # net.pingAll()
-    CLI = CLI(net)
+    #net.pingAll()
+    #CLI = CLI(net)
     # flush()
     net.stop()
+
+if __name__ == '__main__':
+    runMininet()
