91e0acae5fbba4f304a5263e7fe7d7ba93e08f35
Stephen Shirley <kormat@gmail.com>
2015-10-28 16:29:17 +0100

    Move into topology/mininet dir, minor cleanup and comments.

--- topology/mininet/topology.py
+++ topology/mininet/topology.py
@@ -10,11 +10,14 @@ import configparser
 import ipaddress
 
 MAX_INTF_LEN = 15
+NETWORKS_CONF = "gen/networks.conf"
 
 
 class ScionLink(Link):
     @classmethod
     def intfName(cls, node, n):
+        # Override the default intf naming in Link, as the default "NAME-ethX"
+        # form becomes too long.
         name = "%s-%s" % (node.name, n)
         assert len(name) <= MAX_INTF_LEN
         return name
@@ -22,20 +25,23 @@ class ScionLink(Link):
 
 class ScionTopo(Topo):
     """
-    Topology built from a SCION mininet config."
+    Topology built from a SCION network config."
     """
-
     def __init__(self, mnconfig, **params):
 
         # Initialize topology
         Topo.__init__(self, **params)
+        self._genTopo(mnconfig)
 
+    def _genTopo(self, mnconfig):
         switch_map = {}
         for i, name in enumerate(mnconfig.sections()):
             switch_map[name] = self.addSwitch("s%s" % i)
         host_map = {}
         for name, section in mnconfig.items():
             for elem, intf_str in section.items():
+                # The config is utf8, need to convert to a plain string to avoid
+                # tickling bugs in mininet.
                 elem = str(elem)
                 if elem not in host_map:
                     host_map[elem] = self.addHost(
@@ -47,6 +53,8 @@ class ScionTopo(Topo):
     def addLink(self, node1, node2, params=None):
         self.addPort(node1, node2, None, None)
         key = tuple(self.sorted([node1, node2]))
+        # Map the supplied params to the node1 interface, even if sorting turns
+        # it into the second interface.
         if key[0] == node1:
             self.link_info[key] = {"params1": params}
         else:
@@ -55,13 +63,17 @@ class ScionTopo(Topo):
         return key
 
 
-if __name__ == '__main__':
+def main():
     lg.setLogLevel('info')
     topology = configparser.ConfigParser(interpolation=None)
-    topology.read('gen/networks.conf')
+    topology.read_file(open(NETWORKS_CONF), source=NETWORKS_CONF)
     topo = ScionTopo(topology)
     net = Mininet(topo=topo, controller=RemoteController, link=ScionLink,
                   switch=OVSKernelSwitch)
     net.start()
-    CLI = CLI(net)
+    CLI(net)
     net.stop()
+
+
+if __name__ == '__main__':
+    main()
