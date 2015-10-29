028161d4418e8364c185a5624d8ec04a5a4b6050
David Barrera <david.barrera@inf.ethz.ch>
2015-10-29 17:06:14 +0100

    improving further thanks to suggestions from @kormat

--- topology/mininet/run.sh
+++ topology/mininet/run.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-function killpox {
-	for i in $(pgrep -f pox)
-	do
-		kill $i
-	done
-}
-# kill any pox instances before we get started
-killpox
-POX_CMD="$(which pox) forwarding.l2_learning"
-POX_LOG="logs/pox.log"
-if [ $? -eq 1 ]
-then
-	echo "Can't find pox in $PATH."
-	exit 1
+
+POX_PID=/tmp/pox.pid
+POX_LOG=/tmp/pox.log
+POX_PORT=6633
+
+echo $PATH
+if [ -e "$POX_PID" ]; then
+    echo "ERROR: Pox already running, or $POX_PID is stale"
+    exit 1
 fi
-$POX_CMD 2>&1 >$POX_LOG &
+
+pox forwarding.l2_learning misc.pidfile --file=$POX_PID log --no_default --file=$POX_LOG,w --format="%(asctime)s: %(message)s" &
 #wait for pox to start to avoid "can't connect to controller errors"
 sleep 1
+if nc -z localhost $POX_PORT; then
+    echo "POX running on localhost:$POX_PORT"
+else
+    echo "ERROR: Pox not running:"
+    echo "======================="
+    cat "$POX_LOG"
+    exit 1
+fi
 sudo SUPERVISORD=$(which supervisord) python topology/mininet/topology.py
-# kill any remaining pox instances
-killpox
+
+echo "Stopping POX"
+kill $(< $POX_PID)
