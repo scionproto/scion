e994c13d209ed9ab83f50203e4d062ed867252c7
David Barrera <david.barrera@inf.ethz.ch>
2015-10-29 15:22:18 +0100

    improving the mininet run script
    * looks for pox binary, fails if can't find it
    * kills active pox instances before starting to avoid "can't bind to port"
    * sleeps for 1 s to avoid "can't connect to controller"
    * runs mininet as usual
    * kills all remaining pox instances

--- topology/mininet/run.sh
+++ topology/mininet/run.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-
+function killpox {
+	for i in $(pgrep -f pox)
+	do
+		kill $i
+	done
+}
+# kill any pox instances before we get started
+killpox
+POX_CMD="$(which pox) forwarding.l2_learning"
+POX_LOG="logs/pox.log"
+if [ $? -eq 1 ]
+then
+	echo "Can't find pox in $PATH."
+	exit 1
+fi
+$POX_CMD 2>&1 >$POX_LOG &
+#wait for pox to start to avoid "can't connect to controller errors"
+sleep 1
 sudo SUPERVISORD=$(which supervisord) python topology/mininet/topology.py
+# kill any remaining pox instances
+killpox
