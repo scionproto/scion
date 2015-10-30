65032417d598f98e58a4694ab2b26fa3a0c674c0
Stephen Shirley <kormat@gmail.com>
2015-10-30 12:04:41 +0100

    Improve post-mininet cleanup.
    - Add a tiny pox extension to exit cleanly when killed.
    - Cleanup output of run.sh.
    - Log pox to logs/ dir.
    - Kill pox and supervisord's on exit.
    - Move pox into python path to allow extension to work.

--- topology/mininet/run.sh
+++ topology/mininet/run.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
-POX_PID=/tmp/pox.pid
-POX_LOG=/tmp/pox.log
+TMP_DIR=/tmp/mininet
+mkdir -p "$TMP_DIR"
+
+POX_PID="$TMP_DIR/pox.pid"
+POX_LOG="logs/pox.log"
+POX_OUT="logs/pox.out"
 POX_PORT=6633
 
+log() {
+    echo "=====> $@"
+}
+
 if [ -e "$POX_PID" ]; then
-    echo "ERROR: Pox already running, or $POX_PID is stale"
+    log "ERROR: Pox already running, or $POX_PID is stale"
     exit 1
 fi
 
-pox forwarding.l2_learning misc.pidfile --file=$POX_PID log --no_default --file=$POX_LOG,w --format="%(asctime)s: %(message)s" &
+PYTHONPATH=topology/mininet pox \
+    pox_signal \
+    forwarding.l2_learning \
+    misc.pidfile --file=$POX_PID \
+    log --no_default --file="$POX_LOG" --format="%(asctime)s: %(message)s" \
+    &> "$POX_OUT" &
+
 #wait for pox to start to avoid "can't connect to controller errors"
 sleep 1
 if nc -z localhost $POX_PORT; then
-    echo "POX running on localhost:$POX_PORT"
+    log "POX running on localhost:$POX_PORT"
 else
-    echo "ERROR: Pox not running:"
-    echo "======================="
+    log "ERROR: Pox not running:"
     cat "$POX_LOG"
     exit 1
 fi
+
+log "Starting mininet"
 sudo SUPERVISORD=$(which supervisord) python topology/mininet/topology.py
 
-echo "Stopping POX"
-kill $(< $POX_PID)
+for i in "$TMP_DIR"/*.pid; do
+    log "Killing $(basename $i | cut -d. -f -1)"
+    kill $(< $i)
+done
