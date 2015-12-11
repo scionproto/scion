bb50b37af963adb4a5b6fc4bbe6864e3f5d45f92
taka-sasaki <t-sasaki@fb.jp.nec.com>
2015-12-11 10:36:32 +0100

    Add commands to convert file format from YAML to JSON

--- hsr/exec2.sh
+++ hsr/exec2.sh
@@ -1,1 +1,5 @@
 sudo ./build/hsr -c 0x3 -n 4 -- -p 0xf -T 0 er1-11er1-13 ../gen/ISD1/AD11/er1-11er1-13/topology.conf ../gen/ISD1/AD11/er1-11er1-13/ad.conf
+
+python3 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < ../gen/ISD1/AD11/er1-11er1-13/topology.yml  > topology.json
+python3 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < ../gen/ISD1/AD11/er1-11er1-13/ad.yml  > ad.json
+sudo ./build/hsr -c 0x3 -n 4 -- -p 0xf -T 0 er1-13er1-11 ./topology.json ./ad.json
