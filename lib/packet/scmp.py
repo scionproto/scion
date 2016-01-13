309be826f59a9cf9345e078675736c01bdbd0d1f
Steve Matsumoto <steve@stevematsumoto.net>
2016-01-13 15:39:15 +0100

    Fix module docstring title

--- lib/packet/scmp.py
+++ lib/packet/scmp.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-:mod:`scmp` --- SCION ICMP
-==========================
+:mod:`scmp` --- SCION Control Message Protocol
+==============================================
 """
 # Stdlib
 import struct
