f7d2d23ac61092eea075e387aa9da7ded5c921d4
Jason Lee <jason.lee@inf.ethz.ch>
2016-03-17 17:35:31 +0100

    Bug fix in libscion path reversal

--- lib/libscion/path.c
+++ lib/libscion/path.c
@@ -39,19 +39,18 @@ int reverse_path(uint8_t *buf, uint8_t *original, uint8_t *reverse, int len)
         }
     }
 
-    int of_offset = sizeof(SCIONCommonHeader);
-    uint8_t *current_iof = sch->currentIOF + of_offset + buf;
+    uint8_t *current_iof = sch->currentIOF + buf;
     if (current_iof == iof[0])
-        sch->currentIOF = iof[segments - 1] - buf + of_offset;
+        sch->currentIOF = iof[segments - 1] - buf;
     else if (current_iof == iof[segments - 1])
-        sch->currentIOF = iof[0] - buf + of_offset;
+        sch->currentIOF = iof[0] - buf;
 
     int of_count = segments;
     for (i = 0; i < segments; i++)
         of_count += hops[i];
-    int hof_index = (sch->currentOF + of_offset + buf - original) / SCION_OF_LEN;
+    int hof_index = (sch->currentOF + buf - original) / SCION_OF_LEN;
     hof_index = of_count - hof_index;
-    sch->currentOF = original + hof_index * SCION_OF_LEN - buf - of_offset;
+    sch->currentOF = original + hof_index * SCION_OF_LEN - buf;
 
     return 0;
 }
