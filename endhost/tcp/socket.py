69faff44ecc2b7a8cab68acd4e9ae7bd3f914323
pszalach <psz@inf.ethz.ch>
2016-06-07 11:00:38 +0200

    logging for tcp/socket.py & TCP dummy test

--- endhost/tcp/socket.py
+++ endhost/tcp/socket.py
@@ -16,17 +16,15 @@
 ===================================
 """
 # Stdlib
+import logging
 import os
-import socket as stdsock # To avoid name collision
+import socket as stdsock  # To avoid name collision
 import struct
-import time
 import uuid
 
 # SCION
-from lib.packet.host_addr import haddr_parse
 from lib.packet.path import SCIONPath
-from lib.packet.scion import SVCType
-from lib.packet.scion_addr import ISD_AS, SCIONAddr
+from lib.packet.scion_addr import SCIONAddr
 
 # TODO(PSz): that should be somewhere in the SCION python's lib:
 SVC_BS = 0
@@ -39,7 +37,7 @@ LWIP_SOCK_DIR = "/run/shm/lwip/"
 RPCD_SOCKET = "/run/shm/lwip/lwip"
 AF_SCION = 11
 SOCK_STREAM = stdsock.SOCK_STREAM
-MAX_MSG_LEN = 2<<31  # u32_t is used as size_t at rpcd
+MAX_MSG_LEN = 2 << 31  # u32_t is used as size_t at middleware
 CMD_SIZE = 4
 RESP_SIZE = CMD_SIZE + 2  # either "OK" or "ER" is appended
 
@@ -51,7 +49,7 @@ class error(stdsock.error):
 class SCIONSocket(object):
     BUFLEN = 1024
 
-    def __init__(self, family, type_, proto=0, name=''):
+    def __init__(self, family, type_, proto=0):
         assert family == AF_SCION
         assert type_ == SOCK_STREAM
         assert proto == 0
@@ -61,7 +59,6 @@ class SCIONSocket(object):
         self._lwip_sock = None
         self._lwip_accept = None
         self._recv_buf = b''
-        self._name = name # debug only
 
     def bind(self, addr_port, svc=NO_SVC):
         addr, port = addr_port
@@ -70,6 +67,7 @@ class SCIONSocket(object):
         self._to_lwip(req)
         rep = self._from_lwip()
         if rep != b"BINDOK":
+            logging.error("bind() failed: %s" % rep)
             raise error("bind() failed: %s" % rep)
 
     def connect(self, addr_port, path_info):
@@ -84,6 +82,7 @@ class SCIONSocket(object):
         self._to_lwip(req)
         rep = self._from_lwip()
         if rep != b"CONNOK":
+            logging.error("connect() failed: %s" % rep)
             raise error("connect() failed: %s" % rep)
 
     def create_socket(self):
@@ -97,37 +96,39 @@ class SCIONSocket(object):
         rep = self._from_lwip()
         if rep != b"NEWSOK":
             self._lwip_sock.close()
+            logging.error("socket() failed: %s" % rep)
             raise error("socket() failed: %s" % rep)
 
     def _to_lwip(self, req):
-        print(self._name, "Sending to LWIP(%dB):" % len(req), req[:20])
-        self._lwip_sock.sendall(req)  # TODO(PSz): we may consider send(). For
-        # now assuming that local socket is able to transfer req.
+        logging.debug("Sending to LWIP(%dB): %s..." % (len(req), req[:20]))
+        self._lwip_sock.sendall(req)
 
     def _from_lwip(self, buflen=None):
         if buflen is None:
-            buflen=self.BUFLEN
-        rep = self._lwip_sock.recv(buflen)  # read in a loop
-        print(self._name, "Reading from LWIP(%dB):" % len(rep), rep[:20])
+            buflen = self.BUFLEN
+        rep = self._lwip_sock.recv(buflen)  # TODO(PSz): read in a loop.
+        logging.debug("Reading from LWIP(%dB): %s..." % (len(rep), rep[:20]))
         return rep
 
-    def listen(self): # w/o backlog for now, let's use LWIP's default
+    def listen(self):  # w/o backlog for now, let's use LWIP's default
         req = b"LIST"
         self._to_lwip(req)
         rep = self._from_lwip()
         if rep != b"LISTOK":
+            logging.error("list() failed: %s" % rep)
             raise error("list() failed: %s" % rep)
 
     def accept(self):
         self._init_accept_sock()
-        self._lwip_accept.listen(5)  # should be consistent with LWIP's backlog
+        self._lwip_accept.listen(5)  # FIXME(PSz): consistent with LWIP backlog
         req = b"ACCE" + self._lwip_accept.getsockname()[-36:].encode('ascii')
         self._to_lwip(req)
 
         rep = self._from_lwip()
         if rep[:6] != b"ACCEOK":
+            logging.error("accept() failed (old sock): %s" % rep)
             raise error("accept() failed (old sock): %s" % rep)
-        print(self._name, "path, addr", rep)
+        logging.debug("accept() raw reply: %s", rep)
         rep = rep[6:]
         path_len, = struct.unpack("H", rep[:2])
         rep = rep[2:]
@@ -138,10 +139,9 @@ class SCIONSocket(object):
         new_sock, _ = self._lwip_accept.accept()
         rep = new_sock.recv(self.BUFLEN)
         if rep != b"ACCEOK":
+            logging.error("accept() failed (new sock): %s" % rep)
             raise error("accept() failed (new sock): %s" % rep)
-        print(self._name, "From accept socket: ", rep)
-        sock = SCIONSocket(self._family, self._type, self._proto, name="NEW_ACC%s"
-                           % time.time())
+        sock = SCIONSocket(self._family, self._type, self._proto)
         sock.set_lwip_sock(new_sock)
         return sock, addr, path
 
@@ -154,7 +154,7 @@ class SCIONSocket(object):
         fname = "%s%s" % (LWIP_SOCK_DIR, uuid.uuid4())
         while os.path.exists(fname):  # TODO(PSz): add max_tries
             fname = "%s%s" % (LWIP_SOCK_DIR, uuid.uuid4())
-        print(self._name, "_init_accept:", fname)
+        logging.debug("_init_accept_sock(): %s", fname)
         self._lwip_accept = stdsock.socket(stdsock.AF_UNIX, stdsock.SOCK_STREAM)
         self._lwip_accept.bind(fname)
 
@@ -163,11 +163,13 @@ class SCIONSocket(object):
         # of msg if it is sent, or throws exception otherwise.  Thus it might be
         # safer to use it with smaller msgs.
         if len(msg) > MAX_MSG_LEN:
+            logging.error("send() msg too long: %d" % len(msg))
             raise error("send() msg too long: %d" % len(msg))
         req = b"SEND" + struct.pack("I", len(msg)) + msg
         self._to_lwip(req)
         rep = self._from_lwip()
         if rep != b"SENDOK":
+            logging.error("send() failed: %s" % rep)
             raise error("send() failed: %s" % rep)
         return len(msg)
 
@@ -186,15 +188,19 @@ class SCIONSocket(object):
         self._to_lwip(req)
         rep = self._from_lwip()
         if rep is None or len(rep) < RESP_SIZE or rep[:RESP_SIZE] != b"RECVOK":
+            logging.error("recv() failed: %s" % rep)
             raise error("recv() failed: %s" % rep)
         size, = struct.unpack("H", rep[RESP_SIZE:RESP_SIZE+2])
         self._recv_buf = rep[RESP_SIZE+2:]
         while len(self._recv_buf) < size:
             rep = self._from_lwip()
             if rep is None:
+                logging.error("recv() failed, partial read() %s" % rep)
                 raise error("recv() failed, partial read() %s" % rep)
             self._recv_buf += rep
         if len(self._recv_buf) != size:
+            logging.error("recv() read too much: %d/%d",
+                          len(self._recv_buf), size)
             raise error("recv() read too much: ", len(self._recv_buf), size)
 
     def close(self):
@@ -207,7 +213,7 @@ class SCIONSocket(object):
             os.unlink(fname)
 
 
-def socket(family, type_, proto=0, name=''):
-    sock = SCIONSocket(family, type_, proto, name)
+def socket(family, type_, proto=0):
+    sock = SCIONSocket(family, type_, proto)
     sock.create_socket()
     return sock
