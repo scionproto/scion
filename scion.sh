13e96b5fd68778a1d96c1ef633a68e2923c96d39
pszalach <psz@inf.ethz.ch>
2016-06-14 09:46:19 +0200

    LD_LIBRARY_PATH in supervisord

--- scion.sh
+++ scion.sh
@@ -31,7 +31,6 @@ cmd_run() {
     fi
     echo "Running the network..."
     bash gen/zk_datalog_dirs.sh || exit 1
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/sub/lwip-contrib/ports/unix/proj/scion/
     supervisor/supervisor.sh quickstart dispatcher:dispatcher
     supervisor/supervisor.sh quickstart all
 }
