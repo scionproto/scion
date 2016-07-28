dc00fcb6fa90871f1bd48628c4282bf21f5b355d
Jason Lee <jasonlee0513@gmail.com>
2016-07-28 10:27:32 +0200

    Formatting v2

--- lib/packet/path.py
+++ lib/packet/path.py
@@ -527,10 +527,9 @@ class PathCombinator(object):
                                                down_segment.asm(down_index)):
             um = min(up_mtu, pm)
             dm = min(down_mtu, pm)
-            args = cls._path_args(up_iof,
-                                  up_hofs + [uph, up_upstream_hof],
-                                  down_iof,
-                                  [down_upstream_hof, dph] + down_hofs)
+            args = cls._path_args(
+                up_iof, up_hofs + [uph, up_upstream_hof],
+                down_iof, [down_upstream_hof, dph] + down_hofs)
             path = SCIONPath.from_values(*args)
             cls._build_interface_list(path, up_segment, up_index,
                                       down_segment, down_index, (uph, dph))
