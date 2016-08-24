9b342b3bc90ea35fd92b89311379e31578bdd06b
pszalach <psz@inf.ethz.ch>
2016-08-24 10:21:17 +0200

    fix

--- infrastructure/beacon_server/base.py
+++ infrastructure/beacon_server/base.py
@@ -241,7 +241,7 @@ class BeaconServer(SCIONElement, metaclass=ABCMeta):
     def handle_pcb(self, pkt):
         """Receives beacon and stores it for processing."""
         pcb = pkt.get_payload()
-        pcb.p.ifID = pkt.path.hof.ingress_if
+        pcb.p.ifID = pkt.path.get_hof().ingress_if
         if not self.path_policy.check_filters(pcb):
             return
         self.incoming_pcbs.append(pcb)
