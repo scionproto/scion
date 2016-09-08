cf0bb511ebb83d5b4959cec4673aa3c09baf5771
pszalach <psz@inf.ethz.ch>
2016-09-08 09:40:58 +0200

    build_one_hop_path_ext()

--- lib/libscion/extensions.h
+++ lib/libscion/extensions.h
@@ -1,5 +1,6 @@
 #ifndef _EXTENSIONS_H_
 #define _EXTENSIONS_H_
+#include "packet.h"
 
 // Extensions
 #define SCION_EXT_LINE LINE_LEN
@@ -8,15 +9,22 @@
 #define HOP_BY_HOP 0
 #define END_TO_END 222
 
+// Hop by hop
 #define TRACEROUTE 0
 #define SIBRA 1
+#define SCMP 2
+#define ONE_HOP_PATH 3
 
+// End to end
 #define PATH_TRANSPORT 0
 #define PATH_PROBE 1
 
 #define TRACEROUTE_HOP_LEN 8
+// Payload length of one hop path extension.
+#define ONE_HOP_PATH_PLDLEN 5
 
 uint8_t * find_extension(uint8_t *buf, uint8_t ext_class, uint8_t ext_type);
 int get_total_ext_len(uint8_t *buf);
+void build_one_hop_path_ext(seh_t *ext);
 
 #endif
