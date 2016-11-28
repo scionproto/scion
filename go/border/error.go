667b62d5b6f31e2d8b309be87a982dc8edc418f1
Stephen Shirley <kormat@gmail.com>
2016-11-28 10:37:01 +0100

    WIP

--- go/border/error.go
+++ go/border/error.go
@@ -66,24 +66,22 @@ func (r *Router) createSCMPErrorReply(rp *rpkt.RtrPkt, ct scmp.ClassType,
 	if err != nil {
 		return nil, err
 	}
-	// Filter out any existing SCMP HBH headers, and trim the list to
-	// common.ExtnMaxHBH.
-	// https://github.com/golang/go/wiki/SliceTricks#filtering-without-allocating
-	hbh := sp.HBHExt[:0]
-	hbh = append(hbh, nil) // Leave space for new SCMP HBH header.
-	for _, e := range sp.HBHExt {
-		if len(hbh) < common.ExtnMaxHBH && e.Type() != common.ExtnSCMPType {
-			hbh = append(hbh, e)
-		}
-	}
-	sp.HBHExt = hbh
+	oldHBH := sp.HBHExt
+	sp.HBHExt = make([]common.Extension, 0, common.ExtnMaxHBH+1)
 	// Add new SCMP HBH extension at the start.
 	ext := &scmp.Extn{Error: true}
 	if ct.Class == scmp.C_Path && ct.Type == scmp.T_P_RevokedIF {
 		// Revocation SCMP errors have to be inspected by intermediate routers.
 		ext.HopByHop = true
 	}
-	sp.HBHExt[0] = ext
+	sp.HBHExt = append(sp.HBHExt, ext)
+	// Filter out any existing SCMP HBH headers, and trim the list to
+	// common.ExtnMaxHBH.
+	for _, e := range oldHBH {
+		if len(sp.HBHExt) < cap(sp.HBHExt) && e.Type() != common.ExtnSCMPType {
+			sp.HBHExt = append(sp.HBHExt, e)
+		}
+	}
 	// Add SCMP l4 header and payload
 	sp.Pld = scmp.PldFromQuotes(ct, info, sp.L4.L4Type(), rp.GetRaw)
 	sp.L4 = scmp.NewHdr(ct, sp.Pld.Len())
