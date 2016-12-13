11fc82c18016a4eb196eb6cffe9a434e35c08b37
Sam Hitz <samuel.hitz@inf.ethz.ch>
2016-12-13 18:31:05 +0100

    revocation e2e working locally

--- test/integration/revocation_test.sh
+++ test/integration/revocation_test.sh
@@ -15,10 +15,17 @@ if [ $result -ne 0 ]; then
     exit ${result}
 fi
 # Bring down routers.
-log "Stopping routers."
+SLEEP=5
+log "Stopping routers and waiting for ${SLEEP}s."
 supervisorctl -s http://localhost:9011 stop as1-11:br1-11-3
-sleep 5
+supervisorctl -s http://localhost:9011 stop as1-13:br1-13-2
+supervisorctl -s http://localhost:9011 stop as2-26:br2-26-2
+sleep ${SLEEP}s
 # Do another round of e2e test with retries
 log "Testing connectivity between all the hosts (with retries)."
 test/integration/end2end_test.py -l ERROR --retries 3
-exit $?
+result=$?
+if [ $result -ne 0 ]; then
+    log "E2E test with failed routers failed."
+fi
+exit ${result}
