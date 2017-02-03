e7f653117a204ed9776e18a8e199cb0c12dfd467
Jean-Pierre Smith <rougesprit@gmail.com>
2017-02-03 14:05:40 +0100

    Add more parsing code for the path lookup.

--- endhost/protocols/src/sciondlib.c
+++ endhost/protocols/src/sciondlib.c
@@ -72,28 +72,32 @@ int write_path_request(uint8_t *buffer, uint32_t isd_as)
   return offset;
 }
 
-// typedef struct {
-//     uint8_t len;
-//     uint8_t *raw_path;
-//     HostAddr first_hop;
-// } spath_t;
-
 
 /* Parse network byte-order host address and populate the supplied addr with
  * a copy of the data.
  *
- * Returns the number of bytes which contains the host address in the buffer.
+ * On success, returns the number of bytes used for the host address. If the
+ * remaining input in the buffer, as indicated by data_len, is less than the
+ * needed data, zero is returned.
  */
-int parse_host_addr(uint8_t* buffer, HostAddr* host_addr)
+int parse_host_addr(uint8_t* buffer, int data_len, HostAddr* host_addr)
 {
-  int offset = 0;
+  // Fail if we cannot read the address type
+  if (data_len == 0) { return 0; }
 
-  // Get the address type
-  host_addr->addr_type = buffer[offset++];
+  // Read the address type and determine if there is enough data
+  int offset = 0;
+  const uint8_t addr_type = buffer[offset++];  // FIXME(jsmith): Valid type?
+  const int addr_len = get_addr_len(addr_type);
+  const int fixed_addr_len = 3;  // Address type + port number
+  if (data_len < fixed_addr_len + addr_len) { return 0; }
+
+  // Set the address type
+  host_addr->addr_type = addr_type;
 
   // Copy the address data
-  memcpy(host_addr->addr, &buffer[offset], get_addr_len(host_addr->addr_type));
-  offset += get_addr_len(host_addr->addr_type);
+  memcpy(host_addr->addr, &buffer[offset], addr_len);
+  offset += addr_len;
 
   // Copy the 2-byte port and change its edianness
   host_addr->port = ntohs((buffer[offset] << 8) | buffer[offset+1]);
@@ -108,35 +112,114 @@ int parse_host_addr(uint8_t* buffer, HostAddr* host_addr)
 // TODO(jsmith): Assume that the fucntion to deallocate paths that the raw path
 // is null if not assigned otherwise free it.
 // Return ptr to current position or offset from buffer start
-int parse_path(uint8_t* buffer, spath_t* path_ptr)
+int parse_path(uint8_t* buffer, int data_len, spath_t* path_ptr)
 {
+  // Fail if we cannot read the path length
+  if (data_len == 0) { return 0; }
+
+  // Check for sufficient path data
   int offset = 0;
+  const int path_byte_len = buffer[offset++] * LINE_LEN;
+  if (path_byte_len > data_len - offset) { return 0; }
 
-  path_ptr->len = buffer[offset++];
-  // Allocate the buffer space
-  int path_length = path_ptr->len * LINE_LEN;
-  path_ptr->raw_path = malloc(path_length);
+  // First attempt to parse border router address.
+  // Avoid dynamic memory usage on failure.
+  int n_host_bytes = parse_host_addr(&buffer[offset + path_byte_len],
+                                     (data_len - (offset + path_byte_len)),
+                                     &(path_ptr->first_hop));
+  if (n_host_bytes == 0) { return 0; }
+
+  // Now allocate the buffer space and set the path length
+  path_ptr->len = path_byte_len / LINE_LEN;
+  path_ptr->raw_path = malloc(path_byte_len);
   // Copy over the path data
-  memcpy(path_ptr->raw_path, &buffer[offset], path_length);
-  offset += path_length;
-
-  // Copy the border router address
-  offset += parse_host_addr(&buffer[offset], &(path_ptr->first_hop));
+  memcpy(path_ptr->raw_path, &buffer[offset], path_byte_len);
+  offset += path_byte_len + n_host_bytes;
 
   return offset;
 }
 
-// TODO(jsmith): Always read the full buffer. Will make life easier instead of
-// trying to figure out where it stops
-int parse_path_record(uint8_t* buffer, spath_record_t* record)
+/* De-initializes the specified path, freeing any internally allocated memory.
+ * The memory pointed to by the record itself is not freed however.
+ */
+void destroy_spath(spath_t* path)
+{
+  free(path->raw_path);
+}
+
+int parse_interface(uint8_t* buffer, sinterface_t* interface)
+{
+  int offset = 0;
+  // Read in the 4 byte isd_as address and change to host byte ordering
+  memcpy(&interface->isd_as, &buffer[offset], sizeof(interface->isd_as));
+  interface->isd_as = ntohl(interface->isd_as);
+  offset += sizeof(interface->isd_as);
+
+  // Read the 2-byte link identifier
+  interface->link = ntohs((buffer[offset] << 8) | buffer[offset+1]);
+  offset += 2;
+
+  return offset;
+}
+
+int parse_interfaces(uint8_t* buffer, int data_len,
+                     sinterface_t** interface_array, uint8_t* interface_count)
+{
+  // Fail if we cannot read the path length
+  if (data_len == 0) { return 0; }
+
+  // Check for sufficient path data
+  int offset = 0;
+  const int interface_length = 5;
+  const int interface_byte_len = buffer[offset++] * interface_length;
+  if (interface_byte_len > data_len - offset) { return 0; }
+
+  // Parse the interfaces
+  *interface_count = buffer[offset-1];
+  sinterface_t* interface_ptr =
+    malloc(sizeof(sinterface_t) * (*interface_count));
+
+  int i = 0;
+  for (i = 0; i < *interface_count; ++i) {
+    offset += parse_interface(&buffer[offset],
+                              &interface_ptr[i * sizeof(sinterface_t)]);
+  }
+
+  *interface_array = interface_ptr;
+  return offset;
+}
+
+// TODO(jsmith): destroy the path on failure
+int parse_path_record(uint8_t* buffer, int data_len, spath_record_t* record)
 {
   int offset = 0;
 
-  // Parse the path and MTU
-  offset += parse_path(buffer, &(record->path));
-  // TODO: MTU
+  // Parse the path
+  int bytes_used = parse_path(&buffer[offset], data_len, &(record->path));
+  if (bytes_used == 0) { return 0; }
+  offset += bytes_used;
 
+  // Parse the MTU
+  if ((data_len - offset) < 2) {
+    destroy_spath(&record->path);
+    return 0;
+  }
+  record->mtu = ntohs((buffer[offset] << 8) | buffer[offset+1]);
+  offset += 2;
+
+  // Parse the path's interfaces
+  bytes_used = parse_interfaces(&buffer[offset], (data_len - offset),
+                                &record->interfaces,
+                                &record->interface_count);
+  if (bytes_used == 0) {
+    destroy_spath(&record->path);
+    return 0;
+  }
+
+  return offset + bytes_used;
 }
+
+
 
 
 //     bool add = true;
@@ -171,16 +254,3 @@ int parse_path_record(uint8_t* buffer, spath_record_t* record)
 //             delete p;
 //     }
 //     return interfaceOffset + 1 + interfaceCount * IF_TOTAL_LEN;
-
-
-int read_path_response(int sockfd)
-{
-  uint8_t buffer[DP_HEADER_LEN];
-  // Read in the communication header
-  int result = recv_all(sockfd, buffer, DP_HEADER_LEN);
-  if (result == -1) { return -errno; }
-
-  int packet_len;
-  parse_dp_header(buffer, /*addr_len=*/NULL, &packet_len);
-
-}
