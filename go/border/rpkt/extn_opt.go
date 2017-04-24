4bc6586a18415c154cf73a6dd358dadeacf9a323
FR4NK-W <wirzf@student.ethz.ch>
2017-04-24 14:13:18 +0200

    OPT router extension
    WIP
    
    OPT router extension draft

---
+++ go/border/rpkt/extn_opt.go
@@ -0,0 +1,109 @@
+// Copyright 2017 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Router's implementation of the OPT hop-by-hop extension
+
+package rpkt
+
+import (
+	log "github.com/inconshreveable/log15"
+
+	"github.com/netsec-ethz/scion/go/lib/common"
+	"github.com/netsec-ethz/scion/go/lib/opt"
+	"fmt"
+	"crypto/cipher"
+	"github.com/netsec-ethz/scion/go/lib/util"
+	"github.com/netsec-ethz/scion/go/border/conf"
+)
+
+// satisfies the interface rExtension in extns.go
+var _ rExtension = (*rOPTExt)(nil)
+
+// rOPTExt is the router's representation of the OPT extension.
+type rOPTExt struct {
+	*opt.Extn
+	rp  *RtrPkt
+	raw common.RawBytes
+	log.Logger
+}
+
+func rOPTExtFromRaw(rp *RtrPkt, start, end int) (*rOPTExt, *common.Error) {
+	var err *common.Error
+	o := &rOPTExt{rp: rp, raw: rp.Raw[start:end]}
+	o.Extn, err = opt.ExtnFromRaw(o.raw)
+	if err != nil {
+		return nil, err
+	}
+	o.Logger = rp.Logger.New("ext", "opt")
+	return o, nil
+}
+
+func (o *rOPTExt) RegisterHooks(h *hooks) *common.Error {
+	// process field and update pvf
+	h.Process = append(h.Process, o.rp.processOPT)
+	return nil
+}
+
+func (o *rOPTExt) GetExtn() (common.Extension, *common.Error) {
+	return o.Extn, nil
+}
+
+// processOPT is a processing hook used to handle OPT payloads.
+func (o *RtrPkt) processOPT() (HookResult, *common.Error) {
+	exts := o.HBHExt
+	for _, ext := range exts {
+		if ext.Type() == 4 {
+			fmt.Sprintf("%T", ext)
+			repr, err := o.extnParseHBH(common.ExtnOPTType, 0, ext.Len(), 4)
+			// process OPT field here, update pvf
+			if err != nil {
+				return err
+			} else {
+				rOPTExt(repr).UpdatePVF()
+			}
+			break
+		}
+	}
+	return HookFinish, nil
+}
+
+// getDRKeyBlock returns a cipher block for the given DRKey
+func (o *RtrPkt) getDRKeyBlock(drkey common.RawBytes, e *common.Error) (cipher.Block, *common.Error) {
+	if e != nil {
+		return nil, e
+	}
+	return util.InitAES(drkey)
+}
+
+// calcDRKey calculates the DRKey for this packet.
+func (o *RtrPkt) CalcDRKey() (common.RawBytes, *common.Error) {
+	// stuff in with src ISD|src AS, compute CBCMac over it with key DRKeyAESBlock: K_x = cbcmac(DRKeyAESBlock, in)
+	in := make(common.RawBytes, 16)
+	common.Order.PutUint32(in, uint32(o.srcIA.I))
+	common.Order.PutUint32(in[4:], uint32(o.srcIA.A))
+	// blockFstOrder is K_{SV_{AS_i}}
+	blockFstOrder, e := o.getDRKeyBlock(util.CBCMac(conf.C.DRKeyAESBlock, in))
+	if e != nil {
+		return nil, e
+	}
+
+	// stuff in with OPT|src host addr|dst host addr, compute CBCMac over it with key K_x = cbcmac(K_x, in)
+	in = make(common.RawBytes, 48)
+	copy(in[0:3], []byte("OPT"))
+	copy(in[16:32], o.srcHost.Pack())
+	copy(in[32:48], o.dstHost.Pack())
+	// keyOpt is K^OPT_{AS_i -> S:H_S, D:H_D}
+	keyOpt, e := util.CBCMac(blockFstOrder, in)
+	return keyOpt, e
+}
---
+++ go/lib/opt/util.go
@@ -0,0 +1,43 @@
+// Copyright 2017 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package opt
+
+import (
+	"github.com/netsec-ethz/scion/go/border/rpkt"
+	"github.com/netsec-ethz/scion/go/lib/util"
+	"crypto/sha256"
+)
+
+func (e *Extn) DataHashed(packet rpkt.RtrPkt) []byte {
+	shaChecksum := sha256.New
+	return shaChecksum(packet)
+}
+
+func (e *Extn) InitializePVF(sessionKey []byte, payload []byte) {
+	dataHash := e.DataHashed(payload)
+        e.PVF = cbcMAC(sessionKey, dataHash)
+}
+
+func (e *Extn) UpdatePVF(packet *rpkt.RtrPkt) {
+	localSecret, _ := packet.CalcDRKey()
+	// K_{AS_i}^session = PRF_{AS_i -> S, D}(SessionId)
+	sessionKey := cbcMAC(localSecret, e.SessionId)
+	e.PVF = cbcMAC(sessionKey, e.PVF)
+}
+
+func cbcMAC(session []byte, PVF []byte) []byte {
+	mac, _ := util.CBCMac(session, PVF)
+	return mac
+}
