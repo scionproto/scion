0f779983ab0c5761a510841c851271c18220d46b
Tobias Klausmann <klausman@i-no.de>
2017-05-19 14:21:57 +0200

    Add copyright header

--- go/tools/logdog/main.go
+++ go/tools/logdog/main.go
@@ -1,3 +1,17 @@
+// Copyright 2016 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Read and interleave Python and Go long files as produced by log15/fmt15, zlog and Python logging
 //
 // 2017-05-16T13:18:16.539536145+0000 [DBUG] Topology loaded topo=
