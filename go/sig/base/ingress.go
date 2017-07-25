6155550710c828725427cff833a5bd4892a615a8
Sam Hitz <samuel.hitz@inf.ethz.ch>
2017-07-25 18:05:05 +0200

    Async read ingress

--- go/sig/base/ingress.go
+++ go/sig/base/ingress.go
@@ -8,6 +8,8 @@ import (
 
 	log "github.com/inconshreveable/log15"
 
+	"sync"
+
 	"github.com/netsec-ethz/scion/go/lib/addr"
 	"github.com/netsec-ethz/scion/go/lib/common"
 	"github.com/netsec-ethz/scion/go/sig/lib/scion"
@@ -24,15 +26,39 @@ const (
 	MaxSeqNrDist = 10
 	// ReassemblyListCap is the maximum capacity of a reassembly list.
 	ReassemblyListCap = 100
+	// IngressChanSize is the length of the buffered input channel.
+	IngressChanSize = 128
 	// CleanUpInterval is the interval between clean up of outdated reassembly lists.
 	CleanUpInterval = 1 * time.Second
 )
 
 // IngressWorker handles decapsulation of SIG frames. There is one IngressWorker per
 // remote SIG.
-func IngressWorker(scionNet *scion.SCIONNet, listenAddr addr.HostAddr, listenPort uint16) {
+type IngressWorker struct {
+	scionNet        *scion.SCIONNet
+	listenAddr      addr.HostAddr
+	listenPort      uint16
+	reassemblyLists map[int]*ReassemblyList
+	bufPool         *sync.Pool
+	c               chan *FrameBuf
+}
+
+func NewIngressWorker(scionNet *scion.SCIONNet,
+	listenAddr addr.HostAddr, listenPort uint16) *IngressWorker {
+	worker := &IngressWorker{
+		scionNet:        scionNet,
+		listenAddr:      listenAddr,
+		listenPort:      listenPort,
+		bufPool:         &sync.Pool{New: func() interface{} { return NewFrameBuf() }},
+		reassemblyLists: make(map[int]*ReassemblyList),
+		c:               make(chan *FrameBuf, IngressChanSize),
+	}
+	return worker
+}
+
+func (i *IngressWorker) Run() {
 	var err error
-	ExternalIngress, err = scionNet.ListenSCION(listenAddr, listenPort)
+	ExternalIngress, err = i.scionNet.ListenSCION(i.listenAddr, i.listenPort)
 	if err != nil {
 		log.Error("Unable to initialize ExternalIngress", "err", err)
 		return
@@ -42,26 +68,81 @@ func IngressWorker(scionNet *scion.SCIONNet, listenAddr addr.HostAddr, listenPor
 		log.Error("Unable to connect to InternalIngress", "err", err)
 		return
 	}
-	state := NewIngressState()
+	go i.Read()
 	cleanupTimer := time.Tick(CleanUpInterval)
 	for {
 		select {
 		case <-cleanupTimer:
-			state.cleanUp()
+			i.CleanUp()
 		default:
-			frame := state.getFrameBuf()
-			read, err := ExternalIngress.Read(frame.raw)
-			if err != nil {
-				log.Error("IngressWorker: Unable to read from External Ingress", "err", err)
-				// Release Frame
-				frame.Reset()
-				state.bufPool <- frame
-				continue
-			}
-			metrics.FramesRecv.WithLabelValues(scionNet.IA.String()).Inc()
-			metrics.FrameBytesRecv.WithLabelValues(scionNet.IA.String()).Add(float64(read))
-			frame.frameLen = read
-			processFrame(frame, state)
+			frame := <-i.c
+			i.ProcessFrame(frame)
+		}
+	}
+}
+
+func (i *IngressWorker) Read() {
+	for {
+		frame := i.bufPool.Get().(*FrameBuf)
+		read, err := ExternalIngress.Read(frame.raw)
+		if err != nil {
+			log.Error("IngressWorker: Unable to read from External Ingress", "err", err)
+			// Release Frame
+			frame.Reset()
+			i.bufPool.Put(frame)
+			continue
+		}
+		frame.frameLen = read
+		i.c <- frame
+		metrics.FramesRecv.WithLabelValues(i.scionNet.IA.String()).Inc()
+		metrics.FrameBytesRecv.WithLabelValues(i.scionNet.IA.String()).Add(float64(read))
+	}
+}
+
+// processFrame processes a SIG frame by first writing all completely contained
+// packets to the wire and then adding the frame to the corresponding reassembly
+// list if needed.
+func (i *IngressWorker) ProcessFrame(frame *FrameBuf) {
+	seqNr := int(common.Order.Uint32(frame.raw[:4]))
+	index := int(common.Order.Uint16(frame.raw[4:6]))
+	epoch := int(common.Order.Uint16(frame.raw[6:8]))
+	frame.seqNr = seqNr
+	frame.index = index
+	log.Debug("Received Frame", "seqNr", seqNr, "index", index, "epoch", epoch,
+		"len", frame.frameLen)
+	// If index == 1 then we can be sure that there is no fragment at the beginning
+	// of the frame.
+	frame.fragNProcessed = index == 1
+	// If index == 0 then we can be sure that there are no complete packets in this
+	// frame.
+	frame.completePktsProcessed = index == 0
+	// Add to frame buf reassembly list.
+	rlist := i.GetReassemblyList(epoch)
+	rlist.Insert(frame)
+}
+
+func (i *IngressWorker) GetReassemblyList(epoch int) *ReassemblyList {
+	rlist, ok := i.reassemblyLists[epoch]
+	if !ok {
+		rlist = NewReassemblyList(epoch, ReassemblyListCap, i.bufPool)
+		i.reassemblyLists[epoch] = rlist
+	}
+	rlist.markedForDeletion = false
+	return rlist
+}
+
+func (i *IngressWorker) CleanUp() {
+	for epoch, rlist := range i.reassemblyLists {
+		if rlist.markedForDeletion {
+			// Reassembly list has been marked for deletion in a previous cleanup run.
+			// Remove the reassembly list from the map and then release all frames
+			// back to the bufpool.
+			delete(i.reassemblyLists, epoch)
+			go rlist.removeAll()
+		} else {
+			// Mark the reassembly list for deletion. If it is not accessed between now
+			// and the next cleanup interval, it will be removed.
+			rlist.markedForDeletion = true
 		}
 	}
 }
@@ -93,6 +174,12 @@ type FrameBuf struct {
 	pktLen int
 	// The raw bytes buffer for the frame.
 	raw common.RawBytes
+}
+
+func NewFrameBuf() *FrameBuf {
+	buf := &FrameBuf{raw: make(common.RawBytes, FrameBufCap)}
+	buf.Reset()
+	return buf
 }
 
 // Reset resets the metadata of a frame buffer.
@@ -163,80 +250,6 @@ func (fb *FrameBuf) String() string {
 		fb.completePktsProcessed)
 }
 
-// IngressState contains the state needed by an ingress worker.
-type IngressState struct {
-	bufPool         chan *FrameBuf
-	reassemblyLists map[int]*ReassemblyList
-}
-
-func NewIngressState() *IngressState {
-	state := &IngressState{
-		bufPool:         make(chan *FrameBuf, FrameBufPoolCap),
-		reassemblyLists: make(map[int]*ReassemblyList),
-	}
-	for i := 0; i < FrameBufPoolCap; i++ {
-		buf := &FrameBuf{
-			raw: make(common.RawBytes, FrameBufCap),
-		}
-		buf.Reset()
-		state.bufPool <- buf
-	}
-	return state
-}
-
-func (s *IngressState) getFrameBuf() *FrameBuf {
-	log.Debug("GetFrameBuf", "poolSize", len(s.bufPool))
-	return <-s.bufPool
-}
-
-func (s *IngressState) getReassemblyList(epoch int) *ReassemblyList {
-	rlist, ok := s.reassemblyLists[epoch]
-	if !ok {
-		rlist = NewReassemblyList(epoch, ReassemblyListCap, s.bufPool)
-		s.reassemblyLists[epoch] = rlist
-	}
-	rlist.markedForDeletion = false
-	return rlist
-}
-
-func (s *IngressState) cleanUp() {
-	for epoch, rlist := range s.reassemblyLists {
-		if rlist.markedForDeletion {
-			// Reassembly list has been marked for deletion in a previous cleanup run.
-			// Remove the reassembly list from the map and then release all frames
-			// back to the bufpool.
-			delete(s.reassemblyLists, epoch)
-			go rlist.removeAll()
-		} else {
-			// Mark the reassembly list for deletion. If it is not accessed between now
-			// and the next cleanup interval, it will be removed.
-			rlist.markedForDeletion = true
-		}
-	}
-}
-
-// processFrame processes a SIG frame by first writing all completely contained
-// packets to the wire and then adding the frame to the corresponding reassembly
-// list if needed.
-func processFrame(frame *FrameBuf, state *IngressState) {
-	seqNr := int(common.Order.Uint32(frame.raw[:4]))
-	index := int(common.Order.Uint16(frame.raw[4:6]))
-	epoch := int(common.Order.Uint16(frame.raw[6:8]))
-	frame.seqNr = seqNr
-	frame.index = index
-	log.Debug("Received Frame", "seqNr", seqNr, "index", index, "epoch", epoch,
-		"len", frame.frameLen)
-	// If index == 1 then we can be sure that there is no fragment at the beginning
-	// of the frame.
-	frame.fragNProcessed = index == 1
-	// If index == 0 then we can be sure that there are no complete packets in this
-	// frame.
-	frame.completePktsProcessed = index == 0
-	// Add to frame buf reassembly list.
-	rlist := state.getReassemblyList(epoch)
-	rlist.Insert(frame)
-}
-
 // ReassemblyList is used to keep a doubly linked list of SIG frames that are
 // outstanding for reassembly. The frames kept in the reassambly list sorted by
 // their sequence numbers. There is always one reassembly list per epoch to
@@ -244,7 +257,7 @@ func processFrame(frame *FrameBuf, state *IngressState) {
 type ReassemblyList struct {
 	epoch             int
 	capacity          int
-	bufPool           chan *FrameBuf
+	bufPool           *sync.Pool
 	markedForDeletion bool
 	entries           *list.List
 	buf               *bytes.Buffer
@@ -252,7 +265,7 @@ type ReassemblyList struct {
 
 // NewReassemblyList returns a ReassemblyList object for the given epoch, with given
 // maximum capacity and using bufPool to release processed frame buffers into.
-func NewReassemblyList(epoch int, capacity int, bufPool chan *FrameBuf) *ReassemblyList {
+func NewReassemblyList(epoch int, capacity int, bufPool *sync.Pool) *ReassemblyList {
 	list := &ReassemblyList{
 		epoch:             epoch,
 		capacity:          capacity,
@@ -454,7 +467,7 @@ func (l *ReassemblyList) removeBefore(ele *list.Element) {
 
 func (l *ReassemblyList) releaseFrame(frame *FrameBuf) {
 	frame.Reset()
-	l.bufPool <- frame
+	l.bufPool.Put(frame)
 }
 
 func send(packet common.RawBytes) error {
