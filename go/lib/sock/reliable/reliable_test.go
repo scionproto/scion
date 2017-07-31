caba33de879b01603f969b0b264d1bd086e42bb8
Sergiu Costea <sergiu.costea@gmail.com>
2017-07-31 18:30:18 +0200

    Multiple message Reads/Writes for ReliableSock

--- go/lib/sock/reliable/reliable_test.go
+++ go/lib/sock/reliable/reliable_test.go
@@ -20,6 +20,7 @@ import (
 	"math/rand"
 	"net"
 	"os"
+	"strconv"
 	"testing"
 	"time"
 
@@ -87,11 +88,8 @@ func Client(x chan ExitData, tc TestCase, sockName string) {
 		return
 	}
 
-	err = conn.Close()
-	if err != nil {
-		x <- ExitData{err: err}
-		return
-	}
+	// Do not close the connection or the server might not have time
+	// to get the data out
 	x <- ExitData{}
 }
 
@@ -204,4 +202,191 @@ func TestRegister(t *testing.T) {
 			}
 		})
 	})
+}
+
+func getRandFile() string {
+	r := uint32(time.Now().UnixNano() + int64(os.Getpid()))
+	suffix := strconv.Itoa(int(1e9 + r%1e9))[1:]
+	return "/tmp/unix." + suffix
+}
+
+type SetupFunc func() interface{}
+type EndpointFunc func(*Conn, interface{})
+type TestFunc func(t *testing.T, expected interface{}, have interface{}) bool
+
+func setupFunc() interface{} {
+	return make([]byte, 1280)
+}
+
+func readFunc(conn *Conn, data interface{}) {
+	buffer := data.([]byte)
+	for j := 0; j < 1000; j++ {
+		_, err := conn.Read(buffer)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			fmt.Printf("Error reading: %v\n", err)
+		}
+	}
+}
+
+func writeFunc(conn *Conn, data interface{}) {
+	buffer := data.([]byte)
+	for j := 0; j < 1000; j++ {
+		_, err := conn.Write(buffer)
+		if err != nil {
+			fmt.Printf("Error writing: %v\n", err)
+		}
+	}
+}
+
+func setupNFunc() interface{} {
+	msgs := make([]Msg, 1000)
+	for i := 0; i < 1000; i++ {
+		msgs[i].buf = make([]byte, 1280)
+	}
+	return msgs
+}
+
+func readNFunc(conn *Conn, data interface{}) {
+	msgs := data.([]Msg)
+	for readMsgs := 0; readMsgs < len(msgs); {
+		n, err := conn.ReadN(msgs[readMsgs:])
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			fmt.Printf("Error reading: %v\n", err)
+		}
+		readMsgs += n
+	}
+}
+
+func writeNFunc(conn *Conn, data interface{}) {
+	msgs := data.([]Msg)
+	for writtenMsgs := 0; writtenMsgs < len(msgs); {
+		n, err := conn.WriteN(msgs[writtenMsgs:])
+		if err != nil {
+			fmt.Printf("Error writing: %v\n", err)
+		}
+		writtenMsgs += n
+	}
+}
+
+// Run 1000 writes individually
+func BenchmarkReadWrite(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		benchmark(b, setupFunc, readFunc, writeFunc)
+	}
+}
+
+func BenchmarkWriteRead(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		benchmark(b, setupFunc, writeFunc, readFunc)
+	}
+}
+
+// Run 1000 writes as a single batch operation
+func BenchmarkReadNWriteN(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		benchmark(b, setupNFunc, readNFunc, writeNFunc)
+	}
+}
+
+func BenchmarkWriteNReadN(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		benchmark(b, setupNFunc, writeNFunc, readNFunc)
+	}
+}
+
+func benchmark(b *testing.B, setup SetupFunc, client EndpointFunc, server EndpointFunc) {
+	uAddr := make(chan string, 1)
+	// Launch server
+	go func() {
+		file := getRandFile()
+		lconn, err := Listen(file)
+		if err != nil {
+			b.Fatalf("Unable to listen err=%v", err)
+		}
+		uAddr <- file
+		conn, err := lconn.Accept()
+		if err != nil {
+			b.Fatalf("Unable to accept err=%v", err)
+		}
+		data := setup()
+		server(conn, data)
+		conn.Close()
+		lconn.Close()
+	}()
+
+	// Dial after we have the socket address
+	conn, err := DialTimeout(<-uAddr, time.Second)
+	if err != nil {
+		b.Fatalf("Unable to connect err=%v", err)
+	}
+	data := setup()
+	client(conn, data)
+	conn.Close()
+}
+
+func setupTestNFunc() interface{} {
+	rand.Seed(time.Now().UnixNano())
+	msgs := make([]Msg, 1000)
+	for i := 0; i < len(msgs); i++ {
+		msgs[i].buf = make([]byte, rand.Intn(1280))
+		for j := 0; j < len(msgs[i].buf); j++ {
+			msgs[i].buf[j] = byte(rand.Intn(256))
+		}
+	}
+	return msgs
+}
+
+func testNFunc(t *testing.T, expected interface{}, have interface{}) {
+	msgsX := expected.([]Msg)
+	msgsY := have.([]Msg)
+
+	Convey("Sent messages should match received messages", t, func() {
+		SoMsg("Messages slice length", len(msgsY), ShouldEqual, len(msgsX))
+		for i := 0; i < len(msgsX); i++ {
+			SoMsg(fmt.Sprintf("MSG%d", i)+" buffers",
+				msgsY[i].buf[:msgsY[i].copied], ShouldResemble, msgsX[i].buf)
+		}
+	})
+}
+
+func TestReadNWriteN(t *testing.T) {
+	uAddr := make(chan string, 1)
+	readFinished := make(chan bool, 1)
+	serverData := setupNFunc()
+	clientData := setupTestNFunc()
+	// Launch server
+	go func() {
+		file := getRandFile()
+		lconn, err := Listen(file)
+		if err != nil {
+			t.Fatalf("Unable to listen err=%v", err)
+		}
+		uAddr <- file
+		conn, err := lconn.Accept()
+		if err != nil {
+			t.Fatalf("Unable to accept err=%v", err)
+		}
+		readNFunc(conn, serverData)
+		conn.Close()
+		lconn.Close()
+		readFinished <- true
+	}()
+
+	// Dial after we have the socket address
+	conn, err := DialTimeout(<-uAddr, time.Second)
+	if err != nil {
+		t.Fatalf("Unable to connect err=%v", err)
+	}
+	writeNFunc(conn, clientData)
+	conn.Close()
+
+	// Wait for server to finish reading before comparing results
+	<-readFinished
+	testNFunc(t, clientData, serverData)
 }
