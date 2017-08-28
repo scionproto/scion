1e5f38401452681e1398580ba5935e597d3795f9
Sergiu Costea <sergiu.costea@gmail.com>
2017-08-28 16:49:23 +0200

    Fix circleCI

--- go/lib/spath/path.go
+++ go/lib/spath/path.go
@@ -14,10 +14,11 @@
 
 package spath
 
-import
-//log "github.com/inconshreveable/log15"
+import (
+	//log "github.com/inconshreveable/log15"
 
-"github.com/netsec-ethz/scion/go/lib/common"
+	"github.com/netsec-ethz/scion/go/lib/common"
+)
 
 const (
 	MaxTTL      = 24 * 60 * 60 // One day in seconds
