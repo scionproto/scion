06ddbdc190efeb34d5d5ec39f0fb008aefc3b7ae
Oncila <domi.roos@gmail.com>
2017-10-05 18:00:51 +0200

    Certificates in Go
    Add go implementation for:
    - Certificate
    - CertificateChain
    - TrustStore
    - verify and sign based on ed25519
    
    TRC implementation will follow

---
+++ go/lib/crypto/cert.go
@@ -0,0 +1,132 @@
+// Copyright 2017 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Go implementation of the certificate.
+// To create JSON strings, either json.Marshal or json.MarshalIndent
+
+package crypto
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/netsec-ethz/scion/go/lib/addr"
+	"github.com/netsec-ethz/scion/go/lib/common"
+)
+
+type Certificate struct {
+	// The following fields need to be sorted
+
+	// CanIssue describes wheter the subject is able to issue certificates.
+	CanIssue bool
+	// Comment is an arbitrary and optional string used by the subject to describe the certificate.
+	Comment string
+	// EncAlgorithm is the algorithm used to encrypt messages.
+	EncAlgorithm string
+	// ExpirationTime is the time at which the certificate expires.
+	ExpirationTime int64
+	// Issuer is the certificate issuer. It can only be a core AS.
+	Issuer string
+	// IssuingTime is the time at which the certificate was created.
+	IssuingTime int64
+	// SignAlgorithm is the algorithm used to sign the certificate.
+	SignAlgorithm string
+	// Signature is the certificate signature. It is computed over the rest of the certificate.
+	Signature common.RawBytes `json:",omitempty"`
+	// Subject is the certificate subject.
+	Subject string
+	// SubjectEncKey is the public key used for encryption.
+	SubjectEncKey common.RawBytes
+	// SubjectSigKey the public key used for signature verification.
+	SubjectSigKey common.RawBytes
+	// TRCVersion is the version of the issuing trc.
+	TRCVersion int
+	// Version is the certificate version.
+	Version int
+}
+
+func CertificateFromRaw(raw common.RawBytes) (*Certificate, error) {
+	cert := &Certificate{}
+	if err := json.Unmarshal(raw, cert); err != nil {
+		return nil, common.NewCError("Unable to parse Certificate", "err", err)
+	}
+	return cert, nil
+}
+
+func (c *Certificate) IsdAsVer() (*addr.ISD_AS, int, error) {
+	ia, err := addr.IAFromString(c.Subject)
+	if err != nil {
+		return nil, -1, err
+	}
+	return ia, c.Version, err
+}
+
+// Verify checks the signature of the certificate based on a trusted verifying key,
+// and further verifies, that the certificate belongs to the given subject.
+func (c *Certificate) Verify(subject string, verifyKey common.RawBytes, signAlgo string) error {
+	if subject != c.Subject {
+		return common.NewCError("Subject does not match", "expected", c.Subject, "actual", subject)
+	}
+	t := time.Now()
+	if t.Unix() < c.IssuingTime {
+		return common.NewCError("Certificate used before IssuingTime", "IssuingTime",
+			time.Unix(c.IssuingTime, 0), "current", t)
+	}
+	if t.Unix() > c.ExpirationTime {
+		return common.NewCError("Certificate expired", "Expiration Time",
+			time.Unix(c.ExpirationTime, 0), "current", t)
+	}
+	sigInput, err := c.signatureInput()
+	if err != nil {
+		return common.NewCError("Signature input creation faild", "error", err)
+	}
+	return Verify(sigInput, c.Signature, verifyKey, signAlgo)
+}
+
+// Sign adds signature to the certificate. The signature is computed over the certificate
+// without the signature field.
+func (c *Certificate) Sign(signKey common.RawBytes, signAlgo string) error {
+	sigInput, err := c.signatureInput()
+	if err != nil {
+		return err
+	}
+	sig, err := Sign(sigInput, signKey, signAlgo)
+	if err != nil {
+		return err
+	}
+	c.Signature = sig
+	return nil
+
+}
+
+// signatureInput creates a sorted json object of all fields, except for the signature field.
+func (c *Certificate) signatureInput() (common.RawBytes, error) {
+	l := len(c.Signature)
+	c.Signature = c.Signature[:0]
+	sigInput, err := json.Marshal(c)
+	c.Signature = c.Signature[:l]
+	if err != nil {
+		return nil, err
+	}
+	return sigInput, nil
+}
+
+func (c *Certificate) String() string {
+	j, err := json.Marshal(c)
+	if err != nil {
+		return fmt.Sprintf("Certificate for %s not printable. Error: %s", c.Issuer, err)
+	}
+	return string(j)
+}
