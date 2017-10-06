86e0c6f1be5b917b4822d3093f567515b180c65b
Sergiu Costea <sergiu.costea@gmail.com>
2017-10-06 17:55:03 +0200

    less logging

--- go/lib/snet/conn.go
+++ go/lib/snet/conn.go
@@ -15,7 +15,6 @@
 package snet
 
 import (
-	"fmt"
 	"net"
 	"sync"
 	"time"
@@ -289,7 +288,7 @@ func (c *Conn) selectPathEntry(raddr *Addr) (*sciond.PathReplyEntry, error) {
 			}
 		}
 		pathSet = c.sp.Load()
-		log.Info("Current path set is", "set", fmt.Sprintf("%v", pathSet))
+		//log.Info("Current path set is", "set", fmt.Sprintf("%v", pathSet))
 	}
 
 	if len(pathSet) == 0 {
