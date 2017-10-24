78f52f522cc7db2f87fa98e6325302d98882a73e
Oncila <domi.roos@gmail.com>
2017-10-24 16:50:31 +0200

    address Stephen's feedback

--- go/lib/crypto/cert/chain.go
+++ go/lib/crypto/cert/chain.go
@@ -40,11 +40,11 @@ func (k *Key) String() string {
 	return fmt.Sprintf("%s.%d", k.IA, k.Ver)
 }
 
-// Chain contains two certificates, one fore the leave and one for the core. The leave certificate
+// Chain contains two certificates, one fore the leaf and one for the core. The leaf certificate
 // is signed by the core certificate, which is signed by the TRC of the corresponding ISD.
 type Chain struct {
-	// Leave is the leave certificate of the chain. It is signed by the Core certificate.
-	Leave *Certificate `json:"0"`
+	// Leaf is the leaf certificate of the chain. It is signed by the Core certificate.
+	Leaf *Certificate `json:"0"`
 	// Core is the core AS certificate of the chain. It is signed by the TRC of the ISD.
 	Core *Certificate `json:"1"`
 }
@@ -71,7 +71,7 @@ func ChainFromRaw(raw common.RawBytes, lz4_ bool) (*Chain, error) {
 }
 
 func (c *Chain) Verify(subject *addr.ISD_AS, trc interface{}) error {
-	if err := c.Leave.Verify(subject, c.Core.SubjectSigKey, c.Core.SignAlgorithm); err != nil {
+	if err := c.Leaf.Verify(subject, c.Core.SubjectSigKey, c.Core.SignAlgorithm); err != nil {
 		return err
 	}
 	// Fixme(roosd): Verify Core Certificate based on TRC
@@ -95,7 +95,7 @@ func (c *Chain) Compress() (common.RawBytes, error) {
 }
 
 func (c *Chain) String() string {
-	return fmt.Sprintf("CertificateChain %sv%d", c.Leave.Subject, c.Leave.Version)
+	return fmt.Sprintf("CertificateChain %sv%d", c.Leaf.Subject, c.Leaf.Version)
 }
 
 func (c *Chain) JSON(indent bool) ([]byte, error) {
@@ -106,13 +106,13 @@ func (c *Chain) JSON(indent bool) ([]byte, error) {
 }
 
 func (c *Chain) Eq(o *Chain) bool {
-	return c.Leave.Eq(o.Leave) && c.Core.Eq(o.Core)
+	return c.Leaf.Eq(o.Leaf) && c.Core.Eq(o.Core)
 }
 
 func (c *Chain) IAVer() (*addr.ISD_AS, int) {
-	return c.Leave.Subject, c.Leave.Version
+	return c.Leaf.Subject, c.Leaf.Version
 }
 
 func (c *Chain) Key() *Key {
-	return &Key{IA: *c.Leave.Subject, Ver: c.Leave.Version}
+	return &Key{IA: *c.Leaf.Subject, Ver: c.Leaf.Version}
 }
