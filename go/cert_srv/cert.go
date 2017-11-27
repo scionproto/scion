e0a1bee0ed202de10019a1968a1581b101b860c5
Oncilla <domi.roos@gmail.com>
2017-11-27 15:38:28 +0100

    Initial Certificate Server in go (#1325)
    This is an initial commit adding go implementation of the certificate server.
    The current version supports certificate chain requests and replies.
    
    Support for TRCs will be added in a future PR.
    
    main.go is the entry point and initializes the certificate server.
    io.go handles incoming and outgoing scion packets.
    cert.go handles certificate chain requests and replies.

---
+++ go/cert_srv/cert.go
@@ -0,0 +1,128 @@
+// Copyright 2017 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"time"
+
+	log "github.com/inconshreveable/log15"
+
+	"github.com/netsec-ethz/scion/go/lib/addr"
+	"github.com/netsec-ethz/scion/go/lib/crypto/cert"
+	"github.com/netsec-ethz/scion/go/lib/ctrl"
+	"github.com/netsec-ethz/scion/go/lib/ctrl/cert_mgmt"
+	"github.com/netsec-ethz/scion/go/lib/snet"
+)
+
+var (
+	// chainReqCache is an expiring cache for pending requests of certificate chains.
+	chainReqCache = NewReqCache(30*time.Second, 10*time.Minute, 1*time.Second)
+)
+
+type ChainHandler struct {
+	conn *snet.Conn
+}
+
+func NewChainHandler(conn *snet.Conn) *ChainHandler {
+	return &ChainHandler{conn: conn}
+}
+
+// HandleReq handles certificate chain requests. Non-local or cache-only requests are dropped,
+// if the certificate chain is not present.
+func (h *ChainHandler) HandleReq(addr *snet.Addr, req *cert_mgmt.ChainReq) {
+	log.Info("Received certificate chain request", "addr", addr, "req", req)
+	chain := store.GetChain(req.IA(), req.Version)
+	srcLocal := local.IA.Eq(addr.IA)
+	if chain != nil {
+		if err := h.sendChainRep(addr, chain); err != nil {
+			log.Error("Unable to send certificate chain reply", "addr", addr, "req",
+				req, "err", err)
+		}
+	} else if !srcLocal || req.CacheOnly {
+		log.Info("Dropping certificate chain request", "addr", addr, "req", req, "err",
+			"certificate chain not found")
+	} else {
+		if err := h.fetchChain(addr, req); err != nil {
+			log.Error("Unable to fetch certificate chain", "req", req, "err", err)
+		}
+	}
+}
+
+// sendChainRep creates a certificate chain response and sends it to the requester.
+func (h *ChainHandler) sendChainRep(addr *snet.Addr, chain *cert.Chain) error {
+	raw, err := chain.Compress()
+	if err != nil {
+		return err
+	}
+	cpld, err := ctrl.NewCertMgmtPld(&cert_mgmt.ChainRep{RawChain: raw})
+	if err != nil {
+		return err
+	}
+	log.Debug("Send certificate chain reply", "chain", chain, "addr", addr)
+	return SendPayload(h.conn, cpld, addr)
+}
+
+// fetchChain fetches certificate chain from the remote AS.
+func (h *ChainHandler) fetchChain(addr *snet.Addr, req *cert_mgmt.ChainReq) error {
+	key := cert.NewKey(req.IA(), req.Version).String()
+	sendReq := chainReqCache.Put(key, addr)
+	if sendReq { // rate limit
+		return h.sendChainReq(req)
+	}
+	log.Info("Ignoring certificate chain request (same request already pending)",
+		"addr", addr, "req", req)
+	return nil
+}
+
+// sendChainReq sends a certificate chain request to the specified remote AS.
+func (h *ChainHandler) sendChainReq(req *cert_mgmt.ChainReq) error {
+	cpld, err := ctrl.NewCertMgmtPld(req)
+	if err != nil {
+		return err
+	}
+	a := &snet.Addr{IA: req.IA(), Host: addr.SvcCS}
+	log.Debug("Send certificate chain request", "req", req, "addr", a)
+	return SendPayload(h.conn, cpld, a)
+}
+
+// HandleChainRep handles certificate chain replies. Pending requests are answered and removed.
+func (h *ChainHandler) HandleRep(addr *snet.Addr, rep *cert_mgmt.ChainRep) {
+	log.Info("Received certificate chain reply", "addr", addr, "rep", rep)
+	chain, err := rep.Chain()
+	if err != nil {
+		log.Error("Unable to parse certificate reply", "err", err)
+	}
+	if err = store.AddChain(chain, true); err != nil {
+		log.Error("Unable to store certificate chain", "key", chain.Key(), "err", err)
+		return
+	}
+	reqs := chainReqCache.Pop(chain.Key().String())
+	if reqs == nil { // No pending requests
+		return
+	}
+	cpld, err := ctrl.NewCertMgmtPld(rep)
+	if err != nil {
+		log.Error("Unable to create certificate chain reply", "key", chain.Key(),
+			"err", err)
+		return
+	}
+	for _, dst := range reqs.Addrs {
+		if err := SendPayload(h.conn, cpld, dst); err != nil {
+			log.Error("Unable to write certificate chain reply", "key", chain.Key(),
+				"err", err)
+			continue
+		}
+	}
+}
---
+++ go/cert_srv/io.go
@@ -0,0 +1,105 @@
+// Copyright 2017 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	log "github.com/inconshreveable/log15"
+
+	"github.com/netsec-ethz/scion/go/lib/common"
+	"github.com/netsec-ethz/scion/go/lib/ctrl"
+	"github.com/netsec-ethz/scion/go/lib/ctrl/cert_mgmt"
+	"github.com/netsec-ethz/scion/go/lib/snet"
+	"github.com/netsec-ethz/scion/go/proto"
+)
+
+const MaxReadBufSize = 2 << 16
+
+// Dispatcher handles incoming SCION packets.
+type Dispatcher struct {
+	conn         *snet.Conn
+	buf          common.RawBytes
+	chainHandler *ChainHandler
+}
+
+// NewDispatcher creates a new dispatcher listening to SCION traffic on the specified address.
+func NewDispatcher(addr *snet.Addr) (*Dispatcher, error) {
+	// FIXME(roosd): listen to SVC address after #1334 has been addressed
+	conn, err := snet.ListenSCION("udp4", addr)
+	if err != nil {
+		return nil, err
+	}
+	d := &Dispatcher{conn: conn, buf: make(common.RawBytes, MaxReadBufSize)}
+	d.chainHandler = NewChainHandler(d.conn)
+	return d, nil
+}
+
+// run reads SCION packets from snet.
+func (d *Dispatcher) run() {
+	for {
+		read, addr, err := d.conn.ReadFromSCION(d.buf)
+		if err != nil {
+			log.Error("Unable to read from network", "err", err)
+			continue
+		}
+		buf := make(common.RawBytes, read)
+		copy(buf, d.buf[:read])
+		if err = d.dispatch(addr, buf); err != nil {
+			log.Error("Unable to dispatch", "err", err)
+		}
+
+	}
+}
+
+// dispatch hands payload over tho the associated handlers.
+func (d *Dispatcher) dispatch(addr *snet.Addr, buf common.RawBytes) error {
+	signed, err := ctrl.NewSignedPldFromRaw(buf)
+	if err != nil {
+		return err
+	}
+	cpld, err := signed.Pld()
+	if err != nil {
+		return err
+	}
+	c, err := cpld.Union()
+	if err != nil {
+		return err
+	}
+	switch c.ProtoId() {
+	case proto.CertMgmt_TypeID:
+		pld, err := c.(*cert_mgmt.Pld).Union()
+		if err != nil {
+			return err
+		}
+		switch pld.ProtoId() {
+		case proto.CertChainRep_TypeID:
+			d.chainHandler.HandleRep(addr, pld.(*cert_mgmt.ChainRep))
+		case proto.CertChainReq_TypeID:
+			d.chainHandler.HandleReq(addr, pld.(*cert_mgmt.ChainReq))
+		}
+	default:
+		return common.NewCError("Not implemented", "protoID", c.ProtoId())
+	}
+	return nil
+}
+
+// SendPayload is used to send payloads to the specified address using snet.
+func SendPayload(conn *snet.Conn, cpld *ctrl.Pld, addr *snet.Addr) error {
+	buf, err := cpld.PackPld()
+	if err != nil {
+		return err
+	}
+	_, err = conn.WriteToSCION(buf, addr)
+	return err
+}
