35632fee8c19ab75951b1894ee44da34a91726ec
Stephen Shirley <kormat@gmail.com>
2017-12-12 14:44:34 +0100

    Feedback

--- go/lib/common/binary.go
+++ go/lib/common/binary.go
@@ -20,8 +20,8 @@ import (
 )
 
 var (
-	Order       = binOrder{binary.BigEndian}
-	NativeOrder binary.ByteOrder
+	Order       ByteOrderN = newBigEndianN()
+	NativeOrder ByteOrderN
 	IsBigEndian bool
 )
 
@@ -29,29 +29,73 @@ func init() {
 	var v uint16 = 0x11FF
 	if (*[2]uint8)(unsafe.Pointer(&v))[0] == 0x11 {
 		IsBigEndian = true
-		NativeOrder = binary.BigEndian
+		NativeOrder = newBigEndianN()
 	} else {
 		IsBigEndian = false
-		NativeOrder = binary.LittleEndian
+		NativeOrder = newLittleEndianN()
 	}
 }
 
-type binOrder struct {
+type ByteOrderN interface {
+	binary.ByteOrder
+	// Width is the size of the unsigned int value in bytes.
+	UintN(b []byte, width int) uint64
+	// Width is the size of the unsigned int value in bytes.
+	PutUintN(b []byte, v uint64, width int)
+}
+
+var _ ByteOrderN = bigEndianN{}
+
+// bigEndianN is the big-endian implementation of ByteOrderN
+type bigEndianN struct {
 	binary.ByteOrder
 }
 
-func (bo binOrder) UintN(b []byte, width int) uint64 {
+func newBigEndianN() ByteOrderN {
+	return bigEndianN{binary.BigEndian}
+}
+
+func (be bigEndianN) UintN(b []byte, width int) uint64 {
 	_ = b[width-1]
 	var v uint64
 	for i := 0; i < width; i++ {
-		v |= uint64(b[i]) << (uint(width-i-1) * 8)
+		v = (v << 8) | uint64(b[i])
 	}
 	return v
 }
 
-func (bo binOrder) PutUintN(b []byte, v uint64, width int) {
+func (be bigEndianN) PutUintN(b []byte, v uint64, width int) {
 	_ = b[width-1]
 	for i := range b[:width] {
-		b[i] = byte(v >> (uint(width-i-1) * 8))
+		b[width-i-1] = byte(v)
+		v = v >> 8
+	}
+}
+
+var _ ByteOrderN = littleEndianN{}
+
+// littleEndianN is the little-endian implementation of ByteOrderN
+type littleEndianN struct {
+	binary.ByteOrder
+}
+
+func newLittleEndianN() ByteOrderN {
+	return littleEndianN{binary.LittleEndian}
+}
+
+func (le littleEndianN) UintN(b []byte, width int) uint64 {
+	_ = b[width-1]
+	var v uint64
+	for i := width - 1; i >= 0; i-- {
+		v = (v << 8) | uint64(b[i])
+	}
+	return v
+}
+
+func (le littleEndianN) PutUintN(b []byte, v uint64, width int) {
+	_ = b[width-1]
+	for i := range b[:width] {
+		b[i] = byte(v)
+		v = v >> 8
 	}
 }
