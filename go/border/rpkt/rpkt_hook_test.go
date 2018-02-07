01eb4ef75c7542705f3e35a7a50bb4b60d36e7a2
Audrius Meškauskas <andviane@users.noreply.github.com>
2018-02-07 11:21:13 +0100

    Unit tests for Rpkt and Udp (#1355)

---
+++ go/border/rpkt/rpkt_hook_test.go
@@ -0,0 +1,229 @@
+// Copyright 2017 Audrius Meskauskas with all possible permissions granted
+// to ETH Zurich and Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Test hook functions as much as possible. Some hook functions do not define the returned
+// value alone and are not testable this way.
+package rpkt
+
+import (
+	"net"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/spath"
+)
+
+// Track if the hook is only queued once
+var fetched = 0
+var err error
+
+func setupRtrPktHookTest() *RtrPkt {
+	rpkt := NewRtrPkt()
+	rpkt.Id = "id"
+	fetched = 0
+	return rpkt
+}
+
+func TestHooksSrcDstIA(t *testing.T) {
+	Convey("Fetch SrcIA, DstIA via hook functions", t, func() {
+		rpkt := setupRtrPktHookTest()
+
+		srcIA := &addr.ISD_AS{I: 1, A: 2}
+		dstIA := &addr.ISD_AS{I: 3, A: 4}
+
+		rpkt.hooks = hooks{
+			SrcIA: []hookIA{func() (HookResult, *addr.ISD_AS, error) {
+				fetched++
+				return HookFinish, srcIA, nil
+			}},
+			DstIA: []hookIA{func() (HookResult, *addr.ISD_AS, error) {
+				fetched++
+				return HookFinish, dstIA, nil
+			}},
+		}
+		var ia *addr.ISD_AS
+
+		ia, err = rpkt.DstIA()
+		SoMsg("Destination address wrong", ia, ShouldEqual, dstIA)
+		SoMsg("Should be no error when calling destination address getter", err, ShouldBeNil)
+
+		ia, err = rpkt.DstIA()
+		SoMsg("Destination address wrong on second getter call", ia, ShouldEqual, dstIA)
+		SoMsg("Destination address must only be fetched once, cached value must be reused on the"+
+			" second call", fetched, ShouldEqual, 1)
+		SoMsg("Should be no error when calling destination address getter for cached value", err,
+			ShouldBeNil)
+
+		ia, err = rpkt.SrcIA()
+		SoMsg("Source address wrong", ia, ShouldEqual, srcIA)
+		SoMsg("Should be no error when calling source address getter", err, ShouldBeNil)
+
+		ia, err = rpkt.SrcIA()
+		SoMsg("Source address wrong on cached getter call", ia, ShouldEqual, srcIA)
+		SoMsg("Should be no error when calling destination address getter for cached value", err,
+			ShouldBeNil)
+		SoMsg("Two fetches are expected (both source and destination address, each exactly once)",
+			fetched, ShouldEqual, 2)
+	})
+}
+
+func TestHooksSrcDstHost(t *testing.T) {
+	Convey("Fetch SrcHost, DstHost via hook functions", t, func() {
+		rpkt := setupRtrPktHookTest()
+
+		srcHost := addr.HostFromIP(net.IPv4(192, 168, 1, 37))
+		dstHost := addr.HostFromIP(net.IPv4(192, 168, 1, 38))
+
+		rpkt.hooks = hooks{
+			SrcHost: []hookHost{func() (HookResult, addr.HostAddr, error) {
+				fetched++
+				return HookFinish, srcHost, nil
+			}},
+			DstHost: []hookHost{func() (HookResult, addr.HostAddr, error) {
+				fetched++
+				return HookFinish, dstHost, nil
+			}},
+		}
+		var host addr.HostAddr
+
+		host, err = rpkt.DstHost()
+		SoMsg("Destination host wrong", host, ShouldEqual, dstHost)
+		SoMsg("Should be no error when calling destination host getter", err, ShouldBeNil)
+
+		host, err = rpkt.DstHost()
+		SoMsg("Destination host wrong on second getter call", host, ShouldEqual, dstHost)
+		SoMsg("Should be no error when calling destination host getter for cached value", err,
+			ShouldBeNil)
+		SoMsg("Destination host must only be fetched once, cached value must be reused on the "+
+			"second call", fetched, ShouldEqual, 1)
+
+		host, err = rpkt.SrcHost()
+		SoMsg("Source host wrong", host, ShouldEqual, srcHost)
+		SoMsg("Should be no error when calling source host getter", err, ShouldBeNil)
+
+		host, err = rpkt.SrcHost()
+		SoMsg("Source host wrong on cached getter call", host, ShouldEqual, srcHost)
+		SoMsg("Should be no error when calling destination host getter for cached value", err,
+			ShouldBeNil)
+		SoMsg("Two fetches are expected (both source and destination host, each exactly once)",
+			fetched, ShouldEqual, 2)
+	})
+}
+
+func TestHooksInfof(t *testing.T) {
+	Convey("Fetch Infof via hook functions", t, func() {
+		rpkt := setupRtrPktHookTest()
+		infof := spath.InfoField{TsInt: 10, ISD: 11, Hops: 3}
+
+		rpkt.hooks = hooks{
+			Infof: []hookInfoF{func() (HookResult, *spath.InfoField, error) {
+				fetched++
+				return HookFinish, &infof, nil
+			}},
+		}
+		var info *spath.InfoField
+
+		info, err = rpkt.InfoF()
+		SoMsg("Should be no error when calling InfoField getter for fetch", err, ShouldBeNil)
+		SoMsg("Wrong InfoField on the fetch call", *info, ShouldResemble, infof)
+
+		info, err = rpkt.InfoF()
+		SoMsg("Should be no error when calling InfoField getter for cached value", err,
+			ShouldBeNil)
+		SoMsg("Wrong InfoField on the cached getter call", *info, ShouldResemble, infof)
+		SoMsg("Regardless of the two calls, only one fetch of InfoField expected", fetched,
+			ShouldEqual, 1)
+	})
+}
+
+func TestHooksHopf(t *testing.T) {
+	Convey("Fetch HopF via hook functions", t, func() {
+		rpkt := setupRtrPktHookTest()
+		hopfield := spath.HopField{VerifyOnly: true}
+
+		rpkt.hooks = hooks{
+			HopF: []hookHopF{func() (HookResult, *spath.HopField, error) {
+				fetched++
+				return HookFinish, &hopfield, nil
+			}},
+		}
+		var hopf *spath.HopField
+
+		hopf, err = rpkt.HopF()
+		SoMsg("Wrong HopF on the fetch call", hopf.VerifyOnly, ShouldEqual, hopfield.VerifyOnly)
+		SoMsg("Should be no error when calling HopF for fetch", err, ShouldBeNil)
+
+		hopf, err = rpkt.HopF()
+		SoMsg("Wrong HopF on the cached getter call", hopf.VerifyOnly, ShouldEqual,
+			hopfield.VerifyOnly)
+		SoMsg("Should be no error when calling HopF getter for cached value", err, ShouldBeNil)
+		SoMsg("Regardless of the two calls, only one fetch of HopF expected", fetched,
+			ShouldEqual, 1)
+	})
+}
+
+func TestHooksUp(t *testing.T) {
+	Convey("Fetch UpFlag via hook functions", t, func() {
+		rpkt := setupRtrPktHookTest()
+		rpkt.hooks = hooks{
+			UpFlag: []hookBool{func() (HookResult, bool, error) {
+				fetched++
+				return HookFinish, true, nil
+			}},
+		}
+		var up *bool
+
+		up, err = rpkt.UpFlag()
+		SoMsg("Wrong UpFlag on the fetch call", *up, ShouldBeTrue)
+		SoMsg("Should be no error when calling UpFlag for fetch", err, ShouldBeNil)
+
+		up, err = rpkt.UpFlag()
+		SoMsg("Wrong UpFlag on the cached getter call", *up, ShouldBeTrue)
+		SoMsg("Should be no error when calling UpFlag getter for cached value", err, ShouldBeNil)
+		SoMsg("Regardless of the two calls, only one fetch of UpFlag expected", fetched,
+			ShouldEqual, 1)
+	})
+}
+
+func TestLifecycle(t *testing.T) {
+	Convey("Track references", t, func() {
+		rpkt := setupRtrPktHookTest()
+		var free = false
+		rpkt.Free = func(pkt *RtrPkt) {
+			free = true
+		}
+		SoMsg("Must be created with one ref count", rpkt.refCnt, ShouldEqual, 1)
+
+		rpkt.refInc(2)
+		SoMsg("refInc() must increment ref coount from 1 by 2", rpkt.refCnt, ShouldEqual, 3)
+
+		rpkt.Release()
+		SoMsg("Release() must decrement ref count from 3 to 2", rpkt.refCnt, ShouldEqual, 2)
+		SoMsg("Not freed yet, 2 refs remaining", free, ShouldBeFalse)
+
+		rpkt.Release()
+		SoMsg("Not freed yet, 1 ref remaining", free, ShouldBeFalse)
+
+		rpkt.Release()
+		SoMsg("Should be no more refs remaining by now", rpkt.refCnt, ShouldEqual, 0)
+		SoMsg("Freed due to no refs remaining", free, ShouldBeTrue)
+
+		rpkt.Reset()
+		SoMsg("Reset() must set refcount to 1", rpkt.refCnt, ShouldEqual, 1)
+		SoMsg("Reset() must set id to empty", rpkt.Id, ShouldBeEmpty)
+	})
+}
---
+++ go/lib/l4/udp_test.go
@@ -0,0 +1,87 @@
+// Copyright 2017 Audrius Meskauskas with all possible permissions granted
+// to ETH Zurich and Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package l4
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/scionproto/scion/go/lib/common"
+)
+
+// Create UDP structure
+func createUDP() UDP {
+	// Use hex, easier seen in binary dump
+	return UDP{0x1234, 0x5678, 0xA, make(common.RawBytes, 2)}
+}
+
+func TestUDPFromRaw(t *testing.T) {
+	Convey("Content must match", t, func() {
+		raw := common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0}
+		original := createUDP()
+
+		fromRaw, err := UDPFromRaw(raw)
+		SoMsg("Must build from raw without error", err, ShouldBeNil)
+		SoMsg("Wrongly rebuilt from raw", fromRaw, ShouldResemble, &original)
+	})
+}
+
+func TestUDPValidate(t *testing.T) {
+	Convey("It is structure of size 10, must be ok", t, func() {
+		u := createUDP()
+
+		So(u.TotalLen, ShouldEqual, 10)
+		err := u.Validate(int(u.TotalLen) - UDPLen)
+		SoMsg("This structure must be seen as valid", err, ShouldBeNil)
+	})
+}
+
+func TestUDPParse(t *testing.T) {
+	Convey("Must parse into the same representation", t, func() {
+		// assuming we have tested UDPPack
+		u := createUDP()
+		raw, err := u.Pack(true)
+		u2 := UDP{0, 0, 0xA,
+			make(common.RawBytes, 2)}
+
+		u2.Parse(raw)
+		SoMsg("Must parse this valid input", err, ShouldBeNil)
+		SoMsg("Parsed content must match expected", u2, ShouldResemble, u)
+	})
+}
+
+func TestUDPPack(t *testing.T) {
+	Convey("Binary content must match", t, func() {
+		u := createUDP()
+		raw, err := u.Pack(true)
+
+		SoMsg("Must pack without error", err, ShouldBeNil)
+		SoMsg("Packed content must match expected", raw, ShouldResemble,
+			common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0})
+	})
+}
+
+func TestUDPWrite(t *testing.T) {
+	Convey("Binary content must match", t, func() {
+		u := createUDP()
+		raw := make(common.RawBytes, 8)
+		err := u.Write(raw)
+
+		SoMsg("Must write without error", err, ShouldBeNil)
+		SoMsg("Wrong content written", raw, ShouldResemble, common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0})
+	})
+}
