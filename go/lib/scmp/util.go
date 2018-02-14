88cc403ee66912aa0fbe4efc027b3fc72160e8cf
Sergio Gonzalez Monroy <sgmonroy@gmail.com>
2018-02-14 10:44:06 +0100

    Add SCMP Record Path

--- go/lib/scmp/util.go
+++ go/lib/scmp/util.go
@@ -46,8 +46,10 @@ func ParseInfo(b common.RawBytes, ct ClassType) (Info, error) {
 	switch {
 	case ct == ClassType{C_General, T_G_Unspecified}:
 		return InfoString(b), nil
-	case ct.Class == C_General:
+	case ct.Class == C_General && (ct.Type == T_G_EchoRequest || ct.Type == T_G_EchoReply):
 		return InfoEchoFromRaw(b)
+	case ct.Class == C_General && (ct.Type == T_G_RecordPathRequest || ct.Type == T_G_RecordPathReply):
+		return InfoRecordPathFromRaw(b)
 	case ct == ClassType{C_Routing, T_R_OversizePkt} || ct == ClassType{C_CmnHdr, T_C_BadPktLen}:
 		return InfoPktSizeFromRaw(b)
 	case ct == ClassType{C_Path, T_P_PathRequired}:
