56930855232ab656d44679c99b8b227c7bd7579b
Stephen Shirley <kormat@gmail.com>
2018-02-14 11:19:22 +0100

    Update to go1.9.4
    This addresses https://github.com/golang/go/issues/23672, which allows
    arbitrary code execution during "go get".

--- env/go/common.sh
+++ env/go/common.sh
@@ -3,9 +3,9 @@ go_installed() {
 }
 
 go_ver_check() {
-    go version | grep -q ' go1\.9\>'
+    go version | cut -f3 -d' ' | awk -F. '{if ($1 == "go1" && $2 == "9" && $3 >= 4) { exit 0 } else { exit 1 }}'
 }
 
 go_ver_msg() {
-    echo "Go version 1.9.x required. Unsupported go version found ($(type -p go)): $(go version)"
+    echo "Go version 1.9.x (where x >= 4) required. Unsupported go version found ($(type -p go)): $(go version)"
 }
