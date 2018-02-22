6da895ec9211815970b9f0fc8154267eeb1768d3
Sergio Gonzalez Monroy <sgmonroy@gmail.com>
2018-02-22 17:03:16 +0100

    Add scmp-tool with ECHO support

---
+++ go/tools/scmp/main.go
@@ -0,0 +1,207 @@
+// Copyright 2018 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Simple echo application for SCION connectivity tests.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	log "github.com/inconshreveable/log15"
+
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/hpkt"
+	"github.com/scionproto/scion/go/lib/overlay"
+	"github.com/scionproto/scion/go/lib/pathmgr"
+	"github.com/scionproto/scion/go/lib/snet"
+	"github.com/scionproto/scion/go/lib/sock/reliable"
+	"github.com/scionproto/scion/go/lib/spath"
+)
+
+const (
+	DefaultInterval = 1 * time.Second
+	DefaultTimeout  = 2 * time.Second
+	MaxEchoes       = 1 << 16
+)
+
+func GetDefaultSCIONDPath(ia *addr.ISD_AS) string {
+	return fmt.Sprintf("/run/shm/sciond/sd%v.sock", ia)
+}
+
+var (
+	local  snet.Addr
+	remote snet.Addr
+	rnd    *rand.Rand
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	logSetup()
+	defer logPanicAndExit()
+
+	if local.Host == nil {
+		logFatal("Missing local address")
+	}
+	if *sciond == "" {
+		*sciond = GetDefaultSCIONDPath(local.IA)
+	}
+	// Initialize default SCION networking context
+	if err := snet.Init(local.IA, *sciond, *dispatcher); err != nil {
+		logFatal("Unable to initialize SCION network", "err", err)
+	}
+	log.Debug("SCION network successfully initialized")
+
+	// Connect directly to the dispatcher
+	address := &reliable.AppAddr{Addr: local.Host, Port: 0}
+	conn, port, err := reliable.Register(*dispatcher, local.IA, address, nil, addr.SvcNone)
+	if err != nil {
+		logFatal("Unable to register with the dispatcher", "err", err, "addr", local)
+	}
+	defer conn.Close()
+	log.Debug("Registered with dispatcher", "ia", local.IA, "host", address.Addr.String(), "port", port)
+
+	choosePath()
+
+	seed := rand.NewSource(time.Now().UnixNano())
+	rnd = rand.New(seed)
+
+	s := newScmp(*sTypeStr)
+	wg.Add(2)
+	go RecvPkts(&wg, conn, s)
+	go SendPkts(&wg, conn, s)
+
+	wg.Wait()
+}
+
+func SendPkts(wg *sync.WaitGroup, conn *reliable.Conn, s scmpI) {
+	defer wg.Done()
+	defer logPanicAndExit()
+
+	b := make(common.RawBytes, 1<<10)
+
+	nhAddr := reliable.AppAddr{Addr: remote.NextHopHost, Port: remote.NextHopPort}
+	if remote.NextHopHost == nil {
+		nhAddr = reliable.AppAddr{Addr: remote.Host, Port: overlay.EndhostPort}
+	}
+	log.Debug("Path next Hop:", "Host", nhAddr.Addr, "Port", nhAddr.Port)
+	var written int
+	nextPktTS := time.Now()
+	for {
+		// Serialize packet to internal buffer
+		s.setTimestamp(uint64(nextPktTS.UnixNano()) / 1000)
+		pktLen, err := hpkt.WriteScnPkt(s.pktSend(), b)
+		if err != nil {
+			log.Error("Unable to serialize SCION packet", "err", err)
+			break
+		}
+		written, err = conn.WriteTo(b[:pktLen], nhAddr)
+		if err != nil {
+			log.Error("Unable to write", "err", err)
+			break
+		} else if written != pktLen {
+			log.Error("Wrote incomplete message", "expected", len(b), "actual", written)
+			break
+		}
+		nextPktTS = nextPktTS.Add(*interval)
+		if !s.sendNext() {
+			break
+		}
+		sleepTime := time.Until(nextPktTS)
+		time.Sleep(sleepTime)
+	}
+}
+
+func RecvPkts(wg *sync.WaitGroup, conn *reliable.Conn, s scmpI) {
+	defer wg.Done()
+	defer logPanicAndExit()
+
+	b := make([]byte, 1<<10)
+
+	for {
+		pktLen, err := conn.Read(b)
+		if err != nil {
+			log.Error("Unable to read", "err", err)
+			break
+		}
+		now := time.Now()
+		err = hpkt.ParseScnPkt(s.pktRecv(), b[:pktLen])
+		if err != nil {
+			log.Error("SCION packet parse error", "err", err)
+			break
+		}
+		err = s.validate()
+		if err != nil {
+			log.Error("Unexpected SCMP Packet", "err", err)
+			break
+		}
+		// Calculate return time
+		retTime := now.Sub(time.Unix(0, s.getTimestamp()*1000))
+		log.Info(fmt.Sprintf("%d bytes from %v time=%v %s\n", pktLen, remote.String(), retTime, s.String()))
+		if !s.recvNext() {
+			break
+		}
+	}
+}
+
+func choosePath() {
+	pathMgr := snet.DefNetwork.PathResolver()
+	pathSet := pathMgr.Query(local.IA, remote.IA)
+	pathIndeces := make(map[uint64]pathmgr.PathKey)
+	pathIndex := uint64(0)
+	i := uint64(0)
+
+	if len(pathSet) == 0 {
+		// If remote is not in local AS, we need a path!
+		if !remote.IA.Eq(local.IA) {
+			logFatal("No paths available to remote destination")
+		}
+		// If remote in local AS, there is no path
+		return
+	}
+	for k := range pathSet {
+		pathIndeces[i] = k
+		i++
+	}
+	if *interactive {
+		log.Info(fmt.Sprintf("Available paths to %v", remote.IA))
+		for i := range pathIndeces {
+			log.Info(fmt.Sprintf("[%2d] %s\n", i, pathSet[pathIndeces[i]].Entry.Path.String()))
+		}
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			log.Info(fmt.Sprintf("Choose path: "))
+			pathIndexStr, _ := reader.ReadString('\n')
+			var err error
+			pathIndex, err = strconv.ParseUint(pathIndexStr[:len(pathIndexStr)-1], 10, 64)
+			if err == nil && pathIndex < i {
+				break
+			}
+			log.Error("Invalid path index. Valid indeces:", "min", 0, "max", i-1)
+		}
+	}
+	log.Info(fmt.Sprintf("Using path:\n  %s\n", pathSet[pathIndeces[pathIndex]].Entry.Path.String()))
+	entry := pathSet[pathIndeces[pathIndex]].Entry
+	remote.Path = spath.New(entry.Path.FwdPath)
+	remote.Path.InitOffsets()
+	remote.NextHopHost = entry.HostInfo.Host()
+	remote.NextHopPort = entry.HostInfo.Port
+}
