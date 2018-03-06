b1de000a37b8917a893518b3a6bab123d7c08ec1
Sam Hitz <samuel.hitz@gmail.com>
2018-03-06 22:13:17 +0100

    SCION Control Plane PKI tool v0.1

---
+++ go/tools/scion-pki/internal/certs/gen.go
@@ -0,0 +1,303 @@
+// Copyright 2018 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package certs
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"golang.org/x/crypto/curve25519"
+	"golang.org/x/crypto/ed25519"
+
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/crypto"
+	"github.com/scionproto/scion/go/lib/crypto/cert"
+	"github.com/scionproto/scion/go/lib/trust"
+	"github.com/scionproto/scion/go/tools/scion-pki/internal/base"
+	"github.com/scionproto/scion/go/tools/scion-pki/internal/keys"
+)
+
+func runGenCert(cmd *base.Command, args []string) {
+	if len(args) < 1 {
+		cmd.Usage()
+		os.Exit(2)
+	}
+	top, err := base.ProcessSelector(rootDir, args[0], args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		cmd.Usage()
+		os.Exit(2)
+	}
+	// In the first walk, only process core ASes under <top>/
+	if err := filepath.Walk(top, getWalker(true)); err != nil && err != filepath.SkipDir {
+		base.ErrorAndExit("%s\n", err)
+	}
+	// In the second walk, process everything that hasn't been processed yet under <top>/
+	if err := filepath.Walk(top, getWalker(false)); err != nil && err != filepath.SkipDir {
+		base.ErrorAndExit("%s\n", err)
+	}
+}
+
+func getWalker(core bool) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, visitError error) error {
+		if visitError != nil {
+			return visitError
+		}
+		// If not an AS directory, keep walking.
+		if !info.IsDir() || !strings.HasPrefix(info.Name(), "AS") {
+			return nil
+		}
+		var err error
+		// Check that cert.ini exists, otherwise skip directory.
+		cpath := filepath.Join(path, confFile)
+		if _, err = os.Stat(cpath); os.IsNotExist(err) {
+			return filepath.SkipDir
+		}
+		// Check that core-cert.ini exists, otherwise skip directory if walking core ASes.
+		ccpath := filepath.Join(path, coreConfFile)
+		if _, err = os.Stat(ccpath); os.IsNotExist(err) && core {
+			return filepath.SkipDir
+		}
+		// Skip AS if we are processing the non-core ASes and there is a core-cert.ini
+		if err == nil && !core {
+			return filepath.SkipDir
+		}
+		conf, err := loadCertConf(cpath)
+		if err != nil {
+			return common.NewBasicError("Error loading cert.ini", err, "path", cpath)
+		}
+		fmt.Println("Generating Certificate Chain for", conf.Subject)
+		// Generate keys if specified
+		if genKeys {
+			fmt.Println("Generating keys for", conf.Subject)
+			err = keys.GenAll(filepath.Join(path, "keys"), core, force)
+			if err != nil {
+				return err
+			}
+		}
+		// If we are core then we need to generate a core AS cert first.
+		var issuerCert *cert.Certificate
+		if core {
+			issuerCert, err = genIssuerCert(conf.issuerIA, ccpath)
+		} else {
+			issuerCert, err = getIssuerCert(conf.issuerIA)
+		}
+		if err != nil {
+			return err
+		}
+		if issuerCert == nil {
+			return common.NewBasicError("Issuer cert not found", err, "issuer", conf.Issuer)
+		}
+		// Generate the AS certificate chain.
+		chain, err := genASCert(conf, issuerCert)
+		if err != nil {
+			return common.NewBasicError("Error generating cert", err, "subject", conf.Subject)
+		}
+		// Write the cert to disk.
+		if err = writeCert(filepath.Join(path, "certs"), chain); err != nil {
+			return common.NewBasicError("Error writing cert", err, "subject", conf.Subject)
+		}
+		// Skip the rest of this directory.
+		return filepath.SkipDir
+	}
+}
+
+func genCertCommon(conf *certConf) (*cert.Certificate, error) {
+	// Load signing and decryption keys that will be in the certificate.
+	// FIXME(shitz): CoreAS keys should be different from AS keys.
+	keyDir := filepath.Join(path(conf.subjectIA), "keys")
+	signKey, err := trust.LoadKey(filepath.Join(keyDir, trust.SigKeyFile))
+	if err != nil {
+		return nil, err
+	}
+	signPub := common.RawBytes(ed25519.PrivateKey(signKey).Public().(ed25519.PublicKey))
+	decKey, err := trust.LoadKey(filepath.Join(keyDir, trust.DecKeyFile))
+	if err != nil {
+		return nil, err
+	}
+	decKeyFixed := new([32]byte)
+	copy(decKeyFixed[:], decKey)
+	decPub := new([32]byte)
+	curve25519.ScalarBaseMult(decPub, decKeyFixed)
+	// Determine issuingTime and calculate expiration time from validity.
+	issuingTime := uint64(time.Now().Unix())
+	expirationTime := issuingTime + conf.Validity*24*60*60
+	c := &cert.Certificate{
+		CanIssue:       conf.CanIssue,
+		Comment:        conf.Comment,
+		SubjectSignKey: signPub,
+		SignAlgorithm:  conf.SignAlgorithm,
+		SubjectEncKey:  decPub[:],
+		EncAlgorithm:   conf.EncAlgorithm,
+		Issuer:         conf.issuerIA,
+		Subject:        conf.subjectIA,
+		IssuingTime:    issuingTime,
+		ExpirationTime: expirationTime,
+		Version:        conf.Version,
+		TRCVersion:     conf.TRCVersion,
+	}
+
+	return c, nil
+}
+
+// genCoreASCert generates a new core AS certificate according to conf.
+func genCoreASCert(conf *certConf) (*cert.Certificate, error) {
+	c, err := genCertCommon(conf)
+	if err != nil {
+		return nil, err
+	}
+	if c.Comment == "" {
+		c.Comment = fmt.Sprintf("Core AS Certificate for %s version %d.", c.Subject, c.Version)
+	}
+	// For core AS certificates issuer == subject.
+	if !c.Issuer.Eq(c.Subject) {
+		return nil, common.NewBasicError("Subject must match Issuer for Core AS cert.", nil,
+			"subject", c.Subject, "issuer", c.Issuer)
+	}
+	issuerKeyPath := filepath.Join(path(c.Issuer), "keys", trust.OnKeyFile)
+	// Load online root key to sign the certificate.
+	issuerKey, err := trust.LoadKey(issuerKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	// Sign the certificate.
+	// FIXME(shitz): The signing algorithm should be supplied or read from the TRC.
+	if err = c.Sign(issuerKey, crypto.Ed25519); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// genASCert generates a new AS certificate according to 'conf'.
+func genASCert(conf *certConf, issuerCert *cert.Certificate) (*cert.Chain, error) {
+	c, err := genCertCommon(conf)
+	if err != nil {
+		return nil, err
+	}
+	if c.Comment == "" {
+		c.Comment = fmt.Sprintf("AS Certificate for %s version %d.", c.Subject, c.Version)
+	}
+	// Ensure issuer can issue certificates.
+	if !issuerCert.CanIssue {
+		return nil, common.NewBasicError("Issuer cert not authorized to issue new certs.", nil,
+			"issuer", c.Issuer)
+	}
+	issuerKeyPath := filepath.Join(path(issuerCert.Issuer), "keys", trust.SigKeyFile)
+	issuerKey, err := trust.LoadKey(issuerKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	// Sign the certificate.
+	if err = c.Sign(issuerKey, issuerCert.SignAlgorithm); err != nil {
+		return nil, err
+	}
+	// Create certificate chain.
+	chain := &cert.Chain{
+		Leaf: c,
+		Core: issuerCert,
+	}
+	if verify {
+		err = verifyChain(chain, c.Subject)
+		if err != nil {
+			fname := fmt.Sprintf(certNameFmt, c.Subject.I, c.Subject.A, c.Version)
+			return nil, common.NewBasicError("Verification FAILED", err, "cert", fname)
+		}
+	}
+	// Write the cert to disk.
+	return chain, nil
+}
+
+// getIssuerCert returns the newest core certificate of issuer (if any).
+func getIssuerCert(issuer *addr.ISD_AS) (*cert.Certificate, error) {
+	fnames, err := filepath.Glob(fmt.Sprintf("%s/*.crt",
+		filepath.Join(path(issuer), "certs")))
+	if err != nil {
+		return nil, err
+	}
+	var issuerCert *cert.Certificate
+	for _, fname := range fnames {
+		raw, err := ioutil.ReadFile(fname)
+		if err != nil {
+			return nil, err
+		}
+		chain, err := cert.ChainFromRaw(raw, false)
+		if err != nil {
+			return nil, err
+		}
+		if issuerCert == nil || chain.Core.Version > issuerCert.Version {
+			issuerCert = chain.Core
+		}
+	}
+	return issuerCert, nil
+}
+
+func genIssuerCert(issuer *addr.ISD_AS, ccpath string) (*cert.Certificate, error) {
+	coreConf, err := loadCertConf(ccpath)
+	if err != nil {
+		return nil, common.NewBasicError("Error loading core-cert.ini", err, "subject", issuer)
+	}
+	issuerCert, err := getIssuerCert(issuer)
+	if err != nil {
+		return nil, err
+	}
+	// We already have a core AS certificate of the specified version.
+	if issuerCert.Version >= coreConf.Version {
+		return issuerCert, nil
+	}
+	// Need to generate a new core AS certificate.
+	issuerCert, err = genCoreASCert(coreConf)
+	if err != nil {
+		return nil, common.NewBasicError("Error generating core AS cert", err, "subject", issuer)
+	}
+	return issuerCert, nil
+}
+
+func writeCert(path string, c *cert.Chain) error {
+	certRaw, err := c.JSON(true)
+	if err != nil {
+		return err
+	}
+	subject := c.Leaf.Subject
+	fullName := filepath.Join(path, fmt.Sprintf(certNameFmt, subject.I, subject.A, c.Leaf.Version))
+	if !force {
+		// Check if the file already exists.
+		if _, err = os.Stat(fullName); err == nil {
+			fmt.Printf("%s already exists. Use -f to overwrite.\n", fullName)
+			return nil
+		}
+	}
+	// Check if out directory exists and if not create it.
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		if err = os.MkdirAll(path, 0755); err != nil {
+			return common.NewBasicError("Cannot create output dir", err, "path", path)
+		}
+	}
+	if err := ioutil.WriteFile(fullName, append(certRaw, "\n"...), 0644); err != nil {
+		return err
+	}
+	fmt.Println("Successfully written cert to", fullName)
+
+	return nil
+}
+
+func path(ia *addr.ISD_AS) string {
+	return filepath.Join(rootDir, fmt.Sprintf("ISD%d/AS%d", ia.I, ia.A))
+}
---
+++ go/tools/scion-pki/internal/keys/keys.go
@@ -0,0 +1,256 @@
+// Copyright 2018 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package keys provides a generator for AS-level keys involved in the SCION
+// control plane PKI.
+package keys
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/crypto/ed25519"
+	"golang.org/x/crypto/nacl/box"
+
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/trust"
+	"github.com/scionproto/scion/go/tools/scion-pki/internal/base"
+)
+
+const seedFileExt = ".seed"
+
+var CmdKeys = &base.Command{
+	Name:      "keys",
+	Run:       runKeys,
+	UsageLine: "keys [-h] (gen|clean) [<flags>] selector",
+	Short:     "Generate keys for the SCION control plane PKI.",
+	Long: `
+'keys' can be used to generate all the necessary keys used in the SCION control plane PKI.
+
+The following subcommands are available:
+	gen
+		Used to generate new keys.
+	clean
+		Used to remove all keys.
+
+The following flags are available:
+	-d
+		The root directory of all certificates and keys (default '.')
+	-core
+		Used with -all to generate core keys types as well.
+	-f
+		Overwrite existing keys.
+	-all
+		Generate all keys.
+	-sign
+		Generate the AS signing key.
+	-dec
+		Generate the AS decryption key.
+	-online
+		Generate the AS online root key.
+	-offline
+		Generate the AS offline root key.
+
+The following selectors are available:
+	all
+		Apply command to all ASes under the root directory.
+	isd <id>
+		Apply command to all ASes in a given ISD.
+	as <isd>-<as>
+		Apply command to a specific AS, given as ISD-AS identifier (e.g., 1-11)
+`,
+}
+
+var (
+	rootDir string
+	force   bool
+	core    bool
+	all     bool
+	dec     bool
+	sign    bool
+	online  bool
+	offline bool
+)
+
+func init() {
+	CmdKeys.Flag.StringVar(&rootDir, "d", ".", "")
+	CmdKeys.Flag.BoolVar(&force, "f", false, "")
+	CmdKeys.Flag.BoolVar(&core, "core", false, "")
+	CmdKeys.Flag.BoolVar(&all, "all", false, "")
+	CmdKeys.Flag.BoolVar(&dec, "dec", false, "")
+	CmdKeys.Flag.BoolVar(&sign, "sign", false, "")
+	CmdKeys.Flag.BoolVar(&online, "online", false, "")
+	CmdKeys.Flag.BoolVar(&offline, "offline", false, "")
+}
+
+func runKeys(cmd *base.Command, args []string) {
+	if len(args) < 1 {
+		cmd.Usage()
+		os.Exit(2)
+	}
+	subCmd := args[0]
+	cmd.Flag.Parse(args[1:])
+	switch subCmd {
+	case "gen":
+		runGenKey(cmd, cmd.Flag.Args())
+	case "clean":
+		fmt.Println("clean is not implemented yet.")
+		return
+	default:
+		fmt.Fprintf(os.Stderr, "unrecognized subcommand '%s'\n", args[0])
+		fmt.Fprintf(os.Stderr, "run 'scion-pki keys -h' for help.\n")
+		os.Exit(2)
+	}
+}
+
+func runGenKey(cmd *base.Command, args []string) {
+	if len(args) < 1 {
+		cmd.Usage()
+		os.Exit(2)
+	}
+	top, err := base.ProcessSelector(rootDir, args[0], args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		cmd.Usage()
+		os.Exit(2)
+	}
+	if err := filepath.Walk(top, visitKeys); err != nil && err != filepath.SkipDir {
+		base.ErrorAndExit("%s\n", err)
+	}
+	os.Exit(0)
+}
+
+func visitKeys(path string, info os.FileInfo, visitError error) error {
+	if visitError != nil {
+		return visitError
+	}
+	if !info.IsDir() || !strings.HasPrefix(info.Name(), "AS") {
+		return nil
+	}
+	fmt.Println("Generating keys for", info.Name())
+	kpath := filepath.Join(path, "keys")
+	if all {
+		return GenAll(kpath, core, force)
+	}
+	if sign {
+		if err := GenKey(trust.SigKeyFile, kpath, force, genSignKey); err != nil {
+			return err
+		}
+	}
+	if dec {
+		if err := GenKey(trust.DecKeyFile, kpath, force, genEncKey); err != nil {
+			return err
+		}
+	}
+	if online {
+		if err := GenKey(trust.OnKeyFile, kpath, force, genSignKey); err != nil {
+			return err
+		}
+	}
+	if online {
+		if err := GenKey(trust.OffKeyFile, kpath, force, genSignKey); err != nil {
+			return err
+		}
+	}
+	return filepath.SkipDir
+}
+
+type keyGenFunc func(io.Reader) ([]byte, error)
+
+func GenKey(fname, outDir string, override bool, keyGenF keyGenFunc) error {
+	// Check if out directory exists and if not create it.
+	_, err := os.Stat(outDir)
+	if os.IsNotExist(err) {
+		if err = os.MkdirAll(outDir, 0700); err != nil {
+			return common.NewBasicError("Cannot create output dir", err, "key", fname)
+		}
+	} else if err != nil {
+		return common.NewBasicError("Error checking output dir", err, "key", fname)
+	}
+	seedFname := strings.TrimSuffix(fname, filepath.Ext(fname)) + seedFileExt
+	seedPath := filepath.Join(outDir, seedFname)
+	if !override {
+		// Check if the key already exists
+		if _, err := os.Stat(seedPath); err == nil {
+			fmt.Printf("%s already exists. Use -f to overwrite.\n", seedPath)
+			return nil
+		}
+	}
+	// Generate the seed for the public/private key pair.
+	seed := make([]byte, 32)
+	_, err = rand.Read(seed)
+	if err != nil {
+		return common.NewBasicError("Error generating key seed", err, "seed", seedPath)
+	}
+	// Generate a fresh public/private key pair based on seed.
+	privKey, err := keyGenF(bytes.NewReader(seed))
+	if err != nil {
+		return common.NewBasicError("Error generating keys", err, "key", fname)
+	}
+	// Write seed to file.
+	seedEnc := base64.StdEncoding.EncodeToString(seed) + "\n"
+	if err = ioutil.WriteFile(seedPath, []byte(seedEnc), 0600); err != nil {
+		return common.NewBasicError("Cannot write seed file", err, "seed", seedFname)
+	}
+	privKeyPath := filepath.Join(outDir, fname)
+	privKeyEnc := base64.StdEncoding.EncodeToString(privKey) + "\n"
+	if err = ioutil.WriteFile(privKeyPath, []byte(privKeyEnc), 0600); err != nil {
+		return common.NewBasicError("Cannot write key file", err, "key", fname)
+	}
+	fmt.Println("Successfully written seed to", seedPath)
+	fmt.Println("Successfully written key to", privKeyPath)
+	return nil
+}
+
+func genSignKey(rand io.Reader) ([]byte, error) {
+	_, private, err := ed25519.GenerateKey(rand)
+	if err != nil {
+		return nil, err
+	}
+	return private, nil
+}
+
+func genEncKey(rand io.Reader) ([]byte, error) {
+	_, private, err := box.GenerateKey(rand)
+	if err != nil {
+		return nil, err
+	}
+	return (*private)[:], nil
+}
+
+func GenAll(outDir string, core, override bool) error {
+	// Generate signing and decryption keys.
+	if err := GenKey(trust.SigKeyFile, outDir, override, genSignKey); err != nil {
+		return err
+	}
+	if err := GenKey(trust.DecKeyFile, outDir, override, genEncKey); err != nil {
+		return err
+	}
+	// If core was not specified we are done.
+	if !core {
+		return nil
+	}
+	// Generate offline and online root keys if core was specified.
+	if err := GenKey(trust.OffKeyFile, outDir, override, genSignKey); err != nil {
+		return err
+	}
+	return GenKey(trust.OnKeyFile, outDir, override, genSignKey)
+}
