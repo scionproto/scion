e5f0b3ba5cddefd3f1c7fc84636b355f6dcf0fdc
Sergio Gonzalez Monroy <sgmonroy@gmail.com>
2018-03-07 14:58:17 +0100

    Addressing commments

--- go/tools/scmp/main.go
+++ go/tools/scmp/main.go
@@ -20,6 +20,7 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
+	"net"
 	"os"
 	"strconv"
 	"sync"
@@ -29,7 +30,6 @@ import (
 	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/hpkt"
 	"github.com/scionproto/scion/go/lib/overlay"
-	"github.com/scionproto/scion/go/lib/pathmgr"
 	"github.com/scionproto/scion/go/lib/sciond"
 	"github.com/scionproto/scion/go/lib/snet"
 	"github.com/scionproto/scion/go/lib/sock/reliable"
@@ -42,7 +42,7 @@ const (
 	MaxEchoes       = 1 << 16
 )
 
-func GetDefaultSCIONDPath(ia *addr.ISD_AS) string {
+func GetDefaultSCIONDPath(ia addr.IA) string {
 	return fmt.Sprintf("/run/shm/sciond/sd%v.sock", ia)
 }
 
@@ -52,12 +52,15 @@ var (
 	sciondPath  = flag.String("sciond", "", "Path to sciond socket")
 	dispatcher  = flag.String("dispatcher", "/run/shm/dispatcher/default.sock",
 		"Path to dispatcher socket")
-	interval = flag.Duration("interval", DefaultInterval, "time between packets")
-	count    = flag.Uint("c", 10, "Total number of packet to send (ignored if not echo")
-	sTypeStr = &sType
-	local    snet.Addr
-	remote   snet.Addr
-	rnd      *rand.Rand
+	interval  = flag.Duration("interval", DefaultInterval, "time between packets")
+	timeout   = flag.Duration("timeout", DefaultTimeout, "timeout per packet")
+	count     = flag.Uint("c", 0, "Total number of packet to send (ignored if not echo)")
+	sTypeStr  = &sType
+	local     snet.Addr
+	remote    snet.Addr
+	bind      snet.Addr
+	rnd       *rand.Rand
+	pathEntry *sciond.PathReplyEntry
 )
 
 var sType string = "echo"
@@ -65,13 +68,16 @@ var sType string = "echo"
 func init() {
 	flag.Var((*snet.Addr)(&local), "local", "(Mandatory) address to listen on")
 	flag.Var((*snet.Addr)(&remote), "remote", "(Mandatory for clients) address to connect to")
-	flag.Parse()
+	flag.Var((*snet.Addr)(&bind), "bind", "address to bind to, if running behind NAT")
 }
 
 func main() {
 	var wg sync.WaitGroup
 
-	if local.IA == nil {
+	flag.Parse()
+	validate()
+
+	if local.Host == nil {
 		fatal("Missing local address")
 	}
 	if *sciondPath == "" {
@@ -82,16 +88,17 @@ func main() {
 		fatal("Unable to initialize SCION network", "err", err)
 	}
 	// Connect directly to the dispatcher
-	address := &reliable.AppAddr{Addr: local.Host, Port: 0}
-	conn, _, err := reliable.Register(*dispatcher, local.IA, address, nil, addr.SvcNone)
+	address := &reliable.AppAddr{Addr: local.Host}
+	bindAddress := &reliable.AppAddr{Addr: bind.Host}
+	if bind.Host == nil {
+		bindAddress = nil
+	}
+	conn, _, err := reliable.Register(*dispatcher, local.IA, address, bindAddress, addr.SvcNone)
 	if err != nil {
 		fatal("Unable to register with the dispatcher", "err", err, "addr", local)
 	}
 	defer conn.Close()
 
-	validate()
-
-	var pathEntry *sciond.PathReplyEntry
 	// If remote is not in local AS, we need a path!
 	if !remote.IA.Eq(local.IA) {
 		pathEntry = choosePath(*interactive)
@@ -107,114 +114,145 @@ func main() {
 	seed := rand.NewSource(time.Now().UnixNano())
 	rnd = rand.New(seed)
 
-	var send, recv scmpPkt
+	var send, recv scmpCtx
 	initSCMP(&send, &recv, *sTypeStr, *count, pathEntry)
 
+	ch := make(chan time.Time, 20)
 	wg.Add(2)
-	go RecvPkts(&wg, conn, &recv)
-	go SendPkts(&wg, conn, &send)
+	go RecvPkts(&wg, conn, &recv, ch)
+	go SendPkts(&wg, conn, &send, ch)
 
 	wg.Wait()
+
+	ret := 0
+	if send.num != recv.num {
+		ret = 1
+	}
+
+	os.Exit(ret)
 }
 
 func validate() {
-	if local.IA == nil {
+	if local.Host == nil {
 		fatal("Invalid local address")
 	}
-	if remote.IA == nil {
+	if remote.Host == nil {
 		fatal("Invalid remote address")
 	}
 	// scmp-tool does not uses ports, thus they should not be set
 	// Still, the user could set port as 0 ie, ISD-AS,[host]:0 and be valid
 	if local.L4Port != 0 {
-		fatal("Invalid local port", "local port", local.L4Port)
+		fatal("Local port should not be provided")
 	}
 	if remote.L4Port != 0 {
-		fatal("Invalid remote port", "remote port", remote.L4Port)
+		fatal("Remote port should not be provided")
 	}
 }
 
-func SendPkts(wg *sync.WaitGroup, conn *reliable.Conn, s *scmpPkt) {
-	defer wg.Done()
+func sendPKt(conn *reliable.Conn, s *scmpCtx, b common.RawBytes) {
+}
 
-	nextPktTS := time.Now()
-	b := make(common.RawBytes, 1<<10)
+func SendPkts(wg *sync.WaitGroup, conn *reliable.Conn, s *scmpCtx, ch chan time.Time) {
+	defer wg.Done()
+	defer close(ch)
+
+	b := make(common.RawBytes, pathEntry.Path.Mtu)
 
 	nhAddr := reliable.AppAddr{Addr: remote.NextHopHost, Port: remote.NextHopPort}
 	if remote.NextHopHost == nil {
 		nhAddr = reliable.AppAddr{Addr: remote.Host, Port: overlay.EndhostPort}
 	}
-	for {
+	nextPktTS := time.Now()
+	ticker := time.NewTicker(*interval)
+	for ; true; nextPktTS = <-ticker.C {
+		updatePktTS(s, nextPktTS)
 		// Serialize packet to internal buffer
 		pktLen, err := hpkt.WriteScnPkt(s.pkt, b)
 		if err != nil {
-			fmt.Printf("ERROR: Unable to serialize SCION packet %s\n", err.Error())
+			fmt.Fprintf(os.Stderr, "ERROR: Unable to serialize SCION packet %v\n", err)
 			break
 		}
 		written, err := conn.WriteTo(b[:pktLen], nhAddr)
 		if err != nil {
-			fmt.Printf("ERROR: Unable to write %s\n", err.Error())
+			fmt.Fprintf(os.Stderr, "ERROR: Unable to write %v\n", err)
 			break
 		} else if written != pktLen {
-			fmt.Printf("ERROR: Wrote incomplete message. written=%v, expected=%v\n", len(b), written)
+			fmt.Fprintf(os.Stderr, "ERROR: Wrote incomplete message. written=%d, expected=%d\n",
+				len(b), written)
 			break
 		}
-		nextPktTS = nextPktTS.Add(*interval)
-		if !sendNext(s, nextPktTS) {
+		// Notify the receiver
+		ch <- nextPktTS
+		// Update packet fields
+		updatePkt(s)
+		if !morePkts(s) {
 			break
 		}
-		sleepTime := time.Until(nextPktTS)
-		time.Sleep(sleepTime)
 	}
 }
 
-func RecvPkts(wg *sync.WaitGroup, conn *reliable.Conn, s *scmpPkt) {
+func RecvPkts(wg *sync.WaitGroup, conn *reliable.Conn, s *scmpCtx, ch chan time.Time) {
 	defer wg.Done()
+	var sent uint64
 
-	b := make([]byte, 1<<10)
+	b := make(common.RawBytes, pathEntry.Path.Mtu)
 
+	start := time.Now()
+	nextTimeout := start
 	for {
+		nextPktTS, ok := <-ch
+		if ok {
+			sent += 1
+			nextTimeout = nextPktTS.Add(*timeout)
+			conn.SetReadDeadline(nextTimeout)
+		} else if s.num == sent || nextTimeout.Before(time.Now()) {
+			break
+		}
 		pktLen, err := conn.Read(b)
 		if err != nil {
-			fmt.Printf("ERROR: Unable to read %s\n", err.Error())
-			break
+			e, ok := err.(*net.OpError)
+			if ok && e.Timeout() {
+				continue
+			} else {
+				fmt.Fprintf(os.Stderr, "ERROR: Unable to read %v\n", err)
+				break
+			}
 		}
 		now := time.Now()
 		err = hpkt.ParseScnPkt(s.pkt, b[:pktLen])
 		if err != nil {
-			fmt.Printf("ERROR: SCION packet parse error %s\n", err.Error())
+			fmt.Fprintf(os.Stderr, "ERROR: SCION packet parse error %v\n", err)
 			break
 		}
 		err = validatePkt(s)
 		if err != nil {
-			fmt.Printf("ERROR: Unexpected SCMP Packet %s\n", err.Error())
+			fmt.Fprintf(os.Stderr, "ERROR: Unexpected SCMP Packet %v\n", err)
 			break
 		}
+		s.num += 1
 		prettyPrint(s, pktLen, now)
-		if !recvNext(s) {
-			break
-		}
 	}
+	fmt.Printf("%d packets transmitted, %d received, %d%% packet loss, time %v\n",
+		sent, s.num, 100-s.num*100/sent, time.Now().Sub(start))
 }
 
 func choosePath(interactive bool) *sciond.PathReplyEntry {
+	var paths []*sciond.PathReplyEntry
+	var pathIndex uint64
+
 	pathMgr := snet.DefNetwork.PathResolver()
 	pathSet := pathMgr.Query(local.IA, remote.IA)
-	pathIndeces := make(map[uint64]pathmgr.PathKey)
-	pathIndex := uint64(0)
-	i := uint64(0)
 
 	if len(pathSet) == 0 {
 		return nil
 	}
-	for k := range pathSet {
-		pathIndeces[i] = k
-		i++
+	for _, p := range pathSet {
+		paths = append(paths, p.Entry)
 	}
 	if interactive {
 		fmt.Printf("Available paths to %v\n", remote.IA)
-		for i := range pathIndeces {
-			fmt.Printf("[%2d] %s\n", i, pathSet[pathIndeces[i]].Entry.Path.String())
+		for i := range paths {
+			fmt.Printf("[%2d] %s\n", i, paths[i].Path.String())
 		}
 		reader := bufio.NewReader(os.Stdin)
 		for {
@@ -222,14 +260,14 @@ func choosePath(interactive bool) *sciond.PathReplyEntry {
 			pathIndexStr, _ := reader.ReadString('\n')
 			var err error
 			pathIndex, err = strconv.ParseUint(pathIndexStr[:len(pathIndexStr)-1], 10, 64)
-			if err == nil && pathIndex < i {
+			if err == nil && int(pathIndex) < len(paths) {
 				break
 			}
-			fmt.Printf("ERROR: Invalid path index, valid indices range: [0, %v]\n", i-1)
+			fmt.Fprintf(os.Stderr, "ERROR: Invalid path index, valid indices range: [0, %v]\n", len(paths))
 		}
 	}
-	fmt.Printf("Using path:\n  %s\n", pathSet[pathIndeces[pathIndex]].Entry.Path.String())
-	return pathSet[pathIndeces[pathIndex]].Entry
+	fmt.Printf("Using path:\n  %s\n", paths[pathIndex].Path.String())
+	return paths[pathIndex]
 }
 
 func fatal(msg string, a ...interface{}) {
--- go/tools/scmp/types.go
+++ go/tools/scmp/types.go
@@ -4,25 +4,18 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/scionproto/scion/go/lib/addr"
-	"github.com/scionproto/scion/go/lib/assert"
 	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/sciond"
 	"github.com/scionproto/scion/go/lib/scmp"
-	"github.com/scionproto/scion/go/lib/spath"
 	"github.com/scionproto/scion/go/lib/spkt"
 )
 
 func newSCMPPkt(t scmp.Type, info scmp.Info, ext common.Extension) *spkt.ScnPkt {
 	scmpMeta := scmp.Meta{InfoLen: uint8(info.Len())}
-	assert.Must((scmp.MetaLen+info.Len())%common.LineLen == 0, "Bad SCMP payload length")
 	pld := make(common.RawBytes, scmp.MetaLen+info.Len())
 	scmpMeta.Write(pld)
 	info.Write(pld[scmp.MetaLen:])
 	scmpHdr := scmp.NewHdr(scmp.ClassType{Class: scmp.C_General, Type: t}, len(pld))
-	scmpHdr.Timestamp = uint64(time.Now().UnixNano()) / 1000
-	exts := make([]common.Extension, 1)
-	exts[0] = ext
 
 	pkt := &spkt.ScnPkt{
 		DstIA:   remote.IA,
@@ -30,22 +23,22 @@ func newSCMPPkt(t scmp.Type, info scmp.Info, ext common.Extension) *spkt.ScnPkt 
 		DstHost: remote.Host,
 		SrcHost: local.Host,
 		Path:    remote.Path,
-		HBHExt:  exts,
+		HBHExt:  []common.Extension{ext},
 		L4:      scmpHdr,
 		Pld:     pld,
 	}
 	return pkt
 }
 
-type scmpPkt struct {
+type scmpCtx struct {
 	pkt     *spkt.ScnPkt
 	pktType scmp.Type
 	info    scmp.Info
-	total   uint
-	num     uint
+	count   uint64
+	num     uint64
 }
 
-func initSCMP(send, recv *scmpPkt, typeStr string, count uint, pathEntry *sciond.PathReplyEntry) {
+func initSCMP(send, recv *scmpCtx, typeStr string, count uint, pathEntry *sciond.PathReplyEntry) {
 	switch typeStr {
 	case "echo":
 		initEcho(send, false, count)
@@ -55,12 +48,12 @@ func initSCMP(send, recv *scmpPkt, typeStr string, count uint, pathEntry *sciond
 	}
 }
 
-func initEcho(s *scmpPkt, recv bool, count uint) {
+func initEcho(s *scmpCtx, recv bool, count uint) {
 	s.num = 0
-	s.total = count
+	s.count = uint64(count)
 	s.info = &scmp.InfoEcho{Id: rnd.Uint64(), Seq: 0}
 	if recv {
-		s.pkt = &spkt.ScnPkt{DstIA: &addr.ISD_AS{}, SrcIA: &addr.ISD_AS{}, Path: &spath.Path{}}
+		s.pkt = &spkt.ScnPkt{}
 		s.pktType = scmp.T_G_EchoReply
 	} else {
 		ext := &scmp.Extn{Error: false, HopByHop: false}
@@ -69,9 +62,12 @@ func initEcho(s *scmpPkt, recv bool, count uint) {
 	}
 }
 
-func sendNext(s *scmpPkt, ts time.Time) bool {
+func updatePktTS(s *scmpCtx, ts time.Time) {
 	scmpHdr := s.pkt.L4.(*scmp.Hdr)
 	scmpHdr.Timestamp = uint64(ts.UnixNano()) / 1000
+}
+func updatePkt(s *scmpCtx) {
+	//updatePktTS(s, ts)
 	s.num += 1
 	switch info := s.info.(type) {
 	case *scmp.InfoEcho:
@@ -79,19 +75,14 @@ func sendNext(s *scmpPkt, ts time.Time) bool {
 		b := s.pkt.Pld.(common.RawBytes)
 		info.Write(b[scmp.MetaLen:])
 	}
-	return s.num < s.total
 }
 
-func recvNext(s *scmpPkt) bool {
-	switch s.pktType {
-	case scmp.T_G_EchoReply:
-		s.num += 1
-	}
-	return s.num < s.total
+func morePkts(s *scmpCtx) bool {
+	return s.count == 0 || s.num < s.count
 }
 
-func validatePkt(s *scmpPkt) error {
-	scmpHdr, ok := s.pkt.L4.(*scmp.Hdr)
+func validatePkt(s *scmpCtx) error {
+	_, ok := s.pkt.L4.(*scmp.Hdr)
 	if ok == false {
 		return common.NewBasicError("Not a SCMP header", nil)
 	}
@@ -99,27 +90,25 @@ func validatePkt(s *scmpPkt) error {
 	if ok == false {
 		return common.NewBasicError("Not a SCMP payload)", nil)
 	}
-	if scmpHdr.Type != s.pktType {
-		return common.NewBasicError("Bad type", nil,
-			"Received", scmpHdr.Type, "Expected", s.pktType)
-	}
 	switch s.pktType {
 	case scmp.T_G_EchoReply:
 		s.info, ok = scmpPld.Info.(*scmp.InfoEcho)
 		if ok == false {
-			return common.NewBasicError("Not a Info Echo type", nil)
+			return common.NewBasicError("Not an Info Echo type", nil,
+				"type", common.TypeOf(s.info))
 		}
 	}
 	return nil
 }
 
-func prettyPrint(s *scmpPkt, pktLen int, now time.Time) {
+func prettyPrint(s *scmpCtx, pktLen int, now time.Time) {
 	// Calculate return time
 	scmpHdr := s.pkt.L4.(*scmp.Hdr)
-	retTime := now.Sub(time.Unix(0, int64(scmpHdr.Timestamp)*1000))
+	//rtt := now.Sub(time.Unix(0, int64(scmpHdr.Timestamp)*1000))
+	rtt := now.UnixNano() - (int64(scmpHdr.Timestamp) * 1000)
 	switch info := s.info.(type) {
 	case *scmp.InfoEcho:
-		fmt.Printf("%d bytes from %v,[%v] scmp_seq=%v time=%v\n",
-			pktLen, s.pkt.SrcIA, s.pkt.SrcHost, info.Seq, retTime)
+		fmt.Printf("%d bytes from %s,[%s] scmp_seq=%d time=%.3fms\n",
+			pktLen, s.pkt.SrcIA, s.pkt.SrcHost, info.Seq, float64(rtt)/1000000)
 	}
 }
