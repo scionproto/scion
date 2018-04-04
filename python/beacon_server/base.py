deab2f6a22e5cfce6ec31ac5f87a6e78294fe1fa
Lukas Bischofberger <bischofberger@anapaya.net>
2018-04-04 15:24:18 +0200

    linting

--- python/beacon_server/base.py
+++ python/beacon_server/base.py
@@ -830,8 +830,10 @@ class BeaconServer(SCIONElement, metaclass=ABCMeta):
                 host = br.interfaces[ifid].remote[0][0]
                 port = br.interfaces[ifid].remote[0][1]
                 one_hop_path = self._create_one_hop_path(ifid)
-                meta = self._build_meta(ia=dst_ia, host=host, port=port, path=one_hop_path, one_hop=True)
-                self.send_meta(CtrlPayload(IFIDPayload.from_values(ifid)), meta, next_hop_port=br.int_addrs[0].public[0])
+                meta = self._build_meta(
+                    ia=dst_ia, host=host, port=port, path=one_hop_path, one_hop=True)
+                self.send_meta(CtrlPayload(IFIDPayload.from_values(
+                    ifid)), meta, next_hop_port=br.int_addrs[0].public[0])
 
     def _init_metrics(self):
         super()._init_metrics()
