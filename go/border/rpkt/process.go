scionproto/scion

commit bb08e8763807caad091b6d591c45cbca4d5eabaf

Keep-alives from BS (#1476)
Keep-alive messages are now sent from the BS to the neighboring BR, instead of from BR to BR. BR further propagate these to their local BS. This fixes #1473

python/beacon_server/base.py
@@ -120,6 +120,8 @@ class BeaconServer(SCIONElement, metaclass=ABCMeta):
     ZK_REV_OBJ_MAX_AGE = HASHTREE_EPOCH_TIME
     # Interval to checked for timed out interfaces.
     IF_TIMEOUT_INTERVAL = 1
+    # Interval to send keep-alive msgs
+    IFID_INTERVAL = 1
 
     def __init__(self, server_id, conf_dir, spki_cache_dir=GEN_CACHE_PATH, prom_export=None):
         """
@@ -460,6 +462,9 @@ class BeaconServer(SCIONElement, metaclass=ABCMeta):
             target=thread_safety_net, args=(self.worker,),
             name="BS.worker", daemon=True).start()
         # https://github.com/scionproto/scion/issues/308:
+        threading.Thread(
+            target=thread_safety_net, args=(self._send_ifid_updates,),
+            name="BS._send_if_updates", daemon=True).start()
         threading.Thread(
             target=thread_safety_net, args=(self._handle_if_timeouts,),
             name="BS._handle_if_timeouts", daemon=True).start()
@@ -805,6 +810,27 @@ class BeaconServer(SCIONElement, metaclass=ABCMeta):
             self.send_meta(payload.copy(), meta, (meta.host, meta.port))
         for meta in server_metas:
             self.send_meta(payload.copy(), meta)
+
+    def _send_ifid_updates(self):
+        start = time.time()
+        while self.run_flag.is_set():
+            sleep_interval(start, self.IFID_INTERVAL, "BS._send_ifid_updates cycle")
+            start = time.time()
+
+            # only master sends keep-alive messages
+            if not self.zk.have_lock():
+                continue
+
+            # send keep-alives on all known BR interfaces
+            for ifid in self.ifid2br:
+                br = self.ifid2br[ifid]
+                dst_ia = br.interfaces[ifid].isd_as
+                host, port = br.interfaces[ifid].remote[0]
+                one_hop_path = self._create_one_hop_path(ifid)
+                meta = self._build_meta(
+                    ia=dst_ia, host=host, port=port, path=one_hop_path, one_hop=True)
+                self.send_meta(CtrlPayload(IFIDPayload.from_values(ifid)),
+                               meta, next_hop_port=br.int_addrs[0].public[0])
 
     def _init_metrics(self):
         super()._init_metrics()

go/border/gen.go
@@ -28,7 +28,6 @@ import (
 	"github.com/scionproto/scion/go/lib/addr"
 	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/ctrl"
-	"github.com/scionproto/scion/go/lib/ctrl/ifid"
 	"github.com/scionproto/scion/go/lib/ctrl/path_mgmt"
 	"github.com/scionproto/scion/go/lib/l4"
 	"github.com/scionproto/scion/go/lib/log"
@@ -38,8 +37,6 @@ import (
 )
 
 const (
-	// ifIDFreq is how often IFID packets are sent to the neighbouring AS.
-	ifIDFreq = 1 * time.Second
 	// ifStateFreq is how often the router will request an Interface State update
 	// from the beacon service.
 	ifStateFreq = 30 * time.Second
@@ -89,40 +86,6 @@ func (r *Router) genPkt(dstIA addr.IA, dstHost addr.HostAddr, dstL4Port int,
 		rp.Egress = append(rp.Egress, rpkt.EgressPair{S: ctx.ExtSockOut[ifid]})
 	}
 	return rp.Route()
-}
-
-// SyncInterface handles generating periodic Interface ID (IFID) packets that are
-// sent to the Beacon Service in the neighbouring AS. These function as both
-// keep-alives, and to inform the neighbour of the local interface ID.
-func (r *Router) SyncInterface() {
-	defer liblog.LogPanicAndExit()
-	for range time.Tick(ifIDFreq) {
-		ctx := rctx.Get()
-		for ifid := range ctx.Conf.Net.IFs {
-			r.genIFIDPkt(ifid, ctx)
-		}
-	}
-}
-
-// genIFIDPkt generates an IFID-packet for the specified interface.
-func (r *Router) genIFIDPkt(ifID common.IFIDType, ctx *rctx.Ctx) {
-	logger := log.New("ifid", ifID)
-	intf := ctx.Conf.Net.IFs[ifID]
-	srcAddr := intf.IFAddr.PublicAddrInfo(intf.IFAddr.Overlay)
-	cpld, err := ctrl.NewPld(&ifid.IFID{OrigIfID: uint64(ifID)}, nil)
-	if err != nil {
-		logger.Error("Error generating IFID Ctrl payload", "err", err)
-		return
-	}
-	scpld, err := cpld.SignedPld(ctrl.NullSigner)
-	if err != nil {
-		logger.Error("Error generating IFID signed Ctrl payload", "err", err)
-		return
-	}
-	if err := r.genPkt(intf.RemoteIA, addr.HostFromIP(intf.RemoteAddr.IP),
-		intf.RemoteAddr.L4Port, srcAddr, scpld); err != nil {
-		logger.Error("Error generating IFID packet", "err", err)
-	}
 }
 
 // IFStateUpdate handles generating periodic Interface State Request (IFStateReq)

go/border/router.go
@@ -71,7 +71,6 @@ func NewRouter(id, confDir string) (*Router, error) {
 // Run sets up networking, and starts go routines for handling the main packet
 // processing as well as various other router functions.
 func (r *Router) Run() error {
-	go r.SyncInterface()
 	go r.IFStateUpdate()
 	go r.RevInfoFwd()
 	go r.PacketError()

commit e55649a62e909cb5fd20fe9ba719db3566b4dcea

IFID messages hop-by-hop
This PR fixes a problem introduced by #1476.
IFID msgs are now sent from BS to local BR and from there forwarded to the remote BR which forwards it to the local BS.
Like that IFID msgs will not generate SCMP revocations at the BR if the interface has recently come up.

go/border/rpkt/process.go
@@ -156,8 +156,44 @@ func (rp *RtrPkt) processDestSelf() (HookResult, error) {
 	}
 }
 
-// processIFID handles IFID (interface ID) packets from neighbouring ISD-ASes.
+// processIFID handles IFID (interface ID) packets
 func (rp *RtrPkt) processIFID(ifid *ifid.IFID) (HookResult, error) {
+	if rp.DirFrom == rcmn.DirLocal {
+		return rp.processLocalIFID(ifid)
+	} else {
+		return rp.processRemoteIFID(ifid)
+	}
+}
+
+// processLocalIFID handles IFID (interface ID) packets from the local BS
+// and forwards them to the remote ISD-AS BR
+func (rp *RtrPkt) processLocalIFID(ifid *ifid.IFID) (HookResult, error) {
+	intf := rp.Ctx.Conf.Net.IFs[*rp.ifCurr]
+	// Create ScnPkt from RtrPkt and set remote BR as Dst
+	spkt, err := rp.ToScnPkt(true)
+	if err != nil {
+		return HookError, err
+	}
+	spkt.DstIA = intf.RemoteIA
+	spkt.DstHost = addr.HostFromIP(intf.RemoteAddr.IP)
+	// Remove old path and add overlay
+	spkt.Path = nil
+	overlayPort := intf.IFAddr.PublicAddrInfo(intf.IFAddr.Overlay).OverlayPort
+	spkt.L4 = &l4.UDP{SrcPort: uint16(overlayPort), DstPort: uint16(intf.RemoteAddr.OverlayPort)}
+	// Convert back to RtrPkt
+	fwdrp, err := RtrPktFromScnPkt(spkt, rcmn.DirExternal, rp.Ctx)
+	if err != nil {
+		return HookError, err
+	}
+	// Set current IF and forward to remote BR directly
+	fwdrp.ifCurr = rp.ifCurr
+	fwdrp.hooks.Route = append(fwdrp.hooks.Route, fwdrp.forwardFromLocal)
+	fwdrp.Route()
+	return HookFinish, nil
+}
+
+// processRemoteIFID handles IFID (interface ID) packets from neighbouring ISD-ASes.
+func (rp *RtrPkt) processRemoteIFID(ifid *ifid.IFID) (HookResult, error) {
 	// Set the RelayIF field in the payload to the current interface ID.
 	ifid.RelayIfID = uint64(*rp.ifCurr)
 	cpld, err := ctrl.NewPld(ifid, nil)

python/beacon_server/base.py
@@ -825,13 +825,12 @@ class BeaconServer(SCIONElement, metaclass=ABCMeta):
             # send keep-alives on all known BR interfaces
             for ifid in self.ifid2br:
                 br = self.ifid2br[ifid]
-                dst_ia = br.interfaces[ifid].isd_as
-                host, port = br.interfaces[ifid].remote[0]
+                src_ia = self.topology.isd_as
+                host, port = br.int_addrs[0].public[0]
                 one_hop_path = self._create_one_hop_path(ifid)
                 meta = self._build_meta(
-                    ia=dst_ia, host=host, port=port, path=one_hop_path, one_hop=True)
-                self.send_meta(CtrlPayload(IFIDPayload.from_values(ifid)),
-                               meta, next_hop_port=br.int_addrs[0].public[0])
+                    ia=src_ia, host=host, port=port, path=one_hop_path, one_hop=True)
+                self.send_meta(CtrlPayload(IFIDPayload.from_values(ifid)), meta)
 
     def _init_metrics(self):
         super()._init_metrics()