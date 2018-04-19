108615dd63fcc210d4862bf1b22ee019377838bd
Lukas Bischofberger <bischofberger@anapaya.net>
2018-04-19 14:00:42 +0200

    remove path

--- python/beacon_server/base.py
+++ python/beacon_server/base.py
@@ -825,11 +825,9 @@ class BeaconServer(SCIONElement, metaclass=ABCMeta):
             # send keep-alives on all known BR interfaces
             for ifid in self.ifid2br:
                 br = self.ifid2br[ifid]
-                host, port = br.int_addrs[0].public[0]
-                one_hop_path = self._create_one_hop_path(ifid)
-                meta = self._build_meta(
-                    host=host, port=port, path=one_hop_path, one_hop=True)
-                self.send_meta(CtrlPayload(IFIDPayload.from_values(ifid)), meta)
+                br_addr, br_port = br.int_addrs[0].public[0]
+                meta = self._build_meta(host=br_addr, port=br_port)
+                self.send_meta(CtrlPayload(IFIDPayload.from_values(ifid)), meta, (meta.host, meta.port))
 
     def _init_metrics(self):
         super()._init_metrics()
