b81df738a437a8a18fa83df1f2dd3dd3c9e3af63
Tobias Klausmann <klausman@anapaya.net>
2018-07-10 16:42:15 +0200

    Review edits

--- go/cert_srv/main.go
+++ go/cert_srv/main.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/log"
+	"github.com/scionproto/scion/go/lib/sciond"
 )
 
 const (
@@ -34,7 +35,7 @@ const (
 
 var (
 	id         = flag.String("id", "", "Element ID (Required. E.g. 'cs4-ff00:0:2f')")
-	sciondPath = flag.String("sciond", "/run/shm/sciond/default.sock", "SCIOND socket path")
+	sciondPath = flag.String("sciond", sciond.GetDefaultSCIONDPath(nil), "SCIOND socket path")
 	dispPath   = flag.String("dispatcher", "/run/shm/dispatcher/default.sock",
 		"SCION Dispatcher path")
 	confDir  = flag.String("confd", "", "Configuration directory (Required)")
