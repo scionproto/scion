a90c24ef6114eaaecfa25bb03c2558687739a93e
Lukas Vogel <lukedirtwalker@gmail.com>
2018-07-11 15:42:38 +0200

    Use raw strings

--- go/sig/config/config_test.go
+++ go/sig/config/config_test.go
@@ -121,42 +121,42 @@ func TestIPNetUnmarshalJSON(t *testing.T) {
 		{
 			Name:  "Correct Network IPv4 Addr using 32 bits",
 			Error: false,
-			JSON:  "\"192.0.2.255/32\"",
+			JSON:  `"192.0.2.255/32"`,
 		},
 		{
 			Name:  "Correct Network IPv4 Addr using 0 bit",
 			Error: false,
-			JSON:  "\"0.0.0.0/0\"",
+			JSON:  `"0.0.0.0/0"`,
 		},
 		{
 			Name:  "Correct Network IPv4 Addr using 1 bit",
 			Error: false,
-			JSON:  "\"128.0.0.0/1\"",
+			JSON:  `"128.0.0.0/1"`,
 		},
 		{
 			Name:  "Invalid Network IPv4 Addr using 24 bit",
 			Error: true,
-			JSON:  "\"192.0.2.43/24\"",
+			JSON:  `"192.0.2.43/24"`,
 		},
 		{
 			Name:  "Correct Network IPv6 Addr using 128 bits",
 			Error: false,
-			JSON:  "\"2001:0db8:0123:4567:89ab:cdef:1234:5678/128\"",
+			JSON:  `"2001:0db8:0123:4567:89ab:cdef:1234:5678/128"`,
 		},
 		{
 			Name:  "Correct Network IPv6 Addr using 0 bit",
 			Error: false,
-			JSON:  "\"::/0\"",
+			JSON:  `"::/0"`,
 		},
 		{
 			Name:  "Correct Network IPv6 Addr using 1 bit",
 			Error: false,
-			JSON:  "\"8000::/1\"",
+			JSON:  `"8000::/1"`,
 		},
 		{
 			Name:  "Invalid Network IPv6 Addr using 24 bit",
 			Error: true,
-			JSON:  "\"2001::f1/24\"",
+			JSON:  `"2001::f1/24"`,
 		},
 	}
 
