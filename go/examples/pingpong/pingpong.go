42dc12e307163f34cdaa0ae0f0131312d880afd0
Lukas Vogel <lukedirtwalker@gmail.com>
2018-07-12 09:30:17 +0200

    Refactor pingpong example
    * Server can now handle clients with different payloads
    * Improve error handling in server
    * Introduce structs to make methods bound to a type
    * Handle interrupts in client to properly close the connection

--- go/examples/pingpong/pingpong.go
+++ go/examples/pingpong/pingpong.go
@@ -18,13 +18,16 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/gob"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
 
 	"github.com/lucas-clemente/quic-go"
@@ -45,16 +48,18 @@ const (
 	ReqMsg          = "ping!" // ReqMsg and ReplyMsg length need to be the same
 	ReplyMsg        = "pong!"
 	TSLen           = 8
+	ModeServer      = "server"
+	ModeClient      = "client"
 )
 
 var (
 	local  snet.Addr
 	remote snet.Addr
 	file   = flag.String("file", "",
-		"File containing the data to send, optional to test larger data")
+		"File containing the data to send, optional to test larger data (only client)")
 	interactive = flag.Bool("i", false, "Interactive mode")
 	id          = flag.String("id", "pingpong", "Element ID")
-	mode        = flag.String("mode", "client", "Run in client or server mode")
+	mode        = flag.String("mode", ModeClient, "Run in "+ModeClient+" or "+ModeServer+" mode")
 	sciond      = flag.String("sciond", "", "Path to sciond socket")
 	dispatcher  = flag.String("dispatcher", "/run/shm/dispatcher/default.sock",
 		"Path to dispatcher socket")
@@ -83,26 +88,29 @@ func main() {
 		os.Exit(1)
 	}
 	defer log.LogPanicAndExit()
+	initNetwork()
 	switch *mode {
-	case "client":
+	case ModeClient:
 		if remote.Host == nil {
 			LogFatal("Missing remote address")
 		}
 		if remote.L4Port == 0 {
 			LogFatal("Invalid remote port", "remote port", remote.L4Port)
 		}
-		Client()
-	case "server":
-		Server()
+		c := newClient()
+		setSignalHandler(c)
+		c.run()
+	case ModeServer:
+		server{}.run()
 	}
 }
 
 func validateFlags() {
 	flag.Parse()
-	if *mode != "client" && *mode != "server" {
-		LogFatal("Unknown mode, must be either 'client' or 'server'")
+	if *mode != ModeClient && *mode != ModeServer {
+		LogFatal("Unknown mode, must be either '" + ModeClient + "' or '" + ModeServer + "'")
 	}
-	if *mode == "client" && remote.Host == nil {
+	if *mode == ModeClient && remote.Host == nil {
 		LogFatal("Missing remote address")
 	}
 	if local.Host == nil {
@@ -123,160 +131,21 @@ func validateFlags() {
 		LogFatal("Invalid count", "min", 0, "max", MaxPings, "actual", *count)
 	}
 	if *file != "" {
-		var err error
-		fileData, err = ioutil.ReadFile(*file)
-		if err != nil {
-			LogFatal("Could not read data file")
-		}
-	}
-}
-
-// Client dials to a remote SCION address and repeatedly sends ping messages
-// while receiving pong messages. For each successful ping-pong, a message
-// with the round trip time is printed. On errors (including timeouts),
-// the Client exits.
-func Client() {
-	initNetwork()
-
-	// Needs to happen before DialSCION, as it will 'copy' the remote to the connection.
-	// If remote is not in local AS, we need a path!
-	if !remote.IA.Eq(local.IA) {
-		pathEntry := choosePath(*interactive)
-		if pathEntry == nil {
-			LogFatal("No paths available to remote destination")
-		}
-		remote.Path = spath.New(pathEntry.Path.FwdPath)
-		remote.Path.InitOffsets()
-		remote.NextHopHost = pathEntry.HostInfo.Host()
-		remote.NextHopPort = pathEntry.HostInfo.Port
-	}
-
-	// Connect to remote address. Note that currently the SCION library
-	// does not support automatic binding to local addresses, so the local
-	// IP address needs to be supplied explicitly. When supplied a local
-	// port of 0, DialSCION will assign a random free local port.
-	qsess, err := squic.DialSCION(nil, &local, &remote)
-	if err != nil {
-		LogFatal("Unable to dial", "err", err)
-	}
-	defer qsess.Close(nil)
-
-	qstream, err := qsess.OpenStreamSync()
-	if err != nil {
-		LogFatal("quic OpenStream failed", "err", err)
-	}
-	defer qstream.Close()
-	log.Debug("Quic stream opened", "local", &local, "remote", &remote)
-	go Send(qstream)
-	Read(qstream)
-}
-
-func Send(qstream quic.Stream) {
-	reqMsg := requestMsg()
-	reqMsgLen := len(reqMsg)
-	payload := make([]byte, reqMsgLen+TSLen)
-	copy(payload, reqMsg)
-	for i := 0; i < *count || *count == 0; i++ {
-		if i != 0 && *interval != 0 {
-			time.Sleep(*interval)
-		}
-
-		// Send ping message to destination
-		before := time.Now()
-		common.Order.PutUint64(payload[reqMsgLen:], uint64(before.UnixNano()))
-		written, err := qstream.Write(payload)
-		if err != nil {
-			qer := qerr.ToQuicError(err)
-			if qer.ErrorCode == qerr.NetworkIdleTimeout {
-				log.Debug("The connection timed out due to no network activity")
-				break
+		if *mode == ModeClient {
+			var err error
+			fileData, err = ioutil.ReadFile(*file)
+			if err != nil {
+				LogFatal("Could not read data file")
 			}
-			log.Error("Unable to write", "err", err)
-			continue
-		}
-		if written != reqMsgLen+TSLen {
-			log.Error("Wrote incomplete message", "expected", reqMsgLen+TSLen,
-				"actual", written)
-			continue
-		}
-	}
-	// After sending the last ping, set a ReadDeadline on the stream
-	err := qstream.SetReadDeadline(time.Now().Add(*timeout))
-	if err != nil {
-		LogFatal("SetReadDeadline failed", "err", err)
-	}
-}
-
-func Read(qstream quic.Stream) {
-	// Receive pong message (with final timeout)
-	replyMsg := replyMsg()
-	b := make([]byte, len(replyMsg)+TSLen)
-	replyMsgLen := len(replyMsg)
-	for i := 0; i < *count || *count == 0; i++ {
-		read, err := io.ReadFull(qstream, b)
-		after := time.Now()
-		if err != nil {
-			qer := qerr.ToQuicError(err)
-			if qer.ErrorCode == qerr.PeerGoingAway {
-				log.Debug("Quic peer disconnected")
-				break
-			}
-			if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
-				log.Debug("ReadDeadline missed", "err", err)
-				// ReadDeadline is only set after we are done writing
-				// and we don't want to wait indefinitely for the remaining responses
-				break
-			}
-			log.Error("Unable to read", "err", err)
-			continue
-		}
-		if read < replyMsgLen {
-			fmt.Println("Reply too short", "expectedLen", len(replyMsg),
-				"readLen", read)
-			continue
-		}
-		if read < replyMsgLen+TSLen {
-			fmt.Println("Received bad message missing timestamp")
-			continue
-		}
-		if !bytes.Equal(b[:replyMsgLen], replyMsg) {
-			fmt.Println("Received different message than expected", "expectedLen", len(replyMsg),
-				"readLen", read)
-			continue
-		}
-		before := time.Unix(0, int64(common.Order.Uint64(b[replyMsgLen:replyMsgLen+TSLen])))
-		elapsed := after.Sub(before).Round(time.Microsecond)
-		if *verbose {
-			fmt.Printf("[%s]\tReceived %d bytes from %v: seq=%d RTT=%s\n",
-				before.Format(common.TimeFmt), read, &remote, i, elapsed)
 		} else {
-			fmt.Printf("Received %d bytes from %v: seq=%d RTT=%s\n",
-				read, &remote, i, elapsed)
+			log.Info("file argument is ignored for mode " + ModeServer)
 		}
 	}
 }
 
-// Server listens on a SCION address and replies to any ping message.
-// On any error, the server exits.
-func Server() {
-	initNetwork()
-
-	// Listen on SCION address
-	qsock, err := squic.ListenSCION(nil, &local)
-	if err != nil {
-		LogFatal("Unable to listen", "err", err)
-	}
-	log.Debug("Listening", "local", qsock.Addr())
-	for {
-		qsess, err := qsock.Accept()
-		if err != nil {
-			log.Error("Unable to accept quic session", "err", err)
-			// Accept failing means the socket is unusable.
-			break
-		}
-		log.Debug("Quic session accepted", "src", qsess.RemoteAddr())
-		go handleClient(qsess)
-	}
+func LogFatal(msg string, a ...interface{}) {
+	log.Crit(msg, a...)
+	os.Exit(1)
 }
 
 func initNetwork() {
@@ -291,7 +160,207 @@ func initNetwork() {
 	log.Debug("QUIC/SCION successfully initialized")
 }
 
-func handleClient(qsess quic.Session) {
+type message struct {
+	PingPong  string
+	Data      []byte
+	Timestamp int64
+}
+
+func requestMsg() *message {
+	return &message{
+		PingPong: ReqMsg,
+		Data:     fileData,
+	}
+}
+
+func replyMsg(request *message) *message {
+	return &message{
+		ReplyMsg,
+		request.Data,
+		request.Timestamp,
+	}
+}
+
+func (m *message) len() int {
+	return len(m.PingPong) + len(m.Data) + 8
+}
+
+type quicStream struct {
+	qstream quic.Stream
+	encoder *gob.Encoder
+	decoder *gob.Decoder
+}
+
+func newQuicStream(qstream quic.Stream) *quicStream {
+	return &quicStream{
+		qstream,
+		gob.NewEncoder(qstream),
+		gob.NewDecoder(qstream),
+	}
+}
+
+func (qs quicStream) WriteMsg(msg *message) error {
+	return qs.encoder.Encode(msg)
+}
+
+func (qs quicStream) ReadMsg() (*message, error) {
+	var msg message
+	err := qs.decoder.Decode(&msg)
+	if err != nil {
+		return nil, err
+	}
+	return &msg, err
+}
+
+type client struct {
+	*quicStream
+	qsess quic.Session
+}
+
+func newClient() *client {
+	return &client{}
+}
+
+// run dials to a remote SCION address and repeatedly sends ping messages
+// while receiving pong messages. For each successful ping-pong, a message
+// with the round trip time is printed.
+func (c *client) run() {
+	// Needs to happen before DialSCION, as it will 'copy' the remote to the connection.
+	// If remote is not in local AS, we need a path!
+	c.setupPath()
+	defer c.Close()
+
+	// Connect to remote address. Note that currently the SCION library
+	// does not support automatic binding to local addresses, so the local
+	// IP address needs to be supplied explicitly. When supplied a local
+	// port of 0, DialSCION will assign a random free local port.
+	var err error
+	c.qsess, err = squic.DialSCION(nil, &local, &remote)
+	if err != nil {
+		LogFatal("Unable to dial", "err", err)
+	}
+
+	qstream, err := c.qsess.OpenStreamSync()
+	if err != nil {
+		LogFatal("quic OpenStream failed", "err", err)
+	}
+	c.quicStream = newQuicStream(qstream)
+	log.Debug("Quic stream opened", "local", &local, "remote", &remote)
+	go c.send()
+	c.read()
+}
+
+func (c *client) Close() error {
+	var err error
+	if c.qstream != nil {
+		err = c.qstream.Close()
+	}
+	if err == nil && c.qsess != nil {
+		// Note closing the session here is fine since we know that all the traffic went through.
+		// If you are not sure that this is the case you should probably not close the session.
+		// E.g. if you are just sending something to a server and closing the session immediately
+		// it might be that the server does not see the message.
+		// See also: https://github.com/lucas-clemente/quic-go/issues/464
+		err = c.qsess.Close(nil)
+	}
+	return err
+}
+
+func (c client) setupPath() {
+	if !remote.IA.Eq(local.IA) {
+		pathEntry := choosePath(*interactive)
+		if pathEntry == nil {
+			LogFatal("No paths available to remote destination")
+		}
+		remote.Path = spath.New(pathEntry.Path.FwdPath)
+		remote.Path.InitOffsets()
+		remote.NextHopHost = pathEntry.HostInfo.Host()
+		remote.NextHopPort = pathEntry.HostInfo.Port
+	}
+}
+
+func (c client) send() {
+	for i := 0; i < *count || *count == 0; i++ {
+		if i != 0 && *interval != 0 {
+			time.Sleep(*interval)
+		}
+
+		reqMsg := requestMsg()
+		// Send ping message to destination
+		before := time.Now()
+		reqMsg.Timestamp = before.UnixNano()
+		err := c.WriteMsg(reqMsg)
+		if err != nil {
+			log.Error("Unable to write", "err", err)
+			continue
+		}
+	}
+	// After sending the last ping, set a ReadDeadline on the stream
+	err := c.qstream.SetReadDeadline(time.Now().Add(*timeout))
+	if err != nil {
+		LogFatal("SetReadDeadline failed", "err", err)
+	}
+}
+
+func (c client) read() {
+	// Receive pong message (with final timeout)
+	for i := 0; i < *count || *count == 0; i++ {
+		msg, err := c.ReadMsg()
+		after := time.Now()
+		if err != nil {
+			if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
+				log.Debug("ReadDeadline missed", "err", err)
+				// ReadDeadline is only set after we are done writing
+				// and we don't want to wait indefinitely for the remaining responses
+				break
+			}
+			log.Error("Unable to read", "err", err)
+			continue
+		}
+		if msg.PingPong != ReplyMsg {
+			log.Error("Received wrong pingpong", "expected", ReplyMsg, "actual", msg.PingPong)
+		}
+		if !bytes.Equal(msg.Data, fileData) {
+			log.Error("Received different data than sent.")
+			continue
+		}
+		before := time.Unix(0, int64(msg.Timestamp))
+		elapsed := after.Sub(before).Round(time.Microsecond)
+		if *verbose {
+			fmt.Printf("[%s]\tReceived %d bytes from %v: seq=%d RTT=%s\n",
+				before.Format(common.TimeFmt), msg.len(), &remote, i, elapsed)
+		} else {
+			fmt.Printf("Received %d bytes from %v: seq=%d RTT=%s\n",
+				msg.len(), &remote, i, elapsed)
+		}
+	}
+}
+
+type server struct {
+}
+
+// run listens on a SCION address and replies to any ping message.
+// On any error, the server exits.
+func (s server) run() {
+	// Listen on SCION address
+	qsock, err := squic.ListenSCION(nil, &local)
+	if err != nil {
+		LogFatal("Unable to listen", "err", err)
+	}
+	log.Info("Listening", "local", qsock.Addr())
+	for {
+		qsess, err := qsock.Accept()
+		if err != nil {
+			log.Error("Unable to accept quic session", "err", err)
+			// Accept failing means the socket is unusable.
+			break
+		}
+		log.Info("Quic session accepted", "src", qsess.RemoteAddr())
+		go s.handleClient(qsess)
+	}
+}
+
+func (s server) handleClient(qsess quic.Session) {
 	defer qsess.Close(nil)
 	qstream, err := qsess.AcceptStream()
 	if err != nil {
@@ -300,48 +369,36 @@ func handleClient(qsess quic.Session) {
 	}
 	defer qstream.Close()
 
-	reqMsg := requestMsg()
-	b := make([]byte, len(reqMsg)+TSLen)
-	reqMsgLen := len(reqMsg)
+	qs := newQuicStream(qstream)
 	for {
 		// Receive ping message
-		read, err := io.ReadFull(qstream, b)
+		msg, err := qs.ReadMsg()
 		if err != nil {
 			qer := qerr.ToQuicError(err)
-			if qer.ErrorCode == qerr.PeerGoingAway {
-				log.Debug("Quic peer disconnected")
+			// We expect qerr.PeerGoingAway or io.EOF as normal termination conditions.
+			if qer.ErrorCode == qerr.PeerGoingAway ||
+				err == io.EOF {
+				log.Info("Quic peer disconnected", "err", err)
+				break
+			}
+			// NetworkIdleTimeOut if the peer exits ungracefully we will not be notified,
+			// and the session will time out.
+			if qer.ErrorCode == qerr.NetworkIdleTimeout {
+				log.Error("Quic connection timed out", "err", err)
 				break
 			}
 			log.Error("Unable to read", "err", err)
 			break
 		}
-		if !bytes.Equal(b[:reqMsgLen], reqMsg) {
-			fmt.Println("Received different message than expected",
-				"expectedLen", reqMsgLen, "readLen", read)
-		}
-		// extract timestamp
-		ts := common.Order.Uint64(b[reqMsgLen:])
 
 		// Send pong message
-		replyMsg := replyMsg()
-		replyMsgLen := len(replyMsg)
-		copy(b[:replyMsgLen], replyMsg)
-		common.Order.PutUint64(b[replyMsgLen:], ts)
-		written, err := qstream.Write(b[:replyMsgLen+TSLen])
+		replyMsg := replyMsg(msg)
+		err = qs.WriteMsg(replyMsg)
 		if err != nil {
 			log.Error("Unable to write", "err", err)
-			continue
-		} else if written != replyMsgLen+TSLen {
-			log.Error("Wrote incomplete message",
-				"expected", replyMsgLen+TSLen, "actual", written)
-			continue
+			break
 		}
 	}
-}
-
-func LogFatal(msg string, a ...interface{}) {
-	log.Crit(msg, a...)
-	os.Exit(1)
 }
 
 func choosePath(interactive bool) *sd.PathReplyEntry {
@@ -378,16 +435,12 @@ func choosePath(interactive bool) *sd.PathReplyEntry {
 	return paths[pathIndex]
 }
 
-func requestMsg() []byte {
-	return msg(ReqMsg)
-}
-
-func replyMsg() []byte {
-	return msg(ReplyMsg)
-}
-
-func msg(prefix string) []byte {
-	res := make([]byte, 0, len(prefix)+len(fileData))
-	res = append(res, []byte(prefix)...)
-	return append(res, fileData...)
+func setSignalHandler(closer io.Closer) {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		closer.Close()
+		os.Exit(1)
+	}()
 }
