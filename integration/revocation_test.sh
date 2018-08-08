ae6ee61cd9013ce2914b6aff58ae2b75f735dd83
Sergio Gonzalez Monroy <sgmonroy@gmail.com>
2018-08-08 08:55:06 +0200

    Increase number of retries (side effect of change in topology)

--- integration/revocation_test.sh
+++ integration/revocation_test.sh
@@ -40,5 +40,5 @@ fi
 sleep ${SLEEP}s
 # Do another round of e2e test with retries
 log "Testing connectivity between all the hosts (with retries)."
-run Revocation "python/integration/end2end_test.py -l ERROR --retries 3"
+run Revocation "python/integration/end2end_test.py -l ERROR --retries 5"
 exit $?
