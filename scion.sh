14ff8638af5bb3aa6697b5462ff160f6dc44e264
Lukas Bischofberger <bischofberger@anapaya.net>
2018-10-11 11:00:59 +0200

    change uid/gid

--- scion.sh
+++ scion.sh
@@ -12,8 +12,8 @@ cmd_topology() {
     echo "Shutting down: $(./scion.sh stop)"
     supervisor/supervisor.sh shutdown
     mkdir -p logs traces
-    [ -e gen ] && !(is_dir_empty gen) && rm -r gen/*
-    [ -e gen-cache ] && !(is_dir_empty gen-cache) && rm -r gen-cache/*
+    [ gen/* == 'gen/*' ] || rm -r gen/*
+    [ gen-cache/* == 'gen-cache/*' ] || rm -r gen-cache/*
     mkdir -p gen-cache
     if [ "$1" = "zkclean" ]; then
         shift
@@ -195,14 +195,6 @@ is_docker() {
 
 is_supervisor() {
    [ -f gen/dispatcher/supervisord.conf ]
-}
-
-is_dir_empty() {
-    test -e "$1/"* 2>/dev/null
-    case $? in
-        1)   return 0 ;;
-        *)   return 1 ;;
-    esac
 }
 
 cmd_test(){
