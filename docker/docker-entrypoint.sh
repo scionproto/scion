044984719364a4ab7df6113503ce3d31fc0e4e1b
Lukas Bischofberger <bischofberger@anapaya.net>
2018-10-12 11:03:33 +0200

    comments and su-exec

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 usermod -u ${SCION_UID:?} scion
+groupmod -g ${SCION_GID:?} scion
 groupmod -g ${DOCKER_GID:?} docker
 
-SU_EXEC_USERSPEC=scion /sbin/su-exec "$@"
+SU_EXEC_USERSPEC=scion /sbin/su-exec bash -l "$@"
