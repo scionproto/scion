e160a2f076c344302b5a63649d1a9e639fc9228a
Oncilla <domi.roos@gmail.com>
2018-10-23 17:40:07 +0200

    lib: Consistent setup (#2014)
    Synchronize init order between services.
    Log service startup message (fixes #2009)

--- go/lib/env/logging.go
+++ go/lib/env/logging.go
@@ -1,4 +1,4 @@
-// Copyright 2018 ETH Zurich
+// Copyright 2018 ETH Zurich, Anapaya Systems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -87,4 +87,9 @@ func setupFileLogging(cfg *Logging) error {
 		)
 	}
 	return nil
+}
+
+// LogSvcStarted should be called by services as soon as logging is initialized.
+func LogSvcStarted(svcType, elemId string) {
+	log.Info("=====================> Service started", "svc", svcType, "id", elemId)
 }
