aec3816613e18e79eaf9cdfd7ba0cf04099ded05
Lukas Bischofberger <bischofberger@anapaya.net>
2018-10-30 08:57:24 +0100

    Add go e2e test

---
+++ go/integration/end2end/end2end.go
@@ -0,0 +1,146 @@
+// Copyright 2018 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	cmn "github.com/scionproto/scion/go/integration"
+	"github.com/scionproto/scion/go/lib/common"
+	integration "github.com/scionproto/scion/go/lib/integration"
+	"github.com/scionproto/scion/go/lib/log"
+	"github.com/scionproto/scion/go/lib/snet"
+)
+
+var _ cmn.Server = (*e2eServer)(nil)
+
+type e2eServer struct {
+	conn snet.Conn
+}
+
+var _ cmn.Client = (*e2eClient)(nil)
+
+type e2eClient struct {
+	conn snet.Conn
+}
+
+var (
+	ping = "ping:"
+	pong = "pong:"
+)
+
+func main() {
+	switch cmn.Mode {
+	case cmn.ModeClient:
+		if cmn.Remote.Host.L4 == nil {
+			cmn.LogFatal("Missing remote port")
+		}
+		if cmn.Remote.Host.L4.Port() == 0 {
+			cmn.LogFatal("Invalid remote port", "remote port", cmn.Remote.Host.L4.Port())
+		}
+		e2eClient{}.Run()
+	case cmn.ModeServer:
+		if cmn.Local.Host.L4 == nil {
+			cmn.LogFatal("Missing local port")
+		}
+		if cmn.Local.Host.L4.Port() == 0 {
+			cmn.LogFatal("Invalid local port", "local port", cmn.Local.Host.L4.Port())
+		}
+		e2eServer{}.Run()
+	}
+}
+
+func (s e2eServer) Run() {
+	conn, err := snet.ListenSCION("udp4", &cmn.Local)
+	if err != nil {
+		cmn.LogFatal("Error listening", "err", err)
+	}
+	if len(os.Getenv(integration.GoIntegrationEnv)) > 0 {
+		// Needed for integration test ready signal.
+		fmt.Printf("%s%s\n", integration.ReadySignal, cmn.Local.IA)
+	}
+	log.Debug("Listening", "local", conn.LocalAddr())
+	// Receive ping message
+	b := make(common.RawBytes, 1024)
+	for {
+		pktLen, addr, err := conn.ReadFromSCION(b)
+		if err != nil {
+			log.Error("Error reading packet", "err", err)
+			continue
+		}
+		if !strings.HasPrefix(string(b[:pktLen]), ping+cmn.Local.IA.String()) {
+			log.Error("Received unexpected data", "data", b[:pktLen])
+			break
+		}
+		log.Debug(fmt.Sprintf("Ping received from %s,%s, sending pong.", addr.IA, addr.Host))
+		// Send pong
+		reply := []byte(pong + cmn.Local.IA.String() + addr.IA.String())
+		_, err = conn.WriteToSCION(reply, addr)
+		if err != nil {
+			cmn.LogFatal("Unable to send reply", "err", err)
+		}
+		log.Debug(fmt.Sprintf("Sent pong to %s", addr.Desc()))
+	}
+}
+
+func (c e2eClient) Run() {
+	var err error
+	c.conn, err = snet.DialSCION("udp4", &cmn.Local, &cmn.Remote)
+	if err != nil {
+		cmn.LogFatal("Unable to dial", "err", err)
+	}
+	log.Debug("Send on", "local", c.conn.LocalAddr())
+	for i := 0; i <= cmn.Retries; i++ {
+		// Send ping
+		if err = c.ping(); err != nil {
+			log.Error("Could not send packet", "err", err)
+		}
+		// Receive pong
+		if err = c.pong(); err != nil {
+			log.Error("Error receiving pong", "err", err)
+		} else {
+			break
+		}
+		if cmn.Retries > i {
+			log.Debug("Retrying...")
+		}
+	}
+}
+
+func (c e2eClient) ping() error {
+	c.conn.SetWriteDeadline(time.Now().Add(1 * time.Second))
+	b := []byte(ping + cmn.Remote.IA.String())
+	_, err := c.conn.Write(b)
+	return err
+}
+
+func (c e2eClient) pong() error {
+	c.conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	reply := make([]byte, 1024)
+	pktLen, err := c.conn.Read(reply)
+	if err != nil {
+		return err
+	}
+	expected := pong + cmn.Remote.IA.String() + cmn.Local.IA.String()
+	if string(reply[:pktLen]) != expected {
+		return common.NewBasicError("Received unexpected data", nil, "data",
+			string(reply[:pktLen]), "expected", expected)
+	}
+	log.Debug(fmt.Sprintf("Received pong from %s", cmn.Remote.IA))
+	return nil
+}
