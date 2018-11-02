45c621e6d315dc4402541ebe9b928466f050a4bd
Martin Sustrik <sustrik@anapaya.com>
2018-11-02 09:22:47 +0100

    Enforce MaxPaths in SCIOND
    Fixes #2025

--- go/sciond/internal/fetcher/fetcher.go
+++ go/sciond/internal/fetcher/fetcher.go
@@ -369,7 +369,15 @@ func (f *fetcherHandler) buildPathsFromDB(ctx context.Context,
 		}
 	}
 	paths := buildPathsToAllDsts(req, ups, cores, downs)
-	return f.filterRevokedPaths(ctx, paths)
+	filteredPaths, err := f.filterRevokedPaths(ctx, paths)
+	if err != nil {
+		return nil, err
+	}
+	numpaths := len(filteredPaths)
+	if int(req.MaxPaths) < numpaths {
+		numpaths = int(req.MaxPaths)
+	}
+	return filteredPaths[0:numpaths], nil
 }
 
 func (f *Fetcher) getSegmentsFromDB(ctx context.Context, startsAt,
