f8375d2edd7c88db2b1d2b299178e3e9bfd770bc
Martin Sustrik <sustrik@anapaya.net>
2018-11-08 14:09:30 +0100

    Path test in showpaths tool
    Fixes #2039

--- go/tools/showpaths/paths.go
+++ go/tools/showpaths/paths.go
@@ -18,23 +18,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"time"
 
 	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/log"
 	"github.com/scionproto/scion/go/lib/sciond"
+	"github.com/scionproto/scion/go/lib/scmp"
+	"github.com/scionproto/scion/go/lib/snet"
+	"github.com/scionproto/scion/go/lib/sock/reliable"
+	"github.com/scionproto/scion/go/lib/spath"
 )
 
 var (
 	dstIAStr     = flag.String("dstIA", "", "Destination IA address: ISD-AS")
 	srcIAStr     = flag.String("srcIA", "", "Source IA address: ISD-AS")
 	sciondPath   = flag.String("sciond", "", "SCIOND socket path")
-	timeout      = flag.Duration("timeout", 2*time.Second, "SCIOND connection timeout")
+	timeout      = flag.Duration("timeout", 5*time.Second, "Timeout in seconds")
 	maxPaths     = flag.Int("maxpaths", 10, "Maximum number of paths")
 	sciondFromIA = flag.Bool("sciondFromIA", false, "SCIOND socket path from IA address:ISD-AS")
 	expiration   = flag.Bool("expiration", false, "Show path expiration timestamps")
 	refresh      = flag.Bool("refresh", false, "Set refresh flag for SCIOND path request")
+	status       = flag.Bool("p", false, "Probe the paths and print out the statuses")
+	Local        snet.Addr
 )
 
 var (
@@ -45,9 +53,12 @@ var (
 func main() {
 	var err error
 
+	flag.Var((*snet.Addr)(&Local), "local", "Local address to use for health checks")
+
 	log.AddLogConsFlags()
 	validateFlags()
 
+	// Get paths from SCIOND.
 	sd := sciond.NewService(*sciondPath)
 	sdConn, err := sd.ConnectTimeout(*timeout)
 	if err != nil {
@@ -61,15 +72,102 @@ func main() {
 	if reply.ErrorCode != sciond.ErrorOk {
 		LogFatal("SCIOND unable to retrieve paths: %s\n", reply.ErrorCode)
 	}
+
+	pathStatuses := make(map[string]string)
+	if *status {
+		// Check whether paths are alive. This is done by sending a packet
+		// with invalid address via the path. The border router at the destination
+		// is going to reply with SCMP error. Receiving the error means that
+		// the path is alive.
+		if err = snet.Init(srcIA, "", reliable.DefaultDispPath); err != nil {
+			LogFatal("Initializing SNET: %v\n", err)
+		}
+		snetConn, err := snet.ListenSCION("udp4", &Local)
+		if err != nil {
+			LogFatal("Listening failed: %v\n", err)
+		}
+		scionConn := snetConn.(*snet.SCIONConn)
+		err = scionConn.SetReadDeadline(time.Now().Add(*timeout))
+		if err != nil {
+			LogFatal("Cannot set deadline: %v\n", err)
+		}
+		for _, path := range reply.Entries {
+			sPath := spath.New(path.Path.FwdPath)
+			if err = sPath.InitOffsets(); err != nil {
+				LogFatal("Unable to initialize path: %v\n", err)
+			}
+			nextHop, err := path.HostInfo.Overlay()
+			if err != nil {
+				LogFatal("Cannot get overlay info: %v\n", err)
+			}
+			addr := &snet.Addr{
+				IA: dstIA,
+				Host: &addr.AppAddr{
+					L3: addr.HostSVCFromString("NONE"),
+					L4: addr.NewL4UDPInfo(0),
+				},
+				NextHop: nextHop,
+				Path:    sPath,
+			}
+			fmt.Println("Sending test packet to: ", addr)
+			_, err = scionConn.WriteTo([]byte{}, addr)
+			if err != nil {
+				LogFatal("Cannot send packet: %v\n", err)
+			}
+			pathStatuses[string(path.Path.FwdPath)] = "Timeout"
+		}
+		for i := len(pathStatuses); i > 0; i-- {
+			b := make([]byte, 1500, 1500)
+			_, addr, err := scionConn.ReadFromSCION(b)
+			if addr == nil {
+				if basicErr, ok := err.(common.BasicError); ok {
+					if netErr, ok := basicErr.Err.(net.Error); ok && netErr.Timeout() {
+						// Timeout expired before all replies were received.
+						break
+					}
+				}
+				if err != nil {
+					LogFatal("Cannot read packet: %v\n", err)
+				}
+				LogFatal("Packet without an address received: %v\n", err)
+			}
+			path := string(addr.Path.Raw)
+			if pathStatuses[path] != "Timeout" {
+				// Two replies are received for the same path.
+				pathStatuses[path] = "Unknown"
+				continue
+			}
+			if err == nil {
+				// We've got an actual reply instead of SCMP error. This should not happen.
+				pathStatuses[path] = "Unknown"
+				continue
+			}
+			if opErr, ok := err.(*snet.OpError); ok {
+				fmt.Println(opErr)
+				if opErr.SCMP().Class == scmp.C_Routing && opErr.SCMP().Type == scmp.T_R_BadHost {
+					// Expected outcome. The peer complains about SvcNone being an invalid address.
+					pathStatuses[path] = "Alive"
+					continue
+				}
+			}
+			// All other errors are just reported alongside the path.
+			pathStatuses[path] = err.Error()
+		}
+	}
+
+	// Print out the results.
 	fmt.Println("Available paths to", dstIA)
 	i := 0
 	for _, path := range reply.Entries {
+		fmt.Printf("[%2d] %s", i, path.Path.String())
 		if *expiration {
-			fmt.Printf("[%2d] %s Expires: %s (%s)\n", i, path.Path.String(), path.Path.Expiry(),
+			fmt.Printf(" Expires: %s (%s)", path.Path.Expiry(),
 				time.Until(path.Path.Expiry()).Truncate(time.Second))
-		} else {
-			fmt.Printf("[%2d] %s\n", i, path.Path.String())
 		}
+		if *status {
+			fmt.Printf(" Status: %s", pathStatuses[string(path.Path.FwdPath)])
+		}
+		fmt.Printf("\n")
 		i++
 	}
 }
