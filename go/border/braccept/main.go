370fa419820d5a9f2976c4c278baa5eafb7b6fd1
Sergio Gonzalez Monroy <sgmonroy@gmail.com>
2018-11-12 09:28:17 +0100

    Comments r4

--- go/border/braccept/main.go
+++ go/border/braccept/main.go
@@ -8,13 +8,16 @@ import (
 	"hash"
 	"net"
 	"os"
+	"reflect"
 	"strings"
 	"time"
 
+	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"github.com/syndtr/gocapability/capability"
 	"golang.org/x/crypto/pbkdf2"
 
+	"github.com/scionproto/scion/go/border/braccept/tpkt"
 	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/keyconf"
 	"github.com/scionproto/scion/go/lib/scrypto"
@@ -78,6 +81,148 @@ func GenerateKeys(fn string) error {
 	return err
 }
 
+func main() {
+	if err := checkFlags(); err != nil {
+		fmt.Printf("%s\n", err)
+		flag.Usage()
+		os.Exit(-1)
+	}
+	if err := ParseDevInfo(devInfoFilePath); err != nil {
+		fatal("%s\n", err)
+	}
+	if err := GenerateKeys(keysDirPath); err != nil {
+		fatal("%s\n", err)
+	}
+	timerIdx := len(devList)
+	cases := make([]reflect.SelectCase, timerIdx+1)
+	for i, ifi := range devList {
+		var err error
+		ifi.handle, err = pcap.OpenLive(ifi.hostDev, snapshot_len, promiscuous, pcap.BlockForever)
+		if err != nil {
+			fatal("%s\n", err)
+		}
+		packetSource := gopacket.NewPacketSource(ifi.handle, ifi.handle.LinkType())
+		ch := packetSource.Packets()
+		cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ch)}
+		defer ifi.handle.Close()
+	}
+	// Now that everything is set up, drop CAP_NET_ADMIN
+	caps, err := capability.NewPid(0)
+	if err != nil {
+		fatal("Error retrieving capabilities: %s", err)
+	}
+	caps.Clear(capability.CAPS)
+	caps.Apply(capability.CAPS)
+
+	brTests, ok := Tests[borderID]
+	if !ok {
+		fatal("Wrong Border Router ID %s", borderID)
+	}
+	fmt.Printf("Acceptance tests for %s:\n", borderID)
+	var failures int
+	if testIdx != -1 {
+		brTests = brTests[testIdx : testIdx+1]
+	}
+	for _, t := range brTests {
+		if !doTest(t, cases) {
+			failures += 1
+		}
+	}
+	os.Exit(failures)
+}
+
+// doTest just runs a test, which involved generating the packet, sending it in the specified
+// interface, then comparing any packets coming from the border router against the expected
+// packets from the test.
+// It return true if the test was successful, ie. all expected packets and no others were received,
+// otherwise it returns false.
+func doTest(t *BRTest, cases []reflect.SelectCase) bool {
+	devInfo, ok := devByName[t.In.GetDev()]
+	if !ok {
+		fmt.Errorf("No device information for: %s", t.In.GetDev())
+	}
+	raw, err := t.In.Pack(devInfo.mac, hashMac)
+	if err != nil {
+		fatal("%s\n", err)
+	}
+	err = devInfo.handle.WritePacketData(raw)
+	if err != nil {
+		fatal("%s\n", err)
+	}
+	var pass bool
+	if err := checkRecvPkts(t, cases); err != nil {
+		fmt.Println(err)
+	} else {
+		pass = true
+	}
+	fmt.Println(t.Summary(pass))
+	return pass
+}
+
+// checkRecvPkts compares packets received in any interface against the expected packets
+// from the test, checking that they have been received on the expected interface.
+func checkRecvPkts(t *BRTest, cases []reflect.SelectCase) error {
+	timerIdx := len(devList)
+	timerCh := time.After(timeout)
+	cases[timerIdx] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(timerCh)}
+
+	expPkts := make([]tpkt.Matcher, len(t.Out))
+	for i, _ := range t.Out {
+		expPkts[i] = t.Out[i]
+	}
+	for {
+		idx, pktV, ok := reflect.Select(cases)
+		if !ok {
+			cases[idx].Chan = reflect.ValueOf(nil)
+			return fmt.Errorf("Unexpected interface %s/%s closed:\n",
+				devList[idx].hostDev, devList[idx].contDev)
+		}
+		if idx == timerIdx {
+			// Timeout receiving packets
+			if len(expPkts) > 0 {
+				return fmt.Errorf("Timeout receiving packets\n")
+			}
+			return nil
+		}
+		// Packet received
+		pkt := pktV.Interface().(gopacket.Packet)
+		i, err := checkPkt(expPkts, idx, pkt)
+		if err != nil {
+			if len(expPkts) > 0 {
+				fmt.Println(err)
+				continue
+			}
+			// Packet received when no packet is expected
+			return err
+		}
+		expPkts[i] = expPkts[len(expPkts)-1]
+		expPkts = expPkts[:len(expPkts)-1]
+	}
+	return nil
+}
+
+// checkPkt compare a given packet against all the possible expected packets,
+// It return the index of the expected packet matched or an error with a pretty-print
+// packet dump of the unmatched packet.
+func checkPkt(expPkts []tpkt.Matcher, devIdx int, pkt gopacket.Packet) (int, error) {
+	for i, _ := range expPkts {
+		if err := expPkts[i].Match(devList[devIdx].contDev, pkt); err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// Expected packet matched!
+		return i, nil
+	}
+	payload := pkt.ApplicationLayer().LayerContents()
+	scnPkt := gopacket.NewPacket(payload, tpkt.LayerTypeScion, gopacket.NoCopy)
+	if scn := scnPkt.Layer(tpkt.LayerTypeScion).(*tpkt.ScionLayer); scn != nil {
+		scn.Path.Parse(scn.Path.Raw)
+		scn.Path.Raw = nil
+	}
+	return 0, fmt.Errorf("\nUnexpected pkt on interface %s\n%v\n%v",
+		devList[devIdx].contDev, pkt, scnPkt)
+}
+
 func ParseDevInfo(fn string) error {
 	f, err := os.Open(fn)
 	if err != nil {
@@ -111,80 +256,6 @@ func checkFlags() error {
 		return fmt.Errorf("ERROR: Missing devInfoFilePath flag")
 	}
 	return nil
-}
-
-func main() {
-	if err := checkFlags(); err != nil {
-		fmt.Printf("%s\n", err)
-		flag.Usage()
-		os.Exit(-1)
-	}
-	if err := ParseDevInfo(devInfoFilePath); err != nil {
-		fatal("%s\n", err)
-	}
-	if err := GenerateKeys(keysDirPath); err != nil {
-		fatal("%s\n", err)
-	}
-	for _, ifi := range devList {
-		var err error
-		ifi.handle, err = pcap.OpenLive(ifi.hostDev, snapshot_len, promiscuous, pcap.BlockForever)
-		if err != nil {
-			fatal("%s\n", err)
-		}
-		defer ifi.handle.Close()
-	}
-	// Now that everything is set up, drop CAP_NET_ADMIN
-	caps, err := capability.NewPid(0)
-	if err != nil {
-		fatal("Error retrieving capabilities: %s", err)
-	}
-	caps.Clear(capability.CAPS)
-	caps.Apply(capability.CAPS)
-
-	brTests, ok := Tests[borderID]
-	if !ok {
-		fatal("Wrong Border Router ID %s", borderID)
-	}
-	fmt.Printf("Acceptance tests for %s:\n", borderID)
-	var failures int
-	if testIdx != -1 {
-		brTests = brTests[testIdx : testIdx+1]
-	}
-	for _, t := range brTests {
-		if !doTest(t) {
-			failures += 1
-		}
-	}
-	os.Exit(failures)
-}
-
-// doTest just runs a test, which involved generating the packet, sending it in the specified
-// interface, then comparing any packets coming from the border router against the expected
-// packets from the test.
-// It return true if the test was successful, ie. all expected packets and no others were received,
-// otherwise it returns false.
-func doTest(t *BRTest) bool {
-	t.In.Setup()
-	devInfo, ok := devByName[t.In.GetDev()]
-	if !ok {
-		fmt.Errorf("No device information for: %s", t.In.GetDev())
-	}
-	raw, err := t.In.Pack(devInfo.mac, hashMac)
-	if err != nil {
-		fatal("%s\n", err)
-	}
-	err = devInfo.handle.WritePacketData(raw)
-	if err != nil {
-		fatal("%s\n", err)
-	}
-	var pass bool
-	if err := checkRecvPkts(t); err != nil {
-		fmt.Println(err)
-	} else {
-		pass = true
-	}
-	fmt.Println(t.Summary(pass))
-	return pass
 }
 
 func fatal(msg string, a ...interface{}) {
