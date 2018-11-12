9f20f945d879fcf78c8eb93874427b1e3baeaeac
Lukas Vogel <lukedirtwalker@gmail.com>
2018-11-12 16:19:55 +0100

    Update Go prometheus client lib to latest version
    Also update the other Go prometheus libs.

--- go/lib/ringbuf/metrics.go
+++ go/lib/ringbuf/metrics.go
@@ -1,4 +1,5 @@
 // Copyright 2017 ETH Zurich
+// Copyright 2018 ETH Zurich, Anapaya Systems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -63,8 +64,8 @@ type metrics struct {
 	readCalls     prometheus.Counter
 	writesBlocked prometheus.Counter
 	readsBlocked  prometheus.Counter
-	writeEntries  prometheus.Histogram
-	readEntries   prometheus.Histogram
+	writeEntries  prometheus.Observer
+	readEntries   prometheus.Observer
 	maxEntries    prometheus.Gauge
 	usedEntries   prometheus.Gauge
 }
