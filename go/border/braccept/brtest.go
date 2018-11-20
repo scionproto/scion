25ca405dcdccf0e51d4b33e4f15e1dc3f8beeb77
Sergio Gonzalez Monroy <sgmonroy@gmail.com>
2018-11-20 10:42:41 +0100

    Comments r8

--- go/border/braccept/brtest.go
+++ go/border/braccept/brtest.go
@@ -1,9 +1,25 @@
+// Copyright 2018 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/kormat/fmt15"
+	"github.com/mattn/go-isatty"
 
 	"github.com/scionproto/scion/go/border/braccept/tpkt"
 )
@@ -12,10 +28,10 @@ import (
 type BRTest struct {
 	Desc string
 	// In is the packet being sent to the border router
-	In tpkt.Packer
+	In *tpkt.Pkt
 	// Out is the list of expected packets. No expected packets means that the packet
 	// should be dropped by the border router, and nothing is expected.
-	Out []tpkt.Matcher
+	Out []*tpkt.ExpPkt
 }
 
 func (t *BRTest) Summary(testPass bool) string {
@@ -25,11 +41,10 @@ func (t *BRTest) Summary(testPass bool) string {
 	} else {
 		result = fail()
 	}
-	return fmt.Sprintf("Test %s: %s\n%s", t.Desc, result, t.In)
+	return fmt.Sprintf("Test %s: %s\n", t.Desc, result)
 }
 
 const (
-	//	defColorFmt = "\x1b[%dm%s\x1b[0m"
 	passUni = "\u2714"
 	failUni = "\u2715"
 	green   = 32
@@ -37,11 +52,15 @@ const (
 )
 
 func pass() string {
-	//	return fmt.Sprintf(defColorFmt, green, passUni)
-	return fmt15.ColorStr(passUni, green)
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		return fmt15.ColorStr(passUni, green)
+	}
+	return "PASS"
 }
 
 func fail() string {
-	//	return fmt.Sprintf(defColorFmt, red, failUni)
-	return fmt15.ColorStr(failUni, red)
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		return fmt15.ColorStr(failUni, red)
+	}
+	return "FAIL"
 }
