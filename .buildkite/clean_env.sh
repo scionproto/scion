5f3d20c5c74a5f916154d8eb48a0747077d28fe6
Lukas Bischofberger <me@worx.li>
2018-11-21 13:37:10 +0100

    Buildkite: Cleanup env (#2146)
    Add a cleanup step to the Buildkite pipeline which removes containers, networks and volumes.
    Also: Fix the service name of SCIOND. This caused the acceptance tests to fail.

---
+++ .buildkite/clean_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cntrs="$(docker ps -aq)"
+[ -n "$cntrs" ] && { echo "Remove leftover containers: $cntrs"; docker rm -f "$cntrs"; }
+
+echo "Remove leftover networks"
+docker network prune -f
+echo "Remove leftover volumes"
+docker volume prune -f
--- .buildkite/common.sh
+++ .buildkite/common.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 if [ "$BUILDKITE_PULL_REQUEST" == "false" ]; then
@@ -13,3 +15,4 @@ echo "env:"
 echo "  SCION_MOUNT: /tmp/scion_out.$BUILDKITE_BUILD_NUMBER"
 echo "  SCION_CNTR: scion_ci_$BUILDKITE_BUILD_NUMBER"
 echo "  ARTIFACTS: buildkite.${BUILDKITE_ORGANIZATION_SLUG}.${TARGET}.${BUILD}"
+echo "  BASE: $BASE"
