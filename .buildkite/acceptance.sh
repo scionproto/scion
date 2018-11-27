6e51fd069b0da42c00d922f15fdc51425a6d77f0
Lukas Vogel <lukedirtwalker@gmail.com>
2018-11-27 16:02:01 +0100

    Fix build after test logging changes (#2172)
    #2161 Broke the CI build, this commit fixes it.

--- .buildkite/acceptance.sh
+++ .buildkite/acceptance.sh
@@ -3,7 +3,7 @@
 for test in ./acceptance/*_acceptance; do
     echo "- label: ${test}"
     echo "  command:"
-    echo "  - rm -f $SCION_MOUNT/logs/*"
+    echo "  - find \$SCION_MOUNT/logs -mindepth 1 -maxdepth 1 -not -path '*/\.*' -exec rm -r {} +"
     echo "  - ./docker.sh exec ${test}/test setup"
     echo "  - ./docker.sh exec ${test}/test run"
     echo "  - ./docker.sh exec ${test}/test teardown"
