96c168a79583b66e65ce7b686597b05f724e4872
Lukas Bischofberger <bischofberger@anapaya.net>
2018-12-14 09:45:04 +0100

    comments luke r1

--- acceptance/common.sh
+++ acceptance/common.sh
@@ -1,32 +1,56 @@
 # Acceptance tests common functions
 
+#######################################
+# Converts ISD-AS representation to its file format
+# Arguments:
+#   ISD-AS
+#######################################
 ia_file() {
     echo ${1:?} | sed -e "s/:/_/g"
 }
 
+#######################################
+# Converts ISD-AS representation to AS file format, i.e. removes the ISD
+# Arguments:
+#   ISD-AS
+#######################################
 as_file() {
     ia_file ${1:?} | cut -d '-' -f 2
 }
 
+#######################################
+# Collects metrics from services that can be found in the topology file
+# Metrics are saved in METRICS_DIR/
+# Arguments:
+#   Topology path
+#   METRICS_DIR
+#######################################
 collect_metrics() {
-    echo "Reading topology: ${1:?}"
+    TOPOLOGY=${1:?}
+    echo "Reading topology: $TOPOLOGY"
     METRICS_DIR=${2:?}
     echo "Saving metrics in $METRICS_DIR"
     mkdir -p "$METRICS_DIR"
 
-    local elems=$(jq '.BorderRouters | to_entries[] | .key' $1)
-    for elem in $elems; do
-        local ip="$(jq .BorderRouters[$elem].InternalAddrs.IPv4.PublicOverlay.Addr $1 | sed -e 's/^"//' -e 's/"$//')"
-        curl "$ip:30442/metrics" -o "$METRICS_DIR/$(remove_quotes $elem)" -s -S
-    done
-
-    local elems=$(jq '.SIG | to_entries[] | .key' $1)
-    for elem in $elems; do
-        local ip="$(jq .SIG[$elem].Addrs.IPv4.Public.Addr $1 | sed -e 's/^"//' -e 's/"$//')"
-        curl "$ip:30456/metrics" -o "$METRICS_DIR/$(remove_quotes $elem)" -s -S
-    done
+    collect_elem_metrics 'BorderRouters' '30442' 'InternalAddrs.IPv4.PublicOverlay.Addr'
+    collect_elem_metrics 'SIG' '30456'
+    collect_elem_metrics 'BeaconService' '30452'
+    collect_elem_metrics 'CertificateService' '30454'
+    collect_elem_metrics 'PathService' '30453'
 }
 
-remove_quotes() {
-    echo "${1:?}" | sed -e 's/^"//' -e 's/"$//'
+#######################################
+# Collect metrics for one element type
+# Arguments:
+#   Service Type
+#   Port
+#   IP Addr Key (optional)
+#######################################
+collect_elem_metrics() {
+    local elems=$(jq -r ".${1:?} | keys | .[]" $TOPOLOGY)
+    local addr_key=${3:-Addrs.IPv4.Public.Addr}
+    for elem in $elems; do
+        local ip="$(jq -r .$1[\"$elem\"].$addr_key $TOPOLOGY)"
+        curl "$ip:${2:?}/metrics" -o "$METRICS_DIR/$elem" -s -S --connect-timeout 2 || true
+    done
 }
