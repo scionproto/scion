ee5764f97ce280bc6c3f128535f15b0357abe8de
Lukas Bischofberger <bischofberger@anapaya.net>
2018-12-21 15:00:37 +0100

    Reduce CtxTimeout

--- go/lib/integration/integration.go
+++ go/lib/integration/integration.go
@@ -39,9 +39,9 @@ const (
 	// StartServerTimeout is the timeout for starting a server.
 	StartServerTimeout = 4 * time.Second
 	// DefaultRunTimeout is the timeout when running a server or a client.
-	DefaultRunTimeout = 8 * time.Second
+	DefaultRunTimeout = 10 * time.Second
 	// CtxTimeout is the timeout a context waits before being killed
-	CtxTimeout = 6 * time.Second
+	CtxTimeout = 2 * time.Second
 	// RetryTimeout is the timeout between different attempts
 	RetryTimeout = time.Second / 2
 )
