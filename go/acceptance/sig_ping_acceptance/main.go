b527e6d71c0edb55c276531ff85d233007232e52
Martin Sustrik <sustrik@anapaya.net>
2019-01-07 09:39:19 +0100

    Add -fail parameter to sig_ping_acceptance utility
    This allows us to be more specific about which errors
    count as "pings not going through" without having to
    modify each acceptance test that uses this utility.

--- go/acceptance/sig_ping_acceptance/main.go
+++ go/acceptance/sig_ping_acceptance/main.go
@@ -33,6 +33,7 @@ var (
 	name     = "sig_ping_acceptance"
 	cmd      = "ping"
 	attempts = flag.Int("attempts", 5, "Number of ping attempts.")
+	fail     = flag.Bool("fail", false, "Succeed if the pings don't make it through.")
 )
 
 func main() {
@@ -58,9 +59,14 @@ func realMain() int {
 	in := integration.NewBinaryIntegration(name, integration.WrapperCmd, args, nil)
 	err := integration.RunUnaryTests(in, integration.UniqueIAPairs(acceptance.SigAddr),
 		time.Duration(*attempts)*time.Second+integration.DefaultRunTimeout)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to run tests: %s\n", err)
-		return 1
+	if !*fail && err == nil {
+		// The test is supposed to succeed and it did indeed succeeded.
+		return 0
 	}
-	return 0
+	if *fail && err != nil {
+		// The test is supposed to fail it did indeed fail.
+		return 0
+	}
+	fmt.Fprintf(os.Stderr, "Failed to run tests: %s\n", err)
+	return 1
 }
