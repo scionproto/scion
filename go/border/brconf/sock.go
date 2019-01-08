d0ee693c7a3640358114378fa6d8ac9b51e8dbc1
roos <roos@anapaya.net>
2019-01-08 15:43:20 +0100

    BR: Refactor socket setup
    Add `rctx.SocketType` to indicate the type of socket.
    The default is `posix`.
    
    Add `SocketConf` to configure the socket type for the
    local and all interface sockets individually.
    
    Replace hook system for local and external socket setup
    with a `SocketOps` dictionary. `SocketOps` may be added
    by different network stacks.
    This is done in preperation for a future PR that adds
    proper transactional context reloads with rollback and
    teardown capabilites.
    
    Move posix specific socket setup  to `setup-posix.go`.
    
    Add basic unit tests for `setupNet`. These will be
    extended in the future PR that adds rollbaack and
    teardown.

---
+++ go/border/brconf/sock.go
@@ -0,0 +1,41 @@
+// Copyright 2019 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package brconf
+
+import (
+	"github.com/scionproto/scion/go/lib/common"
+)
+
+type SockType string
+
+type SockConf struct {
+	Default       SockType
+	LocalType     SockType
+	ExternalTypes map[common.IFIDType]SockType
+}
+
+func (s SockConf) Loc() SockType {
+	if s.LocalType != "" {
+		return s.LocalType
+	}
+	return s.Default
+}
+
+func (s SockConf) Ext(ifid common.IFIDType) SockType {
+	if t := s.ExternalTypes[ifid]; t != "" {
+		return t
+	}
+	return s.Default
+}
