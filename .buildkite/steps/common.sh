8b9e1b24e0144e7b3c764c19ab563dd7d6b957fa
Lukas Bischofberger <bischofberger@anapaya.net>
2019-01-10 13:10:58 +0100

    BK: Make logs less noisy and add timestamps

---
+++ .buildkite/steps/common.sh
@@ -0,0 +1,22 @@
+log() {
+    echo "$(date -u +"%F %T.%6N%z") $@"
+}
+
+prepare() {
+    mkdir -p "artifacts.out"
+    $BASE/scripts/clean_env &>> "$STEP_LOG"
+    $BASE/scripts/registry_login &>> "$STEP_LOG"
+    docker pull $SCION_IMG &>/dev/null
+    mkdir -p $SCION_MOUNT
+}
+
+cleanup() {
+    $BASE/scripts/pack_logs &>> "$STEP_LOG"
+    local res=$?
+    ./docker.sh stop &>> "$STEP_LOG"
+    res=$((res+$?))
+    $BASE/scripts/clean_env &>> "$STEP_LOG"
+    res=$((res+$?))
+    rm -r $SCION_MOUNT
+    return $res
+}
