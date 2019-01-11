d2393fa202876e55756fd7a29e5e86310528c4bd
Lukas Bischofberger <bischofberger@anapaya.net>
2019-01-11 10:25:59 +0100

    comments kormat

--- go/lib/scrypto/asym.go
+++ go/lib/scrypto/asym.go
@@ -40,6 +40,7 @@ const (
 	InvalidPubKeySize       = "Invalid public key size"
 	InvalidPrivKeySize      = "Invalid private key size"
 	InvalidSignatureSize    = "Invalid signature size"
+	InvalidSignatureFormat  = "Invalid signature format: sig[63]&224 should equal 0"
 	VerificationError       = "Signature verification failed"
 	UnableToGenerateKeyPair = "Unable to generate key pair"
 	UnableToDecrypt         = "Unable to decrypt message"
@@ -94,9 +95,12 @@ func Verify(sigInput, sig, verifyKey common.RawBytes, signAlgo string) error {
 			return common.NewBasicError(InvalidPubKeySize, nil,
 				"expected", ed25519.PublicKeySize, "actual", len(verifyKey))
 		}
-		if len(sig) != ed25519.SignatureSize || sig[63]&224 != 0 {
+		if len(sig) != ed25519.SignatureSize {
 			return common.NewBasicError(InvalidSignatureSize, nil,
 				"expected", ed25519.SignatureSize, "actual", len(sig))
+		}
+		if sig[63]&224 != 0 {
+			return common.NewBasicError(InvalidSignatureFormat, nil)
 		}
 		if !ed25519.Verify(ed25519.PublicKey(verifyKey), sigInput, sig) {
 			return common.NewBasicError(VerificationError, nil, "msg", sigInput)
