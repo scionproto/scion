d5027b9db750c5eec59a69c24e2ada7a5e6d8eea
Lukas Vogel <lukedirtwalker@gmail.com>
2019-01-11 08:38:39 +0100

    Refactor: Move RunsInDocker from env to util package (#2322)
    The function might also be usable for packages that don't rely on env.

--- go/lib/env/env.go
+++ go/lib/env/env.go
@@ -20,13 +20,11 @@
 package env
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
 	"path/filepath"
-	"strings"
 	"syscall"
 	"time"
 
@@ -223,21 +221,4 @@ func (cfg *Metrics) StartPrometheus() {
 type Infra struct {
 	// Type must be one of BS, CS or PS.
 	Type string
-}
-
-// RunsInDocker returns whether the current binary is run in a docker container.
-func RunsInDocker() (bool, error) {
-	f, err := os.Open("/proc/self/cgroup")
-	if err != nil {
-		return false, common.NewBasicError("Failed to open /proc/self/cgroup", err)
-	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		parts := strings.Split(scanner.Text(), ":")
-		if len(parts) == 3 && strings.HasPrefix(parts[2], "/docker/") {
-			return true, nil
-		}
-	}
-	return false, nil
 }
