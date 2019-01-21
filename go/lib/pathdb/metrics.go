52138336ec5b0537cc67a30ebae3fce8b835d86d
Lukas Vogel <lukedirtwalker@gmail.com>
2019-01-21 16:15:21 +0100

    Always increment result counter.

--- go/lib/pathdb/metrics.go
+++ go/lib/pathdb/metrics.go
@@ -48,27 +48,9 @@ const (
 
 var (
 	queriesTotal *prometheus.CounterVec
-	errorsTotal  *prometheus.CounterVec
-)
+	resultsTotal *prometheus.CounterVec
 
-func init() {
-	queriesTotal = prom.NewCounterVec(promNamespace, "", "queries_total",
-		"Total queries to the database.", []string{promDBName, promOpLabel})
-	errorsTotal = prom.NewCounterVec(promNamespace, "", "errors_total",
-		"Amount of pathdb errors.", []string{promDBName, prom.LabelErr})
-}
-
-type dbCounters struct {
-	opCounters      map[promOp]prometheus.Counter
-	errAnyTotal     prometheus.Counter
-	errTimeoutTotal prometheus.Counter
-}
-
-// WithMetrics wraps the given PathDB into one that also exports metrics.
-// dbName will be added as a label to all metrics, so that multiple path DBs can be differentiated.
-func WithMetrics(dbName string, pathDB PathDB) PathDB {
-	opCounters := make(map[promOp]prometheus.Counter)
-	allOps := []promOp{
+	allOps = []promOp{
 		promOpInsert,
 		promOpInsertHpCfg,
 		promOpDelete,
@@ -77,26 +59,64 @@ func WithMetrics(dbName string, pathDB PathDB) PathDB {
 		promOpInsertNextQuery,
 		promOpGetNextQuery,
 	}
+
+	allResults = []string{
+		prom.ResultOk,
+		prom.ErrNotClassified,
+		prom.ErrTimeout,
+	}
+)
+
+func init() {
+	// Cardinality: X (dbName) * 7 (len(allOps))
+	queriesTotal = prom.NewCounterVec(promNamespace, "", "queries_total",
+		"Total queries to the database.", []string{promDBName, promOpLabel})
+	// Cardinality: X (dbNmae) * 7 (len(allOps)) * 3 (len(allResults))
+	resultsTotal = prom.NewCounterVec(promNamespace, "", "results_total",
+		"The results of the pathdb ops.", []string{promDBName, prom.LabelResult, promOpLabel})
+}
+
+type dbCounters struct {
+	opCounters     map[promOp]prometheus.Counter
+	resultCounters map[promOp]map[string]prometheus.Counter
+}
+
+// WithMetrics wraps the given PathDB into one that also exports metrics.
+// dbName will be added as a label to all metrics, so that multiple path DBs can be differentiated.
+func WithMetrics(dbName string, pathDB PathDB) PathDB {
+	return &metricsPathDB{
+		pathDB: pathDB,
+		metrics: &dbCounters{
+			opCounters:     opCounters(dbName),
+			resultCounters: resultCounters(dbName),
+		},
+	}
+}
+
+func opCounters(dbName string) map[promOp]prometheus.Counter {
+	opCounters := make(map[promOp]prometheus.Counter)
 	for _, op := range allOps {
 		opCounters[op] = queriesTotal.With(prometheus.Labels{
 			promDBName:  dbName,
 			promOpLabel: string(op),
 		})
 	}
-	return &metricsPathDB{
-		pathDB: pathDB,
-		metrics: &dbCounters{
-			opCounters: opCounters,
-			errAnyTotal: errorsTotal.With(prometheus.Labels{
-				promDBName:    dbName,
-				prom.LabelErr: prom.ErrNotClassified,
-			}),
-			errTimeoutTotal: errorsTotal.With(prometheus.Labels{
-				promDBName:    dbName,
-				prom.LabelErr: prom.ErrTimeout,
-			}),
-		},
+	return opCounters
+}
+
+func resultCounters(dbName string) map[promOp]map[string]prometheus.Counter {
+	resultCounters := make(map[promOp]map[string]prometheus.Counter)
+	for _, op := range allOps {
+		resultCounters[op] = make(map[string]prometheus.Counter)
+		for _, res := range allResults {
+			resultCounters[op][res] = resultsTotal.With(prometheus.Labels{
+				promDBName:       dbName,
+				promOpLabel:      string(op),
+				prom.LabelResult: res,
+			})
+		}
 	}
+	return resultCounters
 }
 
 var _ (PathDB) = (*metricsPathDB)(nil)
@@ -110,7 +130,7 @@ type metricsPathDB struct {
 func (db *metricsPathDB) Insert(ctx context.Context, meta *seg.Meta) (int, error) {
 	db.incOp(promOpInsert)
 	cnt, err := db.pathDB.Insert(ctx, meta)
-	db.incErr(err)
+	db.incErr(promOpInsert, err)
 	return cnt, err
 }
 
@@ -119,21 +139,21 @@ func (db *metricsPathDB) InsertWithHPCfgIDs(ctx context.Context,
 
 	db.incOp(promOpInsertHpCfg)
 	cnt, err := db.pathDB.InsertWithHPCfgIDs(ctx, meta, hpCfgIds)
-	db.incErr(err)
+	db.incErr(promOpInsertHpCfg, err)
 	return cnt, err
 }
 
 func (db *metricsPathDB) Delete(ctx context.Context, params *query.Params) (int, error) {
 	db.incOp(promOpDelete)
 	cnt, err := db.pathDB.Delete(ctx, params)
-	db.incErr(err)
+	db.incErr(promOpDelete, err)
 	return cnt, err
 }
 
 func (db *metricsPathDB) DeleteExpired(ctx context.Context, now time.Time) (int, error) {
 	db.incOp(promOpDeleteExpired)
 	cnt, err := db.pathDB.DeleteExpired(ctx, now)
-	db.incErr(err)
+	db.incErr(promOpDeleteExpired, err)
 	return cnt, err
 }
 
@@ -142,7 +162,7 @@ func (db *metricsPathDB) Get(ctx context.Context,
 
 	db.incOp(promOpGet)
 	res, err := db.pathDB.Get(ctx, params)
-	db.incErr(err)
+	db.incErr(promOpGet, err)
 	return res, err
 }
 
@@ -151,14 +171,14 @@ func (db *metricsPathDB) InsertNextQuery(ctx context.Context,
 
 	db.incOp(promOpInsertNextQuery)
 	ok, err := db.pathDB.InsertNextQuery(ctx, dst, nextQuery)
-	db.incErr(err)
+	db.incErr(promOpInsertNextQuery, err)
 	return ok, err
 }
 
 func (db *metricsPathDB) GetNextQuery(ctx context.Context, dst addr.IA) (*time.Time, error) {
 	db.incOp(promOpGetNextQuery)
 	t, err := db.pathDB.GetNextQuery(ctx, dst)
-	db.incErr(err)
+	db.incErr(promOpGetNextQuery, err)
 	return t, err
 }
 
@@ -166,15 +186,14 @@ func (db *metricsPathDB) incOp(op promOp) {
 	db.metrics.opCounters[op].Inc()
 }
 
-func (db *metricsPathDB) incErr(err error) {
-	if err == nil {
-		return
-	}
+func (db *metricsPathDB) incErr(op promOp, err error) {
 	// TODO(lukedirtwalker): categorize error better.
 	switch {
+	case err == nil:
+		db.metrics.resultCounters[op][prom.ResultOk].Inc()
 	case common.IsTimeoutErr(err):
-		db.metrics.errTimeoutTotal.Inc()
+		db.metrics.resultCounters[op][prom.ErrTimeout].Inc()
 	default:
-		db.metrics.errAnyTotal.Inc()
+		db.metrics.resultCounters[op][prom.ErrNotClassified].Inc()
 	}
 }
