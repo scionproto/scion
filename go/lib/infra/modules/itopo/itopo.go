a2f62a279c0e73c72e487d3c5e143356b0daebc1
roos <roos@anapaya.net>
2019-01-23 15:05:01 +0100

    feedback

--- go/lib/infra/modules/itopo/itopo.go
+++ go/lib/infra/modules/itopo/itopo.go
@@ -13,122 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package itopo stores the static and dynamic topology. Client packages
-// that grab a reference with Get are guaranteed to receive a stable
-// snapshot of the topology. The returned value is the topology that is
-// currently active.
-//
-// There are two types of topologies, the static and the dynamic topology.
-// For more information see lib/discovery.
-//
-// Initialization
-//
-// The package must be initialized with Init or InitSvc. In subsequent
-// updates through SetStatic or SetDynamic, the new topology is checked
-// whether it is compatible with the previous version. The rules differ
-// between services.
-//
-// If the dynamic topology is set, the initializing client should start
-// the periodic cleaner to evict expired dynamic topologies.
-//
-// Updates
-//
-// The update of the topology is only valid if a set of constraints is
-// met. The constraints differ between dynamic and static topology, and
-// also between the initialized service type.
-//
-// In a static topology update, when the diff is empty, the static
-// topology is only updated if it expires later than the current static.
-// Otherwise, SetStatic succeeds and indicates that the in-memory copy
-// has not been updated.
-//
-// A static topology update can force the dynamic topology to be dropped,
-// if it does no longer meet the constraints.
-//
-// Constraints
-//
-// The topology is split into four parts. An update is valid under the
-// constraints, if the constraints for each part are met.
-//
-// Immutable:
-// This part may not differ from the initial static topology.
-//
-// Mutable:
-// This part may differ from the initial static topology. It may also
-// differ between the currently active static and dynamic topology.
-//
-// Semi-Mutable:
-// This part may differ between static topology versions. However, it
-// may not differ between the current dynamic and static topology.
-// If an update to the static topology modifies this part, the dynamic
-// topology is dropped.
-//
-// Time:
-// This part is ignored when validating the constraints. It is used
-// to determine if a topology shall be updated if there are no
-// differences in the other parts.
-//
-// Default Topology Split
-//
-// The topology file for default initialization (calling Init) is split
-// into immutable, mutable and time.
-//
-//  ISD_AS                Immutable
-//  Core                  Immutable
-//  Overlay               Immutable
-//  MTU                   Immutable
-//
-//  Service Entries       Mutable
-//  BorderRouter Entries  Mutable
-//
-//  Timestamp             Time
-//  TimestampHuman        Time
-//  TTL                   Time
-//
-// Service Topology Split
-//
-// The topology file for services is split into immutable, mutable
-// and time.
-//
-//  ISD_AS                Immutable
-//  Core                  Immutable
-//  Overlay               Immutable
-//  MTU                   Immutable
-//  OwnSvcType[OwnID]     Immutable // The service entry for the initialized element.
-//
-//  Service Entries       Mutable   // Except OwnSvcType[OwnID].
-//  BorderRouter Entries  Mutable
-//
-//  Timestamp             Time
-//  TimestampHuman        Time
-//  TTL                   Time
-//
-// Border Router Topology Split
-//
-// The topology file for border routers is split into immutable,
-// semi-mutable, mutable and time.
-//
-//  ISD_AS                              Immutable
-//  Core                                Immutable
-//  Overlay                             Immutable
-//  MTU                                 Immutable
-//  OwnSvcType[OwnID]                   Immutable
-//  BorderRouters[OwnId][InternalAddrs] Immutable    // Internal address of initialized router.
-//  BorderRouters[OwnId][CtrlAddr]      Immutable    // Control address of initialized router.
-//
-//  BorderRouters[OwnId][Interfaces]    Semi-Mutable // Interfaces of initialized router.
-//
-//  Service Entries       Mutable                    // Except BorderRouters[OwnId].
-//  BorderRouter Entries  Mutable
-//
-//  Timestamp             Time
-//  TimestampHuman        Time
-//  TTL                   Time
-//
-// Callbacks
-//
-// The client package can register callbacks to be notified about
-// certain events.
 package itopo
 
 import (
@@ -138,19 +22,15 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 
-	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/log"
 	"github.com/scionproto/scion/go/lib/topology"
 	"github.com/scionproto/scion/go/proto"
 )
 
-var (
-	validate    validator
-	clbks       Clbks
-	topologyMtx sync.RWMutex
-	staticTopo  *topology.Topo = nil
-	dynamicTopo *topology.Topo = nil
-)
+var s struct {
+	sync.Mutex
+	state *state
+}
 
 // Clbks are callbacks to respond to specific topology update events.
 type Clbks struct {
@@ -162,50 +42,22 @@ type Clbks struct {
 	UpdateStatic func()
 }
 
-// Init initializes itopo without a particular element set. When setting the topology,
-// only the immutable fields are checked.
-func Init(static *topology.Topo, callbacks *Clbks) error {
-	return InitSvc("", proto.ServiceType_unset, static, callbacks)
-}
-
-// InitSvc initializes itopo with a particular element set. When setting the topology,
-// the immutable and semi-mutable fields are checked.
-func InitSvc(id string, svc proto.ServiceType, static *topology.Topo, callbacks *Clbks) error {
-	if staticTopo != nil {
-		return common.NewBasicError("Must not re-initialize itopo", nil)
+// Init initializes itopo with the particular validator. A topology must be
+// initialized by calling SetStatic.
+func Init(svc proto.ServiceType, clbks Clbks) {
+	s.Lock()
+	defer s.Unlock()
+	if s.state != nil {
+		panic("Must not re-initialize itopo")
 	}
-	switch svc {
-	case proto.ServiceType_unset:
-		validate = &generalValidator{}
-	case proto.ServiceType_br:
-		// FIXME(roosd): add validator for border router.
-		validate = &generalValidator{}
-	default:
-		// FIMXE(roosd): add validator for service.
-		validate = &generalValidator{}
-	}
-	if err := validate.General(static); err != nil {
-		return common.NewBasicError("Unable to validate initial static topo", err)
-	}
-	if callbacks != nil {
-		clbks = *callbacks
-	}
-	staticTopo = static
-	return nil
+	s.state = newState(svc, clbks)
 }
 
 // Get atomically gets the pointer to the current topology.
 func Get() *topology.Topo {
-	topologyMtx.Lock()
-	defer topologyMtx.Unlock()
-	return currTopo()
-}
-
-func currTopo() *topology.Topo {
-	if dynamicTopo != nil && dynamicTopo.Active(time.Now()) {
-		return dynamicTopo
-	}
-	return staticTopo
+	s.state.RLock()
+	defer s.state.RUnlock()
+	return s.state.topo.curr()
 }
 
 // SetStatic atomically sets the static topology. Whether semi-mutable fields are
@@ -213,39 +65,89 @@ func currTopo() *topology.Topo {
 // topology is a pointer to the currently active topology. It might differ from
 // the input topology. The second return value indicates whether the in-memory
 // copy of the static topology has been updated.
-func SetStatic(topo *topology.Topo, semiMutAllowed bool) (*topology.Topo, bool, error) {
-	topologyMtx.Lock()
-	defer topologyMtx.Unlock()
-	if err := validate.General(topo); err != nil {
-		return nil, false, err
-	}
-	if err := validate.Immutable(topo, staticTopo); err != nil {
-		return nil, false, err
-	}
-	if err := validate.SemiMutable(topo, staticTopo, semiMutAllowed); err != nil {
-		return nil, false, err
-	}
-	updated := setStatic(topo, validate.DropDynamic(topo, staticTopo))
-	return currTopo(), updated, nil
+func SetStatic(static *topology.Topo, semiMutAllowed bool) (*topology.Topo, bool, error) {
+	return s.state.setStatic(static, semiMutAllowed)
 }
 
-// setStatic sets the static topology and calls the necessary callbacks.
-func setStatic(topo *topology.Topo, dropDynamic bool) bool {
-	expiresLater := staticTopo.TTL != 0 && (topo.TTL == 0 ||
-		topo.Timestamp.Add(topo.TTL).After(staticTopo.Timestamp.Add(staticTopo.TTL)))
+// topo stores the currently active static and dynamic topologies.
+type topo struct {
+	static  *topology.Topo
+	dynamic *topology.Topo
+}
+
+// curr returns the currently active topology.
+func (t *topo) curr() *topology.Topo {
+	if t.dynamic != nil && t.dynamic.Active(time.Now()) {
+		return t.dynamic
+	}
+	return t.static
+}
+
+// state keeps track of the active topologies and enforces update rules.
+type state struct {
+	sync.RWMutex
+	topo      topo
+	validator validator
+	clbks     Clbks
+}
+
+func newState(svc proto.ServiceType, clbks Clbks) *state {
+	s := &state{
+		validator: validatorFactory(svc),
+		clbks:     clbks,
+	}
+	return s
+}
+
+// setStatic atomically sets the static topology.
+func (s *state) setStatic(static *topology.Topo, allowed bool) (*topology.Topo, bool, error) {
+	s.Lock()
+	defer s.Unlock()
+	if err := s.validate(static, allowed); err != nil {
+		return nil, false, err
+	}
+	updated := s.updateStatic(static)
+	return s.topo.curr(), updated, nil
+}
+
+// validate validates that the new topology is valid given the currently active topology.
+func (s *state) validate(static *topology.Topo, allowed bool) error {
+	if err := s.validator.General(static); err != nil {
+		return err
+	}
+	if err := s.validator.Immutable(static, s.topo.static); err != nil {
+		return err
+	}
+	if err := s.validator.SemiMutable(static, s.topo.static, allowed); err != nil {
+		return err
+	}
+	return nil
+}
+
+// updateStatic updates the static topology, if necessary, and calls the corresponding callbacks.
+func (s *state) updateStatic(static *topology.Topo) bool {
 	// Only update static topology if the new one is different or longer valid for.
-	if cmp.Equal(topo, staticTopo, cmpopts.IgnoreFields(topology.Topo{},
-		"Timestamp", "TimestampHuman", "TTL")) && !expiresLater {
+	if cmp.Equal(static, s.topo.static, cmpopts.IgnoreFields(topology.Topo{},
+		"Timestamp", "TimestampHuman", "TTL")) && !expiresLater(static, s.topo.static) {
 		return false
 	}
-	staticTopo = topo
 	// Drop dynamic topology if necessary.
-	if dropDynamic && dynamicTopo != nil {
-		dynamicTopo = nil
-		call(clbks.DropDynamic)
+	if s.validator.MustDropDynamic(static, s.topo.static) && s.topo.dynamic != nil {
+		s.topo.dynamic = nil
+		call(s.clbks.DropDynamic)
 	}
-	call(clbks.UpdateStatic)
+	s.topo.static = static
+	call(s.clbks.UpdateStatic)
 	return true
+}
+
+func expiresLater(newTopo, oldTopo *topology.Topo) bool {
+	if oldTopo == nil {
+		return true
+	}
+	newExpiry := newTopo.Expiry()
+	oldExpiry := oldTopo.Expiry()
+	return !oldExpiry.IsZero() && (newExpiry.IsZero() || newExpiry.After(oldExpiry))
 }
 
 func call(clbk func()) {
