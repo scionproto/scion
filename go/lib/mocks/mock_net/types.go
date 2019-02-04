a7a1bd30fd04d6f017ff5b7e180c40fbb9a8f6ec
Martin Sustrik <sustrik@anapaya.net>
2019-02-04 13:22:11 +0100

    Bazel build system for Golang components
    - There are lots of BUILD files. However, most of them were generated by gazelle tool, and thus
      don't need meticulous review. You can identify manually edited BUILD files by "# gazelle:ignore"
      line on the top of the file.
    - Bazel uses go 1.11 which is an upgrade from out current 0.9. I had to
      modify code in go/sig/mgmt/poll.go to reflect the change of visibility
      semantics in 1.11.
    - External dependencies. These are listed in the WORKSPACE file. Note that number of deps is smaller
      because Bazel works on the level of git repos rather than Go packages
    - Capnp code generation (go-capnp uses Bazel -- I had to increment the version of go-capnp
      so that it's compatible with the version we are using)
    - Mock generation (including an ugly hack to mock system libraries)
    - Linting: done by generating a special lint package containing the tools which are then run from
      the makefile.
    - The process of building base docker containers is changed:
      - WORKSPACE is copied to the image
      - "bazel fetch" is run to gather the dependencies (they are in ~/.cache/bazel)
      - .git dirs are deleted from the cache to keep the image smaller
    - When building docker images chown is very expensive operation; this was alleviated by doing chown
      only on the files where it is actually needed.
    - Tests are now required to state they data dependencies. This was done by hand.
    - "Rebuild as part of the test" hack in go/lib/infra/modules/trust/trust_test.go was removed.
    - Datafiles in tests are loaded within main, not before that.
    - License collection is done by scanning the bazel cache for LICENSE
      files.
    - Fake dependency tree is build in go/vendor to makestandard go tools
      work.
    - Deployment: Done by packing all relevant binaries into a tarball and
      then unpacking in bin/ directory.

---
+++ go/lib/mocks/mock_net/types.go
@@ -0,0 +1,10 @@
+// Copyright 2016 ETH Zurich
+// Copyright 2018 ETH Zurich, Anapaya Systems
+
+package mock_net
+
+import "net"
+
+type Addr = net.Addr
+type Conn = net.Conn
+type PacketConn = net.PacketConn
