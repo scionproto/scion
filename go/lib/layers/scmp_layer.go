8b6d396b3406eca0821d12c2c213a843f9ea26d3
Sergiu Costea <sergiu.costea@gmail.com>
2019-02-05 18:12:42 +0100

    Low level SCMP parser
    This provides parser support for our existing SCMP libraries.
    
    SCMP checksum not implemented yet.

---
+++ go/lib/layers/scmp_layer.go
@@ -0,0 +1,185 @@
+// Copyright 2017 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package layers
+
+import (
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+
+	"github.com/scionproto/scion/go/lib/common"
+)
+
+// FIXME(scrye): add checksum support
+type SCMP struct {
+	layers.BaseLayer
+
+	ClassType uint32
+	Length    uint16
+	Checksum  uint16
+	Timestamp uint64
+
+	InfoBlockLines       uint8
+	CommonHeaderLines    uint8
+	AddressHeaderLines   uint8
+	PathHeaderLines      uint8
+	ExtensionHeaderLines uint8
+	L4Lines              uint8
+	L4ProtoType          common.L4ProtocolType
+	Padding              uint8
+
+	InfoBlock            []byte
+	CommonHeaderBlock    []byte
+	AddressHeaderBlock   []byte
+	PathHeaderBlock      []byte
+	ExtensionHeaderBlock []byte
+	L4Block              []byte
+
+	CustomPayload []byte
+}
+
+func (s *SCMP) DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) error {
+	// Header + mandatory meta = 24 bytes
+	if len(data) < 24 {
+		df.SetTruncated()
+		return common.NewBasicError("Invalid SCMP header, length too short", nil,
+			"actual", len(data), "wanted", 16)
+	}
+	s.ClassType = common.Order.Uint32(data[0:4])
+	s.Length = common.Order.Uint16(data[4:6])
+	s.Checksum = common.Order.Uint16(data[6:8])
+	s.Timestamp = common.Order.Uint64(data[8:16])
+	s.InfoBlockLines = data[16]
+	s.CommonHeaderLines = data[17]
+	s.AddressHeaderLines = data[18]
+	s.PathHeaderLines = data[19]
+	s.ExtensionHeaderLines = data[20]
+	s.L4Lines = data[21]
+	s.L4ProtoType = common.L4ProtocolType(data[22])
+	s.Padding = data[23]
+
+	if len(data) < int(s.Length) {
+		df.SetTruncated()
+		return common.NewBasicError("Invalid SCMP message, length too short", nil,
+			"actual", len(data), "wanted", s.Length)
+	}
+	offset := 24
+	infoBlockLength := getLength(s.InfoBlockLines)
+	commonHeaderLength := getLength(s.CommonHeaderLines)
+	addressHeaderLength := getLength(s.AddressHeaderLines)
+	pathHeaderLength := getLength(s.PathHeaderLines)
+	extensionHeaderLength := getLength(s.ExtensionHeaderLines)
+	l4Length := getLength(s.L4Lines)
+	totalQuoteLength := infoBlockLength + commonHeaderLength + addressHeaderLength +
+		pathHeaderLength + extensionHeaderLength + l4Length
+	if int(s.Length)-24 < totalQuoteLength {
+		df.SetTruncated()
+		return common.NewBasicError("Invalid SCMP data, quotes extend past total length", nil,
+			"actual", int(s.Length)-24, "wanted", totalQuoteLength)
+	}
+	s.InfoBlock = data[offset : offset+infoBlockLength]
+	offset += infoBlockLength
+	s.CommonHeaderBlock = data[offset : offset+commonHeaderLength]
+	offset += commonHeaderLength
+	s.AddressHeaderBlock = data[offset : offset+addressHeaderLength]
+	offset += addressHeaderLength
+	s.PathHeaderBlock = data[offset : offset+pathHeaderLength]
+	offset += pathHeaderLength
+	s.ExtensionHeaderBlock = data[offset : offset+extensionHeaderLength]
+	offset += extensionHeaderLength
+	s.L4Block = data[offset : offset+l4Length]
+	offset += l4Length
+	s.CustomPayload = data[offset:s.Length]
+	return nil
+}
+
+func (s *SCMP) SerializeTo(b gopacket.SerializeBuffer, opts gopacket.SerializeOptions) error {
+	// Header + mandatory meta = 24 bytes
+	totalQuoteLength := len(s.InfoBlock) + len(s.CommonHeaderBlock) + len(s.AddressHeaderBlock) +
+		len(s.PathHeaderBlock) + len(s.ExtensionHeaderBlock) + len(s.L4Block) + len(s.CustomPayload)
+	totalPacketLength := 24 + totalQuoteLength
+	if totalPacketLength > 0xffff {
+		return common.NewBasicError("packet too large", nil, "length", totalPacketLength,
+			"max_allowed", uint16(0xffff))
+	}
+	if opts.FixLengths {
+		if err := checkAligned(s.InfoBlock, "info block"); err != nil {
+			return err
+		}
+		if err := checkAligned(s.CommonHeaderBlock, "common header quote"); err != nil {
+			return err
+		}
+		if err := checkAligned(s.AddressHeaderBlock, "address header quote"); err != nil {
+			return err
+		}
+		if err := checkAligned(s.PathHeaderBlock, "path header quote"); err != nil {
+			return err
+		}
+		if err := checkAligned(s.ExtensionHeaderBlock, "extension header quote"); err != nil {
+			return err
+		}
+		if err := checkAligned(s.L4Block, "L4 header quote"); err != nil {
+			return err
+		}
+		s.Length = uint16(totalPacketLength)
+		s.InfoBlockLines = uint8(len(s.InfoBlock)) / common.LineLen
+		s.CommonHeaderLines = uint8(len(s.CommonHeaderBlock)) / common.LineLen
+		s.AddressHeaderLines = uint8(len(s.AddressHeaderBlock)) / common.LineLen
+		s.PathHeaderLines = uint8(len(s.PathHeaderBlock)) / common.LineLen
+		s.ExtensionHeaderLines = uint8(len(s.ExtensionHeaderBlock)) / common.LineLen
+		s.L4Lines = uint8(len(s.L4Block)) / common.LineLen
+	}
+	bytes, err := b.PrependBytes(totalPacketLength)
+	if err != nil {
+		return err
+	}
+	common.Order.PutUint32(bytes[0:4], s.ClassType)
+	common.Order.PutUint16(bytes[4:6], s.Length)
+	common.Order.PutUint16(bytes[6:8], s.Checksum)
+	common.Order.PutUint64(bytes[8:16], s.Timestamp)
+	bytes[16] = s.InfoBlockLines
+	bytes[17] = s.CommonHeaderLines
+	bytes[18] = s.AddressHeaderLines
+	bytes[19] = s.PathHeaderLines
+	bytes[20] = s.ExtensionHeaderLines
+	bytes[21] = s.L4Lines
+	bytes[22] = byte(s.L4ProtoType)
+	bytes[23] = s.Padding
+	offset := 24
+	offset += copy(bytes[offset:], s.InfoBlock)
+	offset += copy(bytes[offset:], s.CommonHeaderBlock)
+	offset += copy(bytes[offset:], s.AddressHeaderBlock)
+	offset += copy(bytes[offset:], s.PathHeaderBlock)
+	offset += copy(bytes[offset:], s.ExtensionHeaderBlock)
+	offset += copy(bytes[offset:], s.L4Block)
+	offset += copy(bytes[offset:], s.CustomPayload)
+	return nil
+}
+
+func checkAligned(b []byte, section string) error {
+	maxLength := common.LineLen * 0xff
+	if len(b)%common.LineLen != 0 {
+		return common.NewBasicError("cannot fix length, SCMP section is not aligned", nil,
+			"section", section, "length", len(b), "must_be_multiple_of", common.LineLen)
+	}
+	if len(b) > maxLength {
+		return common.NewBasicError("cannot fix length, SCMP section is too long", nil,
+			"section", section, "length", len(b), "max_length", maxLength)
+	}
+	return nil
+}
+
+func getLength(numLines uint8) int {
+	return int(numLines) * common.LineLen
+}
---
+++ go/lib/layers/scmp_layer_test.go
@@ -0,0 +1,479 @@
+// Copyright 2017 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package layers
+
+import (
+	"testing"
+
+	"github.com/google/gopacket"
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/scionproto/scion/go/lib/xtest"
+)
+
+func TestExtensionDecodeFromBytes(t *testing.T) {
+	type TestCase struct {
+		Description   string
+		Data          []byte
+		ExpectedError bool
+		ExpectedSCMP  SCMP
+	}
+	testCases := []*TestCase{
+		{
+			Description:   "nil input",
+			ExpectedError: true,
+		},
+		{
+			Description:   "truncated header",
+			Data:          []byte{1},
+			ExpectedError: true,
+		},
+		{
+			Description: "header data, no quotes",
+			Data: []byte{0x00, 0x01, 0x02, 0x03, 0x00, 0x18, 0x06, 0x07,
+				0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x17,
+			},
+			ExpectedSCMP: SCMP{
+				ClassType:            0x00010203,
+				Length:               24,
+				Checksum:             0x0607,
+				Timestamp:            0x08090a0b0c0d0e0f,
+				L4ProtoType:          0x16,
+				Padding:              0x17,
+				InfoBlock:            []byte{},
+				CommonHeaderBlock:    []byte{},
+				AddressHeaderBlock:   []byte{},
+				PathHeaderBlock:      []byte{},
+				ExtensionHeaderBlock: []byte{},
+				L4Block:              []byte{},
+				CustomPayload:        []byte{},
+			},
+		},
+		{
+			Description: "packet smaller than total length",
+			Data: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x18, 0x19,
+			},
+			ExpectedError: true,
+		},
+		{
+			Description: "data extends past quotes",
+			Data: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x1a, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x18, 0x19,
+			},
+			ExpectedSCMP: SCMP{
+				Length:               26,
+				InfoBlock:            []byte{},
+				CommonHeaderBlock:    []byte{},
+				AddressHeaderBlock:   []byte{},
+				PathHeaderBlock:      []byte{},
+				ExtensionHeaderBlock: []byte{},
+				L4Block:              []byte{},
+				CustomPayload:        []byte{0x18, 0x19},
+			},
+		},
+		{
+			Description: "truncated info block",
+			Data: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			},
+			ExpectedError: true,
+		},
+		{
+			Description: "good info block",
+			Data: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x28, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+				0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+			},
+			ExpectedSCMP: SCMP{
+				Length:         40,
+				InfoBlockLines: 2,
+				InfoBlock: []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+					0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+				},
+				CommonHeaderBlock:    []byte{},
+				AddressHeaderBlock:   []byte{},
+				PathHeaderBlock:      []byte{},
+				ExtensionHeaderBlock: []byte{},
+				L4Block:              []byte{},
+				CustomPayload:        []byte{},
+			},
+		},
+		{
+			Description: "all blocks present, proper length",
+			Data: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00,
+				0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+				0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02,
+				0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03,
+				0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04,
+				0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05,
+				0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06,
+			},
+			ExpectedSCMP: SCMP{
+				Length:               72,
+				InfoBlockLines:       1,
+				CommonHeaderLines:    1,
+				AddressHeaderLines:   1,
+				PathHeaderLines:      1,
+				ExtensionHeaderLines: 1,
+				L4Lines:              1,
+				InfoBlock:            []byte{0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01},
+				CommonHeaderBlock:    []byte{0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02},
+				AddressHeaderBlock:   []byte{0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03},
+				PathHeaderBlock:      []byte{0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04},
+				ExtensionHeaderBlock: []byte{0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05},
+				L4Block:              []byte{0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06},
+				CustomPayload:        []byte{},
+			},
+		},
+	}
+	Convey("", t, func() {
+		for _, tc := range testCases {
+			Convey(tc.Description, func() {
+				var scmp SCMP
+				err := scmp.DecodeFromBytes(tc.Data, gopacket.NilDecodeFeedback)
+				xtest.SoMsgError("err", err, tc.ExpectedError)
+				if !tc.ExpectedError {
+					SoMsg("scmp", scmp, ShouldResemble, tc.ExpectedSCMP)
+				}
+			})
+		}
+	})
+}
+
+func TestSCMPSerializeTo(t *testing.T) {
+	type TestCase struct {
+		Description      string
+		SCMP             SCMP
+		SerializeOptions gopacket.SerializeOptions
+		ExpectedError    bool
+		ExpectedBytes    []byte
+		ExpectedLengths  [7]int
+	}
+	testCases := []*TestCase{
+		{
+			Description: "empty scmp",
+			SCMP:        SCMP{},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+			},
+		},
+		{
+			Description:      "empty scmp with fix lengths",
+			SCMP:             SCMP{},
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x18, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+			},
+			ExpectedLengths: [7]int{24, 0, 0, 0, 0, 0, 0},
+		},
+		{
+			Description: "scmp with bad length",
+			SCMP:        SCMP{Length: 30},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x1e, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+			},
+			ExpectedLengths: [7]int{30, 0, 0, 0, 0, 0, 0},
+		},
+		{
+			Description: "scmp with header data",
+			SCMP: SCMP{
+				ClassType:            0x00010203,
+				Length:               0x0405,
+				Checksum:             0x0607,
+				Timestamp:            0x08090a0b0c0d0e0f,
+				InfoBlockLines:       42,
+				CommonHeaderLines:    43,
+				AddressHeaderLines:   44,
+				PathHeaderLines:      45,
+				ExtensionHeaderLines: 46,
+				L4Lines:              47,
+				L4ProtoType:          48,
+				Padding:              49,
+			},
+			ExpectedBytes: []byte{
+				0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+				0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+				42, 43, 44, 45, 46, 47, 48, 49,
+			},
+			ExpectedLengths: [7]int{0x0405, 42, 43, 44, 45, 46, 47},
+		},
+		{
+			Description: "scmp with bad quote lengths",
+			SCMP: SCMP{
+				InfoBlockLines:       1,
+				CommonHeaderLines:    1,
+				AddressHeaderLines:   1,
+				PathHeaderLines:      1,
+				ExtensionHeaderLines: 1,
+				L4Lines:              1,
+				InfoBlock:            []byte{1, 2},
+				CommonHeaderBlock:    []byte{3, 4},
+				AddressHeaderBlock:   []byte{5, 6},
+				PathHeaderBlock:      []byte{7, 8},
+				ExtensionHeaderBlock: []byte{9, 10},
+				L4Block:              []byte{11, 12},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				1, 1, 1, 1, 1, 1, 0, 0,
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+			},
+			ExpectedLengths: [7]int{0, 1, 1, 1, 1, 1, 1},
+		},
+		{
+			Description: "packet too large with fix lengths",
+			SCMP: SCMP{
+				InfoBlock: make([]byte, 1<<16),
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "non-aligned info block with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				InfoBlock: []byte{1, 2},
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "non-aligned common header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				CommonHeaderBlock: []byte{1, 2},
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "non-aligned address header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				AddressHeaderBlock: []byte{1, 2},
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "non-aligned path header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				PathHeaderBlock: []byte{1, 2},
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "non-aligned extension header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				ExtensionHeaderBlock: []byte{1, 2},
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "non-aligned l4 header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				L4Block: []byte{1, 2},
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "too long info block with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				InfoBlock: make([]byte, 1<<13),
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "too long common header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				CommonHeaderBlock: make([]byte, 1<<13),
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "too long address header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				AddressHeaderBlock: make([]byte, 1<<13),
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "too long path header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				PathHeaderBlock: make([]byte, 1<<13),
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "too long extension header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				ExtensionHeaderBlock: make([]byte, 1<<13),
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "too long l4 header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				L4Block: make([]byte, 1<<13),
+			},
+			ExpectedError: true,
+		},
+		{
+			Description:      "aligned info block with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				InfoBlock: []byte{1, 2, 3, 4, 5, 6, 7, 8},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				1, 0, 0, 0, 0, 0, 0, 0,
+				1, 2, 3, 4, 5, 6, 7, 8,
+			},
+			ExpectedLengths: [7]int{32, 1, 0, 0, 0, 0, 0},
+		},
+		{
+			Description:      "aligned common header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				CommonHeaderBlock: []byte{1, 2, 3, 4, 5, 6, 7, 8},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 1, 0, 0, 0, 0, 0, 0,
+				1, 2, 3, 4, 5, 6, 7, 8,
+			},
+			ExpectedLengths: [7]int{32, 0, 1, 0, 0, 0, 0},
+		},
+		{
+			Description:      "aligned address header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				AddressHeaderBlock: []byte{1, 2, 3, 4, 5, 6, 7, 8},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 1, 0, 0, 0, 0, 0,
+				1, 2, 3, 4, 5, 6, 7, 8,
+			},
+			ExpectedLengths: [7]int{32, 0, 0, 1, 0, 0, 0},
+		},
+		{
+			Description:      "aligned path header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				PathHeaderBlock: []byte{1, 2, 3, 4, 5, 6, 7, 8},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 1, 0, 0, 0, 0,
+				1, 2, 3, 4, 5, 6, 7, 8,
+			},
+			ExpectedLengths: [7]int{32, 0, 0, 0, 1, 0, 0},
+		},
+		{
+			Description:      "aligned extension header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				ExtensionHeaderBlock: []byte{1, 2, 3, 4, 5, 6, 7, 8},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 1, 0, 0, 0,
+				1, 2, 3, 4, 5, 6, 7, 8,
+			},
+			ExpectedLengths: [7]int{32, 0, 0, 0, 0, 1, 0},
+		},
+		{
+			Description:      "aligned L4 header quote with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				L4Block: []byte{1, 2, 3, 4, 5, 6, 7, 8},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 1, 0, 0,
+				1, 2, 3, 4, 5, 6, 7, 8,
+			},
+			ExpectedLengths: [7]int{32, 0, 0, 0, 0, 0, 1},
+		},
+		{
+			Description: "custom payload",
+			SCMP: SCMP{
+				CustomPayload: []byte{1, 2},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+				1, 2,
+			},
+		},
+		{
+			Description:      "custom payload with fix lengths",
+			SerializeOptions: gopacket.SerializeOptions{FixLengths: true},
+			SCMP: SCMP{
+				CustomPayload: []byte{1, 2},
+			},
+			ExpectedBytes: []byte{
+				0, 0, 0, 0, 0x00, 0x1a, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+				1, 2,
+			},
+			ExpectedLengths: [7]int{26, 0, 0, 0, 0, 0, 0},
+		},
+	}
+	Convey("", t, func() {
+		for _, tc := range testCases {
+			Convey(tc.Description, func() {
+				b := gopacket.NewSerializeBuffer()
+				err := tc.SCMP.SerializeTo(b, tc.SerializeOptions)
+				xtest.SoMsgError("err", err, tc.ExpectedError)
+				if !tc.ExpectedError {
+					SoMsg("b", b.Bytes(), ShouldResemble, tc.ExpectedBytes)
+
+					lengths := [7]int{
+						int(tc.SCMP.Length),
+						int(tc.SCMP.InfoBlockLines), int(tc.SCMP.CommonHeaderLines),
+						int(tc.SCMP.AddressHeaderLines), int(tc.SCMP.PathHeaderLines),
+						int(tc.SCMP.ExtensionHeaderLines), int(tc.SCMP.L4Lines),
+					}
+					SoMsg("updated length field", lengths, ShouldEqual, tc.ExpectedLengths)
+				}
+			})
+		}
+	})
+}
