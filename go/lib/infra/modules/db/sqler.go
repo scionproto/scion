fad414d6b14607e4cc58ed43ee45f0fd9b718a48
Lukas Vogel <lukedirtwalker@gmail.com>
2019-03-12 11:08:43 +0100

    Add DoInTx

--- go/lib/infra/modules/db/sqler.go
+++ go/lib/infra/modules/db/sqler.go
@@ -17,6 +17,8 @@ package db
 import (
 	"context"
 	"database/sql"
+
+	"github.com/scionproto/scion/go/lib/common"
 )
 
 var _ Sqler = (*sql.DB)(nil)
@@ -27,4 +29,31 @@ type Sqler interface {
 	ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
 	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
 	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
+}
+
+// DoInTx executes the given action in a transaction. If db is already a transaction the action is
+// executed in the existing transaction, the transaction is not modified. If db is a "normal" db, a
+// transaction is created and action is executed in it. If action errors the created transaction is
+// rollbacked, otherwise it is commited.
+func DoInTx(ctx context.Context, db Sqler, action func(context.Context, *sql.Tx) error) error {
+	tx, ok := db.(*sql.Tx)
+	manageTx := !ok
+	if manageTx {
+		var err error
+		if tx, err = db.(*sql.DB).BeginTx(ctx, nil); err != nil {
+			return common.NewBasicError("Failed to create tx", err)
+		}
+	}
+	if err := action(ctx, tx); err != nil {
+		if manageTx {
+			tx.Rollback()
+		}
+		return err
+	}
+	if manageTx {
+		if err := tx.Commit(); err != nil {
+			return common.NewBasicError("Failed to commit", err)
+		}
+	}
+	return nil
 }
