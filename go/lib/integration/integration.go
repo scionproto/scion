eacc17e3aaac210f9aade9fbec94fe1c503a03db
Lukas Vogel <lukedirtwalker@gmail.com>
2019-03-15 16:30:54 +0100

    wip: Test: better errors for server timeout.

--- go/lib/integration/integration.go
+++ go/lib/integration/integration.go
@@ -37,7 +37,7 @@ import (
 
 const (
 	// StartServerTimeout is the timeout for starting a server.
-	StartServerTimeout = 4 * time.Second
+	StartServerTimeout = 6 * time.Second
 	// DefaultRunTimeout is the timeout when running a server or a client.
 	DefaultRunTimeout = 8 * time.Second
 	// CtxTimeout is the timeout a context waits before being killed
