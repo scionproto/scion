7e1af3d018cbca0e8183410c33530267118189e2
Oncilla <domi.roos@gmail.com>
2019-04-18 08:25:13 +0200

    BS: Add config (#2601)
    This PR adds the beacon server config.
    Currently, the beacon server specific parts are empty.
    They will be filled in as needed.
    
    fixes #2482

---
+++ go/beacon_srv/internal/config/config_test.go
@@ -0,0 +1,59 @@
+// Copyright 2019 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/BurntSushi/toml"
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/scionproto/scion/go/lib/env/envtest"
+	"github.com/scionproto/scion/go/lib/infra/modules/idiscovery/idiscoverytest"
+	"github.com/scionproto/scion/go/lib/truststorage/truststoragetest"
+)
+
+func TestConfigSample(t *testing.T) {
+	Convey("Sample is correct", t, func() {
+		var sample bytes.Buffer
+		var cfg Config
+		cfg.Sample(&sample, nil, nil)
+
+		InitTestConfig(&cfg)
+		meta, err := toml.Decode(sample.String(), &cfg)
+		SoMsg("err", err, ShouldBeNil)
+		SoMsg("unparsed", meta.Undecoded(), ShouldBeEmpty)
+		CheckTestConfig(&cfg, idSample)
+	})
+}
+
+func InitTestConfig(cfg *Config) {
+	envtest.InitTest(&cfg.General, &cfg.Logging, &cfg.Metrics, nil)
+	truststoragetest.InitTestConfig(&cfg.TrustDB)
+	idiscoverytest.InitTestConfig(&cfg.Discovery)
+	InitTestBSConfig(&cfg.BS)
+}
+
+func InitTestBSConfig(cfg *BSConfig) {}
+
+func CheckTestConfig(cfg *Config, id string) {
+	envtest.CheckTest(&cfg.General, &cfg.Logging, &cfg.Metrics, nil, id)
+	truststoragetest.CheckTestConfig(&cfg.TrustDB, id)
+	idiscoverytest.CheckTestConfig(&cfg.Discovery)
+	CheckTestBSConfig(&cfg.BS)
+}
+
+func CheckTestBSConfig(cfg *BSConfig) {}
