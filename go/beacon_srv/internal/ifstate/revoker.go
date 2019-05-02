2ed1277e8676f6a5b0279178cbc98deb13c0e1ea
Lukas Vogel <lukedirtwalker@gmail.com>
2019-05-02 16:45:01 +0200

    BS: Add revoker for IFs with missing keepalive (#2625)
    Adds a Revoker task that issues revocations for interfaces that have timed
    and renews evocations for already revoked interfaces periodically.
    
    Fixes #2566

---
+++ go/beacon_srv/internal/ifstate/revoker.go
@@ -0,0 +1,201 @@
+// Copyright 2019 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ifstate
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/ctrl/path_mgmt"
+	"github.com/scionproto/scion/go/lib/infra"
+	"github.com/scionproto/scion/go/lib/infra/messenger"
+	"github.com/scionproto/scion/go/lib/log"
+	"github.com/scionproto/scion/go/lib/periodic"
+	"github.com/scionproto/scion/go/lib/snet"
+	"github.com/scionproto/scion/go/lib/topology"
+	"github.com/scionproto/scion/go/lib/util"
+	"github.com/scionproto/scion/go/proto"
+)
+
+// DefaultRevOverlap specifies the default for how long before the expiry of an existing revocation
+// the revoker can reissue a new revocation.
+const DefaultRevOverlap = path_mgmt.MinRevTTL / 2
+
+// TopoProvider provides a topology that is up to date.
+type TopoProvider interface {
+	Get() *topology.Topo
+}
+
+// RevConfig configures the revoker.
+type RevConfig struct {
+	RevOverlap time.Duration
+}
+
+// InitDefaults initializes the config fields that are not set to the default values.
+func (c *RevConfig) InitDefaults() {
+	if c.RevOverlap == 0 {
+		c.RevOverlap = DefaultRevOverlap
+	}
+}
+
+var _ periodic.Task = (*Revoker)(nil)
+
+// Revoker issues revocations for interfaces that have timed out.
+// Revocations for already revoked interfaces are renewed periodically.
+type Revoker struct {
+	intfs        *Interfaces
+	msger        infra.Messenger
+	cfg          RevConfig
+	signer       infra.Signer
+	topoProvider TopoProvider
+}
+
+// NewRevoker creates a new revoker from the given arguments.
+func NewRevoker(intfs *Interfaces, msger infra.Messenger,
+	signer infra.Signer, cfg RevConfig, topoProvider TopoProvider) *Revoker {
+
+	cfg.InitDefaults()
+	return &Revoker{
+		intfs:        intfs,
+		msger:        msger,
+		cfg:          cfg,
+		signer:       signer,
+		topoProvider: topoProvider,
+	}
+}
+
+// Run issues revocations for interfaces that have timed out
+// and renews revocations for revoked interfaces.
+func (r *Revoker) Run(ctx context.Context) {
+	revs := make(map[common.IFIDType]*path_mgmt.SignedRevInfo)
+	for ifid, intf := range r.intfs.All() {
+		if intf.Expire() && !r.hasValidRevocation(intf) {
+			if intf.Revocation() == nil {
+				log.Info("[Revoker] interface went down", "ifid", ifid)
+			}
+			srev, err := r.createSignedRev(ifid)
+			if err != nil {
+				log.Error("[Revoker] Failed to create revocation", "ifid", ifid, "err", err)
+				continue
+			}
+			if err := intf.Revoke(srev); err == nil {
+				revs[ifid] = srev
+			} else {
+				log.Error("[Revoker] Failed to revoke!", "ifid", ifid, "err", err)
+			}
+		}
+	}
+	if len(revs) > 0 {
+		wg := &sync.WaitGroup{}
+		r.pushRevocationsToBRs(ctx, revs, wg)
+		r.pushRevocationsToPS(ctx, revs)
+		wg.Wait()
+	}
+}
+
+func (r *Revoker) hasValidRevocation(intf *Interface) bool {
+	if srev := intf.Revocation(); srev != nil {
+		rev, err := srev.RevInfo()
+		return err == nil && rev.RelativeTTL(time.Now()) >= r.cfg.RevOverlap
+	}
+	return false
+}
+
+func (r *Revoker) createSignedRev(ifid common.IFIDType) (*path_mgmt.SignedRevInfo, error) {
+	now := util.TimeToSecs(time.Now())
+	revInfo := &path_mgmt.RevInfo{
+		IfID:         ifid,
+		RawIsdas:     r.topoProvider.Get().ISD_AS.IAInt(),
+		LinkType:     r.topoProvider.Get().IFInfoMap[ifid].LinkType,
+		RawTimestamp: now,
+		RawTTL:       uint32(path_mgmt.MinRevTTL.Seconds()),
+	}
+	rawRevInfo, err := revInfo.Pack()
+	if err != nil {
+		return nil, err
+	}
+	sign, err := r.signer.Sign(rawRevInfo)
+	if err != nil {
+		return nil, err
+	}
+	return path_mgmt.NewSignedRevInfo(revInfo, sign)
+}
+
+func (r *Revoker) pushRevocationsToBRs(ctx context.Context,
+	revs map[common.IFIDType]*path_mgmt.SignedRevInfo, wg *sync.WaitGroup) {
+
+	topo := r.topoProvider.Get()
+	msg := &path_mgmt.IFStateInfos{
+		Infos: make([]*path_mgmt.IFStateInfo, 0, len(revs)),
+	}
+	for ifid, srev := range revs {
+		msg.Infos = append(msg.Infos, &path_mgmt.IFStateInfo{
+			IfID:     uint64(ifid),
+			Active:   false,
+			SRevInfo: srev,
+		})
+	}
+	for brId, br := range topo.BR {
+		r.sendToBr(ctx, brId, br, msg, wg)
+	}
+}
+
+func (r *Revoker) sendToBr(ctx context.Context, brId string, br topology.BRInfo,
+	msg *path_mgmt.IFStateInfos, wg *sync.WaitGroup) {
+
+	wg.Add(1)
+	go func() {
+		defer log.LogPanicAndExit()
+		defer wg.Done()
+
+		topo := r.topoProvider.Get()
+		a := &snet.Addr{
+			IA:      topo.ISD_AS,
+			Host:    br.CtrlAddrs.PublicAddr(topo.Overlay),
+			NextHop: br.CtrlAddrs.OverlayAddr(topo.Overlay),
+		}
+		if err := r.msger.SendIfStateInfos(ctx, msg, a, messenger.NextId()); err != nil {
+			log.Error("[Revoker] Failed to send revocations to BR", "br", brId, "err", err)
+		}
+	}()
+}
+
+func (r *Revoker) pushRevocationsToPS(ctx context.Context,
+	revs map[common.IFIDType]*path_mgmt.SignedRevInfo) {
+
+	topo := r.topoProvider.Get()
+	svcInfo, err := topo.GetSvcInfo(proto.ServiceType_ps)
+	if err != nil {
+		log.Error("[Revoker] Failed to get svcInfo for PS", "err", err)
+		return
+	}
+	topoAddr := svcInfo.GetAnyTopoAddr()
+	if topoAddr == nil {
+		log.Error("[Revoker] No PS found in topology")
+		return
+	}
+	a := &snet.Addr{
+		IA:      topo.ISD_AS,
+		Host:    topoAddr.PublicAddr(topo.Overlay),
+		NextHop: topoAddr.OverlayAddr(topo.Overlay),
+	}
+	for ifid, srev := range revs {
+		if err := r.msger.SendRev(ctx, srev, a, messenger.NextId()); err != nil {
+			log.Error("[Revoker] Failed to send revocation to PS", "ifid", ifid, "err", err)
+		}
+	}
+}
---
+++ go/beacon_srv/internal/ifstate/revoker_test.go
@@ -0,0 +1,324 @@
+// Copyright 2019 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ifstate
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/ctrl"
+	"github.com/scionproto/scion/go/lib/ctrl/path_mgmt"
+	"github.com/scionproto/scion/go/lib/infra"
+	"github.com/scionproto/scion/go/lib/infra/mock_infra"
+	"github.com/scionproto/scion/go/lib/infra/modules/trust"
+	"github.com/scionproto/scion/go/lib/log"
+	"github.com/scionproto/scion/go/lib/scrypto"
+	"github.com/scionproto/scion/go/lib/snet"
+	"github.com/scionproto/scion/go/lib/util"
+	"github.com/scionproto/scion/go/lib/xtest"
+	"github.com/scionproto/scion/go/proto"
+)
+
+var (
+	timeout     = time.Second
+	overlapTime = path_mgmt.MinRevTTL / 2
+	expireTime  = time.Second + DefaultKeepaliveTimeout
+	ia          = xtest.MustParseIA("1-ff00:0:111")
+)
+
+type brMsg struct {
+	msg *path_mgmt.IFStateInfos
+	a   net.Addr
+}
+
+func TestMain(m *testing.M) {
+	log.Root().SetHandler(log.DiscardHandler())
+	os.Exit(m.Run())
+}
+
+// TestNoRevocationIssued tests that if all interfaces receive if keepalives the revoker should do
+// nothing.
+func TestNoRevocationIssued(t *testing.T) {
+	topoProvider := xtest.TopoProviderFromFile(t, "testdata/topology.json")
+	_, priv, err := scrypto.GenKeyPair(scrypto.Ed25519)
+	xtest.FailOnErr(t, err)
+	signer := createTestSigner(t, priv)
+	Convey("TestNoRevocationIssued", t, func() {
+		mctrl := gomock.NewController(t)
+		defer mctrl.Finish()
+		msger := mock_infra.NewMockMessenger(mctrl)
+		intfs := NewInterfaces(topoProvider.Get().IFInfoMap, Config{})
+		activateAll(intfs)
+		revoker := testRevoker(intfs, msger, signer, topoProvider)
+		ctx, cancelF := context.WithTimeout(context.Background(), timeout)
+		defer cancelF()
+		revoker.Run(ctx)
+		// gomock tests that no calls to the messenger are made.
+		checkInterfaces(intfs, map[common.IFIDType]State{})
+	})
+}
+
+// TestRevokeInterface tests that if a keepalive didn't arrive for an interface it should be
+// revoked.
+func TestRevokeInterface(t *testing.T) {
+	topoProvider := xtest.TopoProviderFromFile(t, "testdata/topology.json")
+	pub, priv, err := scrypto.GenKeyPair(scrypto.Ed25519)
+	xtest.FailOnErr(t, err)
+	signer := createTestSigner(t, priv)
+	Convey("TestRevokeInterface", t, func() {
+		mctrl := gomock.NewController(t)
+		defer mctrl.Finish()
+		msger := mock_infra.NewMockMessenger(mctrl)
+		intfs := NewInterfaces(topoProvider.Get().IFInfoMap, Config{})
+		activateAll(intfs)
+		intfs.Get(101).lastActivate = time.Now().Add(-expireTime)
+		checkSentMessages := expectMessengerCalls(msger, 101, topoProvider)
+		revoker := testRevoker(intfs, msger, signer, topoProvider)
+		ctx, cancelF := context.WithTimeout(context.Background(), timeout)
+		defer cancelF()
+		revoker.Run(ctx)
+		checkInterfaces(intfs, map[common.IFIDType]State{101: Revoked})
+		checkSentMessages(t, revVerifier(pub))
+	})
+}
+
+// TestRevokedInterfaceNotRevokedImmediately tests that if an interface was revoked recently it
+// shouldn't be revoked again.
+func TestRevokedInterfaceNotRevokedImmediately(t *testing.T) {
+	topoProvider := xtest.TopoProviderFromFile(t, "testdata/topology.json")
+	_, priv, err := scrypto.GenKeyPair(scrypto.Ed25519)
+	xtest.FailOnErr(t, err)
+	signer := createTestSigner(t, priv)
+	Convey("TestRevokedInterfaceNotRevokedImmediately", t, func() {
+		mctrl := gomock.NewController(t)
+		defer mctrl.Finish()
+		msger := mock_infra.NewMockMessenger(mctrl)
+		intfs := NewInterfaces(topoProvider.Get().IFInfoMap, Config{})
+		activateAll(intfs)
+		intfs.Get(101).state = Expired
+		srev := toSigned(t, &path_mgmt.RevInfo{
+			IfID:         101,
+			RawIsdas:     ia.IAInt(),
+			LinkType:     proto.LinkType_peer,
+			RawTimestamp: util.TimeToSecs(time.Now().Add(-500 * time.Millisecond)),
+			RawTTL:       10,
+		})
+		intfs.Get(101).Revoke(srev)
+		revoker := testRevoker(intfs, msger, signer, topoProvider)
+		ctx, cancelF := context.WithTimeout(context.Background(), timeout)
+		defer cancelF()
+		revoker.Run(ctx)
+		// gomock tests that no calls to the messenger are made.
+		SoMsg("Revocation should be same", intfs.Get(101).Revocation(), ShouldEqual, srev)
+		checkInterfaces(intfs, map[common.IFIDType]State{101: Revoked})
+	})
+}
+
+// TestRevokedInterfaceRevokedAgain test that if an interface was revoked and the overlap period
+// started it should be revoked again.
+func TestRevokedInterfaceRevokedAgain(t *testing.T) {
+	topoProvider := xtest.TopoProviderFromFile(t, "testdata/topology.json")
+	pub, priv, err := scrypto.GenKeyPair(scrypto.Ed25519)
+	xtest.FailOnErr(t, err)
+	signer := createTestSigner(t, priv)
+	Convey("TestRevokedInterfaceRevokedAgain", t, func() {
+		mctrl := gomock.NewController(t)
+		defer mctrl.Finish()
+		msger := mock_infra.NewMockMessenger(mctrl)
+		intfs := NewInterfaces(topoProvider.Get().IFInfoMap, Config{})
+		activateAll(intfs)
+		intfs.Get(101).state = Expired
+		srev := toSigned(t, &path_mgmt.RevInfo{
+			IfID:         101,
+			RawIsdas:     ia.IAInt(),
+			LinkType:     proto.LinkType_peer,
+			RawTimestamp: util.TimeToSecs(time.Now().Add(-6 * time.Second)),
+			RawTTL:       10,
+		})
+		intfs.Get(101).Revoke(srev)
+		checkSentMessages := expectMessengerCalls(msger, 101, topoProvider)
+		revoker := testRevoker(intfs, msger, signer, topoProvider)
+		ctx, cancelF := context.WithTimeout(context.Background(), timeout)
+		defer cancelF()
+		revoker.Run(ctx)
+		// gomock tests that no calls to the messenger are made.
+		checkInterfaces(intfs, map[common.IFIDType]State{101: Revoked})
+		SoMsg("Revocation should be different", intfs.Get(101).Revocation(), ShouldNotEqual, srev)
+		checkSentMessages(t, revVerifier(pub))
+	})
+}
+
+// TODO(lukedirtwalker): test revoking multiple interfaces at once.
+
+func expectMessengerCalls(msger *mock_infra.MockMessenger,
+	revokedIfId common.IFIDType, topoProvider TopoProvider) func(*testing.T, revVerifier) {
+
+	var brMsgs []brMsg
+	var brMsgsMtx sync.Mutex
+	msger.EXPECT().SendIfStateInfos(gomock.Any(),
+		gomock.Any(), gomock.Any(), gomock.Any()).Times(len(topoProvider.Get().BR)).DoAndReturn(
+		func(_ context.Context, msg *path_mgmt.IFStateInfos, a net.Addr, _ uint64) error {
+			brMsgsMtx.Lock()
+			defer brMsgsMtx.Unlock()
+			brMsgs = append(brMsgs, brMsg{msg: msg, a: a})
+			return nil
+		})
+	var psAddr net.Addr
+	var psMsg *path_mgmt.SignedRevInfo
+	msger.EXPECT().SendRev(gomock.Any(), gomock.Any(), gomock.Any(),
+		gomock.Any()).DoAndReturn(
+		func(_ context.Context, msg *path_mgmt.SignedRevInfo, a net.Addr, _ uint64) error {
+			psAddr = a
+			psMsg = msg
+			return nil
+		})
+	return func(t *testing.T, verifier revVerifier) {
+		Convey("Check sent BR messages", func() {
+			SoMsg("Should send correct amount of messages", len(brMsgs),
+				ShouldEqual, len(topoProvider.Get().BR))
+			sentBRs := expectedBRs(topoProvider)
+			for _, brMsg := range brMsgs {
+				brName := brId(t, topoProvider, brMsg.a.(*snet.Addr))
+				delete(sentBRs, brName)
+				checkBRMessage(t, brName, brMsg.msg, revokedIfId, verifier, topoProvider)
+			}
+			SoMsg("Should have sent to all brs", sentBRs, ShouldBeEmpty)
+		})
+		Convey("Check sent PS message", func() {
+			saddr := psAddr.(*snet.Addr)
+			SoMsg("Should send to local IA", saddr.IA, ShouldResemble, ia)
+			topo := topoProvider.Get()
+			isPsAddr := false
+			for _, tAddr := range topo.PS {
+				if tAddr.PublicAddr(topo.Overlay).Equal(saddr.Host) {
+					isPsAddr = true
+					break
+				}
+			}
+			SoMsg("Should send to PS", isPsAddr, ShouldBeTrue)
+			checkRevocation(t, psMsg, revokedIfId, verifier, topoProvider)
+		})
+	}
+}
+
+func checkBRMessage(t *testing.T, brId string, infos *path_mgmt.IFStateInfos,
+	revokedIfId common.IFIDType, verifier revVerifier, topoProvider TopoProvider) {
+
+	Convey(fmt.Sprintf("Check ifstateinfo for %s", brId), func() {
+		SoMsg("Should contain correct amount of infos", len(infos.Infos), ShouldEqual, 1)
+		SoMsg("Correct ifid", infos.Infos[0].IfID, ShouldEqual, revokedIfId)
+		SoMsg("Not active", infos.Infos[0].Active, ShouldBeFalse)
+		checkRevocation(t, infos.Infos[0].SRevInfo, revokedIfId, verifier, topoProvider)
+	})
+}
+
+func checkRevocation(t *testing.T, srev *path_mgmt.SignedRevInfo,
+	revokedIfId common.IFIDType, verifier revVerifier, topoProvider TopoProvider) {
+
+	Convey("Check revocation", func() {
+		verifier.Verify(t, srev)
+		revInfo, err := srev.RevInfo()
+		xtest.FailOnErr(t, err)
+		SoMsg("correct ifId", revInfo.IfID, ShouldEqual, revokedIfId)
+		SoMsg("correct IA", revInfo.RawIsdas, ShouldEqual, ia.IAInt())
+		SoMsg("correct linkType", revInfo.LinkType,
+			ShouldEqual, topoProvider.Get().IFInfoMap[revokedIfId].LinkType)
+		rawNow := util.TimeToSecs(time.Now())
+		SoMsg("recent revocation", revInfo.RawTimestamp, ShouldBeBetween, rawNow-1, rawNow+1)
+		SoMsg("minTTL", revInfo.RawTTL, ShouldEqual, uint32(path_mgmt.MinRevTTL.Seconds()))
+	})
+}
+
+func checkInterfaces(intfs *Interfaces, nonActive map[common.IFIDType]State) {
+	Convey("Check interface state", func() {
+		for ifid, intf := range intfs.All() {
+			expectedState := Active
+			if st, ok := nonActive[ifid]; ok {
+				expectedState = st
+			}
+			SoMsg(fmt.Sprintf("Intf %d state", ifid), intf.State(), ShouldEqual, expectedState)
+		}
+	})
+}
+
+func brId(t *testing.T, topoProvider TopoProvider, saddr *snet.Addr) string {
+	topo := topoProvider.Get()
+	for brId, brInfo := range topo.BR {
+		if brInfo.CtrlAddrs.PublicAddr(topo.Overlay).Equal(saddr.Host) {
+			return brId
+		}
+	}
+	t.Fatalf("Didn't find br ID for %s", saddr.Host)
+	return "" // meh, makes the compiler happy.
+}
+
+// expectedBRs return a set of BR ids for which we expect a if state update push.
+func expectedBRs(topoProvider TopoProvider) map[string]struct{} {
+	brIds := make(map[string]struct{})
+	for brId := range topoProvider.Get().BR {
+		brIds[brId] = struct{}{}
+	}
+	return brIds
+}
+
+func toSigned(t *testing.T, r *path_mgmt.RevInfo) *path_mgmt.SignedRevInfo {
+	t.Helper()
+	sr, err := path_mgmt.NewSignedRevInfo(r, nil)
+	xtest.FailOnErr(t, err)
+	return sr
+}
+
+func activateAll(intfs *Interfaces) {
+	for _, intf := range intfs.All() {
+		intf.Activate(42)
+	}
+}
+
+func testRevoker(intfs *Interfaces, msger infra.Messenger, signer infra.Signer,
+	topoProvider TopoProvider) *Revoker {
+
+	return NewRevoker(intfs, msger, signer, RevConfig{RevOverlap: overlapTime}, topoProvider)
+}
+
+func createTestSigner(t *testing.T, key common.RawBytes) infra.Signer {
+	signer, err := trust.NewBasicSigner(key, infra.SignerMeta{
+		Src: ctrl.SignSrcDef{
+			IA:       xtest.MustParseIA("1-ff00:0:84"),
+			ChainVer: 42,
+			TRCVer:   21,
+		},
+		Algo: scrypto.Ed25519,
+	})
+	xtest.FailOnErr(t, err)
+	return signer
+}
+
+type revVerifier common.RawBytes
+
+func (v revVerifier) Verify(t *testing.T, srev *path_mgmt.SignedRevInfo) {
+	sign := srev.Sign
+	err := scrypto.Verify(sign.SigInput(srev.Blob, false), sign.Signature,
+		common.RawBytes(v), scrypto.Ed25519)
+	xtest.FailOnErr(t, err)
+}
