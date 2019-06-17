462ed6a706ffa74bf090cf7b87baa4126d48378d
Sergiu Costea <sergiu.costea@gmail.com>
2019-06-17 14:58:51 +0200

    Refactor reconnecting logic as a dispatcher service
    This simplifies how reconnecting logic is added to connections inside
    snet.

--- go/lib/snet/snetproxy/io.go
+++ go/lib/snet/snetproxy/io.go
@@ -14,9 +14,7 @@
 
 package snetproxy
 
-import (
-	"github.com/scionproto/scion/go/lib/snet"
-)
+import "net"
 
 // IOOperation provides an abstraction around any Conn reads and writes.  Types
 // that implement this interface contain the Read/Write arguments and return
@@ -24,7 +22,7 @@ import (
 // function without caring what it is.
 type IOOperation interface {
 	// Runs the I/O operation on conn
-	Do(conn snet.Conn) error
+	Do(conn net.PacketConn) error
 	// IsWrite returns true for types implementing write operations
 	IsWrite() bool
 }
@@ -38,33 +36,17 @@ type WriteOperation struct {
 	BaseOperation
 }
 
-func (op *WriteOperation) Do(conn snet.Conn) error {
-	n, err := conn.Write(op.buffer)
-	op.numBytes = n
-	return err
-}
-
 func (_ *WriteOperation) IsWrite() bool {
 	return true
 }
 
 type WriteToOperation struct {
 	WriteOperation
-	address *snet.Addr
+	address net.Addr
 }
 
-func (op *WriteToOperation) Do(conn snet.Conn) error {
+func (op *WriteToOperation) Do(conn net.PacketConn) error {
 	n, err := conn.WriteTo(op.buffer, op.address)
-	op.numBytes = n
-	return err
-}
-
-type WriteToSCIONOperation struct {
-	WriteToOperation
-}
-
-func (op *WriteToSCIONOperation) Do(conn snet.Conn) error {
-	n, err := conn.WriteToSCION(op.buffer, op.address)
 	op.numBytes = n
 	return err
 }
@@ -73,34 +55,17 @@ type ReadOperation struct {
 	BaseOperation
 }
 
-func (op *ReadOperation) Do(conn snet.Conn) error {
-	n, err := conn.Read(op.buffer)
-	op.numBytes = n
-	return err
-}
-
 func (_ *ReadOperation) IsWrite() bool {
 	return false
 }
 
 type ReadFromOperation struct {
 	ReadOperation
-	address *snet.Addr
+	address net.Addr
 }
 
-func (op *ReadFromOperation) Do(conn snet.Conn) error {
+func (op *ReadFromOperation) Do(conn net.PacketConn) error {
 	n, address, err := conn.ReadFrom(op.buffer)
-	op.numBytes = n
-	op.address = address.(*snet.Addr)
-	return err
-}
-
-type ReadFromSCIONOperation struct {
-	ReadFromOperation
-}
-
-func (op *ReadFromSCIONOperation) Do(conn snet.Conn) error {
-	n, address, err := conn.ReadFromSCION(op.buffer)
 	op.numBytes = n
 	op.address = address
 	return err
