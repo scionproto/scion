a7209333ef1efb5d74e1f9a500c925a213312524
Oncilla <roos@anapaya.net>
2019-07-15 13:52:55 +0200

    crypto: Introduce validity period (#2842)
    This change introduces the validity period.
    This will be used for the new TRC and certificate format.
    
    Additionally, adds unix time that marshals and unmarshals in the correct
    format (64 bit uint seconds since epoch).

---
+++ go/lib/scrypto/validity_test.go
@@ -0,0 +1,116 @@
+// Copyright 2019 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scrypto_test
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/scionproto/scion/go/lib/scrypto"
+	"github.com/scionproto/scion/go/lib/util"
+)
+
+func TestValidityContains(t *testing.T) {
+	now := time.Now()
+	validity := scrypto.Validity{
+		NotBefore: util.UnixTime{Time: now},
+		NotAfter:  util.UnixTime{Time: now.Add(time.Minute)},
+	}
+	tests := map[string]struct {
+		Time      time.Time
+		Contained bool
+	}{
+		"Before": {
+			Time: now.Add(-time.Minute),
+		},
+		"Same as NotBefore": {
+			Time:      now,
+			Contained: true,
+		},
+		"Between NotBefore and NotAfter": {
+			Time:      now.Add(30 * time.Second),
+			Contained: true,
+		},
+		"Same as NotAfter": {
+			Time:      now.Add(time.Minute),
+			Contained: true,
+		},
+		"After": {
+			Time: now.Add(time.Hour),
+		},
+	}
+	for name, test := range tests {
+		assert.Equal(t, test.Contained, validity.Contains(test.Time), name)
+	}
+}
+
+func TestValidityUnmarshal(t *testing.T) {
+	tests := map[string]struct {
+		Input  string
+		Assert assert.ErrorAssertionFunc
+	}{
+		"invalid type": {
+			Input:  `{"a": 10}`,
+			Assert: assert.Error,
+		},
+		"NotBefore missing": {
+			Input:  `{"NotAfter": 1356134400}`,
+			Assert: assert.Error,
+		},
+		"NotAfter missing": {
+			Input:  `{"NotBefore": 1356048000}`,
+			Assert: assert.Error,
+		},
+		"correct": {
+			Input: `
+			{
+				"NotBefore": 1356048000,
+				"NotAfter": 1356134400
+			}
+			`,
+			Assert: assert.NoError,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			var v scrypto.Validity
+			err := json.Unmarshal([]byte(test.Input), &v)
+			test.Assert(t, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, util.SecsToTime(1356048000), v.NotBefore.Time)
+			assert.Equal(t, util.SecsToTime(1356134400), v.NotAfter.Time)
+		})
+	}
+}
+
+func TestValidityMarshal(t *testing.T) {
+	s := struct {
+		Validity scrypto.Validity
+	}{
+		Validity: scrypto.Validity{
+			NotBefore: util.UnixTime{Time: util.SecsToTime(1356048000)},
+			NotAfter:  util.UnixTime{Time: util.SecsToTime(1356134400)},
+		},
+	}
+	raw, err := json.Marshal(s)
+	require.NoError(t, err)
+	assert.Equal(t, `{"Validity":{"NotBefore":1356048000,"NotAfter":1356134400}}`, string(raw))
+}
--- go/lib/util/time.go
+++ go/lib/util/time.go
@@ -16,10 +16,41 @@
 package util
 
 import (
+	"encoding/json"
+	"strconv"
 	"time"
 
 	"github.com/scionproto/scion/go/lib/common"
 )
+
+var _ json.Marshaler = (*UnixTime)(nil)
+var _ json.Unmarshaler = (*UnixTime)(nil)
+
+// UnixTime allows parsing and packing timestamps in seconds since epoch.
+type UnixTime struct {
+	time.Time
+}
+
+func (t *UnixTime) UnmarshalJSON(b []byte) error {
+	var seconds uint64
+	// Only allow up to 63-bit to avoid wrap around.
+	seconds, err := strconv.ParseUint(string(b), 10, 63)
+	if err != nil {
+		return err
+	}
+	t.Time = time.Unix(int64(seconds), 0)
+	return nil
+}
+
+// MarshalJSON marshals the time as seconds since unix epoch. This must be a
+// value receiver.
+func (t UnixTime) MarshalJSON() ([]byte, error) {
+	return []byte(strconv.FormatUint(uint64(t.Unix()), 10)), nil
+}
+
+func (t *UnixTime) String() string {
+	return TimeToCompact(t.Time)
+}
 
 // SecsToTime takes seconds stored in a uint32.
 func SecsToTime(t uint32) time.Time {
