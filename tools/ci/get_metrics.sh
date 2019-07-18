eeeeca8fc4ce3862e9eca3b93b021981c5190449
Hermann <schweizer@anapaya.net>
2019-07-18 10:41:09 +0200

    metrics collection, fail if prometheus not in toml. Refactored

--- tools/ci/get_metrics.sh
+++ tools/ci/get_metrics.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 ## Assumes that all services are running as described in the gen and scrapes their metrics
 
+set -e
+
 :> metrics_locations
 
 echo "Collecting Metrics from all scion services in gen"
 
-# Grep out ID and Prom IP/port of all toml files containing prom config.
-while IFS= read -r line
-do
-    grep "ID =" $line | awk '{print $3}' >> metrics_locations
-    grep "Prometheus =" $line | awk '{print $3}' >> metrics_locations
-done < <(grep -rl 'Prometheus = ' gen)
+# Fails if a toml (disp is the exception) in gen doesn't contain metrics->Prometheus or general->ID
+for i in $(find gen/ -iname '*.toml'); do
+    case $i in
+       */disp.toml) id=$(sed -n '/^\[dispatcher\]/,/^$/p' "$i" | awk '/^ID = / {print $3}') 
+       *) id=$(sed -n '/^\[general\]/,/^$/p' "$i" | awk '/^ID = / {print $3}')
+    esac
+    prom=$(sed -n '/^\[metrics\]/,/^$/p' "$i" | awk '/^Prometheus = / {print $3}')
+    echo "${id:?} ${prom:?}" | tr -d '"[]'
+done > metrics_locations
 
-# Gather metrics from the services found
-
-while IFS= read -r ID
-do
-    read -r PROM_IP_PORT
-    # Remove unneccesary chars from ID and PROM_IP_PORT
-    ID=${ID//\"}
-    PROM_IP_PORT=${PROM_IP_PORT//\"}
-    PROM_IP_PORT=${PROM_IP_PORT//\[}
-    PROM_IP_PORT=${PROM_IP_PORT//\]}
-    curl -s $PROM_IP_PORT/metrics > "/home/scion/go/src/github.com/scionproto/scion/metrics/${ID}"
+# Gather metrics from all services that are running
+while read id prom_ip_port; do
+    curl -sS $prom_ip_port/metrics > "/home/scion/go/src/github.com/scionproto/scion/metrics/${id}" || true
 done < metrics_locations
 rm metrics_locations
