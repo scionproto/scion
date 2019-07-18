dbcb08b42e9f84e5c5d15458886d9490b027ca6a
Hermann <schweizer@anapaya.net>
2019-07-18 10:55:08 +0200

    case statement add ;;

--- tools/ci/get_metrics.sh
+++ tools/ci/get_metrics.sh
@@ -10,8 +10,8 @@ echo "Collecting Metrics from all scion services in gen"
 # Fails if a toml (disp is the exception) in gen doesn't contain metrics->Prometheus or general->ID
 for i in $(find gen/ -iname '*.toml'); do
     case $i in
-       */disp.toml) id=$(sed -n '/^\[dispatcher\]/,/^$/p' "$i" | awk '/^ID = / {print $3}') 
-       *) id=$(sed -n '/^\[general\]/,/^$/p' "$i" | awk '/^ID = / {print $3}')
+       */disp.toml) id=$(sed -n '/^\[dispatcher\]/,/^$/p' "$i" | awk '/^ID = / {print $3}');; 
+       *) id=$(sed -n '/^\[general\]/,/^$/p' "$i" | awk '/^ID = / {print $3}');;
     esac
     prom=$(sed -n '/^\[metrics\]/,/^$/p' "$i" | awk '/^Prometheus = / {print $3}')
     echo "${id:?} ${prom:?}" | tr -d '"[]'
