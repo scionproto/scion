ec767c181ac8c2c48d50bace6f48400181fb2645
chaehni <chaehni@student.ethz.ch>
2019-07-19 13:47:59 +0200

    HiddenPath: Design Document (#2833)

--- tools/md/mdlintstyle.rb
+++ tools/md/mdlintstyle.rb
@@ -4,3 +4,4 @@ rule 'MD013', :code_blocks => false
 rule 'MD013', :tables => false
 rule 'MD007', :indent => 4 # see https://github.com/markdownlint/markdownlint/issues/139
 rule 'MD024', :allow_different_nesting => true
+exclude_rule 'MD033'
