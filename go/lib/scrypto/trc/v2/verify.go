fbdddcc5bcaae6d25d1d5689ce33ae81420e55f7
Oncilla <roos@anapaya.net>
2019-07-25 09:35:40 +0200

    TRC: Add update signature verification (#2899)
    This PR adds TRC update signature verification.
    
    fixes #2851

---
+++ go/lib/scrypto/trc/v2/verify.go
@@ -0,0 +1,218 @@
+// Copyright 2019 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package trc
+
+import (
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/scrypto"
+)
+
+const (
+	// DecodeProtectedFailed indicates the signatrue metadata fails to decode.
+	DecodeProtectedFailed = "unable to decode protected signature metadata"
+	// UnexpectedVoteSignature indicates an unexpected voting signature.
+	UnexpectedVoteSignature = "unexpected vote signature"
+	// UnexpectedPOPSignature indicates an unexpected proof of possession signature.
+	UnexpectedPOPSignature = "unexpected proof of possession signature"
+	// InvalidProtected indicates that the protected signature metadata is invalid.
+	InvalidProtected = "invalid protected signature metadata"
+	// MissingVoteSignature indicates a voting signature of an AS is missing.
+	MissingVoteSignature = "missing vote signature"
+	// MissingPOPSignature indicates a missing proof of possession signature.
+	MissingPOPSignature = "missing proof of possession signature"
+	// VoteVerificationError indicates the signature verification of a vote failed.
+	VoteVerificationError = "vote signature verification error"
+	// POPVerificationError indicates the signature verification of a proof of possession failed.
+	POPVerificationError = "proof of possession signature verification error"
+	// DuplicateVoteSignature indicates a duplicate voting signature for the same AS.
+	DuplicateVoteSignature = "duplicate vote signature"
+	// DuplicatePOPSignature indicates a duplicate proof of possession signature
+	// for the same AS and key type.
+	DuplicatePOPSignature = "duplicate proof of possession signature"
+)
+
+// UpdateVerifier verifies a signed TRC update. The caller must first use the
+// UpdateValidator to check the update validity. UpdateVerifier simply checks
+// that the signatures are verifiable.
+type UpdateVerifier struct {
+	// Prev is the previous TRC. Its version must be Next.Version - 1.
+	Prev *TRC
+	// Next is the updated TRC.
+	Next *TRC
+	// NextEncoded is the encoded next TRC used for signature input.
+	NextEncoded Encoded
+	// Signatures contains all signatures attached to the new TRC.
+	Signatures []Signature
+}
+
+// Verify checks that all signatures mentioned in the next TRC are present and
+// verifiable, and that no others are attached.
+func (v UpdateVerifier) Verify() error {
+	votes, pops, err := v.decodeSignatures()
+	if err != nil {
+		return common.NewBasicError(DecodeProtectedFailed, err)
+	}
+	if err := v.checkVotes(votes); err != nil {
+		return err
+	}
+	if err := v.checkPOPs(pops); err != nil {
+		return err
+	}
+	if err := v.verifyVotes(votes); err != nil {
+		return err
+	}
+	if err := v.verifyPOPs(pops); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v UpdateVerifier) decodeSignatures() (map[addr.AS]DecodedSignature,
+	map[addr.AS]map[KeyType]DecodedSignature, error) {
+
+	votes := make(map[addr.AS]DecodedSignature)
+	pops := make(map[addr.AS]map[KeyType]DecodedSignature)
+
+	sigs := make([]DecodedSignature, 0, len(v.Signatures))
+	for _, sig := range v.Signatures {
+		prot, err := sig.EncodedProtected.Decode()
+		if err != nil {
+			return nil, nil, err
+		}
+		sigs = append(sigs, DecodedSignature{
+			EncodedProtected: sig.EncodedProtected,
+			Protected:        prot,
+			Signature:        sig.Signature,
+		})
+	}
+	for _, sig := range sigs {
+		switch sig.Protected.Type {
+		case VoteSignature:
+			if _, ok := votes[sig.Protected.AS]; ok {
+				return nil, nil, common.NewBasicError(DuplicateVoteSignature, nil,
+					"AS", sig.Protected.AS)
+			}
+			votes[sig.Protected.AS] = sig
+		case POPSignature:
+			if _, ok := pops[sig.Protected.AS][sig.Protected.KeyType]; ok {
+				return nil, nil, common.NewBasicError(DuplicatePOPSignature, nil,
+					"AS", sig.Protected.AS, "keyType", sig.Protected.KeyType)
+			}
+			if _, ok := pops[sig.Protected.AS]; !ok {
+				pops[sig.Protected.AS] = make(map[KeyType]DecodedSignature)
+			}
+			pops[sig.Protected.AS][sig.Protected.KeyType] = sig
+		}
+	}
+	return votes, pops, nil
+}
+
+func (v UpdateVerifier) checkVotes(sigs map[addr.AS]DecodedSignature) error {
+	for as, sig := range sigs {
+		vote, ok := v.Next.Votes[as]
+		if !ok {
+			return common.NewBasicError(UnexpectedVoteSignature, nil, "AS", as)
+		}
+		expected := Protected{
+			Algorithm:  v.Prev.PrimaryASes[as].Keys[vote.Type].Algorithm,
+			Type:       VoteSignature,
+			KeyType:    vote.Type,
+			KeyVersion: vote.KeyVersion,
+			AS:         as,
+		}
+		if sig.Protected != expected {
+			return common.NewBasicError(InvalidProtected, nil,
+				"expected", expected, "actual", sig.Protected)
+		}
+	}
+	for as := range v.Next.Votes {
+		if _, ok := sigs[as]; !ok {
+			return common.NewBasicError(MissingVoteSignature, nil, "AS", as)
+		}
+	}
+	return nil
+}
+
+func (v UpdateVerifier) verifyVotes(sigs map[addr.AS]DecodedSignature) error {
+	for as, sig := range sigs {
+		meta := v.Prev.PrimaryASes[as].Keys[sig.Protected.KeyType]
+		input := SigInput(sig.EncodedProtected, v.NextEncoded)
+		if err := scrypto.Verify(input, sig.Signature, meta.Key, meta.Algorithm); err != nil {
+			return common.NewBasicError(VoteVerificationError, err, "AS", as, "meta", meta)
+		}
+	}
+	return nil
+}
+
+func (v UpdateVerifier) checkPOPs(sigs map[addr.AS]map[KeyType]DecodedSignature) error {
+	for as, pops := range sigs {
+		for _, sig := range pops {
+			if !containsKeyType(sig.Protected.KeyType, v.Next.ProofOfPossession[as]) {
+				return common.NewBasicError(UnexpectedPOPSignature, nil,
+					"AS", as, "keyType", sig.Protected.KeyType)
+			}
+			meta := v.Next.PrimaryASes[as].Keys[sig.Protected.KeyType]
+			expected := Protected{
+				Algorithm:  meta.Algorithm,
+				Type:       POPSignature,
+				KeyType:    sig.Protected.KeyType,
+				KeyVersion: meta.KeyVersion,
+				AS:         as,
+			}
+			if sig.Protected != expected {
+				return common.NewBasicError(InvalidProtected, nil,
+					"expected", expected, "actual", sig.Protected)
+			}
+		}
+	}
+	for as, keyTypes := range v.Next.ProofOfPossession {
+		for _, keyType := range keyTypes {
+			if _, ok := sigs[as][keyType]; !ok {
+				return common.NewBasicError(MissingPOPSignature, nil, "AS", as, "keyType", keyType)
+			}
+		}
+	}
+	return nil
+}
+
+func (v UpdateVerifier) verifyPOPs(sigs map[addr.AS]map[KeyType]DecodedSignature) error {
+	for as, pops := range sigs {
+		for keyType, sig := range pops {
+			meta := v.Next.PrimaryASes[as].Keys[keyType]
+			input := SigInput(sig.EncodedProtected, v.NextEncoded)
+			if err := scrypto.Verify(input, sig.Signature, meta.Key, meta.Algorithm); err != nil {
+				return common.NewBasicError(POPVerificationError, err, "AS", as, "keyType", keyType)
+			}
+		}
+	}
+	return nil
+}
+
+// DecodedSignature holds the signature with the decoded protected meta data.
+type DecodedSignature struct {
+	EncodedProtected EncodedProtected
+	Protected        Protected
+	Signature        []byte
+}
+
+func containsKeyType(keyType KeyType, keyTypes []KeyType) bool {
+	for _, t := range keyTypes {
+		if t == keyType {
+			return true
+		}
+	}
+	return false
+}
---
+++ go/lib/scrypto/trc/v2/verify_test.go
@@ -0,0 +1,323 @@
+// Copyright 2019 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package trc_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/scrypto"
+	trc "github.com/scionproto/scion/go/lib/scrypto/trc/v2"
+	"github.com/scionproto/scion/go/lib/xtest"
+)
+
+func TestUpdateVerifierVerify(t *testing.T) {
+
+	recodeProtected := func(t *testing.T, sig *trc.Signature, modify func(*trc.Protected)) {
+		p, err := sig.EncodedProtected.Decode()
+		require.NoError(t, err)
+		modify(&p)
+		sig.EncodedProtected, err = trc.EncodeProtected(p)
+		require.NoError(t, err)
+	}
+
+	tests := map[string]struct {
+		Modify         func(*testing.T, *[]trc.Signature)
+		ExpectedErrMsg string
+	}{
+		"Valid update": {
+			Modify: func(_ *testing.T, _ *[]trc.Signature) {},
+		},
+		"Undecodable": {
+			Modify: func(_ *testing.T, sigs *[]trc.Signature) {
+				(*sigs)[0].EncodedProtected[1] = '!'
+			},
+			ExpectedErrMsg: "illegal base64 data",
+		},
+		"Unexpected vote signature": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				protected := trc.Protected{
+					Algorithm:  scrypto.Ed25519,
+					Type:       trc.VoteSignature,
+					KeyType:    trc.OnlineKey,
+					KeyVersion: 1,
+					AS:         a190,
+				}
+				encProtected, err := trc.EncodeProtected(protected)
+				require.NoError(t, err)
+				*sigs = append(*sigs, trc.Signature{
+					EncodedProtected: encProtected,
+					Signature: xtest.MustParseHexString("e2a08be2a081e2a085e2a0912" +
+						"0e2a08ee2a08ae2a09be2a09de2a081e2a09ee2a0a5e2a097e2a091"),
+				})
+			},
+			ExpectedErrMsg: trc.UnexpectedVoteSignature,
+		},
+		"Unexpected proof of possession signature": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				protected := trc.Protected{
+					Algorithm:  scrypto.Ed25519,
+					Type:       trc.POPSignature,
+					KeyType:    trc.OnlineKey,
+					KeyVersion: 1,
+					AS:         a190,
+				}
+				encProtected, err := trc.EncodeProtected(protected)
+				require.NoError(t, err)
+				*sigs = append(*sigs, trc.Signature{
+					EncodedProtected: encProtected,
+					Signature: xtest.MustParseHexString("e2a09de2a095e2a09e20e2a08" +
+						"de2a08ae2a09de2a091"),
+				})
+			},
+			ExpectedErrMsg: trc.UnexpectedPOPSignature,
+		},
+		"Duplicate vote": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				i := findSignature(t, *sigs, a110, trc.VoteSignature)
+				*sigs = append((*sigs), (*sigs)[i])
+			},
+			ExpectedErrMsg: trc.DuplicateVoteSignature,
+		},
+		"Duplicate proof of possession": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				i := findSignature(t, *sigs, a110, trc.POPSignature)
+				*sigs = append((*sigs), (*sigs)[i])
+			},
+			ExpectedErrMsg: trc.DuplicatePOPSignature,
+		},
+		"Missing vote": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				i := findSignature(t, *sigs, a110, trc.VoteSignature)
+				*sigs = append((*sigs)[:i], (*sigs)[i+1:]...)
+			},
+			ExpectedErrMsg: trc.MissingVoteSignature,
+		},
+		"Missing proof of possession": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				i := findSignature(t, *sigs, a110, trc.POPSignature)
+				*sigs = append((*sigs)[:i], (*sigs)[i+1:]...)
+			},
+			ExpectedErrMsg: trc.MissingPOPSignature,
+		},
+		"Vote wrong Algorithm": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				sig := &(*sigs)[findSignature(t, *sigs, a110, trc.VoteSignature)]
+				recodeProtected(t, sig, func(p *trc.Protected) {
+					p.Algorithm = "invalid"
+				})
+			},
+			ExpectedErrMsg: trc.InvalidProtected,
+		},
+		"Vote wrong KeyType": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				sig := &(*sigs)[findSignature(t, *sigs, a110, trc.VoteSignature)]
+				recodeProtected(t, sig, func(p *trc.Protected) {
+					p.KeyType = trc.OnlineKey
+				})
+			},
+			ExpectedErrMsg: trc.InvalidProtected,
+		},
+		"Vote wrong KeyVersion": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				sig := &(*sigs)[findSignature(t, *sigs, a110, trc.VoteSignature)]
+				recodeProtected(t, sig, func(p *trc.Protected) {
+					p.KeyVersion += 1
+				})
+			},
+			ExpectedErrMsg: trc.InvalidProtected,
+		},
+		"Proof of possession wrong Algorithm": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				sig := &(*sigs)[findSignature(t, *sigs, a110, trc.POPSignature)]
+				recodeProtected(t, sig, func(p *trc.Protected) {
+					p.Algorithm = "invalid"
+				})
+			},
+			ExpectedErrMsg: trc.InvalidProtected,
+		},
+		// A wrong KeyType would be caught by a previous check.
+		"Proof of possession wrong KeyVersion": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				sig := &(*sigs)[findSignature(t, *sigs, a110, trc.POPSignature)]
+				recodeProtected(t, sig, func(p *trc.Protected) {
+					p.KeyVersion += 1
+				})
+			},
+			ExpectedErrMsg: trc.InvalidProtected,
+		},
+		"Mangled Vote signature": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				sig := &(*sigs)[findSignature(t, *sigs, a110, trc.VoteSignature)]
+				sig.Signature[0] ^= 0xFF
+			},
+			ExpectedErrMsg: trc.VoteVerificationError,
+		},
+		"Mangled proof of possession signature": {
+			Modify: func(t *testing.T, sigs *[]trc.Signature) {
+				sig := &(*sigs)[findSignature(t, *sigs, a110, trc.POPSignature)]
+				sig.Signature[0] ^= 0xFF
+			},
+			ExpectedErrMsg: trc.POPVerificationError,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			next, prev := newRegularUpdate()
+			meta := next.PrimaryASes[a110].Keys[trc.OnlineKey]
+			meta.KeyVersion += 1
+			next.PrimaryASes[a110].Keys[trc.OnlineKey] = meta
+			next.ProofOfPossession[a110] = append(next.ProofOfPossession[a110], trc.OnlineKey)
+			vote := next.Votes[a110]
+			vote.Type = trc.OfflineKey
+			next.Votes[a110] = vote
+
+			meta = next.PrimaryASes[a130].Keys[trc.IssuingKey]
+			meta.KeyVersion += 1
+			next.PrimaryASes[a130].Keys[trc.IssuingKey] = meta
+			next.ProofOfPossession[a130] = append(next.ProofOfPossession[a130], trc.IssuingKey)
+
+			nextKeys, prevKeys := generateKeys(t, next, prev)
+			signed := signTRC(t, next, prev, nextKeys, prevKeys)
+			test.Modify(t, &(signed.Signatures))
+
+			u := trc.UpdateValidator{
+				Prev: prev,
+				Next: next,
+			}
+			_, err := u.Validate()
+			require.NoError(t, err)
+
+			v := trc.UpdateVerifier{
+				Prev:        prev,
+				Next:        next,
+				NextEncoded: signed.EncodedTRC,
+				Signatures:  signed.Signatures,
+			}
+			err = v.Verify()
+			if test.ExpectedErrMsg == "" {
+				assert.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), test.ExpectedErrMsg)
+			}
+		})
+	}
+}
+
+func findSignature(t *testing.T, sigs []trc.Signature, as addr.AS, sigType trc.SignatureType) int {
+	for i, sig := range sigs {
+		p, err := sig.EncodedProtected.Decode()
+		require.NoError(t, err)
+		if p.AS == as && p.Type == sigType {
+			return i
+		}
+	}
+	return -1
+}
+
+func generateKeys(t *testing.T, next, prev *trc.TRC) (map[addr.AS]map[trc.KeyType][]byte,
+	map[addr.AS]map[trc.KeyType][]byte) {
+
+	prevKeys := make(map[addr.AS]map[trc.KeyType][]byte)
+	for as, primary := range prev.PrimaryASes {
+		prevKeys[as] = make(map[trc.KeyType][]byte)
+		for keyType, meta := range primary.Keys {
+			pub, priv, err := scrypto.GenKeyPair(meta.Algorithm)
+			require.NoError(t, err)
+			prevKeys[as][keyType] = priv
+			meta.Key = pub
+			prev.PrimaryASes[as].Keys[keyType] = meta
+		}
+	}
+
+	nextKeys := make(map[addr.AS]map[trc.KeyType][]byte)
+	for as, primary := range next.PrimaryASes {
+		nextKeys[as] = make(map[trc.KeyType][]byte)
+		for keyType, meta := range primary.Keys {
+			var pub, priv common.RawBytes
+			if meta.KeyVersion == prev.PrimaryASes[as].Keys[keyType].KeyVersion {
+				pub = prev.PrimaryASes[as].Keys[keyType].Key
+				priv = prevKeys[as][keyType]
+			} else {
+				var err error
+				pub, priv, err = scrypto.GenKeyPair(meta.Algorithm)
+				require.NoError(t, err)
+			}
+			nextKeys[as][keyType] = priv
+			meta.Key = pub
+			next.PrimaryASes[as].Keys[keyType] = meta
+		}
+	}
+	return nextKeys, prevKeys
+}
+
+func signTRC(t *testing.T, next, prev *trc.TRC, nextKeys,
+	prevKeys map[addr.AS]map[trc.KeyType][]byte) trc.Signed {
+
+	encoded, err := trc.Encode(next)
+	require.NoError(t, err)
+
+	signed := trc.Signed{
+		EncodedTRC: encoded,
+	}
+	for as, vote := range next.Votes {
+		protected := trc.Protected{
+			Algorithm:  scrypto.Ed25519,
+			Type:       trc.VoteSignature,
+			KeyType:    vote.Type,
+			KeyVersion: vote.KeyVersion,
+			AS:         as,
+		}
+		encProtected, err := trc.EncodeProtected(protected)
+		require.NoError(t, err)
+
+		sig, err := scrypto.Sign(trc.SigInput(encProtected, encoded), prevKeys[as][vote.Type],
+			scrypto.Ed25519)
+		require.NoError(t, err)
+
+		signed.Signatures = append(signed.Signatures, trc.Signature{
+			EncodedProtected: encProtected,
+			Signature:        sig,
+		})
+	}
+	for as, pops := range next.ProofOfPossession {
+		for _, keyType := range pops {
+			protected := trc.Protected{
+				Algorithm:  scrypto.Ed25519,
+				Type:       trc.POPSignature,
+				KeyType:    keyType,
+				KeyVersion: next.PrimaryASes[as].Keys[keyType].KeyVersion,
+				AS:         as,
+			}
+			encProtected, err := trc.EncodeProtected(protected)
+			require.NoError(t, err)
+
+			sig, err := scrypto.Sign(trc.SigInput(encProtected, encoded),
+				nextKeys[as][protected.KeyType], scrypto.Ed25519)
+			require.NoError(t, err)
+
+			signed.Signatures = append(signed.Signatures, trc.Signature{
+				EncodedProtected: encProtected,
+				Signature:        sig,
+			})
+		}
+	}
+	return signed
+}
