799f51def16b1111f836c048848cbeb06702f1f9
Martin Sustrik <sustrik@anapaya.net>
2019-07-30 09:50:44 +0200

    Add missing copyrights (#2937)
    These should have been added in the past few PRs.

--- go/sig/egress/interface.go
+++ go/sig/egress/interface.go
@@ -1,4 +1,5 @@
 // Copyright 2018 ETH Zurich
+// Copyright 2019 ETH Zurich, Anapaya Systems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
