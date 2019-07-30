a37e42de2bf4ae7cee6cfda86f934140634412cd
Lukas Vogel <lukedirtwalker@gmail.com>
2019-07-30 13:46:53 +0200

    Remove unused code
    That code is no longer used so we don't have to keep it around.

--- go/lib/pktcls/doc.go
+++ go/lib/pktcls/doc.go
@@ -1,4 +1,5 @@
 // Copyright 2017 ETH Zurich
+// Copyright 2019 ETH Zurich, Anapaya Systems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package class implements tools for classifying and acting on network
+// Package pktcls implements tools for classifying and acting on network
 // packets.
 //
 // A class is a named condition that exposes an Eval method; when Eval yields
@@ -28,16 +29,8 @@
 // and ToS/DSCP fields match. Multiple predicates can be checked by enumerating
 // them under AllOf or AnyOf.
 //
-// Actions are marshalable objects that describe a process. Currently, the only
-// supported actions are Path Filters (ActionFilterPaths), which are containers
-// for a spathmeta.PathPredicate object.
-//
-// Marshalable policies can be implemented by external code by mapping Cond
-// items to Action items.
-//
-// Package class supports JSON marshaling and unmarshaling of classes and
-// actions.  Due to the custom formatting of the JSON output, marshaling must
-// be done by first adding the classes and actions to a ClassMap or ActionMap,
-// respectively. Unmarshaling back to the Map is guaranteed to yield an object
-// that is identical to the initial one.
+// The package contains support for JSON marshaling and unmarshaling of
+// classes. Due to the custom formatting of the JSON output, marshaling must be
+// done by first adding the classes to a ClassMap. Unmarshaling back to the Map
+// is guaranteed to yield an object that is identical to the initial one.
 package pktcls
