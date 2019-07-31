46457881f8d338c838ff68f880ce673e005c5d13
Lukas Vogel <lukedirtwalker@gmail.com>
2019-07-31 09:40:15 +0200

    remove goconvey dep

--- go/lib/pathpol/hop_pred_test.go
+++ go/lib/pathpol/hop_pred_test.go
@@ -1,4 +1,5 @@
 // Copyright 2018 ETH Zurich
+// Copyright 2019 ETH Zurich, Anapaya Systems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,175 +19,145 @@ import (
 	"encoding/json"
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/scionproto/scion/go/lib/common"
 )
 
 func TestNewHopPredicate(t *testing.T) {
-	testCases := []struct {
-		Name  string
+	tests := map[string]struct {
 		In    string
 		HP    *HopPredicate
 		Valid bool
 	}{
-		{
-			Name:  "ISD wildcard",
+		"ISD wildcard": {
 			In:    "0",
 			HP:    &HopPredicate{ISD: 0, AS: 0, IfIDs: []common.IFIDType{0}},
 			Valid: true,
 		},
-		{
-			Name:  "AS, IF wildcard omitted",
+		"AS, IF wildcard omitted": {
 			In:    "1",
 			HP:    &HopPredicate{ISD: 1, AS: 0, IfIDs: []common.IFIDType{0}},
 			Valid: true,
 		},
-		{
-			Name:  "IF wildcard omitted",
+		"IF wildcard omitted": {
 			In:    "1-0",
 			HP:    &HopPredicate{ISD: 1, AS: 0, IfIDs: []common.IFIDType{0}},
 			Valid: true,
 		},
-		{
-			Name:  "basic wildcard",
+		"basic wildcard": {
 			In:    "1-0#0",
 			HP:    &HopPredicate{ISD: 1, AS: 0, IfIDs: []common.IFIDType{0}},
 			Valid: true,
 		},
-		{
-			Name:  "AS wildcard, interface set",
+		"AS wildcard, interface set": {
 			In:    "1-0#1",
 			Valid: false,
 		},
-		{
-			Name:  "ISD wildcard, AS set",
+		"ISD wildcard, AS set": {
 			In:    "0-1#0",
 			HP:    &HopPredicate{ISD: 0, AS: 1, IfIDs: []common.IFIDType{0}},
 			Valid: true,
 		},
-		{
-			Name:  "ISD wildcard, AS set, interface set",
+		"ISD wildcard, AS set, interface set": {
 			In:    "0-1#2",
 			HP:    &HopPredicate{ISD: 0, AS: 1, IfIDs: []common.IFIDType{2}},
 			Valid: true,
 		},
-		{
-			Name:  "ISD wildcard, AS set and interface omitted",
+		"ISD wildcard, AS set and interface omitted": {
 			In:    "0-1",
 			HP:    &HopPredicate{ISD: 0, AS: 1, IfIDs: []common.IFIDType{0}},
 			Valid: true,
 		},
-		{
-			Name:  "IF wildcard omitted, AS set",
+		"IF wildcard omitted, AS set": {
 			In:    "1-2",
 			HP:    &HopPredicate{ISD: 1, AS: 2, IfIDs: []common.IFIDType{0}},
 			Valid: true,
 		},
-		{
-			Name:  "two IfIDs",
+		"two IfIDs": {
 			In:    "1-2#3,4",
 			HP:    &HopPredicate{ISD: 1, AS: 2, IfIDs: []common.IFIDType{3, 4}},
 			Valid: true,
 		},
-		{
-			Name:  "three IfIDs",
+		"three IfIDs": {
 			In:    "1-2#3,4,5",
 			Valid: false,
 		},
-		{
-			Name:  "bad -",
+		"bad -": {
 			In:    "1-1-0",
 			Valid: false,
 		},
-		{
-			Name:  "missing AS",
+		"missing AS": {
 			In:    "1#2",
 			Valid: false,
 		},
-		{
-			Name:  "bad #",
+		"bad #": {
 			In:    "1-1#0#",
 			Valid: false,
 		},
-		{
-			Name:  "bad IF",
+		"bad IF": {
 			In:    "1-1#e",
 			Valid: false,
 		},
-		{
-			Name:  "bad second IF",
+		"bad second IF": {
 			In:    "1-2#1,3a",
 			Valid: false,
 		},
-		{
-			Name:  "AS wildcard, second IF defined",
+		"AS wildcard, second IF defined": {
 			In:    "1-0#1,3",
 			Valid: false,
 		},
-		{
-			Name:  "bad AS",
+		"bad AS": {
 			In:    "1-12323433243534#0",
 			Valid: false,
 		},
-		{
-			Name:  "bad ISD",
+		"bad ISD": {
 			In:    "1123212-23#0",
 			Valid: false,
 		},
 	}
-
-	Convey("TestNewHopPredicate", t, func() {
-		for _, tc := range testCases {
-			Convey(tc.Name, func() {
-				hp, err := HopPredicateFromString(tc.In)
-				if tc.Valid {
-					SoMsg("err", err, ShouldBeNil)
-					SoMsg("hp", hp, ShouldResemble, tc.HP)
-				} else {
-					SoMsg("err", err, ShouldNotBeNil)
-				}
-			})
-		}
-	})
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			hp, err := HopPredicateFromString(test.In)
+			if test.Valid {
+				assert.NoError(t, err)
+				assert.Equal(t, test.HP, hp)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
 }
 
 func TestHopPredicateString(t *testing.T) {
-	Convey("TestString", t, func() {
-		hp, _ := HopPredicateFromString("1-2#3,4")
-		SoMsg("hp", hp.String(), ShouldEqual, "1-2#3,4")
-	})
+	hp, _ := HopPredicateFromString("1-2#3,4")
+	assert.Equal(t, "1-2#3,4", hp.String())
 }
 
 func TestJsonConversion(t *testing.T) {
-	testCases := []struct {
+	tests := map[string]struct {
 		Name string
 		HP   *HopPredicate
 	}{
-		{
-			Name: "Normal predicate",
-			HP:   &HopPredicate{ISD: 1, AS: 2, IfIDs: []common.IFIDType{1, 2}},
+		"Normal predicate": {
+			HP: &HopPredicate{ISD: 1, AS: 2, IfIDs: []common.IFIDType{1, 2}},
 		},
-		{
-			Name: "wildcard predicate",
-			HP:   &HopPredicate{ISD: 1, AS: 2, IfIDs: []common.IFIDType{0}},
+		"wildcard predicate": {
+			HP: &HopPredicate{ISD: 1, AS: 2, IfIDs: []common.IFIDType{0}},
 		},
-		{
-			Name: "only ifids",
-			HP:   &HopPredicate{IfIDs: []common.IFIDType{0}},
+		"only ifids": {
+			HP: &HopPredicate{IfIDs: []common.IFIDType{0}},
 		},
 	}
-	Convey("TestJsonConversion", t, func() {
-		for _, tc := range testCases {
-			Convey(tc.Name, func() {
-				jsonHP, err := json.Marshal(tc.HP)
-				SoMsg("err", err, ShouldBeNil)
-
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			jsonHP, err := json.Marshal(test.HP)
+			if assert.NoError(t, err) {
 				var hp HopPredicate
 				err = json.Unmarshal(jsonHP, &hp)
-				SoMsg("err", err, ShouldBeNil)
-				SoMsg("predicate", tc.HP, ShouldResemble, &hp)
-			})
-		}
-	})
+				assert.NoError(t, err)
+				assert.Equal(t, test.HP, &hp)
+			}
+		})
+	}
 }
