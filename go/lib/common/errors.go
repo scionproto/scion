6445787216c4a83ff895626538627d2de7a64a51
Stephen Shirley <kormat@gmail.com>
2019-08-21 11:17:17 +0200

    Reresliced

--- go/lib/common/errors.go
+++ go/lib/common/errors.go
@@ -186,9 +186,9 @@ func innerFmtError(e error) ([]string, error) {
 
 // FmtErrors formats a slice of errors for logging.
 func FmtErrors(es []error) string {
-	s := make([]string, len(es))
-	for i, e := range es {
-		s[i] = e.Error()
+	s := make([]string, 0, len(es))
+	for _, e := range es {
+		s = append(s, e.Error())
 	}
 	return strings.Join(s, "\n")
 }
