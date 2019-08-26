2f8380a05b3ebd3c275b14fa455c7c05cf69a820
Claude Hähni <chaehni@student.ethz.ch>
2019-08-26 17:11:01 +0200

    revised after feedback

--- go/hidden_path_srv/internal/hiddenpathdb/adapter/pathdb_adapter_test.go
+++ go/hidden_path_srv/internal/hiddenpathdb/adapter/pathdb_adapter_test.go
@@ -12,16 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package adapter
+package adapter_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
 
 	"github.com/scionproto/scion/go/hidden_path_srv/internal/hiddenpathdb"
+	"github.com/scionproto/scion/go/hidden_path_srv/internal/hiddenpathdb/adapter"
 	"github.com/scionproto/scion/go/hidden_path_srv/internal/hpsegreq"
 	"github.com/scionproto/scion/go/lib/addr"
 	"github.com/scionproto/scion/go/lib/ctrl/seg"
@@ -29,6 +32,7 @@ import (
 	"github.com/scionproto/scion/go/lib/pathdb/mock_pathdb"
 	"github.com/scionproto/scion/go/lib/pathdb/pathdbtest"
 	"github.com/scionproto/scion/go/lib/pathdb/query"
+	"github.com/scionproto/scion/go/lib/xtest/matchers"
 	"github.com/scionproto/scion/go/proto"
 )
 
@@ -65,51 +69,72 @@ func TestAdapter(t *testing.T) {
 		"Get with params": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
 			_ *gomock.Controller) {
 
-			pdb.EXPECT().Get(ctx, queryParams)
-			New(pdb).Get(ctx, params)
+			pdb.EXPECT().Get(ctx, matchers.EqParams(queryParams))
+			adapter.New(pdb).Get(ctx, params)
 		},
 		"Get no params": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
 			_ *gomock.Controller) {
 
 			pdb.EXPECT().Get(ctx, nil)
-			New(pdb).Get(ctx, nil)
+			adapter.New(pdb).Get(ctx, nil)
 		},
 		"Insert": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
 			ctrl *gomock.Controller) {
 
 			pseg, _ := pathdbtest.AllocPathSegment(t, ctrl, ifs, uint32(10))
-			pdb.EXPECT().InsertWithHPCfgIDs(ctx, getSeg(pseg), hpCfgIDs)
-			New(pdb).Insert(ctx, getSeg(pseg), groupIds)
+			pdb.EXPECT().InsertWithHPCfgIDs(ctx, getSeg(pseg), matchers.EqHPCfgIDs(hpCfgIDs))
+			fmt.Println("want")
+			adapter.New(pdb).Insert(ctx, getSeg(pseg), groupIds)
 		},
 		"Delete with params": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
 			_ *gomock.Controller) {
 
-			pdb.EXPECT().Delete(ctx, queryParams)
-			New(pdb).Delete(ctx, params)
+			pdb.EXPECT().Delete(ctx, matchers.EqParams(queryParams))
+			adapter.New(pdb).Delete(ctx, params)
 		},
 		"Delete no params": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
 			_ *gomock.Controller) {
 
 			pdb.EXPECT().Delete(ctx, nil)
-			New(pdb).Delete(ctx, nil)
+			adapter.New(pdb).Delete(ctx, nil)
 		},
 		"DeleteExpired": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
 			_ *gomock.Controller) {
 
 			timeNow := time.Now()
 			pdb.EXPECT().DeleteExpired(ctx, timeNow)
-			New(pdb).DeleteExpired(ctx, timeNow)
+			adapter.New(pdb).DeleteExpired(ctx, timeNow)
 		},
 		"BeginTransaction": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
 			_ *gomock.Controller) {
 
 			pdb.EXPECT().BeginTransaction(ctx, nil)
-			New(pdb).BeginTransaction(ctx, nil)
+			adapter.New(pdb).BeginTransaction(ctx, nil)
 		},
 		"Close": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
 			_ *gomock.Controller) {
 			pdb.EXPECT().Close()
-			New(pdb).Close()
+			adapter.New(pdb).Close()
+		},
+		"Commit": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
+			ctrl *gomock.Controller) {
+
+			tx := mock_pathdb.NewMockTransaction(ctrl)
+			pdb.EXPECT().BeginTransaction(ctx, gomock.Any()).Return(tx, nil)
+			tx.EXPECT().Commit()
+			pdbTx, err := adapter.New(pdb).BeginTransaction(ctx, nil)
+			require.NoError(t, err)
+			pdbTx.Commit()
+		},
+		"Rollback": func(t *testing.T, ctx context.Context, pdb *mock_pathdb.MockPathDB,
+			ctrl *gomock.Controller) {
+
+			tx := mock_pathdb.NewMockTransaction(ctrl)
+			pdb.EXPECT().BeginTransaction(ctx, gomock.Any()).Return(tx, nil)
+			tx.EXPECT().Rollback()
+			pdbTx, err := adapter.New(pdb).BeginTransaction(ctx, nil)
+			require.NoError(t, err)
+			pdbTx.Rollback()
 		},
 	}
 	for name, test := range tests {
@@ -119,30 +144,6 @@ func TestAdapter(t *testing.T) {
 			ctx := context.Background()
 			pdb := mock_pathdb.NewMockPathDB(ctrl)
 			test(t, ctx, pdb, ctrl)
-		})
-	}
-}
-
-func TestTransaction(t *testing.T) {
-	tests := map[string]func(*testing.T, context.Context, *mock_pathdb.MockTransaction){
-		"Commit": func(t *testing.T, ctx context.Context, txPdb *mock_pathdb.MockTransaction) {
-			txPdb.EXPECT().Commit()
-			tx := &transaction{backend: txPdb}
-			tx.Commit()
-		},
-		"Rollback": func(t *testing.T, ctx context.Context, txPdb *mock_pathdb.MockTransaction) {
-			txPdb.EXPECT().Rollback()
-			tx := &transaction{backend: txPdb}
-			tx.Rollback()
-		},
-	}
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-			ctx := context.Background()
-			tx := mock_pathdb.NewMockTransaction(ctrl)
-			test(t, ctx, tx)
 		})
 	}
 }
