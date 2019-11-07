e75b483ef962b81701627e5cdd50b47be65e4c6b
Lukas Vogel <lukedirtwalker@gmail.com>
2019-11-07 12:35:29 +0100

    r1

--- go/lib/l4/udp_test.go
+++ go/lib/l4/udp_test.go
@@ -30,57 +30,47 @@ func createUDP() UDP {
 }
 
 func TestUDPFromRaw(t *testing.T) {
-	t.Run("Content must match", func(t *testing.T) {
-		raw := common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0}
-		original := createUDP()
+	raw := common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0}
+	original := createUDP()
 
-		fromRaw, err := UDPFromRaw(raw)
-		assert.NoError(t, err)
-		assert.Equal(t, &original, fromRaw)
-	})
+	fromRaw, err := UDPFromRaw(raw)
+	assert.NoError(t, err)
+	assert.Equal(t, &original, fromRaw)
 }
 
 func TestUDPValidate(t *testing.T) {
-	t.Run("It is structure of size 10, must be ok", func(t *testing.T) {
-		u := createUDP()
+	u := createUDP()
 
-		assert.Equal(t, uint16(10), u.TotalLen)
-		err := u.Validate(int(u.TotalLen) - UDPLen)
-		assert.NoError(t, err)
-	})
+	assert.Equal(t, uint16(10), u.TotalLen)
+	err := u.Validate(int(u.TotalLen) - UDPLen)
+	assert.NoError(t, err)
 }
 
 func TestUDPParse(t *testing.T) {
-	t.Run("Must parse into the same representation", func(t *testing.T) {
-		// assuming we have tested UDPPack
-		expected := createUDP()
-		raw, err := expected.Pack(true)
-		actual := UDP{0, 0, 0xA,
-			make(common.RawBytes, 2)}
+	// assuming we have tested UDPPack
+	expected := createUDP()
+	raw, err := expected.Pack(true)
+	actual := UDP{0, 0, 0xA,
+		make(common.RawBytes, 2)}
 
-		actual.Parse(raw)
-		assert.NoError(t, err)
-		assert.Equal(t, expected, actual)
-	})
+	actual.Parse(raw)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
 }
 
 func TestUDPPack(t *testing.T) {
-	t.Run("Binary content must match", func(t *testing.T) {
-		u := createUDP()
-		raw, err := u.Pack(true)
+	u := createUDP()
+	raw, err := u.Pack(true)
 
-		assert.NoError(t, err)
-		assert.Equal(t, common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0}, raw)
-	})
+	assert.NoError(t, err)
+	assert.Equal(t, common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0}, raw)
 }
 
 func TestUDPWrite(t *testing.T) {
-	t.Run("Binary content must match", func(t *testing.T) {
-		u := createUDP()
-		raw := make(common.RawBytes, 8)
-		err := u.Write(raw)
+	u := createUDP()
+	raw := make(common.RawBytes, 8)
+	err := u.Write(raw)
 
-		assert.NoError(t, err)
-		assert.Equal(t, common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0}, raw)
-	})
+	assert.NoError(t, err)
+	assert.Equal(t, common.RawBytes{0x12, 0x34, 0x56, 0x78, 0, 0xA, 0, 0}, raw)
 }
