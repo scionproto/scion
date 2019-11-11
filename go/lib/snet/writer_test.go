cb21f47bae41665985926a61e3482b7c46a0d120
Konstantinos <karampok@gmail.com>
2019-11-11 13:40:51 +0100

    snet: remove goconvey (#3350)
    Contributes #3016

--- go/lib/snet/writer_test.go
+++ go/lib/snet/writer_test.go
@@ -21,9 +21,8 @@ import (
 	"time"
 
 	"github.com/golang/mock/gomock"
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 
-	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/mocks/net/mock_net"
 	"github.com/scionproto/scion/go/lib/overlay"
 	"github.com/scionproto/scion/go/lib/pathmgr/mock_pathmgr"
@@ -43,141 +42,148 @@ func buildNullMonitorMock(ctrl *gomock.Controller) ctxmonitor.Monitor {
 }
 
 func TestConnRemoteAddressResolver(t *testing.T) {
-	Convey("Given a remote address resolver", t, func() {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		resolver := &remoteAddressResolver{monitor: buildNullMonitorMock(ctrl)}
-		Convey("If both addresses are unknown, error out", func() {
-			address, err := resolver.resolveAddrPair(nil, nil)
-			SoMsg("err", err, ShouldNotBeNil)
-			SoMsg("address", address, ShouldBeNil)
-		})
-		Convey("If both address are known, error out", func() {
-			connRemoteAddress := MustParseAddr("1-ff00:0:113,[127.0.0.1]:80")
-			argRemoteAddress := MustParseAddr("1-ff00:0:110,[127.0.0.1]:80")
-			address, err := resolver.resolveAddrPair(connRemoteAddress, argRemoteAddress)
-			SoMsg("err", err, ShouldNotBeNil)
-			SoMsg("address", address, ShouldBeNil)
-		})
+	t.Log("Given a remote address resolver")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	resolver := &remoteAddressResolver{monitor: buildNullMonitorMock(ctrl)}
+	t.Run("If both addresses are unknown, error out", func(t *testing.T) {
+		address, err := resolver.resolveAddrPair(nil, nil)
+		assert.Error(t, err, "err")
+		assert.Nil(t, address, "address")
+	})
+	t.Run("If both address are known, error out", func(t *testing.T) {
+		connRemoteAddress := MustParseAddr("1-ff00:0:113,[127.0.0.1]:80")
+		argRemoteAddress := MustParseAddr("1-ff00:0:110,[127.0.0.1]:80")
+		address, err := resolver.resolveAddrPair(connRemoteAddress, argRemoteAddress)
+		assert.Error(t, err, "err")
+		assert.Nil(t, address, "address")
 	})
 }
 
 func TestRemoteAddressResolver(t *testing.T) {
-	Convey("Given a single remote address resolver", t, func() {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		pathSource := mock_pathsource.NewMockPathSource(ctrl)
-		resolver := &remoteAddressResolver{
-			localIA:      xtest.MustParseIA("1-ff00:0:110"),
-			pathResolver: pathSource,
-			monitor:      buildNullMonitorMock(ctrl),
-		}
-		Convey("error if address is nil", func() {
-			address, err := resolver.resolveAddr(nil)
-			SoMsg("err", common.GetErrorMsg(err), ShouldEqual, ErrAddressIsNil)
-			SoMsg("address", address, ShouldBeNil)
-		})
-		Convey("error if app address is unset", func() {
-			address := &Addr{}
-			address, err := resolver.resolveAddr(address)
-			SoMsg("err", common.GetErrorMsg(err), ShouldEqual, ErrNoApplicationAddress)
-			SoMsg("address", address, ShouldBeNil)
-		})
-		Convey("if destination is in local AS", func() {
+	t.Log("Given a single remote address resolver")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	pathSource := mock_pathsource.NewMockPathSource(ctrl)
+	resolver := &remoteAddressResolver{
+		localIA:      xtest.MustParseIA("1-ff00:0:110"),
+		pathResolver: pathSource,
+		monitor:      buildNullMonitorMock(ctrl),
+	}
+
+	t.Run("error if address is nil", func(t *testing.T) {
+		address, err := resolver.resolveAddr(nil)
+		assert.EqualError(t, err, string(ErrAddressIsNil))
+		assert.Nil(t, address, "address")
+	})
+
+	t.Run("error if app address is unset", func(t *testing.T) {
+		address := &Addr{}
+		address, err := resolver.resolveAddr(address)
+		assert.EqualError(t, err, string(ErrNoApplicationAddress))
+		assert.Nil(t, address, "address")
+	})
+
+	t.Run("if destination is in local AS", func(t *testing.T) {
+		t.Run("error if path set.", func(t *testing.T) {
 			inAddress := MustParseAddr("1-ff00:0:110,[127.0.0.1]:80")
-			Convey("error if path set.", func() {
-				inAddress.Path = &spath.Path{}
-				outAddress, err := resolver.resolveAddr(inAddress)
-				SoMsg("err", common.GetErrorMsg(err), ShouldEqual, ErrExtraPath)
-				SoMsg("address", outAddress, ShouldBeNil)
-			})
-			Convey("return same address if path unset, and overlay address set.", func() {
-				inAddress.NextHop = &overlay.OverlayAddr{}
-				outAddress, err := resolver.resolveAddr(inAddress)
-				SoMsg("err", err, ShouldBeNil)
-				SoMsg("address", outAddress, ShouldEqual, inAddress)
-			})
-			Convey("inherit overlay data if overlay address unset.", func() {
-				outAddress, err := resolver.resolveAddr(inAddress)
-				SoMsg("err", err, ShouldBeNil)
-				SoMsg("address", outAddress, ShouldNotBeNil)
-				SoMsg("overlay addr", outAddress.NextHop.L3(), ShouldResemble, outAddress.Host.L3)
-				SoMsg("overlay port", outAddress.NextHop.L4(), ShouldResemble,
-					uint16(overlay.EndhostPort))
-			})
+			inAddress.Path = &spath.Path{}
+			outAddress, err := resolver.resolveAddr(inAddress)
+			assert.EqualError(t, err, string(ErrExtraPath))
+			assert.Nil(t, outAddress, "address")
 		})
-		Convey("if destination is not in local AS", func() {
+		t.Run("return same address if path unset, and overlay address set.", func(t *testing.T) {
+			inAddress := MustParseAddr("1-ff00:0:110,[127.0.0.1]:80")
+			inAddress.NextHop = &overlay.OverlayAddr{}
+			outAddress, err := resolver.resolveAddr(inAddress)
+			assert.NoError(t, err, "err")
+			assert.Equal(t, outAddress, inAddress)
+		})
+		t.Run("inherit overlay data if overlay address unset.", func(t *testing.T) {
+			inAddress := MustParseAddr("1-ff00:0:110,[127.0.0.1]:80")
+			outAddress, err := resolver.resolveAddr(inAddress)
+			assert.NoError(t, err)
+			assert.NotNil(t, outAddress)
+			assert.Equal(t, outAddress.NextHop.L3(), outAddress.Host.L3, "overlay addr")
+			assert.Equal(t, outAddress.NextHop.L4(), uint16(overlay.EndhostPort), "overlay port")
+		})
+	})
+	t.Run("if destination is not in local AS", func(t *testing.T) {
+		t.Run("error if path set but overlay address unset.", func(t *testing.T) {
 			inAddress := MustParseAddr("1-ff00:0:113,[127.0.0.1]:80")
-			Convey("error if path set but overlay address unset.", func() {
-				inAddress.Path = &spath.Path{}
+			inAddress.Path = &spath.Path{}
+			outAddress, err := resolver.resolveAddr(inAddress)
+			assert.EqualError(t, err, string(ErrBadOverlay))
+			assert.Nil(t, outAddress, "address")
+		})
+		t.Run("error if overlay set but path unset.", func(t *testing.T) {
+			inAddress := MustParseAddr("1-ff00:0:113,[127.0.0.1]:80")
+			inAddress.NextHop = &overlay.OverlayAddr{}
+			outAddress, err := resolver.resolveAddr(inAddress)
+			assert.EqualError(t, err, string(ErrMustHavePath))
+			assert.Nil(t, outAddress, "address")
+		})
+		t.Run("return same address if path and overlay set.", func(t *testing.T) {
+			inAddress := MustParseAddr("1-ff00:0:113,[127.0.0.1]:80")
+			inAddress.Path = &spath.Path{}
+			inAddress.NextHop = &overlay.OverlayAddr{}
+			outAddress, err := resolver.resolveAddr(inAddress)
+			assert.NoError(t, err, "err")
+			assert.Equal(t, outAddress, inAddress)
+		})
+		t.Run("request path if path and overlay unset", func(t *testing.T) {
+			t.Run("if request not successful, error.", func(t *testing.T) {
+				inAddress := MustParseAddr("1-ff00:0:113,[127.0.0.1]:80")
+				pathSource.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(nil, nil, fmt.Errorf("some error"))
 				outAddress, err := resolver.resolveAddr(inAddress)
-				SoMsg("err", common.GetErrorMsg(err), ShouldEqual, ErrBadOverlay)
-				SoMsg("address", outAddress, ShouldBeNil)
+				assert.EqualError(t, err, string(ErrPath))
+				assert.Nil(t, outAddress, "address")
 			})
-			Convey("error if overlay set but path unset.", func() {
-				inAddress.NextHop = &overlay.OverlayAddr{}
+			t.Run("if request successful, return address.", func(t *testing.T) {
+				inAddress := MustParseAddr("1-ff00:0:113,[127.0.0.1]:80")
+				path := &spath.Path{}
+				overlayAddr := &overlay.OverlayAddr{}
+				pathSource.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(overlayAddr, path, nil)
 				outAddress, err := resolver.resolveAddr(inAddress)
-				SoMsg("err", common.GetErrorMsg(err), ShouldEqual, ErrMustHavePath)
-				SoMsg("address", outAddress, ShouldBeNil)
-			})
-			Convey("return same address if path and overlay set.", func() {
-				inAddress.Path = &spath.Path{}
-				inAddress.NextHop = &overlay.OverlayAddr{}
-				outAddress, err := resolver.resolveAddr(inAddress)
-				SoMsg("err", err, ShouldBeNil)
-				SoMsg("address", outAddress, ShouldResemble, inAddress)
-			})
-			Convey("request path if path and overlay unset", func() {
-				Convey("if request not successful, error.", func() {
-					pathSource.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).
-						Return(nil, nil, fmt.Errorf("some error"))
-					outAddress, err := resolver.resolveAddr(inAddress)
-					SoMsg("err", common.GetErrorMsg(err), ShouldEqual, ErrPath)
-					SoMsg("address", outAddress, ShouldBeNil)
-				})
-				Convey("if request successful, return address.", func() {
-					path := &spath.Path{}
-					overlayAddr := &overlay.OverlayAddr{}
-					pathSource.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).
-						Return(overlayAddr, path, nil)
-					outAddress, err := resolver.resolveAddr(inAddress)
-					SoMsg("err", err, ShouldBeNil)
-					SoMsg("address", outAddress, ShouldNotBeNil)
-					SoMsg("path", outAddress.Path, ShouldEqual, path)
-					SoMsg("overlay", outAddress.NextHop, ShouldEqual, overlayAddr)
-				})
+				assert.NoError(t, err)
+				assert.NotNil(t, outAddress)
+				assert.Equal(t, outAddress.Path, path)
+				assert.Equal(t, outAddress.NextHop, overlayAddr)
 			})
 		})
 	})
 }
 
 func TestSetDeadline(t *testing.T) {
-	Convey("Given an snet write connection", t, func() {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
+	t.Log("Given an snet write connection")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-		done := make(chan struct{}, 3)
+	done := make(chan struct{}, 3)
 
-		resolverMock := mock_pathmgr.NewMockResolver(ctrl)
-		resolverMock.EXPECT().Query(gomock.Any(), gomock.Any(), gomock.Any(),
-			gomock.Any()).DoAndReturn(
+	resolverMock := mock_pathmgr.NewMockResolver(ctrl)
+	resolverMock.EXPECT().Query(gomock.Any(), gomock.Any(), gomock.Any(),
+		gomock.Any()).DoAndReturn(
 
-			func(ctx context.Context, _, _, _ interface{}) spathmeta.AppPathSet {
-				select {
-				case <-ctx.Done():
-					done <- struct{}{}
-				}
-				return nil
-			},
-		).AnyTimes()
-		connMock := mock_net.NewMockPacketConn(ctrl)
-		connMock.EXPECT().SetWriteDeadline(gomock.Any()).AnyTimes().Return(nil)
-		packetConn := NewSCIONPacketConn(connMock, nil)
+		func(ctx context.Context, _, _, _ interface{}) spathmeta.AppPathSet {
+			select {
+			case <-ctx.Done():
+				done <- struct{}{}
+			}
+			return nil
+		},
+	).AnyTimes()
+	connMock := mock_net.NewMockPacketConn(ctrl)
+	connMock.EXPECT().SetWriteDeadline(gomock.Any()).AnyTimes().Return(nil)
+	packetConn := NewSCIONPacketConn(connMock, nil)
 
-		conn := newScionConnWriter(&scionConnBase{
-			laddr: MustParseAddr("2-ff00:0:1,[127.0.0.1]:80"),
-		}, resolverMock, packetConn)
-		Convey("And writes to multiple destinations for which path resolution is slow", func() {
+	conn := newScionConnWriter(&scionConnBase{
+		laddr: MustParseAddr("2-ff00:0:1,[127.0.0.1]:80"),
+	}, resolverMock, packetConn)
+	t.Run("And writes to multiple destinations for which path resolution is slow",
+		func(t *testing.T) {
 			addresses := []*Addr{
 				MustParseAddr("1-ff00:0:1,[127.0.0.1]:80"),
 				MustParseAddr("1-ff00:0:2,[127.0.0.1]:80"),
@@ -186,14 +192,13 @@ func TestSetDeadline(t *testing.T) {
 			for _, address := range addresses {
 				go conn.WriteTo([]byte{1, 2, 3}, address)
 			}
-			Convey("Setting the deadline in the past unlocks all writers", func() {
+			t.Run("Setting the deadline in the past unlocks all writers", func(t *testing.T) {
 				conn.SetWriteDeadline(time.Now().Add(-time.Second))
 				for range addresses {
 					xtest.AssertReadReturnsBefore(t, done, time.Second)
 				}
 			})
 		})
-	})
 }
 
 func MustParseAddr(str string) *Addr {
