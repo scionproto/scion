836d6ec2ac85a93a7a7a9be5dd18109d2593981d
Lukas Vogel <lukedirtwalker@gmail.com>
2019-11-20 14:00:45 +0100

    Change dependency organization in bazel (#3407)
    Instead of having all go dependencies in the `WORKSPACE` have 2 files:
    * `tool_deps.bzl` contains all deps for linting & tooling.
    * `go_deps.bzl` contains all deps for go code.
    
    Also remove unused deps.

--- tools/fetch.sh
+++ tools/fetch.sh
@@ -9,6 +9,56 @@ set -e
 
 ROOTDIR=$(dirname "$0")/..
 
+extra_import() {
+    # For dependencies listed below the go code is in a sub folder.
+    # All go_repositories which don't have go code in the root folder must be
+    # listed here. Otherwise the fetching doesn't work.
+    case "$1" in
+        com_github_antlr_antlr4)
+            echo "runtime/Go/antlr" ;;
+        com_github_beorn7_perks)
+            echo "histogram" ;;
+        com_github_golang_mock)
+            echo "gomock" ;;
+        com_github_google_go_cmp)
+            echo "cmp" ;;
+        com_github_matttproud_golang_protobuf_extensions)
+            echo "ext" ;;
+        com_github_oncilla_gochecks)
+            echo "serrorscheck" ;;
+        com_github_pierrec_xxhash)
+            echo "xxhsum" ;;
+        com_github_prometheus_client_golang)
+            echo "prometheus" ;;
+        com_github_prometheus_common)
+            echo "model" ;;
+        com_github_syndtr_gocapability)
+            echo "capability" ;;
+        org_golang_x_crypto)
+            echo "acme" ;;
+        com_github_buildkite_go_buildkite)
+            echo "buildkite" ;;
+        com_github_google_go_querystring)
+            echo "query" ;;
+        com_github_uber_jaeger_lib)
+            echo "metrics" ;;
+        com_github_sergi_go_diff)
+            echo "diffmatchpatch" ;;
+        org_golang_x_net)
+            echo "ipv4" ;;
+        org_golang_x_sys)
+            echo "unix" ;;
+        org_golang_x_sync)
+            echo "semaphore" ;;
+        com_github_pmezard_go_difflib)
+            echo "difflib" ;;
+        com_github_bazelbuild_buildtools)
+            echo "build" ;;
+        *)
+            echo "" ;;
+    esac
+}
+
 # Add any bazel packages to prefetch to the beginning of the following block.
 cat <<EOF
 load("@com_github_jmhodges_bazel_gomock//:gomock.bzl", "gomock")
@@ -28,26 +78,16 @@ genrule(
         "@package_bundle//file:packages.bzl",
 EOF
 
-cat  $ROOTDIR/WORKSPACE \
-    | sed -n -e '/# Dependencies/,$p' \
-    | grep -E "name|importpath" \
-    | sed 'N;s/\n/ /' \
-    | grep -v com_github_jmhodges_bazel_gomock \
-    | while IFS=" " read -r LINE
-    do
-        if [[ $LINE =~ \"(.*)\".*\".*\" ]]; then
-            NAME=${BASH_REMATCH[1]}
-        else
-            echo "External dependency name not found: $LINE"
-            exit 1
-        fi
-        if [[ $LINE =~ \#[[:blank:]]*(.*) ]]; then
-            NAME=$NAME//${BASH_REMATCH[1]}
-        else
-            NAME=$NAME//
-        fi
-        echo "        \"@$NAME:go_default_library\","
-    done
+for q in $(bazel query "kind('go_repository rule', //external:*)" --noshow_progress); do
+    if [[ $q == *com_github_jmhodges_bazel_gomock ]]; then
+        continue
+    fi
+    if [[ $q == //external* ]]; then
+        dep_name=${q#//external:} # remove "//external:" in front.
+        extra=$(extra_import $dep_name)
+        echo "        \"@$dep_name//$extra:go_default_library\","
+    fi
+done
 
 cat <<EOF
     ]
