2bbe9de80649e3f339cc8d6d8fe8ad05173acf82
Lukas Bischofberger <bischofberger@anapaya.net>
2019-11-29 11:09:35 +0100

    test

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -11,6 +11,8 @@ if [ -z "$RUN_ALL_TESTS" ]; then
     [ "$BUILDKITE_PULL_REQUEST" = "false" ] && export RUN_ALL_TESTS=y
 fi
 
+export RUN_ALL_TESTS=y
+
 # begin the pipeline.yml file
 "$BASE/common.sh"
 echo "steps:"
