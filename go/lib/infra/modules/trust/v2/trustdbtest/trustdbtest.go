429cccb8bd5b123141ecf45a5cc5330e5db54a49
roos <roos@anapaya.net>
2019-12-06 17:02:18 +0100

    fix licenses

--- go/lib/infra/modules/trust/v2/trustdbtest/trustdbtest.go
+++ go/lib/infra/modules/trust/v2/trustdbtest/trustdbtest.go
@@ -1,4 +1,4 @@
-// Copyright 2018 ETH Zurich, Anapaya Systems
+// Copyright 2019 Anapaya Systems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
