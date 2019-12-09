121a4645da8ce940963ca9af621fc9164ea17bd7
roos <roos@anapaya.net>
2019-12-09 17:31:18 +0100

    TrustStore: Add TRC request handler
    Add TRC request handler to the trust store.
    Currently, the PR is missing unit tests.
    
    fixes #3484

---
+++ go/lib/infra/modules/trust/v2/handlers.go
@@ -0,0 +1,76 @@
+// Copyright 2019 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package trust
+
+import (
+	"context"
+	"time"
+
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/ctrl/cert_mgmt"
+	"github.com/scionproto/scion/go/lib/infra"
+	"github.com/scionproto/scion/go/lib/log"
+)
+
+// HandlerTimeout is the lifetime of the handlers.
+const HandlerTimeout = 3 * time.Second
+
+// trcReqHandler contains the state of a handler for a specific TRC Request
+// message, received via the Messenger's ListenAndServe method.
+type trcReqHandler struct {
+	request  *infra.Request
+	provider CryptoProvider
+}
+
+func (h *trcReqHandler) Handle() *infra.HandlerResult {
+	logger := log.FromCtx(h.request.Context())
+	trcReq, ok := h.request.Message.(*cert_mgmt.TRCReq)
+	if !ok {
+		logger.Error("[TrustStore:trcReqHandler] wrong message type, expected cert_mgmt.TRCReq",
+			"msg", h.request.Message, "type", common.TypeOf(h.request.Message))
+		return infra.MetricsErrInternal
+	}
+	logger.Debug("[TrustStore:trcReqHandler] Received request", "trcReq", trcReq,
+		"peer", h.request.Peer)
+	rw, ok := infra.ResponseWriterFromContext(h.request.Context())
+	if !ok {
+		logger.Warn("[TrustStore:trcReqHandler] Unable to service request, no Messenger found")
+		return infra.MetricsErrInternal
+	}
+	subCtx, cancelF := context.WithTimeout(h.request.Context(), HandlerTimeout)
+	defer cancelF()
+	opts := infra.TRCOpts{
+		TrustStoreOpts: infra.TrustStoreOpts{
+			LocalOnly: trcReq.CacheOnly,
+		},
+		AllowInactive: true,
+	}
+	raw, err := h.provider.GetRawTRC(h.request.Context(), trcReq.ISD, trcReq.Version,
+		opts, h.request.Peer)
+	if err != nil {
+		logger.Error("[TrustStore:trcReqHandler] Unable to retrieve TRC", "err", err)
+		return infra.MetricsErrTrustStore(err)
+	}
+	trcMessage := &cert_mgmt.TRC{
+		RawTRC: raw,
+	}
+	if err := rw.SendTRCReply(subCtx, trcMessage); err != nil {
+		logger.Error("[TrustStore:trcReqHandler] Messenger error", "err", err)
+		return infra.MetricsErrMsger(err)
+	}
+	logger.Debug("[TrustStore:trcReqHandler] Replied with TRC", "isd", trcReq.ISD,
+		"version", trcReq.Version, "peer", h.request.Peer)
+	return infra.MetricsResultOk
+}
