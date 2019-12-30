50d578504eed8539e749e73e8cf02ffcdf2e3381
Lukas Vogel <lukedirtwalker@gmail.com>
2019-12-30 15:32:06 +0100

    CI: add revocation tests to pipeline 2
    Also:
    - use accept artifacts for diff tests in check generated tasks, this dir always exists also on the local CI.
    - generate acceptance steps for bazelified acceptance tests.

--- .buildkite/pipeline2.sh
+++ .buildkite/pipeline2.sh
@@ -23,5 +23,26 @@ gen_acceptance() {
     done
 }
 
+# gen_acceptance2 generates steps for bazel tests in acceptance folder.
+gen_acceptance2() {
+    for test in $(bazel query 'kind(sh_test, //acceptance/...)' 2>/dev/null); do
+        # test has the format //acceptance/<name>:<name>_test
+        name=$(echo $test | cut -d ':' -f 1)
+        name=${name#'//acceptance/'}
+        echo "  - label: \"Acceptance: $name\""
+        echo "    command:"
+        echo "      - mkdir -p \$\$ACCEPTANCE_ARTIFACTS"
+        echo "      - bazel test --action_env=ACCEPTANCE_ARTIFACTS $test"
+        echo "    key: ${name}_acceptance"
+        echo "    artifact_paths:"
+        echo "      - \"artifacts.out/**/*\""
+        echo "    retry:"
+        echo "      automatic:"
+        echo "        - exit_status: -1 # Agent was lost"
+        echo "        - exit_status: 255 # Forced agent shutdown"
+    done
+}
+
 cat .buildkite/pipeline_buildlint.yml
+gen_acceptance2
 gen_acceptance
