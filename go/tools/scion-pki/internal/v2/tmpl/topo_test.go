scionproto/scion

commit 7566bfb825541d5ae85c5db1d679819a95bdf936

Topo file: add support for v2 trust attributes (#3586)
The source file for topology generation now has support for  primary attributes as listed in
https://github.com/Anapaya/scion/blob/anapaya/doc/ControlPlanePKI.md#primary-ases

Also add support to scion-pki for those properties.

go/tools/scion-pki/internal/v2/tmpl/topo.go
@@ -89,7 +89,7 @@ func (g topoGen) genTRCs(topo topoFile) (map[addr.ISD]conf.TRC2, error) {
 }
 
 func (g topoGen) genTRC(isd addr.ISD, topo topoFile) conf.TRC2 {
-	cores := topo.Cores(isd)
+	primaries := topo.Primaries(isd)
 	reset := true
 	cfg := conf.TRC2{
 		Description: fmt.Sprintf("ISD %d", isd),
@@ -97,21 +97,14 @@ func (g topoGen) genTRC(isd addr.ISD, topo topoFile) conf.TRC2 {
 		BaseVersion: 1,
 		// XXX(roosd): Choose quorum according to your security needs.
 		// This simply serves an example.
-		VotingQuorum:      uint16(len(cores)/2 + 1),
+		VotingQuorum:      uint16(primaries.Voters()/2 + 1),
 		TrustResetAllowed: &reset,
 		Votes:             []addr.AS{},
 		Validity:          g.Validity,
 		PrimaryASes:       make(map[addr.AS]conf.Primary),
 	}
-	for _, as := range cores {
-		iss, on, off := scrypto.KeyVersion(1), scrypto.KeyVersion(1), scrypto.KeyVersion(1)
-		cfg.PrimaryASes[as] = conf.Primary{
-			Attributes: []trc.Attribute{trc.Authoritative, trc.Core, trc.Issuing,
-				trc.Voting},
-			IssuingKeyVersion:       &iss,
-			VotingOfflineKeyVersion: &off,
-			VotingOnlineKeyVersion:  &on,
-		}
+	for _, primaryAS := range primaries {
+		cfg.PrimaryASes[primaryAS.AS] = primaryAS.ToConf()
 	}
 	return cfg
 }
@@ -253,17 +246,63 @@ func (t topoFile) ISDs() map[addr.ISD]struct{} {
 	return m
 }
 
-func (t topoFile) Cores(isd addr.ISD) []addr.AS {
-	var cores []addr.AS
+func (t topoFile) Primaries(isd addr.ISD) primaryASes {
+	var primaries primaryASes
 	for ia, entry := range t.ASes {
-		if ia.I == isd && entry.Core {
-			cores = append(cores, ia.A)
+		if ia.I == isd && entry.Primary() {
+			primaries = append(primaries, primaryAS{AS: ia.A, asEntry: entry})
 		}
 	}
-	return cores
+	return primaries
 }
 
 type asEntry struct {
-	Core   bool    `yaml:"core"`
-	Issuer addr.IA `yaml:"cert_issuer"`
+	Authoritative bool    `yaml:"authoritative"`
+	Core          bool    `yaml:"core"`
+	Issuing       bool    `yaml:"issuing"`
+	Voting        bool    `yaml:"voting"`
+	Issuer        addr.IA `yaml:"cert_issuer"`
+}
+
+func (e asEntry) Primary() bool {
+	return e.Authoritative || e.Core || e.Issuing || e.Voting
+}
+
+type primaryAS struct {
+	AS addr.AS
+	asEntry
+}
+
+func (p primaryAS) ToConf() conf.Primary {
+	cp := conf.Primary{}
+	if p.Authoritative {
+		cp.Attributes = append(cp.Attributes, trc.Authoritative)
+	}
+	if p.Core {
+		cp.Attributes = append(cp.Attributes, trc.Core)
+	}
+	if p.Issuing {
+		iss := scrypto.KeyVersion(1)
+		cp.Attributes = append(cp.Attributes, trc.Issuing)
+		cp.IssuingKeyVersion = &iss
+	}
+	if p.Voting {
+		on, off := scrypto.KeyVersion(1), scrypto.KeyVersion(1)
+		cp.Attributes = append(cp.Attributes, trc.Voting)
+		cp.VotingOnlineKeyVersion = &on
+		cp.VotingOfflineKeyVersion = &off
+	}
+	return cp
+}
+
+type primaryASes []primaryAS
+
+func (p primaryASes) Voters() int {
+	voters := 0
+	for _, pAS := range p {
+		if pAS.Voting {
+			voters++
+		}
+	}
+	return voters
 }

go/tools/scion-pki/internal/v2/tmpl/topo_test.go
@@ -43,7 +43,7 @@ func TestTopoGen(t *testing.T) {
 
 	topo := topoFile{
 		ASes: map[addr.IA]asEntry{
-			ia110: {Core: true},
+			ia110: {Core: true, Authoritative: true, Issuing: true, Voting: true},
 			ia111: {Issuer: ia110},
 			ia112: {Issuer: ia110},
 		},

acceptance/topo_br_reload_util/Tinier.topo
@@ -2,6 +2,9 @@
 ASes:
   "1-ff00:0:110":
     core: true
+    voting: true
+    authoritative: true
+    issuing: true
     mtu: 1400
   "1-ff00:0:111":
     cert_issuer: 1-ff00:0:110

python/topology/topo.py
@@ -187,8 +187,13 @@ class TopoGenerator(object):
         mtu = as_conf.get('mtu', self.args.default_mtu)
         assert mtu >= SCION_MIN_MTU, mtu
         self.topo_dicts[topo_id] = {
-            'Core': as_conf.get('core', False), 'ISD_AS': str(topo_id),
-            'MTU': mtu, 'Overlay': self.overlay
+            'Core': as_conf.get('core', False),
+            'Voting': as_conf.get('voting', False),
+            'Authoritative': as_conf.get('authoritative', False),
+            'Issuing': as_conf.get('issuing', False),
+            'ISD_AS': str(topo_id),
+            'MTU': mtu,
+            'Overlay': self.overlay,
         }
         for i in SCION_SERVICE_NAMES:
             self.topo_dicts[topo_id][i] = {}

topology/Default.topo
@@ -2,11 +2,20 @@
 ASes:
   "1-ff00:0:110": # old 1-11
     core: true
+    voting: true
+    authoritative: true
+    issuing: true
     path_servers: 3
   "1-ff00:0:120": # old 1-12
     core: true
+    voting: true
+    authoritative: true
+    issuing: true
   "1-ff00:0:130": # old 1-13
     core: true
+    voting: true
+    authoritative: true
+    issuing: true
     beacon_servers: 2
   "1-ff00:0:111": # old 1-14
     cert_issuer: 1-ff00:0:110
@@ -27,9 +36,15 @@ ASes:
     cert_issuer: 1-ff00:0:130
   "2-ff00:0:210": # old 2-21
     core: true
+    voting: true
+    authoritative: true
+    issuing: true
     mtu: 1280
   "2-ff00:0:220": # old 2-22
     core: true
+    voting: true
+    authoritative: true
+    issuing: true
   "2-ff00:0:211": # old 2-23
     cert_issuer: 2-ff00:0:210
   "2-ff00:0:212": # old 2-25

topology/Tiny.topo
@@ -2,6 +2,9 @@
 ASes:
   "1-ff00:0:110": # old 1-11
     core: true
+    voting: true
+    authoritative: true
+    issuing: true
     mtu: 1400
   "1-ff00:0:111": # old 1-12
     cert_issuer: 1-ff00:0:110

topology/Wide.topo
@@ -1,21 +1,21 @@
 ---
 ASes:
-  "1-ff00:0:110": {core: true}
-  "1-ff00:0:120": {core: true}
+  "1-ff00:0:110": {core: true, voting: true, authoritative: true, issuing: true}
+  "1-ff00:0:120": {core: true, voting: true, authoritative: true, issuing: true}
   "1-ff00:0:111": {cert_issuer: "1-ff00:0:110"}
   "1-ff00:0:121": {cert_issuer: "1-ff00:0:120"}
-  "2-ff00:0:210": {core: true}
-  "2-ff00:0:220": {core: true}
+  "2-ff00:0:210": {core: true, voting: true, authoritative: true, issuing: true}
+  "2-ff00:0:220": {core: true, voting: true, authoritative: true, issuing: true}
   "2-ff00:0:221": {cert_issuer: "2-ff00:0:220"}
   "2-ff00:0:222": {cert_issuer: "2-ff00:0:220"}
-  "3-ff00:0:310": {core: true}
+  "3-ff00:0:310": {core: true, voting: true, authoritative: true, issuing: true}
   "3-ff00:0:311": {cert_issuer: "3-ff00:0:310"}
   "3-ff00:0:312": {cert_issuer: "3-ff00:0:310"}
   "3-ff00:0:313": {cert_issuer: "3-ff00:0:310"}
-  "4-ff00:0:410": {core: true}
-  "5-ff00:0:510": {core: true}
-  "5-ff00:0:520": {core: true}
-  "5-ff00:0:530": {core: true}
+  "4-ff00:0:410": {core: true, voting: true, authoritative: true, issuing: true}
+  "5-ff00:0:510": {core: true, voting: true, authoritative: true, issuing: true}
+  "5-ff00:0:520": {core: true, voting: true, authoritative: true, issuing: true}
+  "5-ff00:0:530": {core: true, voting: true, authoritative: true, issuing: true}
   "5-ff00:0:511": {cert_issuer: "5-ff00:0:510"}
   "5-ff00:0:531": {cert_issuer: "5-ff00:0:530"}
 links:

commit bcae9b6959ea9095146013bf8c1f4f4571b58a8d

SPKI: Fix template generation
This fixes template generation bugs introduced by #3586

go/tools/scion-pki/internal/v2/tmpl/topo.go
@@ -168,7 +168,7 @@ func (g topoGen) genCerts(topo topoFile) error {
 
 func (g topoGen) genIssuerCerts(topo topoFile) error {
 	for ia, entry := range topo.ASes {
-		if !entry.Core {
+		if !entry.Issuing {
 			continue
 		}
 		cfg := g.genIssuerCert(ia)
@@ -201,7 +201,7 @@ func (g topoGen) genIssuerCert(ia addr.IA) conf.Issuer {
 func (g topoGen) genASCerts(topo topoFile) error {
 	for ia, entry := range topo.ASes {
 		issuer := entry.Issuer
-		if entry.Core {
+		if entry.Issuing {
 			issuer = ia
 		}
 		cfg := g.genASCert(ia, issuer)

go/tools/scion-pki/internal/v2/tmpl/topo_test.go
@@ -35,6 +35,9 @@ var (
 	ia110 = xtest.MustParseIA("1-ff00:0:110")
 	ia111 = xtest.MustParseIA("1-ff00:0:111")
 	ia112 = xtest.MustParseIA("1-ff00:0:112")
+
+	ia120 = xtest.MustParseIA("1-ff00:0:120")
+	ia130 = xtest.MustParseIA("1-ff00:0:130")
 )
 
 func TestTopoGen(t *testing.T) {
@@ -44,6 +47,8 @@ func TestTopoGen(t *testing.T) {
 	topo := topoFile{
 		ASes: map[addr.IA]asEntry{
 			ia110: {Core: true, Authoritative: true, Issuing: true, Voting: true},
+			ia120: {Voting: true, Issuer: ia110},
+			ia130: {Issuing: true},
 			ia111: {Issuer: ia110},
 			ia112: {Issuer: ia110},
 		},
@@ -69,7 +74,7 @@ func TestTopoGen(t *testing.T) {
 		assert.Equal(t, "ISD 1", cfg.Description)
 		assert.Equal(t, scrypto.Version(1), cfg.Version)
 		assert.Equal(t, scrypto.Version(1), cfg.BaseVersion)
-		assert.Equal(t, uint16(1), cfg.VotingQuorum)
+		assert.Equal(t, uint16(2), cfg.VotingQuorum)
 		assert.Equal(t, util.DurWrap{}, cfg.GracePeriod)
 		assert.Equal(t, true, *cfg.TrustResetAllowed)
 		assert.Equal(t, []addr.AS{}, cfg.Votes)
@@ -82,6 +87,15 @@ func TestTopoGen(t *testing.T) {
 				IssuingKeyVersion:       &iss,
 				VotingOfflineKeyVersion: &off,
 				VotingOnlineKeyVersion:  &on,
+			},
+			ia120.A: {
+				Attributes:              trc.Attributes{trc.Voting},
+				VotingOfflineKeyVersion: &off,
+				VotingOnlineKeyVersion:  &on,
+			},
+			ia130.A: {
+				Attributes:        trc.Attributes{trc.Issuing},
+				IssuingKeyVersion: &iss,
 			},
 		}
 		assert.Equal(t, exp, cfg.PrimaryASes)
@@ -101,18 +115,19 @@ func TestTopoGen(t *testing.T) {
 			checkMeta(t, cfg.AS[cert.SigningKey][1], scrypto.Ed25519)
 			checkMeta(t, cfg.AS[cert.RevocationKey][1], scrypto.Ed25519)
 			checkMeta(t, cfg.AS[cert.EncryptionKey][1], scrypto.Curve25519xSalsa20Poly1305)
-			if !entry.Core {
-				return
+			if entry.Issuing {
+				checkMeta(t, cfg.Issuer[cert.IssuingKey][1], scrypto.Ed25519)
+				checkMeta(t, cfg.Primary[trc.IssuingKey][1], scrypto.Ed25519)
 			}
-			checkMeta(t, cfg.Issuer[cert.IssuingKey][1], scrypto.Ed25519)
-			checkMeta(t, cfg.Primary[trc.IssuingKey][1], scrypto.Ed25519)
-			checkMeta(t, cfg.Primary[trc.OnlineKey][1], scrypto.Ed25519)
-			checkMeta(t, cfg.Primary[trc.OfflineKey][1], scrypto.Ed25519)
+			if entry.Voting {
+				checkMeta(t, cfg.Primary[trc.OnlineKey][1], scrypto.Ed25519)
+				checkMeta(t, cfg.Primary[trc.OfflineKey][1], scrypto.Ed25519)
+			}
 		})
 	}
 
 	for ia, entry := range topo.ASes {
-		if !entry.Core {
+		if !entry.Issuing {
 			continue
 		}
 		t.Run("Issuer config "+ia.String(), func(t *testing.T) {
@@ -131,7 +146,7 @@ func TestTopoGen(t *testing.T) {
 
 	for ia, entry := range topo.ASes {
 		issuer := entry.Issuer
-		if entry.Core {
+		if entry.Issuing {
 			issuer = ia
 		}
 		t.Run("AS config "+ia.String(), func(t *testing.T) {