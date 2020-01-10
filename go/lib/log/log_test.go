5632bfe31ef85391c264d00136bc419e76935d11
karampok <karampok@gmail.com>
2020-01-10 11:01:36 +0100

    Make sure file logs works
    Our code base tries to create the log dir if does not exists.
    It returns an error if the above is not possible.
    
    Fixes #2340

--- go/lib/log/log_test.go
+++ go/lib/log/log_test.go
@@ -15,52 +15,82 @@
 package log_test
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/inconshreveable/log15"
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/scionproto/scion/go/lib/log"
 	"github.com/scionproto/scion/go/lib/log/mock_log"
+	"github.com/scionproto/scion/go/lib/xtest"
 )
 
+func TestSetupLogConsole(t *testing.T) {
+	tmpDir, cleanF := xtest.MustTempDir("", "test-folder")
+	defer cleanF()
+
+	tests := map[string]struct {
+		dir       string
+		assertErr assert.ErrorAssertionFunc
+	}{
+		"cannot create, errors": {
+			dir:       "/sys/aka/doesnt/exist",
+			assertErr: assert.Error,
+		},
+
+		"can create, nil": {
+			dir:       filepath.Join(tmpDir, "new"),
+			assertErr: assert.NoError,
+		},
+	}
+
+	for td, tc := range tests {
+		t.Run(td, func(t *testing.T) {
+			err := log.SetupLogFile("test", tc.dir, "debug", 0, 0, 0, 0, false)
+			tc.assertErr(t, err)
+		})
+	}
+}
+
 func TestTraceFilterHandler(t *testing.T) {
-	Convey("Given a base handler...", t, func() {
+	t.Log("Given a base handler...")
+
+	t.Run("by default...", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
 		mockHandler := mock_log.NewMockHandler(ctrl)
 		logger := log.Root()
-		Convey("by default...", func() {
-			logger.SetHandler(mockHandler)
-			var msgSeenByMockHandler string
-			mockHandler.EXPECT().Log(gomock.Any()).Do(func(record *log15.Record) {
-				msgSeenByMockHandler = record.Msg
-			})
-			Convey("debug messages are printed", func() {
-				logger.Debug("foo")
-				So(msgSeenByMockHandler, ShouldEqual, "foo")
-			})
-			Convey("trace messages are printed", func() {
-				logger.Trace("foo")
-				So(msgSeenByMockHandler, ShouldEqual, log.TraceMsgPrefix+"foo")
-			})
-		})
-		Convey("if wrapped by a trace filter handler...", func() {
-			handler := log.FilterTraceHandler(mockHandler)
-			logger.SetHandler(handler)
-			Convey("debug messages are printed", func() {
-				var msgSeenByMockHandler string
-				mockHandler.EXPECT().Log(gomock.Any()).Do(func(record *log15.Record) {
-					msgSeenByMockHandler = record.Msg
-				})
-				logger.Debug("foo")
-				So(msgSeenByMockHandler, ShouldEqual, "foo")
-			})
-			Convey("trace messages are not printed", func() {
-				logger.Trace("foo")
-			})
-		})
+		logger.SetHandler(mockHandler)
+		var msgSeenByMockHandler string
+		mockHandler.EXPECT().Log(gomock.Any()).Do(func(record *log15.Record) {
+			msgSeenByMockHandler = record.Msg
+		}).AnyTimes()
+		t.Log("debug messages are printed")
+		logger.Debug("foo")
+		assert.Equal(t, msgSeenByMockHandler, "foo")
+		t.Log("trace messages are printed")
+		logger.Trace("foo")
+		assert.Equal(t, msgSeenByMockHandler, log.TraceMsgPrefix+"foo")
+	})
+
+	t.Run("if wrapped by a trace filter handler...", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		mockHandler := mock_log.NewMockHandler(ctrl)
+		logger := log.Root()
+		handler := log.FilterTraceHandler(mockHandler)
+		logger.SetHandler(handler)
+		t.Log("debug messages are printed")
+		var msgSeenByMockHandler string
+		mockHandler.EXPECT().Log(gomock.Any()).Do(func(record *log15.Record) {
+			msgSeenByMockHandler = record.Msg
+		}).AnyTimes()
+		logger.Debug("foo")
+		assert.Equal(t, msgSeenByMockHandler, "foo")
+		t.Log("trace messages are not printed")
+		logger.Trace("foo")
 	})
 
 }
