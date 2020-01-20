9799d526e017e41491dfcac9cd0fce0011aee8b8
karampok <karampok@gmail.com>
2020-01-20 12:10:39 +0100

    fix multiline

--- go/lib/scrypto/cert/renewal/request_signed_test.go
+++ go/lib/scrypto/cert/renewal/request_signed_test.go
@@ -19,22 +19,22 @@ var (
 	ccert = newCert()
 
 	//skPub, skPriv, _ = scrypto.GenKeyPair(scrypto.Ed25519)
-	skPub, skPriv = `e2c1f8c483077d345dcebf2bc6a133051b29d1eb6d7decd862fe4689df47c375
-	c4ab34f8849fd5024c1710f187921feb1db27161e2ddb7c05ab6ca4b3e155157`,
+	skPub, skPriv = "e2c1f8c483077d345dcebf2bc6a133051b29d1eb6d7decd862fe4689df47" +
+		"c375c4ab34f8849fd5024c1710f187921feb1db27161e2ddb7c05ab6ca4b3e155157",
 		"c4ab34f8849fd5024c1710f187921feb1db27161e2ddb7c05ab6ca4b3e155157"
 
 	// rkPub, rkPriv, _ = scrypto.GenKeyPair(scrypto.Ed25519)
-	rkPub, rkPriv = `c0c700869dae3ad40bdbff6808c12e0c23ac39a8235c09798aa75334304eb384
-	753f23a6e6390ff419f308e32d8706b9b4e8c79fe74a9446a4fa1663820f8148`,
+	rkPub, rkPriv = "c0c700869dae3ad40bdbff6808c12e0c23ac39a8235c09798aa75334304e" +
+		"b384753f23a6e6390ff419f308e32d8706b9b4e8c79fe74a9446a4fa1663820f8148",
 		"753f23a6e6390ff419f308e32d8706b9b4e8c79fe74a9446a4fa1663820f8148"
 
 	// ckPub, ckPriv, _ = scrypto.GenKeyPair(scrypto.Ed25519)
-	ckPub, ckPriv = `9a0fa05a864af7cc7da18a2cf23294997100653098b3337d54af1dfbdc692023
-	a8b4b32f8770d1ee9dd0a7b0284865b36b5e823be0cdedfb1d643160ea1eac05`,
+	ckPub, ckPriv = "9a0fa05a864af7cc7da18a2cf23294997100653098b3337d54af1dfbdc69" +
+		"2023a8b4b32f8770d1ee9dd0a7b0284865b36b5e823be0cdedfb1d643160ea1eac05",
 		"a8b4b32f8770d1ee9dd0a7b0284865b36b5e823be0cdedfb1d643160ea1eac05"
 
-	sigResult = `cKWdBxGgpGTAhENqSNBBO8pdSn2RRrCjy6+iKcO6xvsyUOR1Y5STHPh2Ua2qX3le412p
-	6ACUFYWzwDvDKV2HCQ==`
+	sigResult = "cKWdBxGgpGTAhENqSNBBO8pdSn2RRrCjy6+iKcO6xvsyUOR1Y5STHPh2Ua2qX3le" +
+		"412p6ACUFYWzwDvDKV2HCQ=="
 )
 
 func newCert() *cert.AS {
