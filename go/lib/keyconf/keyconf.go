7b73c92646fc0275128e5ec2f3f9b6e67eb3913b
roos <roos@anapaya.net>
2020-01-21 17:06:23 +0100

    keyconf: Remove unused code

--- go/lib/keyconf/keyconf.go
+++ go/lib/keyconf/keyconf.go
@@ -27,29 +27,9 @@ import (
 	"github.com/scionproto/scion/go/lib/scrypto"
 )
 
-type Conf struct {
-	// IssSigKey is the AS issuer signing Key.
-	IssSigKey common.RawBytes
-	// DecryptKey is the AS decryption key.
-	DecryptKey common.RawBytes
-	// OffRootKey is the AS offline root key.
-	OffRootKey common.RawBytes
-	// OnRootKey is the AS online root key.
-	OnRootKey common.RawBytes
-	// SignKey is the AS signing key.
-	SignKey common.RawBytes
-	// Master contains the AS master keys.
-	Master Master
-}
-
 const (
-	IssSigKeyFile = "core-sig.seed" // TODO(roosd): rename "core-sig.key" -> "iss-sig.key"
-	DecKeyFile    = "as-decrypt.key"
-	OffKeyFile    = "offline-root.seed"
-	OnKeyFile     = "online-root.seed"
-	SigKeyFile    = "as-sig.seed"
-	MasterKey0    = "master0.key"
-	MasterKey1    = "master1.key"
+	MasterKey0 = "master0.key"
+	MasterKey1 = "master1.key"
 
 	// FIXME(roosd): removed unused keys above.
 
@@ -74,55 +54,8 @@ const (
 	ErrUnknown common.ErrMsg = "Unknown algorithm"
 )
 
-// Load loads key configuration from specified path.
-// issSigKey, onKey, offKey, master can be set true, to load the respective keys.
-func Load(path string, issSigKey, onKey, offKey, master bool) (*Conf, error) {
-	conf := &Conf{}
-	var err error
-	conf.DecryptKey, err = loadKeyCond(filepath.Join(path, DecKeyFile),
-		scrypto.Curve25519xSalsa20Poly1305, true)
-	if err != nil {
-		return nil, err
-	}
-	conf.SignKey, err = loadKeyCond(filepath.Join(path, SigKeyFile), scrypto.Ed25519, true)
-	if err != nil {
-		return nil, err
-	}
-	conf.IssSigKey, err = loadKeyCond(filepath.Join(path, IssSigKeyFile),
-		scrypto.Ed25519, issSigKey)
-	if err != nil {
-		return nil, err
-	}
-	conf.OffRootKey, err = loadKeyCond(filepath.Join(path, OffKeyFile), scrypto.Ed25519, offKey)
-	if err != nil {
-		return nil, err
-	}
-	conf.OnRootKey, err = loadKeyCond(filepath.Join(path, OnKeyFile), scrypto.Ed25519, onKey)
-	if err != nil {
-		return nil, err
-	}
-	if conf.Master, err = loadMasterCond(path, master); err != nil {
-		return nil, err
-	}
-	return conf, nil
-}
-
-func loadKeyCond(file string, algo string, load bool) (common.RawBytes, error) {
-	if !load {
-		return nil, nil
-	}
-	return LoadKey(file, algo)
-}
-
-func loadMasterCond(path string, load bool) (Master, error) {
-	if !load {
-		return Master{}, nil
-	}
-	return LoadMaster(path)
-}
-
 // LoadKey decodes a base64 encoded key stored in file and returns the raw bytes.
-func LoadKey(file string, algo string) (common.RawBytes, error) {
+func loadKey(file string, algo string) (common.RawBytes, error) {
 	b, err := ioutil.ReadFile(file)
 	if err != nil {
 		return nil, common.NewBasicError(ErrOpen, err)
@@ -143,26 +76,18 @@ func LoadKey(file string, algo string) (common.RawBytes, error) {
 	}
 }
 
-func (c *Conf) String() string {
-	return fmt.Sprintf("DecryptKey:%s SigningKey:%s IssSigningKey: %s "+
-		"OfflineRootKey:%s OnlineRootKey:%s Master:%s",
-		//XXX(shitz): Uncomment for debugging.
-		//c.DecryptKey, c.SignKey, c.IssSigKey, c.OffRootKey, c.OnRootKey, c.Master)
-		"<redacted>", "<redacted>", "<redacted>", "<redacted>", "<redacted>", "<redacted>")
-}
-
 type Master struct {
-	Key0 common.RawBytes
-	Key1 common.RawBytes
+	Key0 []byte
+	Key1 []byte
 }
 
 func LoadMaster(path string) (Master, error) {
 	var err error
 	m := Master{}
-	if m.Key0, err = LoadKey(filepath.Join(path, MasterKey0), RawKey); err != nil {
+	if m.Key0, err = loadKey(filepath.Join(path, MasterKey0), RawKey); err != nil {
 		return m, err
 	}
-	if m.Key1, err = LoadKey(filepath.Join(path, MasterKey1), RawKey); err != nil {
+	if m.Key1, err = loadKey(filepath.Join(path, MasterKey1), RawKey); err != nil {
 		return m, err
 	}
 	return m, nil
