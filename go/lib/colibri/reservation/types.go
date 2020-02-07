fbbe456d2e39ce3d914b2216793a27ce8ca00a10
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-02-07 19:44:33 +0100

    moved pkg lib/colibri to lib/colibri/reservation

--- go/lib/colibri/reservation/types.go
+++ go/lib/colibri/reservation/types.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package colibri
+package reservation
 
 import (
 	"github.com/scionproto/scion/go/lib/addr"
