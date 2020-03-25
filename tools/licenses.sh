658eab5b184e2001b27ee9299051ef307d325af5
Martin Sustrik <sustrik@anapaya.net>
2020-03-25 06:23:19 +0100

    CI checks licenses

--- tools/licenses.sh
+++ tools/licenses.sh
@@ -7,20 +7,22 @@ echo $ROOTDIR
 
 bazel build //:all
 
-rm -rf $ROOTDIR/licenses/data
+DSTDIR=${1:-$ROOTDIR/licenses/data}
+
+rm -rf $DSTDIR
 
 find -L $ROOTDIR/bazel-scion/external -iregex '.*\(LICENSE\|COPYING\).*' | cut -d/ -f5- | while IFS= read -r path ; do
-    dst=$ROOTDIR/licenses/data/$(dirname $path)
+    dst=$DSTDIR/$(dirname $path)
     mkdir -p $dst
     cp $ROOTDIR/bazel-scion/external/$path $dst
 done
 
-# Bazel tools are used only for building. We don't need the licenses
-# to be distributed in the containers.
-rm -rf $ROOTDIR/licenses/data/bazel_tools
+# Bazel tools are used only for building.
+# We don't need these licenses to be distributed with the containers.
+rm -rf $DSTDIR/bazel_tools
 
 # These are not actual licenses.
-rm -rf $ROOTDIR/licenses/data/com_github_spf13_cobra/cobra
-rm -rf $ROOTDIR/licenses/data/com_github_uber_jaeger_client_go/scripts
-rm -rf $ROOTDIR/licenses/data/com_github_uber_jaeger_lib/scripts
-rm -rf $ROOTDIR/licenses/data/com_github_prometheus_procfs/scripts
+rm -rf $DSTDIR/com_github_spf13_cobra/cobra
+rm -rf $DSTDIR/com_github_uber_jaeger_client_go/scripts
+rm -rf $DSTDIR/com_github_uber_jaeger_lib/scripts
+rm -rf $DSTDIR/com_github_prometheus_procfs/scripts
