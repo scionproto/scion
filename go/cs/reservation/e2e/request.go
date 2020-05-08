abfcd03bd64e8376a8f8456131127abe878ffbb7
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-05-08 15:28:23 +0200

    e2e reservation request and UT

--- go/cs/reservation/e2e/request.go
+++ go/cs/reservation/e2e/request.go
@@ -14,8 +14,87 @@
 
 package e2e
 
-// "github.com/scionproto/scion/go/lib/colibri/reservation"
+import (
+	"time"
 
-type SetupReq struct {
-	//
+	"github.com/scionproto/scion/go/lib/colibri/reservation"
+	"github.com/scionproto/scion/go/lib/ctrl/colibri_mgmt"
+	"github.com/scionproto/scion/go/lib/serrors"
+)
+
+// SetupReq is the interface for an e2e setup request.
+// Currently it's implemented by either SuccessSetupReq or FailureSetupReq.
+type SetupReq interface {
+	Reservation() *Reservation
+	Timestamp() time.Time
+}
+
+// BaseSetupReq is the common part of any e2e setup request.
+type BaseSetupReq struct {
+	reservation *Reservation
+	timestamp   time.Time
+}
+
+func (s *BaseSetupReq) Timestamp() time.Time      { return s.timestamp }
+func (s *BaseSetupReq) Reservation() *Reservation { return s.reservation }
+
+// SuccessSetupReq is a successful e2e resevation setup request.
+type SuccessSetupReq struct {
+	BaseSetupReq
+	ID    reservation.E2EID
+	Token reservation.Token
+}
+
+var _ SetupReq = (*SuccessSetupReq)(nil)
+
+// FailureSetupReq is a failing e2e resevation setup request.
+type FailureSetupReq struct {
+	BaseSetupReq
+	ErrorCode  int
+	InfoField  reservation.InfoField
+	MaxBWTrail []reservation.BWCls
+}
+
+var _ SetupReq = (*FailureSetupReq)(nil)
+
+// NewRequestFromCtrlMsg will return a SuccessSetupReq or FailSetupReq depending on the
+// success flag of the ctrl message.
+func NewRequestFromCtrlMsg(setup *colibri_mgmt.E2ESetup, ts time.Time) (SetupReq, error) {
+	var s SetupReq
+	switch {
+	case setup.Success != nil:
+		id, err := reservation.E2EIDFromRawBuffers(setup.Success.ReservationID.ASID,
+			setup.Success.ReservationID.Suffix)
+		if err != nil {
+			return nil, err
+		}
+		tok, err := reservation.TokenFromRaw(setup.Success.Token)
+		if err != nil {
+			return nil, err
+		}
+		s = &SuccessSetupReq{
+			BaseSetupReq: BaseSetupReq{timestamp: ts},
+			ID:           *id,
+			Token:        *tok,
+		}
+	case setup.Failure != nil:
+		ifield, err := reservation.InfoFieldFromRaw(setup.Failure.InfoField)
+		if err != nil {
+			return nil, err
+		}
+		bwTrail := make([]reservation.BWCls, len(setup.Failure.MaxBWs))
+		for i, bw := range setup.Failure.MaxBWs {
+			bwTrail[i] = reservation.BWCls(bw)
+		}
+		s = &FailureSetupReq{
+			BaseSetupReq: BaseSetupReq{timestamp: ts},
+			ErrorCode:    int(setup.Failure.ErrorCode),
+			InfoField:    *ifield,
+			MaxBWTrail:   bwTrail,
+		}
+	default:
+		return nil, serrors.New("invalid E2E setup request received, neither successful or failed",
+			"success_ptr", setup.Success, "failure_ptr", setup.Failure)
+	}
+	return s, nil
 }
