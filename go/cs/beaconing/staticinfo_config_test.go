c1631b1221dbd1c5847d60d3917fbc86857c73d3
Lukas Vogel <lukedirtwalker@gmail.com>
2020-05-08 08:28:32 +0200

    fix tests & licences

--- go/cs/beaconing/staticinfo_config_test.go
+++ go/cs/beaconing/staticinfo_config_test.go
@@ -1,3 +1,17 @@
+// Copyright 2020 ETH Zurich, Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package beaconing
 
 import (
@@ -9,8 +23,8 @@ import (
 	"github.com/scionproto/scion/go/lib/ctrl/seg"
 )
 
-func getTestConfigData() StaticInfoCfg {
-	return StaticInfoCfg{
+func getTestConfigData() *StaticInfoCfg {
+	return &StaticInfoCfg{
 		Latency: map[common.IFIDType]InterfaceLatencies{
 			1: {
 				Inter: 30,
@@ -99,7 +113,7 @@ func TestParsing(t *testing.T) {
 // TestGenerateStaticinfo tests whether or not GenerateStaticinfo works properly.
 func TestGenerateStaticinfo(t *testing.T) {
 	test := struct {
-		configData StaticInfoCfg
+		configData *StaticInfoCfg
 		peers      map[common.IFIDType]struct{}
 		egIfid     common.IFIDType
 		inIfid     common.IFIDType
