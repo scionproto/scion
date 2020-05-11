ca00edc8a351c6eefa4c2734563f911379ac8720
NE1997 <ebelingn@student.ethz.ch>
2020-05-11 22:27:46 +0200

    Implemented cerealizable interface for staticinfo extn

--- go/lib/ctrl/seg/staticinfo_extn.go
+++ go/lib/ctrl/seg/staticinfo_extn.go
@@ -14,7 +14,11 @@
 
 package seg
 
-import "github.com/scionproto/scion/go/lib/common"
+import (
+	"fmt"
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/proto"
+)
 
 type LatencyInfo struct {
 	Egresslatency          uint16         `capnp:"egressLatency"`
@@ -87,4 +91,108 @@ type StaticInfoExtn struct {
 	Bandwidth BandwidthInfo    `capnp:"bandwidth"`
 	Hops      InternalHopsInfo `capnp:"internalHops"`
 	Note      string           `capnp:"note"`
+}
+
+func (s *ChildLatency) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_LatencyInfo_ChildLatency_TypeID
+}
+
+func (s *ChildLatency) String() string{
+	return fmt.Sprintf("Intralatency: %d\nIfID: %d\n", s.Intradelay, s.IfID)
+}
+
+func (s *PeerLatency) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_LatencyInfo_PeerLatency_TypeID
+}
+
+func (s *PeerLatency) String() string{
+	return fmt.Sprintf("Intralatency: %d\nInterlatency: %d\nIfID: %d\n", s.IntraDelay, s.Interdelay, s.IfID)
+}
+
+func (s *LatencyInfo) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_LatencyInfo_TypeID
+}
+
+func (s *LatencyInfo) String() string{
+	return fmt.Sprintf("IngressToEgressLatency: %d\nEgressLatency: %d\nChildlatencies: %v\nPeerlatencies: %v\n", s.IngressToEgressLatency, s.Egresslatency, s.Childlatencies, s.Peerlatencies)
+}
+
+func (s *InterfaceBandwidth) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_BandwidthInfo_InterfaceBandwidth_TypeID
+}
+
+func (s *InterfaceBandwidth) String() string{
+	return fmt.Sprintf("BW: %d\nIfID: %d\n", s.BW, s.IfID)
+}
+
+func (s *BandwidthInfo) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_BandwidthInfo_TypeID
+}
+
+func (s *BandwidthInfo) String() string{
+	return fmt.Sprintf("IngressToEgressBW: %d\nEgressBW: %d\nInterfaceBandwidths: %v\n", s.IngressToEgressBW, s.EgressBW, s.Bandwidths)
+}
+
+func (s *Coordinates) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_GeoInfo_Location_Coordinates_TypeID
+}
+
+func (s *Coordinates) String() string{
+	return fmt.Sprintf("Latitude %f\nLongitude: %f\nAddress: %s\n", s.Latitude, s.Longitude, s.Address)
+}
+
+func (s *Location) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_GeoInfo_Location_TypeID
+}
+
+func (s *Location) String() string{
+	return fmt.Sprintf("Location: %v\nIfIDs: %v\n", s.GPSData, s.IfIDs)
+}
+
+func (s *GeoInfo) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_GeoInfo_TypeID
+}
+
+func (s *GeoInfo) String() string{
+	return fmt.Sprintf("Locations: %v\n", s.Locations)
+}
+
+func (s *InterfaceLinkType) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_LinkTypeInfo_InterfaceLinkType_TypeID
+}
+
+func (s *InterfaceLinkType) String() string{
+	return fmt.Sprintf("LinkType: %d\nIfID: %d\n", s.LinkType, s.IfID)
+}
+
+func (s *LinktypeInfo) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_LinkTypeInfo_TypeID
+}
+
+func (s *LinktypeInfo) String() string{
+	return fmt.Sprintf("EgressLinkType: %d\nPeerLinkTypes: %v\n", s.EgressLinkType, s.Peerlinks)
+}
+
+func (s *InterfaceHops) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_InternalHopsInfo_InterfaceHops_TypeID
+}
+
+func (s *InterfaceHops) String() string{
+	return fmt.Sprintf("Hops: %d\nIfID: %d\n", s.Hops, s.IfID)
+}
+
+func (s *InternalHopsInfo) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_InternalHopsInfo_TypeID
+}
+
+func (s *InternalHopsInfo) String() string{
+	return fmt.Sprintf("InToOutHops: %d\nInterfaceHops: %v\n", s.InToOutHops, s.InterfaceHops)
+}
+
+func (s *StaticInfoExtn) ProtoId() proto.ProtoIdType {
+	return proto.StaticInfoExtn_TypeID
+}
+
+func (s *StaticInfoExtn) String() string{
+	return fmt.Sprintf("Latency: %v\nGeo: %v\nLinktype: %v\nBandwidth: %v\nHops: %v\nNote: %v\n", s.Latency, s.Geo, s.Linktype, s.Bandwidth, s.Hops, s.Note)
 }
