8329d9a26a7d9dee9a82d4d57a00cc0e049dee5b
NE1997 <ebelingn@student.ethz.ch>
2020-05-17 13:01:33 +0200

    Update graph.go

--- go/lib/infra/modules/combinator/graph.go
+++ go/lib/infra/modules/combinator/graph.go
@@ -361,6 +361,8 @@ func (solution *PathSolution) GetFwdPathMetadata() *Path {
 	}
 	path.reverseDownSegment()
 	path.aggregateInterfaces()
+	fmt.Println("path:")
+	fmt.Println(path)
 	path.StaticInfo = solution.GatherASEntries().CombineSegments().Condensemetadata()
 	return path
 }
