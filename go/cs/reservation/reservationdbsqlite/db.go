e23a8de01e3362027769c24a7af9b1beaeddd6ca
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-06-11 17:27:06 +0200

    schema and implementing sqlite methods

--- go/cs/reservation/reservationdbsqlite/db.go
+++ go/cs/reservation/reservationdbsqlite/db.go
@@ -19,6 +19,7 @@ import (
 	"database/sql"
 	"sync"
 
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/scionproto/scion/go/cs/reservation/e2e"
@@ -28,6 +29,7 @@ import (
 	"github.com/scionproto/scion/go/lib/colibri/reservation"
 	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/infra/modules/db"
+	"github.com/scionproto/scion/go/lib/serrors"
 )
 
 type Backend struct {
@@ -37,9 +39,20 @@ type Backend struct {
 
 var _ backend.DB = (*Backend)(nil)
 
+// New returns a new SQLite backend opening a database at the given path. If
+// no database exists a new database is be created. If the schema version of the
+// stored database is different from the one in schema.go, an error is returned.
 func New(path string) (*Backend, error) {
-	// TODO(juagargi)
-	return nil, nil
+	db, err := db.NewSqlite(path, Schema, SchemaVersion)
+	if err != nil {
+		return nil, err
+	}
+	return &Backend{
+		executor: &executor{
+			db: db,
+		},
+		db: db,
+	}, nil
 }
 
 // SetMaxOpenConns sets the maximum number of open connections.
@@ -52,6 +65,7 @@ func (b *Backend) SetMaxIdleConns(maxIdleConns int) {
 	b.db.SetMaxIdleConns(maxIdleConns)
 }
 
+// BeginTransaction begins a transaction on the database.
 func (b *Backend) BeginTransaction(ctx context.Context, opts *sql.TxOptions) (
 	backend.Transaction, error) {
 
@@ -69,9 +83,9 @@ func (b *Backend) BeginTransaction(ctx context.Context, opts *sql.TxOptions) (
 	}, nil
 }
 
+// Close closes the databse.
 func (b *Backend) Close() error {
-	// TODO(juagargi)
-	return nil
+	return b.db.Close()
 }
 
 type transaction struct {
@@ -142,10 +156,41 @@ func (x *executor) GetSegmentRsvsFromIFPair(ctx context.Context, ingress, egress
 	return nil, nil
 }
 
+func insertNewSegReservation(ctx context.Context, x db.Sqler, rsv *segment.Reservation,
+	suffix uint32) error {
+	const query = `INSERT INTO seg_reservation (id_as, id_suffix, inout_ingress ,inout_egress,
+		path, src_as, dst_as) VALUES ($1, $2, $3, $4, $5, $6, $7)`
+	_, err := x.ExecContext(ctx, query, rsv.Path.GetSrcIA().A, suffix,
+		rsv.InOutIFIDs.Ingress, rsv.InOutIFIDs.Egress,
+		rsv.Path.ToRaw(), rsv.Path.GetSrcIA().IAInt(), rsv.Path.GetDstIA().IAInt())
+	return err
+}
+
 // NewSegmentRsv creates a new segment reservation in the DB, with an unused reservation ID.
 // The created ID is set in the reservation pointer argument.
 func (x *executor) NewSegmentRsv(ctx context.Context, rsv *segment.Reservation) error {
-	return nil
+	var err error
+	for retries := 0; retries < 3; retries++ {
+		err = db.DoInTx(ctx, x.db, func(ctx context.Context, tx *sql.Tx) error {
+			suffix, err := newSuffix(ctx, tx, rsv.ID.ASID)
+			if err != nil {
+				return err
+			}
+			if err := insertNewSegReservation(ctx, tx, rsv, suffix); err != nil {
+				return err
+			}
+			common.Order.PutUint32(rsv.ID.Suffix[:], suffix)
+			return nil
+		})
+		if err == nil {
+			return nil
+		}
+		sqliteError, ok := err.(sqlite3.Error)
+		if !ok || sqliteError.Code != sqlite3.ErrConstraint {
+			return db.NewTxError("error inserting segment reservation", err)
+		}
+	}
+	return db.NewTxError("error inserting segment reservation after 3 retries", err)
 }
 
 // SetActiveIndex updates the active index for the segment reservation.
@@ -215,4 +260,22 @@ func (x *executor) DeleteE2EIndex(ctx context.Context, rsv *e2e.Reservation,
 	idx reservation.IndexNumber) error {
 
 	return nil
+}
+
+// newSuffix finds a segment reservation ID suffix not being used at the moment. Should be called
+// inside a transaction so the suffix is not used in the meantime, or fail.
+func newSuffix(ctx context.Context, x db.Sqler, ASID addr.AS) (uint32, error) {
+	query := `SELECT MIN(id_suffix)+1 FROM (
+		SELECT 0 AS id_suffix UNION ALL
+		SELECT id_suffix FROM seg_reservation WHERE id_as = $1
+		) WHERE id_suffix+1 NOT IN (SELECT id_suffix FROM seg_reservation WHERE id_as = $1);`
+	var suffix uint32
+	err := x.QueryRowContext(ctx, query, uint64(ASID)).Scan(&suffix)
+	switch {
+	case err == sql.ErrNoRows:
+		return 0, serrors.New("unexpected error getting new suffix: no rows")
+	case err != nil:
+		return 0, serrors.WrapStr("unexpected error getting new suffix", err)
+	}
+	return suffix, nil
 }
