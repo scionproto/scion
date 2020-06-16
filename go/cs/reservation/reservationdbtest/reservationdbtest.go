626ec7607dac3ccca5b2d76d11a70f0d15e9e13c
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-06-16 16:44:28 +0200

    fix segment reservation schema.
    Missing end properties and traffic split.
    Added UT.

--- go/cs/reservation/reservationdbtest/reservationdbtest.go
+++ go/cs/reservation/reservationdbtest/reservationdbtest.go
@@ -22,6 +22,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/scionproto/scion/go/cs/reservation/segment"
+	"github.com/scionproto/scion/go/cs/reservation/segmenttest"
 	"github.com/scionproto/scion/go/cs/reservationstorage/backend"
 	"github.com/scionproto/scion/go/lib/colibri/reservation"
 	"github.com/scionproto/scion/go/lib/xtest"
@@ -36,6 +37,7 @@ func TestDB(t *testing.T, db TestableDB) {
 	tests := map[string]func(context.Context, *testing.T, backend.DB){
 		"insert segment reservations create ID": testNewSegmentRsv,
 		"insert segment index":                  testNewSegmentIndex,
+		"get segment reservation from ID":       testGetSegmentRsvFromID,
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
@@ -65,26 +67,88 @@ func testNewSegmentRsv(ctx context.Context, t *testing.T, db backend.DB) {
 
 func testNewSegmentIndex(ctx context.Context, t *testing.T, db backend.DB) {
 	r := segment.NewReservation()
-	db.NewSegmentRsv(ctx, r)
-	expTime := time.Unix(1, 0)
-	_, err := r.NewIndex(expTime, *newToken())
-	r.Indices[0].Token.BWCls = 2
+
+	p := segmenttest.NewPathFromComponents(0, "1-ff00:0:1", 1, 1, "1-ff00:0:2", 0)
+	r.Path = &p
+	r.ID.ASID = xtest.MustParseAS("ff00:0:1")
+	r.IngressIFID = 1
+	r.EgressIFID = 2
+	r.TrafficSplit = 3
+	r.PathEndProps = reservation.EndLocal | reservation.EndTransfer | reservation.StartLocal
 	ctx := context.Background()
+	// no index
+	err := db.NewSegmentRsv(ctx, r)
+	require.Error(t, err)
+	// one index (add 3, remove 2 to change its IndexNumber)
+	expTime := time.Unix(1, 0)
+	_, err = r.NewIndex(expTime, *newToken())
+	require.NoError(t, err)
+	_, err = r.NewIndex(expTime, *newToken())
+	require.NoError(t, err)
+	_, err = r.NewIndex(expTime, *newToken())
+	require.NoError(t, err)
+	err = r.RemoveIndex(1)
+	require.NoError(t, err)
+	require.Len(t, r.Indices, 1)
+	r.SetIndexConfirmed(reservation.IndexNumber(2))
+	err = r.SetIndexConfirmed(2)
+	require.NoError(t, err)
+	r.Indices[0].MinBW = 3
+	r.Indices[0].MaxBW = 4
+	r.Indices[0].AllocBW = 5
+	r.Indices[0].Token.BWCls = 2
 	err = db.NewSegmentRsv(ctx, r)
 	require.NoError(t, err)
-	// create new index
-	idx, err := r.NewIndex(expTime, *newToken())
-	r.Indices[1].Token.BWCls = 3
-	require.NoError(t, err)
-	err = db.NewSegmentIndex(ctx, r, idx)
-	require.NoError(t, err)
+	// read the reservation
 	r2, err := db.GetSegmentRsvFromID(ctx, &r.ID)
 	require.NoError(t, err)
 	require.Equal(t, r, r2)
 }
 
 func testGetSegmentRsvFromID(t *testing.T, db backend.DB) {
-
+	r := segment.NewReservation()
+	p := segmenttest.NewPathFromComponents(0, "1-ff00:0:1", 1, 1, "1-ff00:0:2", 0)
+	r.Path = &p
+	r.ID.ASID = xtest.MustParseAS("ff00:0:1")
+	r.IngressIFID = 1
+	r.EgressIFID = 2
+	r.TrafficSplit = 3
+	r.PathEndProps = reservation.EndLocal | reservation.StartLocal
+	expTime := time.Unix(1, 0)
+	_, err := r.NewIndex(expTime, *newToken())
+	require.NoError(t, err)
+	r.Indices[0].Token.BWCls = 2
+	err = r.SetIndexConfirmed(0)
+	require.NoError(t, err)
+	ctx := context.Background()
+	err = db.NewSegmentRsv(ctx, r)
+	require.NoError(t, err)
+	// create new index
+	idx, err := r.NewIndex(expTime, *newToken())
+	require.NoError(t, err)
+	r.Indices[1].Token.BWCls = 3
+	err = db.NewSegmentIndex(ctx, r, idx)
+	require.NoError(t, err)
+	r2, err := db.GetSegmentRsvFromID(ctx, &r.ID)
+	require.NoError(t, err)
+	require.Equal(t, r, r2)
+	// 14 more indices for a total of 16
+	require.Len(t, r.Indices, 2)
+	for i := 2; i < 16; i++ {
+		expTime = time.Unix(int64(i), 0)
+		idx, err = r.NewIndex(expTime, *newToken())
+		require.NoError(t, err)
+		r.Indices[i].MinBW = reservation.BWCls(i)
+		r.Indices[i].MaxBW = reservation.BWCls(i)
+		r.Indices[i].AllocBW = reservation.BWCls(i)
+		r.Indices[i].Token.BWCls = reservation.BWCls(i)
+		err = db.NewSegmentIndex(ctx, r, idx)
+		require.NoError(t, err)
+	}
+	require.Len(t, r.Indices, 16)
+	r2, err = db.GetSegmentRsvFromID(ctx, &r.ID)
+	require.NoError(t, err)
+	require.Equal(t, r, r2)
 }
 
 // newToken just returns a token that can be serialized. This one has two HopFields.
