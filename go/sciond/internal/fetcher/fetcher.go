9411cc0d42d2ca5e651792eff64eb24662a93da5
NE1997 <ebelingn@student.ethz.ch>
2020-06-16 15:02:24 +0200

    Fixed testing for pathmetadata

--- go/sciond/internal/fetcher/fetcher.go
+++ go/sciond/internal/fetcher/fetcher.go
@@ -181,7 +181,7 @@ func (f *fetcher) translate(path *combinator.Path) (sciond.PathReplyEntry, error
 			ExpTime:    uint32(path.ComputeExpTime().Unix()),
 		},
 		HostInfo:   hostinfo.FromUDPAddr(*nextHop),
-		StaticInfo: Condensemetadata(path.StaticInfo),
+		StaticInfo: CondenseMetadata(path.StaticInfo),
 	}
 	return entry, nil
 }
