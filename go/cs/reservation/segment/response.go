d6fa04080f23ba2174c06b50a2fda9fbccc9e7f1
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-08-03 12:09:07 +0200

    WIP response translation

--- go/cs/reservation/segment/response.go
+++ go/cs/reservation/segment/response.go
@@ -15,19 +15,48 @@
 package segment
 
 import (
+	"time"
+
 	base "github.com/scionproto/scion/go/cs/reservation"
 	"github.com/scionproto/scion/go/lib/colibri/reservation"
+	"github.com/scionproto/scion/go/lib/serrors"
+	"github.com/scionproto/scion/go/lib/spath"
 )
+
+// Response is the base struct for any type of COLIBRI segment response.
+type Response struct {
+	base.RequestMetadata                         // information about the request (forwarding path)
+	ID                   reservation.SegmentID   // the ID this request refers to
+	Index                reservation.IndexNumber // the index this request refers to
+}
+
+// NewResponse contructs the segment Response type.
+func NewResponse(ts time.Time, id *reservation.SegmentID, idx reservation.IndexNumber,
+	path *spath.Path) (*Response, error) {
+
+	metadata, err := base.NewRequestMetadata(path)
+	if err != nil {
+		return nil, serrors.WrapStr("new segment request", err)
+	}
+	if id == nil {
+		return nil, serrors.New("new segment response with nil ID")
+	}
+	return &Response{
+		RequestMetadata: *metadata,
+		ID:              *id,
+		Index:           reservation.IndexNumber(idx),
+	}, nil
+}
 
 // ResponseSetupSuccess is the response to a success setup. It's sent on the reverse direction.
 type ResponseSetupSuccess struct {
-	base.RequestMetadata // information about the request (forwarding path)
-	Token                reservation.Token
+	Response
+	Token reservation.Token
 }
 
 // ResponseSetupFailure is the response to a failed setup. It's sent on the reverse direction.
 type ResponseSetupFailure struct {
-	base.RequestMetadata // information about the request (forwarding path)
-	FailedSetup          *SetupReq
-	FailedHop            uint8
+	Response
+	FailedSetup SetupReq
+	FailedHop   uint8
 }
