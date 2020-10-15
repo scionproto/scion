cc2d4b3cb496acdff310040bf050e87626c4739f
Matthias Frei <matthias.frei@inf.ethz.ch>
2020-10-15 10:03:10 +0200

    Remove adding mypy as lint step

--- scion.sh
+++ scion.sh
@@ -284,11 +284,6 @@ py_lint() {
     local py_executables=$(find -type f -executable -regex '.*/[^.]*$' | xargs file -i | grep 'text/x-python' | cut -d: -f1)
     lint_step "flake8"
     flake8 python acceptance ${py_executables} | sort -t: -k1,1 -k2n,2 -k3n,3 || ((ret++))
-    lint_step "mypy"
-    mypy -p python -p acceptance || ((ret++))
-    for i in $py_executables; do
-      mypy $i || ((ret++))  # needs to be run seperately for each __main__ module
-    done
     return $ret
 }
 
