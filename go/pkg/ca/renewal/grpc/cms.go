644fe731701f3108cc320c44cc11c613c7e76459
Dominik Roos <roos@anapaya.net>
2021-04-01 19:20:14 +0200

    renewal: refactor request verifier
    Refactor the `RequestVerifier` to make it more reusable.
    Furthermore reduce code duplication and improve error messages.
    
    GitOrigin-RevId: cef2b7b57e919648a8c85f1010df9b861a97f67d

--- go/pkg/ca/renewal/grpc/cms.go
+++ go/pkg/ca/renewal/grpc/cms.go
@@ -24,9 +24,7 @@ import (
 	"github.com/scionproto/scion/go/lib/addr"
 	"github.com/scionproto/scion/go/lib/log"
 	"github.com/scionproto/scion/go/lib/metrics"
-	"github.com/scionproto/scion/go/lib/scrypto/cms/protocol"
 	"github.com/scionproto/scion/go/lib/scrypto/cppki"
-	"github.com/scionproto/scion/go/lib/serrors"
 	"github.com/scionproto/scion/go/pkg/ca/renewal"
 	cppb "github.com/scionproto/scion/go/pkg/proto/control_plane"
 	cryptopb "github.com/scionproto/scion/go/pkg/proto/crypto"
@@ -74,20 +72,10 @@ func (s CMS) HandleCMSRequest(
 
 	logger := log.FromCtx(ctx)
 
-	// Check that the requester is actually a client of the CA.
-	chain, err := extractClientChain(req.CmsSignedRequest)
+	issuerIA, err := extractIssuerIA(req.CmsSignedRequest, logger)
 	if err != nil {
-		logger.Debug("Failed to extract client certificate", "err", err)
 		metrics.CounterInc(s.Metrics.ParseError)
-		return nil, status.Error(codes.InvalidArgument,
-			"request malformed: cannot extract client chain")
-	}
-	issuerIA, err := cppki.ExtractIA(chain[1].Subject)
-	if err != nil {
-		logger.Debug("Failed to extract IA from issuer certificate", "err", err)
-		metrics.CounterInc(s.Metrics.ParseError)
-		return nil, status.Error(codes.InvalidArgument,
-			"request malformed: cannot extract issuer subject")
+		return nil, err
 	}
 	if !issuerIA.Equal(s.IA) {
 		logger.Debug("Renewal requester is not a client", "issuer_isd_as", issuerIA)
@@ -118,36 +106,18 @@ func (s CMS) HandleCMSRequest(
 	return newClientChain, nil
 }
 
-func extractClientChain(raw []byte) ([]*x509.Certificate, error) {
-	ci, err := protocol.ParseContentInfo(raw)
+func extractIssuerIA(raw []byte, logger log.Logger) (addr.IA, error) {
+	chain, err := extractChain(raw)
 	if err != nil {
-		return nil, serrors.WrapStr("parsing ContentInfo", err)
+		logger.Debug("Failed to extract client certificate", "err", err)
+		return addr.IA{}, status.Error(codes.InvalidArgument,
+			"request malformed: cannot extract client chain")
 	}
-	sd, err := ci.SignedDataContent()
+	issuerIA, err := cppki.ExtractIA(chain[1].Subject)
 	if err != nil {
-		return nil, serrors.WrapStr("parsing SignedData", err)
+		logger.Debug("Failed to extract IA from issuer certificate", "err", err)
+		return addr.IA{}, status.Error(codes.InvalidArgument,
+			"request malformed: cannot extract issuer subject")
 	}
-	if len(sd.SignerInfos) != 1 {
-		return nil, serrors.New("unexpected number of signers", "expected", 1,
-			"actual", len(sd.SignerInfos))
-	}
-	si := sd.SignerInfos[0]
-	certs, err := sd.X509Certificates()
-	if certs == nil {
-		err = protocol.ErrNoCertificate
-	} else if len(certs) != 2 {
-		err = serrors.New("unexpected number of certificates")
-	}
-	if err != nil {
-		return nil, serrors.WrapStr("parsing client chain", err)
-	}
-	cert, err := si.FindCertificate(certs)
-	if err != nil {
-		return nil, serrors.WrapStr("selecting client certificate", err)
-	}
-	if cert != certs[0] {
-		certs[0], certs[1] = certs[1], certs[0]
-	}
-
-	return certs, nil
+	return issuerIA, nil
 }
