9f1e37f19016ae92adf962f37f88d111730d34e7
Dominik Roos <roos@anapaya.net>
2021-04-26 13:50:42 +0200

    scion-pki: add key sub-command
    Add `scion-pki key` sub-command that helps manage private and public keys.
    
    The currently supported commands are:
    
    - `scion-pki key private` generates a private key.
    - `scion-pki key public` generates a public key from a private key.
    
    GitOrigin-RevId: 8eec870bbc45a0164f3f23d6d33ede180e320d92

---
+++ go/scion-pki/file/file.go
@@ -0,0 +1,80 @@
+// Copyright 2021 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package file contains helper functions to interact with files.
+package file
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+
+	"github.com/scionproto/scion/go/lib/serrors"
+)
+
+// Option is the type to add optional behavior changes.
+type Option func(o *options)
+
+type options struct {
+	force bool
+}
+
+// WithForce overwrites an existing file if it already exists.
+func WithForce(force bool) Option {
+	return func(o *options) {
+		o.force = force
+	}
+}
+
+func apply(opts []Option) options {
+	var o options
+	for _, option := range opts {
+		option(&o)
+	}
+	return o
+}
+
+// CheckDirExists checks that whether the provided directory exists.
+func CheckDirExists(dir string) error {
+	stat, err := os.Stat(dir)
+	if errors.Is(err, os.ErrNotExist) {
+		return serrors.New("directory does not exist")
+	}
+	if err != nil {
+		return err
+	}
+	if !stat.IsDir() {
+		return serrors.New("not a directory")
+	}
+	return nil
+}
+
+func WriteFile(filename string, data []byte, perm os.FileMode, opts ...Option) error {
+	options := apply(opts)
+	if options.force {
+		return ioutil.WriteFile(filename, data, perm)
+	}
+
+	st, err := os.Stat(filename)
+	if errors.Is(err, os.ErrNotExist) {
+		return ioutil.WriteFile(filename, data, perm)
+	}
+	if err != nil {
+		return serrors.WrapStr("reading stat information", err)
+	}
+	if st.IsDir() {
+		return serrors.New("file is a directory")
+	}
+	return os.ErrExist
+}
---
+++ go/scion-pki/key/public.go
@@ -0,0 +1,115 @@
+// Copyright 2021 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package key
+
+import (
+	"crypto"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+	"github.com/scionproto/scion/go/lib/serrors"
+	"github.com/scionproto/scion/go/pkg/command"
+	"github.com/scionproto/scion/go/scion-pki/file"
+)
+
+// NewKeyPublicCmd returns a cobra command that returns the public key for a
+// given private key.
+func NewKeyPublicCmd(pather command.Pather) *cobra.Command {
+	var flags struct {
+		out   string
+		force bool
+	}
+	var cmd = &cobra.Command{
+		Use:   "public [flags] <private-key-file>",
+		Short: "Generate public key for the provided private key",
+		Example: fmt.Sprintf(`  %[1]s public cp-as.key
+  %[1]s public cp-as.key --out cp-as.pub`, pather.CommandPath()),
+		Long: `'public' generates a PEM encoded public key.
+
+By default, the public key is written to standard out.
+`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+
+			filename := args[0]
+			raw, err := ioutil.ReadFile(filename)
+			if err != nil {
+				return serrors.WrapStr("reading private key", err)
+			}
+			p, rest := pem.Decode(raw)
+			if p == nil {
+				return serrors.New("parsing private key failed")
+			}
+			if len(rest) != 0 {
+				return serrors.New("file must only contain private key")
+			}
+			if p.Type != "PRIVATE KEY" {
+				return serrors.New("file does not contain a private key", "type", p.Type)
+			}
+
+			key, err := x509.ParsePKCS8PrivateKey(p.Bytes)
+			if err != nil {
+				return serrors.WrapStr("parsing private key", err)
+			}
+
+			priv, ok := key.(interface{ Public() crypto.PublicKey })
+			if !ok {
+				return serrors.New("cannot get public key from private key",
+					"type", fmt.Sprintf("%T", priv),
+				)
+			}
+
+			out, err := x509.MarshalPKIXPublicKey(priv.Public())
+			if err != nil {
+				return serrors.WrapStr("encoding public key", err)
+			}
+			encoded := pem.EncodeToMemory(&pem.Block{
+				Type:  "PUBLIC KEY",
+				Bytes: out,
+			})
+			if encoded == nil {
+				panic("PEM encoding failed")
+			}
+			if flags.out == "" {
+				fmt.Print(string(encoded))
+				return nil
+			}
+
+			// Write public key to file system instead of stdout.
+			if err := file.CheckDirExists(filepath.Dir(flags.out)); err != nil {
+				return serrors.WrapStr("checking that directory of public key exists", err)
+			}
+			err = file.WriteFile(flags.out, raw, 0644, file.WithForce(flags.force))
+			if err != nil {
+				return serrors.WrapStr("writing public key", err)
+			}
+			fmt.Printf("Public key successfully written to %q\n", flags.out)
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&flags.out, "out", "",
+		"Path to write public key",
+	)
+	cmd.Flags().BoolVar(&flags.force, "force", false,
+		"Force overwritting existing public key",
+	)
+	return cmd
+}
