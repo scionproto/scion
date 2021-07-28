5df8893e6942ee00318caf46d8a58a26fcb3bdfd
Dominik Roos <roos@anapaya.net>
2021-07-28 13:37:39 +0200

    ci: improve tooling to download buildkite artifacts
    GitOrigin-RevId: c53cdd93c8f94c03038219d31307dbef46d8664e

---
+++ go/tools/buildkite_artifacts/buildkite/buildkite.go
@@ -0,0 +1,167 @@
+// Copyright 2021 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buildkite
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/buildkite/go-buildkite/v2/buildkite"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/scionproto/scion/go/lib/serrors"
+)
+
+// NewClient constructs a new buildkite client by taking the API token
+// from the environment.
+func NewClient() (*buildkite.Client, error) {
+	token := os.Getenv("BUILDKITE_TOKEN")
+	if token == "" {
+		fmt.Fprint(os.Stderr, `API token is not set.
+Please create a token at https://buildkite.com/user/api-access-tokens and pass it
+via the BUILDKITE_TOKEN environment variable.
+
+The following permissions are required:
+- read_artifacts
+- read_builds
+`)
+		return nil, fmt.Errorf("BUILDKITE_TOKEN not set")
+	}
+	config, err := buildkite.NewTokenConfig(token, false)
+	if err != nil {
+		return nil, err
+	}
+	return buildkite.NewClient(config.Client()), nil
+}
+
+type Downloader struct {
+	Client *buildkite.Client
+	Dir    string
+	All    bool
+	StdOut io.Writer
+	StdErr io.Writer
+}
+
+func (d *Downloader) ArtifactsFromBuild(build *buildkite.Build) error {
+	jobGroup, ctx := errgroup.WithContext(context.Background())
+	for _, job := range build.Jobs {
+		job := job
+
+		if job.ArtifactsURL == nil {
+			d.info("Ignoring job without artifacts: %s\n", *job.ID)
+			continue
+		}
+		if job.StepKey == nil {
+			d.info("Ignoring job without step key: %s (%s)\n", *job.ID, *job.Command)
+			continue
+		}
+		if !d.All && job.ExitStatus != nil && *job.ExitStatus == 0 {
+			d.info("Ignoring job that succeeded: %s", *job.Name)
+			continue
+		}
+
+		jobGroup.Go(func() error {
+			artifacts, err := d.artifactsByURL(*job.ArtifactsURL)
+			if err != nil {
+				return serrors.WrapStr("fetching artifacts", err, "job", job.Name)
+			}
+
+			artifactsGroup, _ := errgroup.WithContext(ctx)
+			for _, a := range artifacts {
+				a := a
+
+				if a.DownloadURL == nil {
+					d.info("Ignoring artifact %s without download URL\n", *a.ID)
+					continue
+				}
+				if a.Filename == nil || !strings.HasPrefix(*a.Filename, "buildkite") {
+					d.info("Ignore artifact %s because of filename: %s\n", *a.ID, *a.Filename)
+					continue
+				}
+
+				artifactsGroup.Go(func() error {
+					base := fmt.Sprintf("%s.%s", *job.StepKey, *job.ID)
+					base = strings.ReplaceAll(base, ":", "_")
+
+					start := time.Now()
+					file := filepath.Join(d.Dir, base+".tar.gz")
+					d.info("Start downloading: %s\n", file)
+					if err := d.downloadArtifact(a, file); err != nil {
+						return serrors.WrapStr("downloading artifact", err, "job", job.Name)
+					}
+					d.info("Done downloading: %s (%s)\n", file, time.Since(start))
+
+					start = time.Now()
+					dir := filepath.Join(d.Dir, base)
+					d.info("Start unpacking: %s\n", dir)
+					if err := os.MkdirAll(dir, 0755); err != nil {
+						return err
+					}
+					cmd := exec.Command("tar", "-xf", file, "-C", dir, "--strip-components", "1")
+					if out, err := cmd.CombinedOutput(); err != nil {
+						d.error(string(out))
+						return err
+					}
+					d.info("Done unpacking: %s (%s)\n", dir, time.Since(start))
+					return nil
+				})
+			}
+			return artifactsGroup.Wait()
+		})
+	}
+	return jobGroup.Wait()
+}
+
+func (d *Downloader) artifactsByURL(url string) ([]buildkite.Artifact, error) {
+	req, err := d.Client.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var artifacts []buildkite.Artifact
+	if _, err := d.Client.Do(req, &artifacts); err != nil {
+		return nil, err
+	}
+	return artifacts, nil
+}
+
+func (d *Downloader) downloadArtifact(artifact buildkite.Artifact, file string) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = d.Client.Artifacts.DownloadArtifactByURL(*artifact.DownloadURL, f)
+	return err
+
+}
+
+func (d *Downloader) info(format string, ctx ...interface{}) {
+	if d.StdOut != nil {
+		fmt.Fprintf(d.StdOut, format, ctx...)
+	}
+}
+
+func (d *Downloader) error(format string, ctx ...interface{}) {
+	if d.StdErr != nil {
+		fmt.Fprintf(d.StdErr, format, ctx...)
+	}
+}
---
+++ go/tools/buildkite_artifacts/main.go
@@ -0,0 +1,105 @@
+// Copyright 2021 Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A tool to download all artifacts from a specific build.
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+	"github.com/scionproto/scion/go/lib/serrors"
+	"github.com/scionproto/scion/go/pkg/app"
+	"github.com/scionproto/scion/go/tools/buildkite_artifacts/buildkite"
+)
+
+func main() {
+	var flags struct {
+		dir      string
+		org      string
+		pipeline string
+		all      bool
+		verbose  bool
+	}
+
+	executable := filepath.Base(os.Args[0])
+	cmd := &cobra.Command{
+		Use:   fmt.Sprintf("%s <build>", executable),
+		Short: "Buildkite artifacts downloader",
+		Long: `A small tool to streamline artifacts downloading from buildkite.
+
+To interact with buildkite, the BUILDKITE_TOKEN environment variable has to be
+set. The token must have the following permissions:
+
+- read_artifacts
+- read_builds
+`,
+		Args:          cobra.ExactArgs(1),
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			build := args[0]
+			cmd.SilenceUsage = true
+			client, err := buildkite.NewClient()
+			if err != nil {
+				return err
+			}
+
+			b, _, err := client.Builds.Get(flags.org, flags.pipeline, build, nil)
+			if err != nil {
+				return serrors.WrapStr("fetching build", err)
+			}
+			d := buildkite.Downloader{
+				Client: client,
+				StdErr: os.Stderr,
+				All:    flags.all,
+				Dir:    filepath.Join(flags.dir, build),
+			}
+			if flags.verbose {
+				d.StdOut = os.Stdout
+			}
+			if err := os.MkdirAll(d.Dir, 0755); err != nil {
+				return err
+			}
+			if err := d.ArtifactsFromBuild(b); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&flags.dir, "dir", "/tmp/buildkite-artifacts",
+		"Directory where the artifacts are stored",
+	)
+	cmd.Flags().StringVar(&flags.org, "organization", "scionproto",
+		"Buildkite organization slug",
+	)
+	cmd.Flags().StringVar(&flags.pipeline, "pipeline", "scion",
+		"Buildkite pipeline slug",
+	)
+	cmd.Flags().BoolVar(&flags.all, "all", false,
+		"Download all artifacts, even from successful jobs",
+	)
+	cmd.Flags().BoolVarP(&flags.verbose, "verbose", "v", false,
+		"Verbose info output",
+	)
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		if code := app.ExitCode(err); code != -1 {
+			os.Exit(code)
+		}
+		os.Exit(1)
+	}
+}
