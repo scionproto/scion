ff97396c0405923236dc92031a924739d26a15a1
mawyss <mawyss@ethz.ch>
2021-11-12 11:56:18 +0100

    Add comment.

--- go/dispatcher/dispatcher/underlay.go
+++ go/dispatcher/dispatcher/underlay.go
@@ -304,6 +304,8 @@ func (h SCMPHandler) reverse(pkt *respool.Packet) ([]byte, error) {
 	if err := h.reverseSCION(pkt); err != nil {
 		return nil, err
 	}
+	// XXX(roosd): This does not take HBH and E2E extensions into consideration.
+	// See: https://github.com/scionproto/scion/issues/4128
 	pkt.SCION.NextHdr = common.L4SCMP
 	// FIXME(roosd): Consider moving this to a resource pool.
 	buf := gopacket.NewSerializeBuffer()
