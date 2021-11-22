545c97bdf691ebaee2955133ff55cf579abbd270
mawyss <mawyss@ethz.ch>
2021-11-22 14:33:56 +0100

    Implement feedback.

--- go/pkg/showpaths/showpaths.go
+++ go/pkg/showpaths/showpaths.go
@@ -363,7 +363,7 @@ func Run(ctx context.Context, dst addr.IA, cfg Config) (*Result, error) {
 			LocalIP:    cfg.Local,
 			ID:         uint16(rand.Uint32()),
 			Dispatcher: cfg.Dispatcher,
-		}.GetStatuses(ctx, p, pathprobe.Options{EPIC: cfg.Epic})
+		}.GetStatuses(ctx, p, pathprobe.WithEpic(cfg.Epic))
 		if err != nil {
 			return nil, serrors.WrapStr("getting statuses", err)
 		}
