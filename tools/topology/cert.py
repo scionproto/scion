a574cee1da8c159d7e860472a359ed7706e1f27a
Matthias Frei <matthias.frei@inf.ethz.ch>
2022-03-22 14:37:56 +0100

    repo: consolidate tools
    Follow up on #4163, further consolidate tools under `tools/`.
    
    - `lint/` -> `tools/lint/`
    - `scripts/cryptoplayground/` -> `tools/cryptoplayground/`
    - `env/` -> `tools/env/`, `tools/install_deps`
    - `supervisor/` -> `tools/`
    - `python/` -> `tools/topology/`, `tools/topogen.py`
    - `integration/` -> `tools/integration/`
    
    Also includes some housekeeping
    - further reduce python library code, by simplifying the ISD_AS (no integer representation needed) and LinkType enum
    - remove left-over integration bash scripts
    - remove topogentar and the related bazel infrastructure
    
    Closes #4167
    
    GitOrigin-RevId: 202b24809b241a0d2381ceeeaa81fb8bb7ff4833

---
+++ tools/topology/cert.py
@@ -0,0 +1,70 @@
+# Copyright 2014 ETH Zurich
+# Copyright 2018 ETH Zurich, Anapaya Systems
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+:mod:`cert` --- SCION topology certificate generator
+=============================================
+"""
+import base64
+import collections
+import os
+import sys
+
+from plumbum import local, CommandNotFound
+
+from topology import common
+from topology.util import write_file
+
+
+class CertGenArgs(common.ArgsTopoConfig):
+    pass
+
+
+class CertGenerator(object):
+    def __init__(self, args):
+        """
+        :param CertGenArgs args: Contains the passed command line
+        arguments and the parsed topo config.
+        """
+        self.args = args
+        self.pki = local['./bin/scion-pki']
+        if not local.path('./bin/scion-pki').exists():
+            try:
+                self.pki = local[local.which('scion-pki')]
+            except CommandNotFound:
+                sys.exit("ERROR: scion-pki executable not found. Run `make` first.")
+        self.core_count = collections.defaultdict(int)
+
+    def generate(self, topo_dicts):
+        self.pki('testcrypto', '-t', self.args.topo_config, '-o', self.args.output_dir)
+        self._master_keys(topo_dicts)
+        self._copy_files(topo_dicts)
+
+    def _master_keys(self, topo_dicts):
+        for topo_id in topo_dicts:
+            base = topo_id.base_dir(self.args.output_dir)
+            write_file(os.path.join(base, 'keys', 'master0.key'),
+                       base64.b64encode(os.urandom(16)).decode())
+            write_file(os.path.join(base, 'keys', 'master1.key'),
+                       base64.b64encode(os.urandom(16)).decode())
+
+    def _copy_files(self, topo_dicts):
+        cp = local['cp']
+        mkdir = local['mkdir']
+        # Copy the certs and key dir for all elements.
+        for topo_id, as_topo in topo_dicts.items():
+            base = local.path(self.args.output_dir)
+            as_dir = local.path(topo_id.base_dir(self.args.output_dir))
+            mkdir('-p', as_dir / 'certs')
+            cp(base // '*/trcs/*.trc', as_dir / 'certs/')
