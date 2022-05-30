98036ec74324a090eb3bb0f38befdaa97fbc0364
Martin Sustrik <sustrik@anapaya.net>
2022-05-30 15:57:46 +0200

    make members public
    so that the structure can be easily serialized to JSON, if need be

--- gateway/pathhealth/selector.go
+++ gateway/pathhealth/selector.go
@@ -97,23 +97,23 @@ func (f *FilteringPathSelector) Select(selectables []Selectable, current Fingerp
 	var pathInfo PathInfo
 	for _, a := range allowed {
 		pathInfo = append(pathInfo, &pathInfoEntry{
-			current: a.IsCurrent,
-			revoked: a.IsRevoked,
-			path:    fmt.Sprintf("%s", a.Path),
+			Current: a.IsCurrent,
+			Revoked: a.IsRevoked,
+			Path:    fmt.Sprintf("%s", a.Path),
 		})
 	}
 	for _, path := range dead {
 		pathInfo = append(pathInfo, &pathInfoEntry{
-			rejected:     true,
-			rejectReason: deadInfo,
-			path:         fmt.Sprintf("%s", path),
+			Rejected:     true,
+			RejectReason: deadInfo,
+			Path:         fmt.Sprintf("%s", path),
 		})
 	}
 	for _, path := range rejected {
 		pathInfo = append(pathInfo, &pathInfoEntry{
-			rejected:     true,
-			rejectReason: rejectedInfo,
-			path:         fmt.Sprintf("%s", path),
+			Rejected:     true,
+			RejectReason: rejectedInfo,
+			Path:         fmt.Sprintf("%s", path),
 		})
 	}
 
