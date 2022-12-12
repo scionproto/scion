ffca954c2de7eeb7bdbed9332cf8ed308fa728ba
Matthias Frei <matzf@scion.org>
2022-12-12 09:00:11 +0100

    ci: doc pipeline for doc-only PRs

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -2,6 +2,14 @@
 
 set -eou pipefail
 
+if [ -n "${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-}" ]; then
+  if git diff --quiet "${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-}" ':!doc/' ':!**.md'; then
+    # No diff other than doc/ and *.md
+    cat .buildkite/pipeline-doc.yml
+    exit 0
+  fi
+fi
+
 # Following values are useful when debugging the CI.
 # Modify this value to run only a single test in the CI.
 export SINGLE_TEST=
