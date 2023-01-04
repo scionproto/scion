3d3db02e6b9fc09380636ee494730d9b0a36b8ab
Matthias Frei <matzf@scion.org>
2023-01-04 16:29:53 +0100

    lint: cleanup lint output, consolidate lint steps (#4303)
    * Only print verbose lint output if errors occur, in particular for the
      "spectral" OpenAPI linter.
    
      Tweak the `tools/quiet` script (equivalent to the `run_silently`
      function in the `tools/lint.sh` script) in the lint script to retain
      colored output when printing to a TTY.
    
    * Integrate `golangci-lint` into the the normal `make lint` target
    
    * Use `gofmt`, `lll`, `missspell`, `goheader` from golangci-lint to
      replace previously used individual lint tools
      
      Replace running individual lint tools by a run of golangci-lint. This
      simplifies the scripts and gives us a more consistent way to configure
      which files are ignored from the linter etc.
    
      The `//:lint` binary tar is no longer needed. The only remaining
      linter binary (buf) can be invoked more easily by `bazel run`.
    
      Fix a few small errors that are newly reported by this setup.
      Also, reformat golangci-lint configuration for errorcheck exceptions,
      making it more compact and sorted.
      Also, add a `--config=quiet` profile to `.bazelrc` to keep down bazel
      chatter for various `bazel run` invocations; errors are still
      reported. Arguably this should just be a commandline option of bazel.
    
    * Move the lint step execution to the Makefile.
      In contrast to the removed `tools/lint.sh` script, this offers a
      simple way to run only a subset of the lint steps:
    
      - `make lint` runs all lint steps
      - `make lint-<group>` runs a group of lint steps, for the groups `go,
        bazel, protobuf, openapi`.
        E.g. `make lint-go`
      - `make lint-<group>-<step>` runs an individual lint step.
        E.g. `make lint-go-golangci`
    
      Note that in many shells, we get auto-completion of make targets for
      free.

--- acceptance/cmd/sig_ping_acceptance/main.go
+++ acceptance/cmd/sig_ping_acceptance/main.go
@@ -12,10 +12,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// The sig_ping acceptance test checks for basic connectivity between AS through the SIG, using
-// standard ping.
 
+// Sig_ping_acceptance runs checks for basic connectivity between AS through the SIG, using
+// standard IP ping.
 package main
 
 import (
