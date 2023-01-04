b6374231e4f9da8ccbc75e7e90ed8c53d594788e
Matthias Frei <matzf@scion.org>
2023-01-04 12:37:20 +0100

    fixup! lint: use gofmt/lll/misspell/goheader from golangci-lint

--- acceptance/cmd/sig_ping_acceptance/main.go
+++ acceptance/cmd/sig_ping_acceptance/main.go
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// The sig_ping acceptance test checks for basic connectivity between AS through the SIG, using
-// standard ping.
+// Sig_ping_acceptance runs checks for basic connectivity between AS through the SIG, using
+// standard IP ping.
 package main
 
 import (
