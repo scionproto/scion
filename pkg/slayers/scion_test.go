scionproto/scion

commit 9f77b13ed8b36273de21f72d01d67bb0cd301e4e

refactor: replace addr.HostAddr hierarchy with tagged union addr.Host (#4346)
Replace the `addr.HostAddr` type hierarchy, consisting of the `HostAddr` interface and the `HostNone`, `HostIPv4`, `HostIPv6` and `HostSVC` implementations with a single type `addr.Host`, representing all the different host address types as a tagged union ("sum type"). This uses, and follows the spirit of, the `net/netip.Addr` types to represent IP addresses for both IPv4 and IPv6.

The `addr.Host` type is a simple value type, which can be created and copied without heap allocations and can be used in comparisons and as map keys.
In addition to replacing all uses of `addr.HostAddr`, the new `addr.Host` type is used in the `slayers.SCION.Get/SetSrc/DstAddr` interfaces, as well as in the DRKey infrastructure. Using a consistent representation of the addresses allows to get rid of duplicate functionality and a number of address conversions, e.g. in `snet`.
In contrast to the `addr.HostAddr` interface, the new `addr.Host`  does not implement the `net.Addr` interface. In particular, service addresses were passed through layers requiring a `net.Addr`. Use `snet.SVCAddr` in these cases.

Additionally, add a new `addr.Addr` type representing a full SCION address (ISD, AS and host address), including parsing functionality. This definition is identical to the `snet.SCIONAddress` type, which is now only kept as a type alias for compatibility.

Future work:
- `pkg/addr` is a public API and should have a more SCION-specific and less clash-likely name, e.g. `saddr`
- replace `snet.SCIONAddress` by  `addr.Addr`
- adopt using `addr.Addr` in the ping, traceroute etc tools, in place of the ill-fitting use of `snet.UDPAddr`.
- add `addr.MustParse{ISD,AS,IA}` for completeness sake (currently in xtest package)
- get rid of the IP-slice to `netip.Addr` conversions by gradually increasing the adoption of `netip.Addr` and related APIs

pkg/slayers/scion.go
@@ -16,7 +16,7 @@ package slayers
 
 import (
 	"encoding/binary"
-	"net"
+	"net/netip"
 
 	"github.com/google/gopacket"
 
@@ -349,64 +349,66 @@ func scionNextLayerTypeL4(t L4ProtocolType) gopacket.LayerType {
 	}
 }
 
-// DstAddr parses the destination address into a net.Addr. The returned net.Addr references data
-// from the underlaying layer data. Changing the net.Addr object might lead to inconsistent layer
-// information and thus should be treated read-only. Instead, SetDstAddr should be used to update
-// the destination address.
-func (s *SCION) DstAddr() (net.Addr, error) {
-	return parseAddr(s.DstAddrType, s.RawDstAddr)
+// DstAddr parses the destination address into a addr.Host.
+func (s *SCION) DstAddr() (addr.Host, error) {
+	return ParseAddr(s.DstAddrType, s.RawDstAddr)
 }
 
-// SrcAddr parses the source address into a net.Addr. The returned net.Addr references data from the
-// underlaying layer data. Changing the net.Addr object might lead to inconsistent layer information
-// and thus should be treated read-only. Instead, SetDstAddr should be used to update the source
-// address.
-func (s *SCION) SrcAddr() (net.Addr, error) {
-	return parseAddr(s.SrcAddrType, s.RawSrcAddr)
+// SrcAddr parses the source address into a addr.Host.
+func (s *SCION) SrcAddr() (addr.Host, error) {
+	return ParseAddr(s.SrcAddrType, s.RawSrcAddr)
 }
 
 // SetDstAddr sets the destination address and updates the DstAddrType field accordingly.
-// SetDstAddr takes ownership of dst and callers should not write to it after calling SetDstAddr.
-// Changes to dst might leave the layer in an inconsistent state.
-func (s *SCION) SetDstAddr(dst net.Addr) error {
+func (s *SCION) SetDstAddr(dst addr.Host) error {
 	var err error
-	s.DstAddrType, s.RawDstAddr, err = packAddr(dst)
+	s.DstAddrType, s.RawDstAddr, err = PackAddr(dst)
 	return err
 }
 
 // SetSrcAddr sets the source address and updates the DstAddrType field accordingly.
-// SetSrcAddr takes ownership of src and callers should not write to it after calling SetSrcAddr.
-// Changes to src might leave the layer in an inconsistent state.
-func (s *SCION) SetSrcAddr(src net.Addr) error {
+func (s *SCION) SetSrcAddr(src addr.Host) error {
 	var err error
-	s.SrcAddrType, s.RawSrcAddr, err = packAddr(src)
+	s.SrcAddrType, s.RawSrcAddr, err = PackAddr(src)
 	return err
 }
 
-func parseAddr(addrType AddrType, raw []byte) (net.Addr, error) {
+func ParseAddr(addrType AddrType, raw []byte) (addr.Host, error) {
 	switch addrType {
 	case T4Ip:
-		return &net.IPAddr{IP: net.IP(raw)}, nil
+		var raw4 [4]byte
+		copy(raw4[:], raw)
+		return addr.HostIP(netip.AddrFrom4(raw4)), nil
 	case T4Svc:
-		return addr.HostSVC(binary.BigEndian.Uint16(raw[:addr.HostLenSVC])), nil
+		svc := addr.SVC(binary.BigEndian.Uint16(raw[:2]))
+		return addr.HostSVC(svc), nil
 	case T16Ip:
-		return &net.IPAddr{IP: net.IP(raw)}, nil
+		var raw16 [16]byte
+		copy(raw16[:], raw)
+		return addr.HostIP(netip.AddrFrom16(raw16)), nil
 	}
-	return nil, serrors.New("unsupported address type/length combination",
+	return addr.Host{}, serrors.New("unsupported address type/length combination",
 		"type", addrType, "len", addrType.Length())
 }
 
-func packAddr(hostAddr net.Addr) (AddrType, []byte, error) {
-	switch a := hostAddr.(type) {
-	case *net.IPAddr:
-		if ip := a.IP.To4(); ip != nil {
-			return T4Ip, ip, nil
+func PackAddr(host addr.Host) (AddrType, []byte, error) {
+	switch host.Type() {
+	case addr.HostTypeIP:
+		ip := host.IP()
+		if !ip.IsValid() {
+			break
 		}
-		return T16Ip, a.IP, nil
-	case addr.HostSVC:
-		return T4Svc, a.PackWithPad(2), nil
+		t := T4Ip
+		if ip.Is6() {
+			t = T16Ip
+		}
+		return t, ip.AsSlice(), nil
+	case addr.HostTypeSVC:
+		raw := make([]byte, 4)
+		binary.BigEndian.PutUint16(raw, uint16(host.SVC()))
+		return T4Svc, raw, nil
 	}
-	return 0, nil, serrors.New("unsupported address", "addr", hostAddr)
+	return 0, nil, serrors.New("unsupported address", "addr", host)
 }
 
 // AddrHdrLen returns the length of the address header (destination and source ISD-AS-Host triples)

pkg/slayers/scion_test.go
@@ -17,7 +17,6 @@ package slayers_test
 import (
 	"encoding/binary"
 	"fmt"
-	"net"
 	"testing"
 
 	"github.com/google/gopacket"
@@ -34,9 +33,9 @@ import (
 )
 
 var (
-	ip6Addr = &net.IPAddr{IP: net.ParseIP("2001:db8::68")}
-	ip4Addr = &net.IPAddr{IP: net.ParseIP("10.0.0.100").To4()}
-	svcAddr = addr.HostSVCFromString("Wildcard")
+	ip6Addr = addr.MustParseHost("2001:db8::68")
+	ip4Addr = addr.MustParseHost("10.0.0.100")
+	svcAddr = addr.MustParseHost("Wildcard")
 	rawPath = func() []byte {
 		return []byte("\x00\x00\x20\x80\x00\x00\x01\x11\x00\x00\x01\x00\x01\x00\x02\x22\x00" +
 			"\x00\x01\x00\x00\x3f\x00\x01\x00\x00\x01\x02\x03\x04\x05\x06\x00\x3f\x00\x03\x00" +
@@ -57,10 +56,10 @@ func TestSCIONLayerString(t *testing.T) {
 		DstIA:        ia1,
 		SrcIA:        ia2,
 	}
-	if err := sc.SetDstAddr(&net.IPAddr{IP: net.ParseIP("1.2.3.4").To4()}); err != nil {
+	if err := sc.SetDstAddr(addr.MustParseHost("1.2.3.4")); err != nil {
 		assert.NoError(t, err)
 	}
-	if err := sc.SetSrcAddr(&net.IPAddr{IP: net.ParseIP("5.6.7.8").To4()}); err != nil {
+	if err := sc.SetSrcAddr(addr.MustParseHost("5.6.7.8")); err != nil {
 		assert.NoError(t, err)
 	}
 
@@ -256,8 +255,8 @@ func TestSCIONSerializeLengthCheck(t *testing.T) {
 
 func TestSetAndGetAddr(t *testing.T) {
 	testCases := map[string]struct {
-		srcAddr net.Addr
-		dstAddr net.Addr
+		srcAddr addr.Host
+		dstAddr addr.Host
 	}{
 		"set/get IPv4/IPv4": {
 			srcAddr: ip4Addr,
@@ -293,22 +292,15 @@ func TestSetAndGetAddr(t *testing.T) {
 			gotDst, err := s.DstAddr()
 			assert.NoError(t, err)
 
-			equalAddr := func(t *testing.T, expected, actual net.Addr) {
-				if _, ok := expected.(*net.IPAddr); !ok {
-					assert.Equal(t, expected, actual)
-					return
-				}
-				assert.True(t, expected.(*net.IPAddr).IP.Equal(actual.(*net.IPAddr).IP))
-			}
-			equalAddr(t, tc.srcAddr, gotSrc)
-			equalAddr(t, tc.dstAddr, gotDst)
+			assert.Equal(t, tc.srcAddr, gotSrc)
+			assert.Equal(t, tc.dstAddr, gotDst)
 		})
 	}
 }
 
 func TestPackAddr(t *testing.T) {
 	testCases := map[string]struct {
-		addr      net.Addr
+		addr      addr.Host
 		addrType  slayers.AddrType
 		rawAddr   []byte
 		errorFunc assert.ErrorAssertionFunc
@@ -316,19 +308,19 @@ func TestPackAddr(t *testing.T) {
 		"pack IPv4": {
 			addr:      ip4Addr,
 			addrType:  slayers.T4Ip,
-			rawAddr:   []byte(ip4Addr.IP),
+			rawAddr:   ip4Addr.IP().AsSlice(),
 			errorFunc: assert.NoError,
 		},
 		"pack IPv6": {
 			addr:      ip6Addr,
 			addrType:  slayers.T16Ip,
-			rawAddr:   []byte(ip6Addr.IP),
+			rawAddr:   ip6Addr.IP().AsSlice(),
 			errorFunc: assert.NoError,
 		},
 		"pack SVC": {
-			addr:      svcAddr,
+			addr:      addr.HostSVC(addr.SvcWildcard),
 			addrType:  slayers.T4Svc,
-			rawAddr:   svcAddr.PackWithPad(2),
+			rawAddr:   []byte{0, 0x10, 0, 0},
 			errorFunc: assert.NoError,
 		},
 	}
@@ -350,31 +342,31 @@ func TestParseAddr(t *testing.T) {
 	testCases := map[string]struct {
 		addrType  slayers.AddrType
 		rawAddr   []byte
-		want      net.Addr
+		want      addr.Host
 		errorFunc assert.ErrorAssertionFunc
 	}{
 		"parse IPv4": {
 			addrType:  slayers.T4Ip,
-			rawAddr:   []byte(ip4Addr.IP.To4()),
+			rawAddr:   ip4Addr.IP().AsSlice(),
 			want:      ip4Addr,
 			errorFunc: assert.NoError,
 		},
 		"parse IPv6": {
 			addrType:  slayers.T16Ip,
-			rawAddr:   []byte(ip6Addr.IP),
+			rawAddr:   ip6Addr.IP().AsSlice(),
 			want:      ip6Addr,
 			errorFunc: assert.NoError,
 		},
 		"parse SVC": {
 			addrType:  slayers.T4Svc,
-			rawAddr:   svcAddr.PackWithPad(2),
-			want:      svcAddr,
+			rawAddr:   []byte{0, 0x10, 0, 0},
+			want:      addr.HostSVC(addr.SvcWildcard),
 			errorFunc: assert.NoError,
 		},
 		"parse unknown type": {
 			addrType:  0b0001, // T=0,Len=8
 			rawAddr:   []byte{0, 0, 0, 0, 0, 0, 0, 0},
-			want:      nil,
+			want:      addr.Host{},
 			errorFunc: assert.Error,
 		},
 	}
@@ -547,9 +539,9 @@ func TestSCIONComputeChecksum(t *testing.T) {
 					SrcIA: xtest.MustParseIA("1-ff00:0:110"),
 					DstIA: xtest.MustParseIA("1-ff00:0:112"),
 				}
-				err := s.SetSrcAddr(&net.IPAddr{IP: net.ParseIP("174.16.4.1").To4()})
+				err := s.SetSrcAddr(addr.MustParseHost("174.16.4.1"))
 				require.NoError(t, err)
-				err = s.SetDstAddr(&net.IPAddr{IP: net.ParseIP("172.16.4.2").To4()})
+				err = s.SetDstAddr(addr.MustParseHost("172.16.4.2"))
 				require.NoError(t, err)
 				return s
 			},
@@ -563,9 +555,9 @@ func TestSCIONComputeChecksum(t *testing.T) {
 					SrcIA: xtest.MustParseIA("1-ff00:0:110"),
 					DstIA: xtest.MustParseIA("1-ff00:0:112"),
 				}
-				err := s.SetSrcAddr(&net.IPAddr{IP: net.ParseIP("174.16.4.1").To4()})
+				err := s.SetSrcAddr(addr.MustParseHost("174.16.4.1"))
 				require.NoError(t, err)
-				err = s.SetDstAddr(&net.IPAddr{IP: net.ParseIP("172.16.4.2").To4()})
+				err = s.SetDstAddr(addr.MustParseHost("172.16.4.2"))
 				require.NoError(t, err)
 				return s
 			},
@@ -579,9 +571,9 @@ func TestSCIONComputeChecksum(t *testing.T) {
 					SrcIA: xtest.MustParseIA("1-ff00:0:110"),
 					DstIA: xtest.MustParseIA("1-ff00:0:112"),
 				}
-				err := s.SetSrcAddr(&net.IPAddr{IP: net.ParseIP("174.16.4.1").To4()})
+				err := s.SetSrcAddr(addr.MustParseHost("174.16.4.1"))
 				require.NoError(t, err)
-				err = s.SetDstAddr(&net.IPAddr{IP: net.ParseIP("dead::beef")})
+				err = s.SetDstAddr(addr.MustParseHost("dead::beef"))
 				require.NoError(t, err)
 				return s
 			},
@@ -595,9 +587,9 @@ func TestSCIONComputeChecksum(t *testing.T) {
 					SrcIA: xtest.MustParseIA("1-ff00:0:110"),
 					DstIA: xtest.MustParseIA("1-ff00:0:112"),
 				}
-				err := s.SetSrcAddr(&net.IPAddr{IP: net.ParseIP("174.16.4.1").To4()})
+				err := s.SetSrcAddr(addr.MustParseHost("174.16.4.1"))
 				require.NoError(t, err)
-				err = s.SetDstAddr(addr.SvcCS)
+				err = s.SetDstAddr(addr.HostSVC(addr.SvcCS))
 				require.NoError(t, err)
 				return s
 			},

commit 4069fad297f6bda0e4ac5be7b647b93002e92d9b

slayers: unmap IPv4-mapped IPv6 addresses
The Go standard library can produce IPv4-mapped IPv6 addresses when
resolving IP addresses. These IP addresses need to be unmapped before
putting them on the wire.

Before this patch, we could observe the following with tshark:

    Len=1304 SCION 1-ff00:0:110,[::ffff:172.20.2.2] -> 1-ff00:0:111,[::ffff:172.20.3.2] UDP 32769 -> 32768 1208

The regression was introduced in #4346, which removed the unmapping behavior
in slayers.PackAddr. This patch restores the behavior to ensure only
unmapped IPv4 addresses make it on the wire.

pkg/slayers/scion.go
@@ -398,6 +398,10 @@ func PackAddr(host addr.Host) (AddrType, []byte, error) {
 		if !ip.IsValid() {
 			break
 		}
+		// If the IP is IPv4-in-IPv6, we need to unmap it.
+		if ip.Is4In6() {
+			return T4Ip, ip.Unmap().AsSlice(), nil
+		}
 		t := T4Ip
 		if ip.Is6() {
 			t = T16Ip

pkg/slayers/scion_test.go
@@ -33,10 +33,11 @@ import (
 )
 
 var (
-	ip6Addr = addr.MustParseHost("2001:db8::68")
-	ip4Addr = addr.MustParseHost("10.0.0.100")
-	svcAddr = addr.MustParseHost("Wildcard")
-	rawPath = func() []byte {
+	ip6Addr    = addr.MustParseHost("2001:db8::68")
+	ip4Addr    = addr.MustParseHost("10.0.0.100")
+	ip4in6Addr = addr.MustParseHost("::ffff:10.0.0.100")
+	svcAddr    = addr.MustParseHost("Wildcard")
+	rawPath    = func() []byte {
 		return []byte("\x00\x00\x20\x80\x00\x00\x01\x11\x00\x00\x01\x00\x01\x00\x02\x22\x00" +
 			"\x00\x01\x00\x00\x3f\x00\x01\x00\x00\x01\x02\x03\x04\x05\x06\x00\x3f\x00\x03\x00" +
 			"\x02\x01\x02\x03\x04\x05\x06\x00\x3f\x00\x00\x00\x02\x01\x02\x03\x04\x05\x06\x00" +
@@ -309,6 +310,12 @@ func TestPackAddr(t *testing.T) {
 			addr:      ip4Addr,
 			addrType:  slayers.T4Ip,
 			rawAddr:   ip4Addr.IP().AsSlice(),
+			errorFunc: assert.NoError,
+		},
+		"pack IPv4-mapped IPv6": {
+			addr:      ip4in6Addr,
+			addrType:  slayers.T4Ip,
+			rawAddr:   []byte{0xa, 0x0, 0x0, 0x64},
 			errorFunc: assert.NoError,
 		},
 		"pack IPv6": {