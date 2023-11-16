602bc1f77c9f5222706da40bda0c2d2789d89d4e
Jean-Christophe Hugly <jice@scion.org>
2023-11-16 14:01:45 +0100

    router: update copyright notices.

--- tools/integration/integrationlib/common.go
+++ tools/integration/integrationlib/common.go
@@ -1,5 +1,6 @@
 // Copyright 2018 ETH Zurich
 // Copyright 2019 ETH Zurich, Anapaya Systems
+// Copyright 2023 SCION Association
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
