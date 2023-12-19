1f5d07ae4cc5f896d63c7ff61388c436b31e07a4
Jordi Subirà Nieto <jordi.subira.nieto@gmail.com>
2023-12-19 11:55:56 +0100

    remove dispatcher shim support for Windows

--- dispatcher/cmd/dispatcher/main.go
+++ dispatcher/cmd/dispatcher/main.go
@@ -13,6 +13,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build linux || darwin
+// +build linux darwin
+
 package main
 
 import (
