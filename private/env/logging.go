1e322d884a61755232e0c8566a54d2c03254072f
Jordi Subira Nieto <jordi.subira.nieto@gmail.com>
2024-04-03 16:28:16 +0200

    checking up if we run in Linux before calling RunInDocker

--- private/env/logging.go
+++ private/env/logging.go
@@ -17,6 +17,7 @@ package env
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/scionproto/scion/pkg/log"
 	"github.com/scionproto/scion/pkg/private/serrors"
@@ -31,10 +32,18 @@ var (
 // LogAppStarted should be called by applications as soon as logging is
 // initialized.
 func LogAppStarted(svcType, elemID string) error {
-	inDocker, err := RunsInDocker()
-	if err != nil {
-		return serrors.WrapStr("Unable to determine if running in docker", err)
+	// XXX(JordiSubira): Right now RunsInDocker() only is Linux-compatible.
+	// If we are going to run apps in docker also in macOS (and potentially in Windows)
+	// we should make the method compatible.
+	inDocker := false
+	if runtime.GOOS == "linux" {
+		var err error
+		inDocker, err = RunsInDocker()
+		if err != nil {
+			return serrors.WrapStr("Unable to determine if running in docker", err)
+		}
 	}
+
 	info := fmt.Sprintf("=====================> Service started %s %s\n"+
 		"%s  %s\n  %s\n  %s\n  %s\n",
 		svcType,
