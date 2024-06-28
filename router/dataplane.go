a0393b7d0674cd318fd355562745d5f72251b03a
João Pereira <joaopereira.19@gmail.com>
2024-06-28 13:55:45 +0200

    strengthen the checks performed in parsePath

--- router/dataplane.go
+++ router/dataplane.go
@@ -1294,6 +1294,14 @@ func (p *scionPacketProcessor) parsePath() (processResult, error) {
 		// TODO(lukedirtwalker) parameter problem invalid path?
 		return processResult{}, err
 	}
+	// All segments without the Peering flag need to consist of at least two HFs.
+	// (https://github.com/scionproto/scion/issues/4524)
+	if !p.infoField.Peer && p.path.PathMeta.SegLen[p.path.PathMeta.CurrINF] < 2 {
+		return processResult{}, malformedPath
+	}
+	if !p.path.CurrINFMatchesCurrHF() {
+		return processResult{}, malformedPath
+	}
 	return processResult{}, nil
 }
 
