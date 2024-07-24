ac2ca5ee1f98a6d4ceddbc6295ad2ae690ccd96f
Jean-Christophe Hugly <jice@scion.org>
2024-07-24 10:49:30 +0200

    Add more header fields to the heuristic SCION checker.

--- tools/wireshark/scion.lua
+++ tools/wireshark/scion.lua
@@ -136,10 +136,14 @@ scion_proto.experts = {
 -- However, with the removal of the dispatcher, we have no narrow set of UDP ports to identify
 -- SCION traffic from. So, there's no choice.
 local function scion_proto_filter(tvbuf, pktinfo, root)
-    local version = bit.rshift(tvbuf(0,1):uint(), 4)
-    local path_type = tvbuf(8, 1):uint()
-    local rsv = tvbuf(10, 2):uint()
-    if version == 0 and path_type < 5 and rsv == 0 then
+    local version_valid = (bit.rshift(tvbuf(0,1):uint(), 4) == 0)
+    local next_hdr_valid = (hdrTypes[tvbuf(4, 1):uint()] != nil)
+    local path_type_valid = (tvbuf(8, 1):uint() < 5)
+    local addr_type_dst_valid = (addrTypes[bit.rshift(tvbuf(9, 1):uint(), 4)] != nil)
+    local addr_type_src_valid = (addrTypes[bit.band(tvbuf(9, 1):uint(), 0xf)] != nil)
+    local rsv_valid = (tvbuf(10, 2):uint() == 0)
+    
+    if version_valid and next_hdr_valid and path_type_valid and addr_type_dst_valid and addr_type_src_valid and rsv_valid then
        pktinfo.conversation = scion_proto
        return true
     end
