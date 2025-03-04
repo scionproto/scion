8a4ef2709a1e929ead31c35c1718c9b7191d7aef
Katya Titkova <katyatitkova@gmail.com>
2025-03-04 17:27:53 +0100

    cleanup

--- .buildkite/provision-agent.sh
+++ .buildkite/provision-agent.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 # The installation scripts are no-ops when nothing has changed.
 # To circumvent running any checks (and cluttering the output), we only
 # rerun the scripts if any of them have changed since the last run.
-#if sha1sum --check /tmp/buildkite-scionproto-runner-provision.sum --status; then
-#  exit 0
-#fi
+if sha1sum --check /tmp/buildkite-scionproto-runner-provision.sum --status; then
+  exit 0
+fi
 
 echo "~~~ Install build tools"
 tools/install_bazel
