c45516e898dc512ffff7a0070020f437a2a24a26
Katya Titkova <katyatitkova@gmail.com>
2025-03-12 09:36:12 +0100

    Revert "try out installing golang in Buildkite via script"
    This reverts commit 4629439e34c4a92b12b79d63ae20668178a7ab21.

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo dnf -y install golang
-
 set -eou pipefail
 
 if [ -n "${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-}" ]; then
@@ -22,3 +20,5 @@ export PARALLELISM=1
 
 cat .buildkite/pipeline.yml
 gen_bazel_test_steps
+
+sudo dnf -y install golang
