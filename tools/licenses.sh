2b800faa5015e5708f9494a4bfc77800728d8a83
Katya Titkova <katyatitkova@gmail.com>
2025-03-12 16:19:45 +0100

    rm python folder from licenses folder

--- tools/licenses.sh
+++ tools/licenses.sh
@@ -39,7 +39,7 @@ rm -rf $DSTDIR/org_uber_go_zap/checklicense.sh
 rm -rf $DSTDIR/org_golang_x_tools/gopls/
 rm -rf $DSTDIR/org_golang_x_tools/internal/lsp/cmd/usage/licenses.hlp
 rm -rf $DSTDIR/com_github_google_certificate_transparency_go/scripts
-rm -rf $DSTDIR/python3_10_x86_64-unknown-linux-gnu/
+rm -rf $DSTDIR/python_3_11_x86_64-unknown-linux-gnu/
 rm -rf $DSTDIR/aspect_bazel_lib/
 rm -rf $DSTDIR/aspect_rules_js/
 rm -rf $DSTDIR/npm__*/
