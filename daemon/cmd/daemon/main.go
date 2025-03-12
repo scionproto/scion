31de900ef06462e7e2d5980e8369adf2e3fbced0
Roman Sharkov <roman.scharkov@gmail.com>
2025-03-12 17:52:45 +0100

    fix: Include actual value in PathSegType.String()
    refactor: Simplify code, use Stringer method

--- daemon/cmd/daemon/main.go
+++ daemon/cmd/daemon/main.go
@@ -128,8 +128,8 @@ func realMain(ctx context.Context) error {
 		SvcResolver: func(dst addr.SVC) []resolver.Address {
 			if base := dst.Base(); base != addr.SvcCS {
 				panic(fmt.Errorf(
-					"unsupported address type, possible implementation error: %d (%s)",
-					base, base.String(),
+					"unsupported address type, possible implementation error: %s",
+					base.String(),
 				))
 			}
 			targets := []resolver.Address{}
