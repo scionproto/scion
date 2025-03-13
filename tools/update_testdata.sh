e97f6ea43980e88568598b9076e41327ae56482a
Katya Titkova <katyatitkova@gmail.com>
2025-03-13 13:16:27 +0100

    run go with bazel in tools/update_testdata.sh

--- tools/update_testdata.sh
+++ tools/update_testdata.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-export PATH=$(bazel info output_base)/external/go_sdk/bin:$PATH
-
 folders=$(grep \
             -lR \
             --include=\*_test.go \
@@ -12,5 +10,5 @@ folders=$(grep \
 
 GO_BUILD_TAGS_ARG=$(bazel info --ui_event_filters=-stdout,-stderr --announce_rc --noshow_progress 2>&1 | grep "'build' options" | sed -n "s/^.*--define gotags=\(\S*\).*/-tags \1/p")
 
-echo $folders -update | xargs go test ${GO_BUILD_TAGS_ARG}
-echo $folders -count=1 | xargs go test ${GO_BUILD_TAGS_ARG}
+echo $folders -update | xargs bazel run @io_bazel_rules_go//go -- test ${GO_BUILD_TAGS_ARG}
+echo $folders -count=1 | xargs bazel run @io_bazel_rules_go//go -- test ${GO_BUILD_TAGS_ARG}
