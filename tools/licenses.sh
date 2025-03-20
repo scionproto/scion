bf5ce1c7462c89bab840798544934e750cf1c94f
Katya Titkova <katyatitkova@gmail.com>
2025-03-20 11:38:27 +0100

    update python to 3.12

--- tools/licenses.sh
+++ tools/licenses.sh
@@ -45,7 +45,7 @@ rm -rf $DSTDIR/org_uber_go_zap/checklicense.sh
 rm -rf $DSTDIR/org_golang_x_tools/gopls/
 rm -rf $DSTDIR/org_golang_x_tools/internal/lsp/cmd/usage/licenses.hlp
 rm -rf $DSTDIR/com_github_google_certificate_transparency_go/scripts
-rm -rf $DSTDIR/python_3_11_x86_64-unknown-linux-gnu/
+rm -rf $DSTDIR/python_3_12_x86_64-unknown-linux-gnu/
 rm -rf $DSTDIR/npm__*/
 find $DSTDIR/ -name "*.go" -type f -delete
 find $DSTDIR/ -name "*.sh" -type f -delete
