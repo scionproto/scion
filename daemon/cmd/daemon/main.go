3585c5fec51a13a1f0096faccc3ec801f63ba5b1
Roman Sharkov <roman.scharkov@gmail.com>
2025-04-14 12:11:30 +0200

    fix: Avoid exceeding line limit

--- daemon/cmd/daemon/main.go
+++ daemon/cmd/daemon/main.go
@@ -153,7 +153,9 @@ func realMain(ctx context.Context) error {
 			[]string{"driver", "operation", prom.LabelResult},
 		),
 	})
-	engine, err := daemon.TrustEngine(errCtx, globalCfg.General.ConfigDir, topo.IA(), trustDB, dialer)
+	engine, err := daemon.TrustEngine(
+		errCtx, globalCfg.General.ConfigDir, topo.IA(), trustDB, dialer,
+	)
 	if err != nil {
 		return serrors.Wrap("creating trust engine", err)
 	}
