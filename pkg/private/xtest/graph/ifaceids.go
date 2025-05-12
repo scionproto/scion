f4dc2b6312a1548ffa178509d18bbf1208a1bf13
Katya Titkova <katyatitkova@gmail.com>
2025-05-12 14:13:11 +0200

    fix copyright

--- pkg/private/xtest/graph/ifaceids.go
+++ pkg/private/xtest/graph/ifaceids.go
@@ -1,3 +1,4 @@
+// Copyright 2018 Anapaya Systems
 // Copyright 2025 SCION Association
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
--- pkg/private/xtest/graphupdater/main.go
+++ pkg/private/xtest/graphupdater/main.go
@@ -1,4 +1,5 @@
 // Copyright 2018 Anapaya Systems
+// Copyright 2025 SCION Association
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,8 +20,6 @@ import (
 	"fmt"
 	"os"
 )
-
-// TODO check that generated files are up-to-date in CI
 
 func main() {
 	topoFile := flag.String("topoFile", "", "")
