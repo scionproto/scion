46da136320ac9b5970e1dd8770ac077dfe65b3cd
Lukas Vogel <vogel@anapaya.net>
2025-12-09 14:36:13 +0100

    beaconing: fix diversity calculation
    Currently the beacon diversity calculation was wrong. We only considered
    ingress interface in construction direction. However the first ASEntry
    will always have a zero ingress interface. Furthermore with this
    approach we did not really consider the last ingress interface, which
    was the ingress interface into the AS, which is not yet represented by
    an ASEntry (as the termination/extension only happens after selection).
    
    By using the egress interfaces interfaces instead we solve both
    problems.

--- control/beacon/beacon_test.go
+++ control/beacon/beacon_test.go
@@ -20,14 +20,19 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/scionproto/scion/control/beacon"
+	"github.com/scionproto/scion/pkg/addr"
 	"github.com/scionproto/scion/pkg/private/xtest/graph"
+	"github.com/scionproto/scion/pkg/segment"
 )
 
 // TestBeaconDiversity tests that diversity is calculated correctly.
 func TestBeaconDiversity(t *testing.T) {
 	var tests = []struct {
-		name      string
-		beacon    []uint16
+		name   string
+		beacon []uint16
+		// mBeacon allows to manually specify the other beacon.
+		mBeacon   beacon.Beacon
 		diversity int
 	}{
 		{
@@ -51,6 +56,36 @@ func TestBeaconDiversity(t *testing.T) {
 			},
 			diversity: 2,
 		},
+		{
+			name: "Last link distinct",
+			mBeacon: beacon.Beacon{
+				Segment: &segment.PathSegment{
+					ASEntries: []segment.ASEntry{
+						{
+							Local: addr.MustParseIA("1-ff00:0:130"),
+							Next:  addr.MustParseIA("1-ff00:0:110"),
+							HopEntry: segment.HopEntry{
+								HopField: segment.HopField{
+									ConsEgress: graph.If_130_A_110_X,
+								},
+							},
+						},
+						{
+							Local: addr.MustParseIA("1-ff00:0:110"),
+							Next:  addr.MustParseIA("2-ff00:0:210"),
+							HopEntry: segment.HopEntry{
+								HopField: segment.HopField{
+									ConsIngress: graph.If_110_X_130_A,
+									ConsEgress:  2321,
+								},
+							},
+						},
+					},
+				},
+				InIfID: 2123,
+			},
+			diversity: 1,
+		},
 	}
 	mctrl := gomock.NewController(t)
 
@@ -58,7 +93,10 @@ func TestBeaconDiversity(t *testing.T) {
 	bseg := testBeacon(g, tests[0].beacon...)
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			other := testBeacon(g, test.beacon...)
+			other := test.mBeacon
+			if len(test.beacon) > 0 {
+				other = testBeacon(g, test.beacon...)
+			}
 			diversity := bseg.Diversity(other)
 			assert.Equal(t, test.diversity, diversity)
 		})
