3b15e91fd2e3848001845e6b6a6a0c97a38a83d8
Emanuel Mairoll <accounts@emanuelmairoll.at>
2025-12-21 00:43:34 +0100

    cli: address

--- scion/cmd/scion/address.go
+++ scion/cmd/scion/address.go
@@ -21,6 +21,9 @@ import (
 	"net"
 	"time"
 
+	daemon2 "github.com/scionproto/scion/pkg/daemon/standalone/daemon"
+	"github.com/scionproto/scion/pkg/log"
+	"github.com/scionproto/scion/private/tracing"
 	"github.com/spf13/cobra"
 
 	"github.com/scionproto/scion/pkg/addr"
@@ -58,16 +61,45 @@ case, the host could have multiple SCION addresses.
 			if err := envFlags.LoadExternalVars(); err != nil {
 				return err
 			}
-			daemonAddr := envFlags.Daemon()
-
 			cmd.SilenceUsage = true
+
+			span, traceCtx := tracing.CtxWith(context.Background(), "run")
+			defer span.Finish()
+
+			// Check if we should use a local daemon or connect to a remote one
+			var sd daemon.Connector
+			topoFile := envFlags.Topology()
+			if topoFile != "" {
+				// Use local daemon with topology file
+				log.Debug("Using local daemon with topology file", "topology", topoFile)
+				standalone, err := daemon2.NewStandaloneService(traceCtx, daemon2.StandaloneOptions{TopoFile: topoFile})
+				if err != nil {
+					return serrors.Wrap("creating local daemon", err)
+				}
+				sd = standalone
+			} else {
+				// Connect to remote daemon
+				daemonAddr := envFlags.Daemon()
+				log.Debug("Connecting to SCION daemon", "daemon", daemonAddr)
+
+				ctx, cancelF := context.WithTimeout(traceCtx, time.Second)
+				defer cancelF()
+				remoteSd, err := daemon.NewService(daemonAddr).Connect(ctx)
+				if err != nil {
+					return serrors.Wrap("connecting to SCION Daemon", err)
+				}
+				sd = remoteSd
+			}
+
+			defer func(sd daemon.Connector) {
+				err := sd.Close()
+				if err != nil {
+					log.Error("Closing SCION Daemon connection", "err", err)
+				}
+			}(sd)
+
 			ctx, cancelF := context.WithTimeout(cmd.Context(), time.Second)
 			defer cancelF()
-			sd, err := daemon.NewService(daemonAddr).Connect(ctx)
-			if err != nil {
-				return serrors.Wrap("connecting to SCION Daemon", err)
-			}
-			defer sd.Close()
 
 			info, err := app.QueryASInfo(ctx, sd)
 			if err != nil {
