c28e4f8f4c95004ca3a230699eeb53b6643bbf88
Jan Luan <jan.luan@outlook.de>
2025-12-31 16:53:26 +0000

    remove nat-tester folder

--- nat-tester/generateTopology.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$(basename "$PWD")" != "scion" ]; then
-  echo "Error: this script must be run from the scion directory." >&2
-  exit 1
-fi
-
-sh -c "./scion.sh topology -d -c topology/tiny.topo" &&
-python3 nat-tester/modify_topology.py
--- nat-tester/test-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/share/nat-tester/test-server -local 1-ff00:0:110,172.20.0.22:31000
