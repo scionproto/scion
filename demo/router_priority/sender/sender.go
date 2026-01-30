10c8d38cb32083425aadde3a45252901db51b4d9
Juan A. Garcia Pardo <juagargi@gmail.com>
2026-01-30 14:55:45 +0100

    WIP Adding UDP blaster to the demo test.

---
+++ demo/router_priority/sender/sender.go
@@ -0,0 +1,114 @@
+// Copyright 2026 ETH Zurich
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/scionproto/scion/pkg/daemon"
+	"github.com/scionproto/scion/pkg/snet"
+	"github.com/scionproto/scion/pkg/snet/metrics"
+)
+
+// -daemon 127.0.0.19:30255 -remote 1-ff00:0:110,172.20.0.18:12345
+func main() {
+	ctx, cancelF := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelF()
+
+	var daemonAddr, localAddr, blastDuration string
+	var remote snet.UDPAddr
+	var payloadSize int
+
+	flag.StringVar(&daemonAddr, "daemon", "", "local daemon address")
+	flag.StringVar(&localAddr, "local", "127.0.0.1:0", "local address")
+	flag.Var(&remote, "remote", "address to send to")
+	flag.StringVar(&blastDuration, "duration", "1s", "duration of the SCION UDP blast")
+	flag.IntVar(&payloadSize, "payloadsize", 1100, "size of the payload in bytes")
+	flag.Parse()
+
+	// Duration:
+	duration, err := time.ParseDuration(blastDuration)
+	panicOnError(err)
+
+	// Find daemon.
+	daemonConn, err := daemon.NewService(daemonAddr).Connect(ctx)
+	panicOnError(err)
+
+	fmt.Printf("remote: %s\n", &remote)
+
+	// Where am I?
+	localIA, err := daemonConn.LocalIA(ctx)
+	panicOnError(err)
+	// local := net.UDPAddrFromAddrPort(
+	// 	netip.AddrPortFrom(netip.AddrFrom4([4]byte{127, 0, 0, 1}),
+	// 		0))
+	local, err := net.ResolveUDPAddr("udp", localAddr)
+	panicOnError(err)
+	fmt.Printf("On local IA: %s, host: %s\n", localIA, local)
+
+	// Network:
+	metrics := metrics.NewSCIONPacketConnMetrics()
+	topo, err := daemon.LoadTopology(ctx, daemonConn)
+	panicOnError(err)
+	network := &snet.SCIONNetwork{
+		SCMPHandler: snet.DefaultSCMPHandler{
+			RevocationHandler: daemon.RevHandler{Connector: daemonConn},
+			SCMPErrors:        metrics.SCMPErrors,
+		},
+		PacketConnMetrics: metrics,
+		Topology:          topo,
+	}
+
+	// Get paths.
+	paths, err := daemonConn.Paths(ctx, remote.IA, localIA, daemon.PathReqFlags{})
+	panicOnError(err)
+	if len(paths) == 0 {
+		panic("no paths")
+	}
+	path := paths[0]
+	remote.Path = path.Dataplane()
+	remote.NextHop = path.UnderlayNextHop()
+
+	// Blast the remote endpoint with packets.
+	conn, err := network.Dial(ctx, "udp", local, &remote)
+	panicOnError(err)
+	payload := make([]byte, payloadSize)
+	var packetCount int
+	t0 := time.Now()
+	tLastLog := time.Now()
+	for packetCount = 0; ; packetCount++ {
+		n, err := conn.Write(payload)
+		panicOnError(err)
+		if n != len(payload) {
+			panic(fmt.Errorf("only sent %d out of %d", n, len(payload)))
+		}
+		if time.Since(tLastLog) >= time.Second {
+			fmt.Printf("sent %d packets (%d bytes payload) to %s\n",
+				packetCount, len(payload), remote.String())
+			tLastLog = time.Now()
+		}
+		if time.Since(t0) >= duration {
+			break
+		}
+	}
+	fmt.Printf("sent %d packets in total.\n", packetCount)
+}
+
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- demo/router_priority/test.py
+++ demo/router_priority/test.py
@@ -25,7 +25,7 @@ import subprocess
 import sys
 import time
 import yaml
-# from plumbum import local
+from plumbum import local
 
 
 # deleteme remove once we run this as a test.
@@ -203,95 +203,6 @@ def measure_br(url: str):
     return metrics
 
 
-
-
-
-def aggregate_metrics(full_text:str, metric_label:str) -> Tuple[int, Dict[str, int]]:
-    """returns the total and per interface aggregated (sum) metrics"""
-    total = 0
-    by_interface = defaultdict(int)
-
-    for family in text_string_to_metric_families(full_text):
-        if family.name != metric_label:
-            continue
-
-        for sample in family.samples:
-            labels = sample.labels
-            value = sample.value
-
-            total += value
-            by_interface[labels["interface"]] += value
-
-    return total, dict(by_interface)
-
-
-def measure_metrics_rate(url: str, sample_seconds:float, labels: List[str]) -> List[
-        Tuple[float, Dict[str, float]]]:
-    # Get initial values:
-    text = requests.get(url).text
-    t0 = time.monotonic()
-    totals = [] # one per label
-    by_ifs = [] # one per label
-    for label in labels:
-        last_total, last_by_if = aggregate_metrics(text, label)
-        totals.append(last_total)
-        by_ifs.append(last_by_if)
-
-    # Sleep.
-    time.sleep(sample_seconds)
-
-    # Get new values:
-    text = requests.get(url).text
-    duration = time.monotonic() - t0
-    for i in range(len(labels)):
-        label = labels[i]
-        total, by_if = aggregate_metrics(text, label)
-        # Relative to last measurement:
-        rel_total = total - totals[i]
-        rel_by_if = {k:by_if[k] - v for k,v in by_ifs[i].items()}
-
-        # Compute average.
-        avg_total = rel_total / duration
-        avg_by_if = {k:v / duration for k, v in rel_by_if.items()}
-
-        # Update lists:
-        totals[i] = avg_total
-        by_ifs[i] = avg_by_if
-    return totals, by_ifs
-
-
-# def measure_metrics():
-#     text = requests.get("http://172.20.0.26:30442/metrics").text
-#     labels = [
-#         "router_bfd_sent_packets",
-#         "router_bfd_state_changes",
-#         "router_dropped_pkts",
-#         "router_output_pkts",
-#         "router_output_bytes",
-#     ]
-#     for label in labels:
-#         total, by_if = aggregate_metrics(text, label)
-#         print(f"{label} TOTAL :", total)
-#         print(f"{label} BY IF:", by_if)
-
-#     totals, by_ifs = measure_metrics_rate(
-#         "http://172.20.0.26:30442/metrics",
-#         2.0,
-#         labels,
-#     )
-#     print("------- RATES ---------")
-#     for i in range(len(labels)):
-#         print(f"{labels[i]} total = {totals[i]}, by_if = {by_ifs[i]}")
-
-
-
-
-
-
-
-
-
-
 def run_scion_ping(src_container:str, dst_endpoint:str, count:int, size:int, interval:str) -> float:
     """Returns the loss rate 0..100"""
     cmd = ["scion","ping","--format", "yaml",
@@ -299,16 +210,7 @@ def run_scion_ping(src_container:str, dst_endpoint:str, count:int, size:int, int
     lines = selfdc("exec", src_container, *cmd)
     ping = yaml.safe_load(lines)
     stats = ping["statistics"]
-    print(f"sent: {stats["sent"]}")
-    print(f"recv: {stats["received"]}")
-    print(f"loss: {stats["packet_loss"]}")
     return float(stats["packet_loss"])
-
-
-def run_heavy_scion_ping(src_container: str, dst_endpoint:str) -> float:
-    # docker compose -f gen/scion-dc.yml exec tester_1-ff00_0_111 scion ping --format yaml -c 3000 -s 2000 --interval 1ms 1-ff00:0:112,fd00:f00d:cafe::7f00:15
-    loss = run_scion_ping(src_container,dst_endpoint,count=3000,size=2000, interval="1ms")
-    return loss
 
 
 def increase_load(src_service:str, dst_endpoint: str, duration: str) -> None:
@@ -335,25 +237,13 @@ class Test(base.TestTopogen):
     def setup_prepare(self):
         super().setup_prepare()
 
-        # We need to set up bandwidth limits to the interface of BR-1 @ 1-ff00:0:111,
-        # to ensure that we will see packet drops when running a high bandwidth test with
-        # scion ping. We should see no losses on priority traffic, e.g. BFD packets.
-
-        # From the host, we obtain all the interfaces of the BR-1 @ 111 container:
-
-        # sudo nsenter -t $(docker inspect -f '{{.State.Pid}}' scion-br1-ff00_0_111-1-1 ) -n ip link
-        # ip link | grep veth
-
-
-        # with (open(self.artifacts / "gen/scion-dc.yml", "r") as file):
-        #     scion_dc = yaml.safe_load(file)
-
-        # with open(self.artifacts / "gen/scion-dc.yml", "w") as file:
-        #     yaml.dump(scion_dc, file)
-
     def _run(self):
-        print("deleteme running priority test")
-        print(f"interfaces: {get_interface_indices("scion-br1-ff00_0_111-1-1")}")
+        print("deleteme running router_priority test")
+        self.await_connectivity()
+
+        sender_bin = local["realpath"](self.get_executable("sender").executable).strip()
+        self.dc("cp", sender_bin, "tester_1-ff00_0_111" + ":/bin/")
+        print(f"deleteme finished router_priority test")
 
 
 def deleteme():
@@ -367,51 +257,7 @@ def deleteme():
     print(f"bridge is: {bridge}")
     # sudo tc qdisc add dev veth31e7685@if3 root tbf rate 1mbit burst 32kbit latency 400ms
     # Limit the BR-111 -> BR-110 interface to 1Mbps.
-    set_tc_limits(bridge, rate="1mbit", burst="32kbit", latency="400ms")
-
-    # # curl  http://172.20.0.26:30442/metrics | grep bfd_sent
-    # measure_metrics()
-    # # # Test: measure rates continuously:
-    # # labels = [
-    # #     "router_bfd_sent_packets",
-    # #     "router_dropped_pkts",
-    # #     "router_output_pkts",
-    # #     "router_output_bytes",
-    # # ]
-    # # while True:
-    # #     t0 = time.monotonic()
-    # #     totals, by_ifs = measure_metrics_rate(
-    # #         "http://172.20.0.26:30442/metrics",
-    # #         1.0,
-    # #         labels,
-    # #     )
-    # #     t1 = time.monotonic()
-    # #     print(f"------- RATES --------- (after {(t1-t0):.3f} seconds)")
-    # #     for i in range(len(labels)):
-    # #         print(f"{labels[i]} total = {totals[i]}, by_if = {by_ifs[i]}")
-
-
-    # Increment bandwidth load significantly:
-
-    # # ---------------------
-    # loss = run_heavy_scion_ping("tester_1-ff00_0_111", "1-ff00:0:112,fd00:f00d:cafe::7f00:15")
-    # print(f"heavy traffic scion ping has a loss of {loss}")
-    # # Wait a bit and check that all works again
-    # time.sleep(1)
-    # loss = run_scion_ping("tester_1-ff00_0_111", "1-ff00:0:112,fd00:f00d:cafe::7f00:15",
-    #                count=3,size=1000,interval="1s")
-    # print(f"regular scion ping has a loss of {loss}")
-    # # --------------------------
-    #
-    # go run ./demo/router_priority/sender/ -daemon 127.0.0.19:30255 -remote 1-ff00:0:110,127.0.0.1:12345
-    # go run ./demo/router_priority/sender/ -daemon 172.20.0.28:30255 -local 172.20.0.29:0 -remote 1-ff00:0:112,[fd00:f00d:cafe::7f00:15]:12345
-
-    # Build sender binary:
-    # go build -o sender ./demo/router_priority/sender/
-    # Run sender but in the tester-111 network namespace:
-    # sudo nsenter -t $(docker inspect -f '{{.State.Pid}}' scion-tester_1-ff00_0_111-1 ) -n ./sender -daemon 172.20.0.28:30255 -local 172.20.0.29:0 -remote 1-ff00:0:112,[fd00:f00d:cafe::7f00:15]:12345
-
-    # go build -o sender ./demo/router_priority/sender/ &&   sudo nsenter -t $(docker inspect -f '{{.State.Pid}}' scion-tester_1-ff00_0_111-1 ) -n ./sender -daemon 172.20.0.28:30255 -local 172.20.0.29:0 -remote 1-ff00:0:112,[fd00:f00d:cafe::7f00:15]:12345 -duration 60s
+    set_tc_limits(bridge, rate="512kbit", burst="32kbit", latency="400ms")
 
     # Measure ping loss before loading the BR:
     loss = run_scion_ping("tester_1-ff00_0_111", "1-ff00:0:112,fd00:f00d:cafe::7f00:15",
@@ -446,14 +292,10 @@ def deleteme():
     if busy_fwd == 0:
         print(f"Insufficient load: no packet drop occurred.")
         sys.exit(1)
-    print(f"router metrics follow. Before:\n{metrics_before}\n"
-          f"After: {metrics_after}")
-    return
-    # scion ping -s 4000 --interval 1ms 1-ff00:0:112,fd00:f00d:cafe::7f00:15
-    # scion ping -s 4000 1-ff00:0:112,fd00:f00d:cafe::7f00:15
-
-    # # This works:
-    # scion ping -s 1000 --interval 100ms 1-ff00:0:110,172.20.0.22
+    print(f"router metrics follow.\n"
+          f"Before: -----8<-----\n{metrics_before}\n-----8<-----\n"
+          f"After:  -----8<-----\n{metrics_after}\n-----8<-----")
+    print("Success.")
 
 
 
