6cd7664e3c64ef129e31b77196ae2db22861d792
Katya Titkova <katyatitkova@gmail.com>
2026-06-24 14:03:13 +0200

    fix copyright comments

--- pkg/scrypto/signed/algo_test.go
+++ pkg/scrypto/signed/algo_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Anapaya Systems
+// Copyright 2026 SCION Association
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
